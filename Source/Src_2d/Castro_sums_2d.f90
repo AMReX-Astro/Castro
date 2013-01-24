66706329d5aff926f51ef9699e5c17d715a85df4
Max Katz <maximilian.katz@stonybrook.edu>
2013-01-24 12:11:18 -0500

    Adjusted some functions in sum_utils.cpp to be useable in 2D in addition to 3D, including sumlocmass2d.

--- Source/Src_2d/Castro_sums_2d.f90
+++ Source/Src_2d/Castro_sums_2d.f90
@@ -176,6 +176,81 @@
 
        end subroutine ca_sumlocmass
 
+! :: ----------------------------------------------------------
+! :: sumlocmass2d
+! ::             MASS = sum{ vol(i,j)*rho(i,j) }
+! ::
+! :: INPUTS / OUTPUTS:
+! ::  rho        => density field
+! ::  rlo,rhi    => index limits of rho aray
+! ::  lo,hi      => index limits of grid interior
+! ::  dx         => cell size
+! ::  mass      <=  total mass
+! ::  r          => radius at cell center
+! ::  irlo,hi    => index limits of r array
+! ::  idir1,2    => directions of location weighting
+! :: ----------------------------------------------------------
+! ::
+
+       subroutine ca_sumlocmass2d(rho,r_l1,r_l2,r_h1,r_h2,lo,hi, &
+                                  problo,dx,mass,r,irlo,irhi,idir1,idir2)
+
+       use prob_params_module, only : coord_type
+       use probdata_module, only : center
+
+       implicit none
+
+       integer          :: irlo, irhi, idir1, idir2
+       integer          :: r_l1,r_l2,r_h1,r_h2
+       integer          :: lo(2), hi(2)
+       double precision :: mass, problo(2), dx(2)
+       double precision :: rho(r_l1:r_h1,r_l2:r_h2)
+       double precision :: r(irlo:irhi)
+
+       double precision, parameter ::  Pi = 3.1415926535897932384d0
+
+       integer i, j
+       double precision x,y,vol_weighting
+
+       if (coord_type .eq. 0) then
+          vol_weighting = dx(1) * dx(2)
+       else
+          vol_weighting = 2.d0 * Pi * dx(1) * dx(2)
+       endif
+
+       mass = 0.d0
+       if (idir1 .eq. 0) then
+          do i = lo(1), hi(1)
+             x = problo(1) + (dble(i)+0.5d0) * dx(1) - center(1)
+             if (idir2 .eq. 0) then
+               do j = lo(2), hi(2)
+                  mass = mass + (vol_weighting * r(i)) * rho(i,j) * x * x
+               enddo
+             else
+               do j = lo(2), hi(2)
+                  y = problo(2) + (dble(j)+0.5d0) * dx(2) - center(2)
+                  mass = mass + (vol_weighting * r(i)) * rho(i,j) * x * y
+               enddo
+             endif
+          enddo
+       else 
+          do j = lo(2), hi(2)
+             y = problo(2) + (dble(j)+0.5d0) * dx(2) - center(2)
+             if (idir2 .eq. 0 ) then
+               do i = lo(1), hi(1)
+                  x = problo(1) + (dble(i)+0.5d0) * dx(1) - center(1)
+                  mass = mass + (vol_weighting * r(i)) * rho(i,j) * y * x
+               enddo
+             else
+               do i = lo(1), hi(1)
+                  mass = mass + (vol_weighting * r(i)) * rho(i,j) * y * y
+               enddo
+             endif
+          enddo
+       end if
+
+       end subroutine ca_sumlocmass2d
+
 ! ::
 ! :: ----------------------------------------------------------
 ! :: sumproduct
