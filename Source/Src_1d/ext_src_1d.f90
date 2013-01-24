f2944ba3909ab261387c4fa13d2c20e62bcf7c47
Michael Zingale <zingale@bender.astro.sunysb.edu>
2013-01-24 14:20:16 -0500

    add comments + some formatting changes

--- Source/Src_1d/ext_src_1d.f90
+++ Source/Src_1d/ext_src_1d.f90
@@ -1,25 +1,44 @@
+! compute the external sources for all the conservative equations.
+! 
+! This is called twice in the evolution:
+! 
+! first, for the predictor, it is called with (old, old) states
+!
+! This is also used in the first pass of the conservative update
+! (adding dt * S there)
+!
+! Next we correct the source terms in the conservative update to
+! time-center them.  Here we call ext_src(old, new), and then
+! in time_center_source_terms we subtract off 1/2 of the first S
+! and add 1/2 of the new S.
+!
+! Therefore, to get a properly time-centered source, generally
+! speaking, you always want to use the "new" state here.  That
+! will be the time n state in the first call and the n+1 in the
+! second call.
+!
 
-      subroutine ca_ext_src(lo,hi, &
-                            old_state,old_state_l1,old_state_h1, &
-                            new_state,new_state_l1,new_state_h1, &
-                            src,src_l1,src_h1,problo,dx,time,dt)
+subroutine ca_ext_src(lo,hi, &
+                      old_state,old_state_l1,old_state_h1, &
+                      new_state,new_state_l1,new_state_h1, &
+                      src,src_l1,src_h1,problo,dx,time,dt)
 
-      use meth_params_module, only : NVAR, UMX
-      use probdata_module   , only : center
+  use meth_params_module, only : NVAR, UMX
+  use probdata_module   , only : center
+  
+  implicit none
+  integer         ,intent(in   ) :: lo(1),hi(1)
+  integer         ,intent(in   ) :: old_state_l1,old_state_h1
+  integer         ,intent(in   ) :: new_state_l1,new_state_h1
+  integer         ,intent(in   ) :: src_l1,src_h1
+  double precision,intent(in   ) :: old_state(old_state_l1:old_state_h1,NVAR)
+  double precision,intent(in   ) :: new_state(new_state_l1:new_state_h1,NVAR)
+  double precision,intent(  out) :: src(src_l1:src_h1,NVAR)
+  double precision,intent(in   ) :: problo(1),dx(1),time,dt
+  
+  integer          :: i
+  
+  src = 0.d0
  
-      implicit none
-      integer         ,intent(in   ) :: lo(1),hi(1)
-      integer         ,intent(in   ) :: old_state_l1,old_state_h1
-      integer         ,intent(in   ) :: new_state_l1,new_state_h1
-      integer         ,intent(in   ) :: src_l1,src_h1
-      double precision,intent(in   ) :: old_state(old_state_l1:old_state_h1,NVAR)
-      double precision,intent(in   ) :: new_state(new_state_l1:new_state_h1,NVAR)
-      double precision,intent(  out) :: src(src_l1:src_h1,NVAR)
-      double precision,intent(in   ) :: problo(1),dx(1),time,dt
-
-      integer          :: i
-
-      src = 0.d0
- 
-      end subroutine ca_ext_src
+end subroutine ca_ext_src
 
--- Source/Src_2d/ext_src_2d.f90
+++ Source/Src_2d/ext_src_2d.f90
@@ -1,23 +1,42 @@
+! compute the external sources for all the conservative equations.
+! 
+! This is called twice in the evolution:
+! 
+! first, for the predictor, it is called with (old, old) states
+!
+! This is also used in the first pass of the conservative update
+! (adding dt * S there)
+!
+! Next we correct the source terms in the conservative update to
+! time-center them.  Here we call ext_src(old, new), and then
+! in time_center_source_terms we subtract off 1/2 of the first S
+! and add 1/2 of the new S.
+!
+! Therefore, to get a properly time-centered source, generally
+! speaking, you always want to use the "new" state here.  That
+! will be the time n state in the first call and the n+1 in the
+! second call.
+!
 
-       subroutine ca_ext_src(lo,hi, &
-                             old_state,old_state_l1,old_state_l2,old_state_h1,old_state_h2,&
-                             new_state,new_state_l1,new_state_l2,new_state_h1,new_state_h2,&
-                             src,src_l1,src_l2,src_h1,src_h2,problo,dx,time,dt)
+subroutine ca_ext_src(lo,hi, &
+                      old_state,old_state_l1,old_state_l2,old_state_h1,old_state_h2,&
+                      new_state,new_state_l1,new_state_l2,new_state_h1,new_state_h2,&
+                      src,src_l1,src_l2,src_h1,src_h2,problo,dx,time,dt)
 
-       use meth_params_module, only : NVAR
+  use meth_params_module, only : NVAR
 
-       implicit none
-       integer         , intent(in   ) :: lo(2),hi(2)
-       integer         , intent(in   ) :: old_state_l1,old_state_l2,old_state_h1,old_state_h2
-       integer         , intent(in   ) :: new_state_l1,new_state_l2,new_state_h1,new_state_h2
-       integer         , intent(in   ) :: src_l1,src_l2,src_h1,src_h2
-       double precision, intent(in   ) :: old_state(old_state_l1:old_state_h1,old_state_l2:old_state_h2,NVAR)
-       double precision, intent(in   ) :: new_state(new_state_l1:new_state_h1,new_state_l2:new_state_h2,NVAR)
-       double precision, intent(  out) :: src(    src_l1:  src_h1,  src_l2:src_h2  ,NVAR)
-       double precision, intent(in   ) :: problo(2),dx(2),time,dt
-
-       integer          :: i,j
-
-       src = 0.d0
-
-       end subroutine ca_ext_src
+  implicit none
+  integer         , intent(in   ) :: lo(2),hi(2)
+  integer         , intent(in   ) :: old_state_l1,old_state_l2,old_state_h1,old_state_h2
+  integer         , intent(in   ) :: new_state_l1,new_state_l2,new_state_h1,new_state_h2
+  integer         , intent(in   ) :: src_l1,src_l2,src_h1,src_h2
+  double precision, intent(in   ) :: old_state(old_state_l1:old_state_h1,old_state_l2:old_state_h2,NVAR)
+  double precision, intent(in   ) :: new_state(new_state_l1:new_state_h1,new_state_l2:new_state_h2,NVAR)
+  double precision, intent(  out) :: src(    src_l1:  src_h1,  src_l2:src_h2  ,NVAR)
+  double precision, intent(in   ) :: problo(2),dx(2),time,dt
+  
+  integer          :: i,j
+  
+  src = 0.d0
+  
+end subroutine ca_ext_src
