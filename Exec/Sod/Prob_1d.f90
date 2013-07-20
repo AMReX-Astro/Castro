5599b37438ce2eb69ae98e20869f8ed886ff0f8f
Michael Zingale <zingale@localhost.localdomain>
2013-07-20 16:55:05 -0400

    convert this to allow for a general (non-gammalaw) EOS.

--- Exec/Sod/Prob_1d.f90
+++ Exec/Sod/Prob_1d.f90
@@ -1,76 +1,103 @@
+subroutine PROBINIT (init,name,namlen,problo,probhi)
 
-      subroutine PROBINIT (init,name,namlen,problo,probhi)
+  use eos_module
+  use eos_type_module
+  use bl_error_module
+  use network
+  use probdata_module
+  implicit none
 
-      use probdata_module
-      use eos_module, only : gamma_const
-      implicit none
+  integer init, namlen
+  integer name(namlen)
+  double precision problo(1), probhi(1)
+  double precision xn(nspec)
+  
+  integer untin,i
 
-      integer init, namlen
-      integer name(namlen)
-      double precision problo(1), probhi(1)
+  type (eos_t) :: eos_state
 
-      integer untin,i
+  namelist /fortin/ p_l, u_l, rho_l, p_r, u_r, rho_r, frac, idir, &
+       denerr,  dengrad,  max_denerr_lev,  max_dengrad_lev, &
+       velgrad,  max_velgrad_lev, &
+       presserr,pressgrad,max_presserr_lev,max_pressgrad_lev
 
-      namelist /fortin/ p_l, u_l, rho_l, p_r, u_r, rho_r, frac, idir, &
-           denerr,  dengrad,  max_denerr_lev,  max_dengrad_lev, &
-           velgrad,  max_velgrad_lev, &
-           presserr,pressgrad,max_presserr_lev,max_pressgrad_lev
+  !
+  !     Build "probin" filename -- the name of file containing fortin namelist.
+  !     
+  integer maxlen
+  parameter (maxlen=256)
+  character probin*(maxlen)
 
-!
-!     Build "probin" filename -- the name of file containing fortin namelist.
-!     
-      integer maxlen
-      parameter (maxlen=256)
-      character probin*(maxlen)
+  if (namlen .gt. maxlen) then
+     call bl_error("probin file name too long")
+  end if
 
-      if (namlen .gt. maxlen) then
-         write(6,*) 'probin file name too long'
-         stop
-      end if
-
-      do i = 1, namlen
-         probin(i:i) = char(name(i))
-      end do
+  do i = 1, namlen
+     probin(i:i) = char(name(i))
+  end do
          
-! set namelist defaults
+  ! set namelist defaults
 
-      p_l = 1.0               ! left pressure (erg/cc)
-      u_l = 0.0               ! left velocity (cm/s)
-      rho_l = 1.0             ! left density (g/cc)
+  p_l = 1.0               ! left pressure (erg/cc)
+  u_l = 0.0               ! left velocity (cm/s)
+  rho_l = 1.0             ! left density (g/cc)
+  
+  p_r = 0.1               ! right pressure (erg/cc)
+  u_r = 0.0               ! right velocity (cm/s)
+  rho_r = 0.125           ! right density (g/cc)
 
-      p_r = 0.1               ! right pressure (erg/cc)
-      u_r = 0.0               ! right velocity (cm/s)
-      rho_r = 0.125           ! right density (g/cc)
+  idir = 1                ! direction across which to jump
+  frac = 0.5              ! fraction of the domain for the interface
 
-      idir = 1                ! direction across which to jump
-      frac = 0.5              ! fraction of the domain for the interface
+  denerr = 1.d20
+  dengrad = 1.d20
+  max_denerr_lev = -1
+  max_dengrad_lev = -1
 
-      denerr = 1.d20
-      dengrad = 1.d20
-      max_denerr_lev = -1
-      max_dengrad_lev = -1
+  presserr = 1.d20
+  pressgrad = 1.d20
+  max_presserr_lev = -1
+  max_pressgrad_lev = -1
 
-      presserr = 1.d20
-      pressgrad = 1.d20
-      max_presserr_lev = -1
-      max_pressgrad_lev = -1
+  velgrad = 1.d20
+  max_velgrad_lev = -1
 
-      velgrad = 1.d20
-      max_velgrad_lev = -1
+  !     Read namelists
+  untin = 9
+  open(untin,file=probin(1:namlen),form='formatted',status='old')
+  read(untin,fortin)
+  close(unit=untin)
+  
+  center(1) = frac*(problo(1)+probhi(1))
 
-!     Read namelists
-      untin = 9
-      open(untin,file=probin(1:namlen),form='formatted',status='old')
-      read(untin,fortin)
-      close(unit=untin)
+  !     compute the internal energy (erg/cc) for the left and right state
+  xn(:) = 0.0d0
+  xn(1) = 1.0d0
 
-      center(1) = frac*(problo(1)+probhi(1))
+  eos_state%rho = rho_l
+  eos_state%p = p_l
+  eos_state%T = 100000.d0   ! initial guess
+  eos_state%xn(:) = xn(:)
 
-!     compute the internal energy (erg/cc) for the left and right state
-      rhoe_l = p_l/(gamma_const - 1.d0)
-      rhoe_r = p_r/(gamma_const - 1.d0)
+  call eos(eos_input_rp, eos_state, .false.)
 
-      end subroutine PROBINIT
+  rhoe_l = rho_l*eos_state%e
+  T_l = eos_state%T
+
+  eos_state%rho = rho_r
+  eos_state%p = p_r
+  eos_state%T = 100000.d0   ! initial guess
+  eos_state%xn(:) = xn(:)
+
+  call eos(eos_input_rp, eos_state, .false.)
+
+  rhoe_r = rho_r*eos_state%e
+  T_r = eos_state%T
+
+  print *, 'done with initialization'
+
+end subroutine PROBINIT
+
 
 ! ::: -----------------------------------------------------------
 ! ::: This routine is called at problem setup time and is used
@@ -93,119 +120,123 @@
 ! :::              right hand corner of grid.  (does not include
 ! :::		   ghost region).
 ! ::: -----------------------------------------------------------
-     subroutine ca_initdata(level,time,lo,hi,nscal, &
-                            state,state_l1,state_h1,delta,xlo,xhi)
+subroutine ca_initdata(level,time,lo,hi,nscal, &
+                      state,state_l1,state_h1,delta,xlo,xhi)
 
-     use probdata_module
-     use meth_params_module, only : NVAR, URHO, UMX, UEDEN, UEINT, UTEMP, UFS
+  use probdata_module
+  use meth_params_module, only : NVAR, URHO, UMX, UEDEN, UEINT, UTEMP, UFS
 
-     implicit none
-     integer level, nscal
-     integer lo(1), hi(1)
-     integer state_l1,state_h1
-     double precision state(state_l1:state_h1,NVAR)
-     double precision time, delta(1)
-     double precision xlo(1), xhi(1)
+  implicit none
+  integer level, nscal
+  integer lo(1), hi(1)
+  integer state_l1,state_h1
+  double precision state(state_l1:state_h1,NVAR)
+  double precision time, delta(1)
+  double precision xlo(1), xhi(1)
+  
+  double precision xcen
+  integer i
 
-      double precision xcen
-      integer i
+  do i = lo(1), hi(1)
+     xcen = xlo(1) + delta(1)*(float(i-lo(1)) + 0.5d0)
+     
+     if (xcen <= center(1)) then
+        state(i,URHO ) = rho_l
+        state(i,UMX  ) = rho_l*u_l
+        state(i,UEDEN) = rhoe_l + 0.5*rho_l*u_l*u_l
+        state(i,UEINT) = rhoe_l
+        state(i,UTEMP) = T_l
+     else
+        state(i,URHO ) = rho_r
+        state(i,UMX  ) = rho_r*u_r
+        state(i,UEDEN) = rhoe_r + 0.5*rho_r*u_r*u_r
+        state(i,UEINT) = rhoe_r
+        state(i,UTEMP) = T_r
+     endif
 
-      do i = lo(1), hi(1)
-         xcen = xlo(1) + delta(1)*(float(i-lo(1)) + 0.5d0)
-            
-         if (xcen <= center(1)) then
-            state(i,URHO ) = rho_l
-            state(i,UMX  ) = rho_l*u_l
-            state(i,UEDEN) = rhoe_l + 0.5*rho_l*u_l*u_l
-            state(i,UEINT) = rhoe_l
-         else
-            state(i,URHO ) = rho_r
-            state(i,UMX  ) = rho_r*u_r
-            state(i,UEDEN) = rhoe_r + 0.5*rho_r*u_r*u_r
-            state(i,UEINT) = rhoe_r
-         endif
+     state(i,UFS  ) = state(i,URHO)
 
-         state(i,UFS  ) = state(i,URHO)
-         state(i,UTEMP) = 0.d0
-            
-      enddo
 
-      end subroutine ca_initdata
+     
+  enddo
 
-! ::: 
-! ::: -----------------------------------------------------------
-! :::
+end subroutine ca_initdata
 
-     subroutine ca_hypfill(adv,adv_l1,adv_h1, &
-                           domlo,domhi,delta,xlo,time,bc)
-
-     use meth_params_module, only : NVAR
-     implicit none
-     include 'bc_types.fi'
-
-     integer          :: adv_l1,adv_h1
-     integer          :: bc(1,2,*)
-     integer          :: domlo(1), domhi(1)
-     double precision :: delta(1), xlo(1), time
-     double precision :: adv(adv_l1:adv_h1,NVAR)
-
-     integer n
-
-     do n = 1,NVAR
-        call filcc(adv(adv_l1,n),adv_l1,adv_h1, &
-                   domlo,domhi,delta,xlo,bc(1,1,n))
-     enddo
-
-     do n = 1, NVAR
-
-!        XLO
-         if ( bc(1,1,n).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
-            print *,'SHOULD NEVER GET HERE bc(1,1,n) .eq. EXT_DIR) '
-            stop
-         end if
-
-!        XHI
-         if ( bc(1,2,n).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
-            print *,'SHOULD NEVER GET HERE bc(1,2,n) .eq. EXT_DIR) '
-            stop
-         end if
-
-     end do
-
-     end subroutine ca_hypfill
 
 ! ::: 
 ! ::: -----------------------------------------------------------
 ! :::
 
-      subroutine ca_denfill(adv,adv_l1,adv_h1,domlo,domhi,delta,xlo,time,bc)
+subroutine ca_hypfill(adv,adv_l1,adv_h1, &
+                      domlo,domhi,delta,xlo,time,bc)
 
-      implicit none
-      include 'bc_types.fi'
+  use bl_error_module
+  use meth_params_module, only : NVAR
+  implicit none
+  include 'bc_types.fi'
 
-      integer          :: adv_l1,adv_h1
-      integer          :: bc(1,2,*)
-      integer          :: domlo(1), domhi(1)
-      double precision :: delta(1), xlo(1), time
-      double precision :: adv(adv_l1:adv_h1)
+  integer          :: adv_l1,adv_h1
+  integer          :: bc(1,2,*)
+  integer          :: domlo(1), domhi(1)
+  double precision :: delta(1), xlo(1), time
+  double precision :: adv(adv_l1:adv_h1,NVAR)
 
-!     Note: this function should not be needed, technically, but is provided
-!     to filpatch because there are many times in the algorithm when just
-!     the density is needed.  We try to rig up the filling so that the same
-!     function is called here and in hypfill where all the states are filled.
+  integer n
 
-      call filcc(adv,adv_l1,adv_h1,domlo,domhi,delta,xlo,bc)
+  do n = 1,NVAR
+     call filcc(adv(adv_l1,n),adv_l1,adv_h1, &
+                domlo,domhi,delta,xlo,bc(1,1,n))
+  enddo
 
-!     XLO
-      if ( bc(1,1,1).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
-         print *,'SHOULD NEVER GET HERE bc(1,1,1) .eq. EXT_DIR) '
-         stop
-      end if
+  do n = 1, NVAR
 
-!     XHI
-      if ( bc(1,2,1).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
-         print *,'SHOULD NEVER GET HERE bc(1,2,1) .eq. EXT_DIR) '
-         stop
-      end if
+     !        XLO
+     if ( bc(1,1,n).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
+        call bl_error('SHOULD NEVER GET HERE bc(1,1,n) .eq. EXT_DIR) ')
+     end if
 
-      end subroutine ca_denfill
+     !        XHI
+     if ( bc(1,2,n).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
+        call bl_error('SHOULD NEVER GET HERE bc(1,2,n) .eq. EXT_DIR) ')
+     end if
+
+  end do
+
+end subroutine ca_hypfill
+
+
+! ::: 
+! ::: -----------------------------------------------------------
+! :::
+
+subroutine ca_denfill(adv,adv_l1,adv_h1,domlo,domhi,delta,xlo,time,bc)
+
+  use bl_error_module
+
+  implicit none
+  include 'bc_types.fi'
+
+  integer          :: adv_l1,adv_h1
+  integer          :: bc(1,2,*)
+  integer          :: domlo(1), domhi(1)
+  double precision :: delta(1), xlo(1), time
+  double precision :: adv(adv_l1:adv_h1)
+
+  !     Note: this function should not be needed, technically, but is provided
+  !     to filpatch because there are many times in the algorithm when just
+  !     the density is needed.  We try to rig up the filling so that the same
+  !     function is called here and in hypfill where all the states are filled.
+  
+  call filcc(adv,adv_l1,adv_h1,domlo,domhi,delta,xlo,bc)
+
+  !     XLO
+  if ( bc(1,1,1).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
+     call bl_error('SHOULD NEVER GET HERE bc(1,1,1) .eq. EXT_DIR) ')
+  end if
+
+  !     XHI
+  if ( bc(1,2,1).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
+     call bl_error('SHOULD NEVER GET HERE bc(1,2,1) .eq. EXT_DIR) ')
+  end if
+
+end subroutine ca_denfill
