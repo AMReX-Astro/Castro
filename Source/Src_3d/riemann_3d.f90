7d7a7ba068053adbcface48ead545f9577f194bc
Weiqun Zhang <weiqunzhang@lbl.gov>
2013-08-23 06:58:16 -0700

    fixed an OMP problem

--- Source/Src_3d/riemann_3d.f90
+++ Source/Src_3d/riemann_3d.f90
@@ -93,7 +93,7 @@ contains
     !$OMP PRIVATE(ro,uo,po,tauo,reo,gamco,gameo,co,clsq,wosq,sgnm,wo,dpjmp) &
     !$OMP PRIVATE(rstar,entho,estar,cstar,spout,spin,ushock,scr,frac) &
     !$OMP PRIVATE(v1gdnv,v2gdnv,rgdnv,gamgdnv) &
-    !$OMP PRIVATE(rhoetot,n,nq,qavg,rho_K_contrib,iadv,ispec,iaux)
+    !$OMP PRIVATE(rhoetot,n,nq,qavg,rho_K_contrib,iadv,ispec,iaux) &
     !$OMP PRIVATE(pstar_hist)
     do j = jlo, jhi
        do i = ilo, ihi
