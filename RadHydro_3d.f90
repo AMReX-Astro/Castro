49039b91414831fc1d4870e43fc6c897402ef0f8
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-02-13 15:41:22 -0800

    sync with Castro

--- RadHydro_3d.f90
+++ RadHydro_3d.f90
@@ -36,7 +36,7 @@ subroutine ctoprim_rad(lo,hi, &
   use radhydro_params_module, only : QRADVAR, qrad, qradhi, qptot, qreitot, comoving, &
        flatten_pp_threshold, first_order_hydro
   use rad_params_module, only : ngroups
-  use advection_module, only : uflaten
+  use flatten_module, only : uflaten
   use fluxlimiter_module, only : Edd_factor
 
   implicit none
