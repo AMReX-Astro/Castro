f757a8cb9a97abb7f8d7adbf089bb1b4f2f1b2c3
Michael Zingale <zingale@localhost.localdomain>
2014-02-22 19:04:28 -0500

    update the unit test to be the same as the one for helmeos -- it tests every input method

--- EOS/gamma_law_general/test/testeos.f90
+++ EOS/gamma_law_general/test/testeos.f90
@@ -1,4 +1,4 @@
-program testburn
+program testeos
 
   use bl_types
   use network
@@ -6,192 +6,182 @@ program testburn
 
   implicit none
 
-  real(kind=dp_t) :: dens, temp, pres, entr
+  real(kind=dp_t) :: dens_good, temp_good, pres_good, entr_good, enth_good, eint_good
   real(kind=dp_t), dimension(nspec) :: Xin
   
   integer :: ic12, io16, img24
 
   logical :: do_diag
 
-  call network_init()
-  call eos_init(gamma_in=5.0d0/3.0d0)
+  real(kind=dp_t) :: err1, err2
 
-  do_diag = .false.
+  type (eos_t) :: state
+
+  call network_init()
+  call eos_init()
 
   ic12 = network_species_index("carbon-12")
   io16 = network_species_index("oxygen-16")
   img24 = network_species_index("magnesium-24")
 
-  dens = 1.e4_dp_t
-  temp = 1.e8_dp_t
+  dens_good = 2.6e9_dp_t
+  temp_good = 1.e9_dp_t
 
   Xin(ic12) = 0.5_dp_t
   Xin(io16) = 0.5_dp_t
   Xin(img24) = 0.0_dp_t
 
+  do_diag = .false.
 
-  den_eos(1) = dens
-  temp_eos(1) = temp
-  xn_eos(1,:) = Xin(:)
+  !---------------------------------------------------------------------------
+  ! get the initial state -- this will be considered the "right" answer --
+  ! make sure we can get these values back when using the other input 
+  ! methods
+  !---------------------------------------------------------------------------
+
+  state % rho = dens_good
+  state % T   = temp_good
+  state % xn  = Xin
+
+  call eos(eos_input_rt, state, do_diag)
+
+  pres_good = state%p
+  entr_good = state%s
+  enth_good = state%h
+  eint_good = state%e
+
+  print *, 'eos_input_rt:'
+  print *, 'dens: ', state%rho, ' temp: ', state%T
+  print *, 'X: ', Xin
+  print *, 'pres: ', state % p,  ' ener: ', state % e
+  print *, 'h:    ', state % h,  ' entr: ', state % s
+  print *, 'c_v:  ', state % cv, ' c_p : ', state % cp
+  print *, 'dpdT: ', state % dpdT, ' dpdr: ', state % dpdr
+  print *, 'dedT: ', state % dedT, ' dedr: ', state % dedr
+  print *, 'dpdX: ', state % dpdX
+  print *, 'dhdX: ', state % dhdX
 
   
-  ! test eos_input_rt
-  call eos(eos_input_rt, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           do_diag)
+  !---------------------------------------------------------------------------
+  ! now try the other input methods and make sure we get back what we 
+  ! started with
+  !---------------------------------------------------------------------------
 
-  print *, 'input: eos_input_rt'
-  print *, 'dens: ', dens, ' temp: ', temp
-  print *, 'X: ', Xin
-  print *, 'pres: ', p_eos(1),  ' ener: ', e_eos(1)
-  print *, 'h:    ', h_eos(1)
-  print *, 'c_v:  ', cv_eos(1), ' c_p : ', cp_eos(1)
-  print *, 'dpdT: ', dpdt_eos(1), ' dpdr: ', dpdr_eos(1)
-  print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
-  print *, 'dpdX: ', dpdX_eos(1,:)
-  print *, 'dhdX: ', dhdX_eos(1,:)
+  !---------------------------------------------------------------------------
+  ! density, enthalpy
+  !---------------------------------------------------------------------------
+  state%rho = dens_good
+  state%h = enth_good
 
-  print *, ' '
-  print *, 'setting pres = ', p_eos(1)
-  pres = p_eos(1)
-  print *, 'setting entr = ', s_eos(1)
-  entr = s_eos(1)
+  state%T = 0.1*temp_good
 
+  call eos(eos_input_rh, state, do_diag)
+
+  ! compute the error in T
+  err1 = abs(temp_good - state%T)/temp_good
+  print *, " "
+  print *, "eos_input_rh, err: ", err1
+
+
+  !---------------------------------------------------------------------------
+  ! temp, pres
+  !---------------------------------------------------------------------------
+  state%T = temp_good
+  state%p = pres_good
+
+  state%rho = 0.1*dens_good
+
+  call eos(eos_input_tp, state, do_diag)
+
+  ! compute the error in rho
+  err1 = abs(dens_good - state%rho)/dens_good
+  print *, " "
+  print *, "eos_input_tp, err: ", err1
   
-  ! test eos_input_rh
-  temp_eos(1) = 0.d0
-  call eos(eos_input_rh, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           do_diag)
 
-  print *, ' '
-  print *, 'input: eos_input_rh'
-  print *, 'dens: ', dens, ' temp: ', temp
-  print *, 'X: ', Xin
-  print *, 'pres: ', p_eos(1),  ' ener: ', e_eos(1)
-  print *, 'entropy: ', s_eos(1)
-  print *, 'h:    ', h_eos(1)
-  print *, 'c_v:  ', cv_eos(1), ' c_p : ', cp_eos(1)
-  print *, 'dpdT: ', dpdt_eos(1), ' dpdr: ', dpdr_eos(1)
-  print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
-  print *, 'dpdX: ', dpdX_eos(1,:)
-  print *, 'dhdX: ', dhdX_eos(1,:)
+  !---------------------------------------------------------------------------
+  ! dens, pres
+  !---------------------------------------------------------------------------
+  state%rho = dens_good
+  state%p = pres_good
 
-  ! test eos_input_tp
-  den_eos(1) = 0.d0
-  call eos(eos_input_tp, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           do_diag)
+  state%T = 0.1*temp_good
 
-  print *, ' '
-  print *, 'input: eos_input_tp'
-  print *, 'dens: ', dens, ' temp: ', temp
-  print *, 'X: ', Xin
-  print *, 'pres: ', p_eos(1),  ' ener: ', e_eos(1)
-  print *, 'h:    ', h_eos(1)
-  print *, 'c_v:  ', cv_eos(1), ' c_p : ', cp_eos(1)
-  print *, 'dpdT: ', dpdt_eos(1), ' dpdr: ', dpdr_eos(1)
-  print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
-  print *, 'dpdX: ', dpdX_eos(1,:)
-  print *, 'dhdX: ', dhdX_eos(1,:)
+  call eos(eos_input_rp, state, do_diag)
 
-  ! test eos_input_rp
-  temp_eos(1) = 0.d0
-  call eos(eos_input_rp, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           do_diag)
-
-  print *, ' '
-  print *, 'input: eos_input_rp'
-  print *, 'dens: ', dens, ' temp: ', temp
-  print *, 'X: ', Xin
-  print *, 'pres: ', p_eos(1),  ' ener: ', e_eos(1)
-  print *, 'h:    ', h_eos(1)
-  print *, 'c_v:  ', cv_eos(1), ' c_p : ', cp_eos(1)
-  print *, 'dpdT: ', dpdt_eos(1), ' dpdr: ', dpdr_eos(1)
-  print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
-  print *, 'dpdX: ', dpdX_eos(1,:)
-  print *, 'dhdX: ', dhdX_eos(1,:)
-
-  ! test eos_input_re
-  temp_eos(1) = 0.d0
-  call eos(eos_input_re, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           do_diag)
-
-  print *, ' '
-  print *, 'input: eos_input_re'
-  print *, 'dens: ', dens, ' temp: ', temp
-  print *, 'X: ', Xin
-  print *, 'pres: ', p_eos(1),  ' ener: ', e_eos(1)
-  print *, 'h:    ', h_eos(1)
-  print *, 'c_v:  ', cv_eos(1), ' c_p : ', cp_eos(1)
-  print *, 'dpdT: ', dpdt_eos(1), ' dpdr: ', dpdr_eos(1)
-  print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
-  print *, 'dpdX: ', dpdX_eos(1,:)
-  print *, 'dhdX: ', dhdX_eos(1,:)
+  ! compute the error in T
+  err1 = abs(temp_good - state%T)/temp_good
+  print *, " "
+  print *, "eos_input_rp, err: ", err1
 
 
-  ! test eos_input_ps
-  p_eos(1) = pres
-  s_eos(1) = entr
-  call eos(eos_input_ps, den_eos, temp_eos, &
-           npts, &
-           xn_eos, &
-           p_eos, h_eos, e_eos, &
-           cv_eos, cp_eos, xne_eos, eta_eos, pele_eos, &
-           dpdt_eos, dpdr_eos, dedt_eos, dedr_eos, &
-           dpdX_eos, dhdX_eos, &
-           gam1_eos, cs_eos, s_eos, &
-           dsdt_eos, dsdr_eos, &
-           do_diag)
+  !---------------------------------------------------------------------------
+  ! dens, energy
+  !---------------------------------------------------------------------------
+  state%rho = dens_good
+  state%e = eint_good
 
-  print *, ' '
-  print *, 'input: eos_input_ps'
-  print *, 'pres: ', pres, 'entr: ', entr
-  print *, 'dens: ', den_eos(1), ' temp: ', temp_eos(1)
-  print *, 'X: ', Xin
-  print *, 'pres_eos: ', p_eos(1),  'entr_eos: ', s_eos(1)
-  print *, 'h:    ', h_eos(1), ' ener: ', e_eos(1)
-  print *, 'c_v:  ', cv_eos(1), ' c_p : ', cp_eos(1)
-  print *, 'dpdT: ', dpdt_eos(1), ' dpdr: ', dpdr_eos(1)
-  print *, 'dedT: ', dedt_eos(1), ' dedr: ', dedr_eos(1)
-  print *, 'dpdX: ', dpdX_eos(1,:)
-  print *, 'dhdX: ', dhdX_eos(1,:)
+  state%T = 0.1*temp_good
 
-end program testburn
+  call eos(eos_input_re, state, do_diag)
+
+  ! compute the error in T
+  err1 = abs(temp_good - state%T)/temp_good
+  print *, " "
+  print *, "eos_input_re, err: ", err1
+
+
+  !---------------------------------------------------------------------------
+  ! temp, enthalpy
+  !---------------------------------------------------------------------------
+  state%rho = dens_good
+  state%h = enth_good
+
+  state%rho = 0.1*dens_good
+
+  call eos(eos_input_th, state, do_diag)
+
+  ! compute the error in rho
+  err1 = abs(dens_good - state%rho)/dens_good
+  print *, " "
+  print *, "eos_input_th, err: ", err1
+
+
+  !---------------------------------------------------------------------------
+  ! pres, entropy
+  !---------------------------------------------------------------------------
+  state%p = pres_good
+  state%s = entr_good
+
+  state%T = 0.1*temp_good
+  state%rho = 0.1*dens_good
+
+  call eos(eos_input_ps, state, do_diag)
+
+  ! compute the error in T and rho
+  err1 = abs(temp_good - state%T)/temp_good
+  err2 = abs(dens_good - state%rho)/dens_good
+  print *, " "
+  print *, "eos_input_ps, err: ", err1, err2
+
+
+  !---------------------------------------------------------------------------
+  ! pres, enthalpy
+  !---------------------------------------------------------------------------
+  state%p = pres_good
+  state%h = enth_good
+
+  state%T = 0.1*temp_good
+  state%rho = 0.1*dens_good
+
+  call eos(eos_input_ph, state, do_diag)
+
+  ! compute the error in T and rho
+  err1 = abs(temp_good - state%T)/temp_good
+  err2 = abs(dens_good - state%rho)/dens_good
+  print *, " "
+  print *, "eos_input_ph, err: ", err1, err2
+
+
+
+end program testeos
