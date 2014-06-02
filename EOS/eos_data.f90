b3fd1258fbb4f119d4f17deb5532f5179e3e84e1
Chris Malone <malone@ucolick.org>
2014-06-02 15:26:38 -0700

    add some more error codes

--- EOS/eos_data.f90
+++ EOS/eos_data.f90
@@ -20,16 +20,18 @@ module eos_data_module
   integer, parameter :: ientr = 5
   integer, parameter :: ipres = 6
 
-  integer, parameter :: ierr_general   = 1
-  integer, parameter :: ierr_input     = 2
-  integer, parameter :: ierr_iter_conv = 3
-  integer, parameter :: ierr_neg_e     = 4
-  integer, parameter :: ierr_neg_p     = 5
-  integer, parameter :: ierr_neg_h     = 6
-  integer, parameter :: ierr_neg_s     = 7
-  integer, parameter :: ierr_iter_var  = 8
-  integer, parameter :: ierr_init      = 9
-  integer, parameter :: ierr_init_xn   = 10
+  integer, parameter :: ierr_general         = 1
+  integer, parameter :: ierr_input           = 2
+  integer, parameter :: ierr_iter_conv       = 3
+  integer, parameter :: ierr_neg_e           = 4
+  integer, parameter :: ierr_neg_p           = 5
+  integer, parameter :: ierr_neg_h           = 6
+  integer, parameter :: ierr_neg_s           = 7
+  integer, parameter :: ierr_iter_var        = 8
+  integer, parameter :: ierr_init            = 9
+  integer, parameter :: ierr_init_xn         = 10
+  integer, parameter :: ierr_out_of_bounds   = 11
+  integer, parameter :: ierr_not_implemented = 12
 
   real(kind=dp_t), save :: smallt
   real(kind=dp_t), save :: smalld
