44117cdc09a0427778e737ec56c1c175d2a22b7f
Max Katz <maximilian.katz@stonybrook.edu>
2014-06-13 11:32:24 -0400

    eos_data module integer types are no longer parameters -- necessary so they can be sent to the accelerator.

--- EOS/eos_data.f90
+++ EOS/eos_data.f90
@@ -4,34 +4,34 @@ module eos_data_module
 
   implicit none
 
-  integer, parameter :: eos_input_rt = 1  ! rho, T are inputs
-  integer, parameter :: eos_input_rh = 2  ! rho, h are inputs
-  integer, parameter :: eos_input_tp = 3  ! T, p are inputs
-  integer, parameter :: eos_input_rp = 4  ! rho, p are inputs
-  integer, parameter :: eos_input_re = 5  ! rho, e are inputs
-  integer, parameter :: eos_input_ps = 6  ! p, s are inputs
-  integer, parameter :: eos_input_ph = 7  ! p, h are inputs
-  integer, parameter :: eos_input_th = 8  ! T, h are inputs
+  integer :: eos_input_rt = 1  ! rho, T are inputs
+  integer :: eos_input_rh = 2  ! rho, h are inputs
+  integer :: eos_input_tp = 3  ! T, p are inputs
+  integer :: eos_input_rp = 4  ! rho, p are inputs
+  integer :: eos_input_re = 5  ! rho, e are inputs
+  integer :: eos_input_ps = 6  ! p, s are inputs
+  integer :: eos_input_ph = 7  ! p, h are inputs
+  integer :: eos_input_th = 8  ! T, h are inputs
 
-  integer, parameter :: itemp = 1
-  integer, parameter :: idens = 2
-  integer, parameter :: iener = 3
-  integer, parameter :: ienth = 4
-  integer, parameter :: ientr = 5
-  integer, parameter :: ipres = 6
+  integer :: itemp = 1
+  integer :: idens = 2
+  integer :: iener = 3
+  integer :: ienth = 4
+  integer :: ientr = 5
+  integer :: ipres = 6
 
-  integer, parameter :: ierr_general         = 1
-  integer, parameter :: ierr_input           = 2
-  integer, parameter :: ierr_iter_conv       = 3
-  integer, parameter :: ierr_neg_e           = 4
-  integer, parameter :: ierr_neg_p           = 5
-  integer, parameter :: ierr_neg_h           = 6
-  integer, parameter :: ierr_neg_s           = 7
-  integer, parameter :: ierr_iter_var        = 8
-  integer, parameter :: ierr_init            = 9
-  integer, parameter :: ierr_init_xn         = 10
-  integer, parameter :: ierr_out_of_bounds   = 11
-  integer, parameter :: ierr_not_implemented = 12
+  integer :: ierr_general         = 1
+  integer :: ierr_input           = 2
+  integer :: ierr_iter_conv       = 3
+  integer :: ierr_neg_e           = 4
+  integer :: ierr_neg_p           = 5
+  integer :: ierr_neg_h           = 6
+  integer :: ierr_neg_s           = 7
+  integer :: ierr_iter_var        = 8
+  integer :: ierr_init            = 9
+  integer :: ierr_init_xn         = 10
+  integer :: ierr_out_of_bounds   = 11
+  integer :: ierr_not_implemented = 12
 
   real(kind=dp_t), save :: smallt
   real(kind=dp_t), save :: smalld
