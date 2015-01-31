7153ee34d7fbef60bd20773743b4d71b1f11f1df
Michael Zingale <michael.zingale@stonybrook.edu>
2015-01-30 19:41:42 -0500

    comments

--- Source/prob_params.f90
+++ Source/prob_params.f90
@@ -5,10 +5,16 @@
 module prob_params_module
 
   implicit none
+
+  ! boundary condition information
   integer         , save, allocatable :: physbc_lo(:)
   integer         , save, allocatable :: physbc_hi(:)
   integer         , save :: Outflow, Symmetry, SlipWall, NoSlipWall
+
+  ! geometry information
   integer         , save :: coord_type
+
+  ! domain physical coordinates
   double precision, save :: xmin, xmax, ymin, ymax, zmin, zmax
   
 end module prob_params_module
