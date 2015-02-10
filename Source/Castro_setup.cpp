d233abfbf0b95cdf532e5e71e53a1796091fdaa5
Max Katz <maximilian.katz@stonybrook.edu>
2015-02-09 23:55:06 -0500

    Move extern_init() to be after read_params().

--- Source/Castro_setup.cpp
+++ Source/Castro_setup.cpp
@@ -133,16 +133,16 @@ Castro::variableSetUp ()
 
     BL_ASSERT(desc_lst.size() == 0);
 
-    // Initialize the runtime parameters for any of the external
-    // microphysics
-    extern_init();
-
-
     // Initialize the network
     network_init();
 
     // Get options, set phys_bc
     read_params();
+
+    // Initialize the runtime parameters for any of the external
+    // microphysics
+    extern_init();
+
     //
     // Set number of state variables and pointers to components
     //
