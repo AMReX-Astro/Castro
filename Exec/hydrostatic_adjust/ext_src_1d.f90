baccf6cad57e6de569ee19806cc499378566e9a6
Michael Zingale <michael.zingale@stonybrook.edu>
2015-02-15 10:46:01 -0500

    move the hydrostatic_adjust problem over from Castro_Exec/ This problem is designed to test the 1-d base state evolution algorithm from Maestro.  Note that you need to make sure that you are running with the same network as Maestro does (to get the species and therefore thermodynamics correct).

---
+++ Exec/hydrostatic_adjust/ext_src_1d.f90
@@ -0,0 +1,87 @@
+
+  subroutine ca_ext_src(lo,hi, &
+                        old_state,old_state_l1,old_state_h1, &
+                        new_state,new_state_l1,new_state_h1, &
+                        src,src_l1,src_h1,problo,dx,time,dt)
+
+    use meth_params_module, only : NVAR, URHO, UEDEN, UEINT, UFS
+    use probdata_module   , only : xmin, &
+                                   heating_time, heating_rad, &
+                                   heating_peak, heating_sigma, prob_type
+    use prob_params_module, only: center
+    use network
+    
+    implicit none
+    integer         ,intent(in   ) :: lo(1),hi(1)
+    integer         ,intent(in   ) :: old_state_l1,old_state_h1
+    integer         ,intent(in   ) :: new_state_l1,new_state_h1
+    integer         ,intent(in   ) :: src_l1,src_h1
+    double precision,intent(in   ) :: old_state(old_state_l1:old_state_h1,NVAR)
+    double precision,intent(in   ) :: new_state(new_state_l1:new_state_h1,NVAR)
+    double precision,intent(  out) :: src(src_l1:src_h1,NVAR)
+    double precision,intent(in   ) :: problo(1),dx(1),time,dt
+    
+    integer          :: i
+    double precision :: x, r_0, H_0, W_0, Hext, t_stop
+
+    integer :: ihe4
+
+    r_0 = heating_rad
+
+    H_0 = heating_peak
+    W_0 = heating_sigma
+
+    t_stop = heating_time
+
+    if (prob_type == 1) then
+
+       ! For heating at the center
+       if (time .le. t_stop) then
+          if (lo(1) .eq. 0) print *,'TIME vs TSTOP ',time, t_stop 
+          do i = lo(1), hi(1)
+
+             x = problo(1) + ((dble(i)+0.5d0)*dx(1) + xmin) - center(1)
+
+             Hext = H_0*exp(-((x-r_0)**2)/W_0**2)
+
+             src(i,UEINT) = old_state(i,URHO)*Hext
+             src(i,UEDEN) = old_state(i,URHO)*Hext
+             
+          enddo
+       else
+
+          src(lo(1):hi(1),UEINT) = 0.d0
+          src(lo(1):hi(1),UEDEN) = 0.d0
+
+       end if
+
+    else if (prob_type == 3) then
+
+       ihe4 = network_species_index("helium-4")
+
+       ! sub-chandra heating -- modulate by He
+       if (time .le. t_stop) then
+          if (lo(1) .eq. 0) print *,'TIME vs TSTOP ',time, t_stop 
+          do i = lo(1), hi(1)
+
+             x = problo(1) + ((dble(i)+0.5d0)*dx(1) + xmin) - center(1)
+
+             Hext = H_0*exp(-((x-r_0)**2)/W_0**2)*old_state(i,UFS-1+ihe4)/old_state(i,URHO)
+
+             src(i,UEINT) = old_state(i,URHO)*Hext
+             src(i,UEDEN) = old_state(i,URHO)*Hext
+             
+          enddo
+       else
+
+          src(lo(1):hi(1),UEINT) = 0.d0
+          src(lo(1):hi(1),UEDEN) = 0.d0
+
+       end if
+
+
+    endif
+
+
+  end subroutine ca_ext_src
+
---
+++ Exec/hydrostatic_adjust/extra_derives.f90
@@ -0,0 +1,136 @@
+subroutine ca_derpi(p,p_l1,p_h1,ncomp_p, &
+                    u,u_l1,u_h1,ncomp_u,lo,hi,domlo, &
+                    domhi,dx,xlo,time,dt,bc,level,grid_no)
+
+  use network, only : nspec, naux
+  use eos_module
+  use eos_type_module
+  use eos_data_module
+  use meth_params_module, only : URHO, UMX, UEINT, UTEMP, UFS, UFX, &
+                                 allow_negative_energy
+  use probdata_module, only: hse_p
+
+  implicit none
+  
+  integer p_l1,p_h1,ncomp_p
+  integer u_l1,u_h1,ncomp_u
+  integer lo(1), hi(1), domlo(1), domhi(1)
+  double precision p(p_l1:p_h1,ncomp_p)
+  double precision u(u_l1:u_h1,ncomp_u)
+  double precision dx(1), xlo(1), time, dt
+  integer bc(1,2,ncomp_u), level, grid_no
+  
+  double precision :: e, T, X(nspec+naux)
+  double precision :: rhoInv
+  integer          :: i,n
+
+  type (eos_t) :: eos_state
+
+  !     Compute pressure from the EOS
+  do i = lo(1),hi(1)
+     
+     rhoInv = 1.d0/u(i,URHO)
+     e  = u(i,UEINT)*rhoInv
+     T  = u(i,UTEMP)
+     do n=1,nspec
+        X(n) = u(i,UFS+n-1)*rhoInv
+     enddo
+     do n=1,naux
+        X(nspec+n) = u(i,UFX+n-1)*rhoInv
+     enddo
+     
+     ! Protect against negative internal energy
+     if (allow_negative_energy .eq. 0 .and. e .le. 0.d0) then
+        eos_state%rho = u(i,URHO)
+        eos_state%T = T
+        eos_state%xn(:) = X
+
+        call eos(eos_input_rt, eos_state, .false.)
+
+        p(i,1) = eos_state%p
+     else
+        eos_state%rho = u(i,URHO)
+        eos_state%T = T
+        eos_state%xn(:) = X
+        eos_state%e = e
+
+        call eos(eos_input_re, eos_state, .false.)
+
+        p(i,1) = eos_state%p
+
+     end if
+     
+     p(i,1) = p(i,1) - hse_p(i)
+     
+  enddo
+  
+end subroutine ca_derpi
+
+!-----------------------------------------------------------------------
+
+subroutine ca_derpioverp0(p,p_l1,p_h1,ncomp_p, &
+                          u,u_l1,u_h1,ncomp_u,lo,hi,domlo, &
+                          domhi,dx,xlo,time,dt,bc,level,grid_no)
+
+  use network, only : nspec, naux
+  use eos_module
+  use meth_params_module, only : URHO, UMX, UEINT, UTEMP, UFS, UFX, &
+                                 allow_negative_energy
+  use probdata_module, only: hse_p
+
+  implicit none
+
+  integer p_l1,p_h1,ncomp_p
+  integer u_l1,u_h1,ncomp_u
+  integer lo(1), hi(1), domlo(1), domhi(1)
+  double precision p(p_l1:p_h1,ncomp_p)
+  double precision u(u_l1:u_h1,ncomp_u)
+  double precision dx(1), xlo(1), time, dt
+  integer bc(1,2,ncomp_u), level, grid_no
+  
+  double precision :: e, T, X(nspec+naux)
+  double precision :: rhoInv
+  integer          :: i,n
+
+  type (eos_t) :: eos_state
+  
+  !     Compute pressure from the EOS
+  do i = lo(1),hi(1)
+
+     rhoInv = 1.d0/u(i,URHO)
+     e  = u(i,UEINT)*rhoInv
+     T  = u(i,UTEMP)
+     do n=1,nspec
+        X(n) = u(i,UFS+n-1)*rhoInv
+     enddo
+     do n=1,naux
+        X(nspec+n) = u(i,UFX+n-1)*rhoInv
+     enddo
+     
+     ! Protect against negative internal energy
+     if (allow_negative_energy .eq. 0 .and. e .le. 0.d0) then
+        eos_state%rho = u(i,URHO)
+        eos_state%T = T
+        eos_state%xn(:) = X
+
+        call eos(eos_input_rt, eos_state, .false.)
+
+        p(i,1) = eos_state%p
+     else
+        eos_state%rho = u(i,URHO)
+        eos_state%T = T
+        eos_state%xn(:) = X
+        eos_state%e = e
+
+        call eos(eos_input_re, eos_state, .false.)
+
+        p(i,1) = eos_state%p
+
+     end if
+     
+     p(i,1) = abs(p(i,1) - hse_p(i)) / hse_p(i)
+     
+  enddo
+  
+end subroutine ca_derpioverp0
+
