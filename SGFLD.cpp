c3dd0f09fe4a5bd5513afec64e8ab4076cad48be
Weiqun Zhang <weiqunzhang@lbl.gov>
2015-02-23 13:22:17 -0800

    tiling ca_compute_powerlaw_kappa_s for SGFLD

--- SGFLD.cpp
+++ SGFLD.cpp
@@ -19,14 +19,17 @@ void Radiation::SGFLD_compute_rosseland(MultiFab& kappa_r, const MultiFab& state
   else if (const_scattering[0] > 0.0) {
     Real k_exp_p = 0.0;
     Real s_exp_p = 0.0;
-    for (MFIter mfi(kappa_r); mfi.isValid(); ++mfi) {
-      const Box& kbox = kappa_r[mfi].box();
-      BL_FORT_PROC_CALL(CA_COMPUTE_POWERLAW_KAPPA_S, ca_compute_powerlaw_kappa_s)
-	  (kbox.loVect(), kbox.hiVect(), 
-	   BL_TO_FORTRAN(kappa_r[mfi]), BL_TO_FORTRAN(state[mfi]),
-	   &const_kappa_r[0], &kappa_r_exp_m[0], &kappa_r_exp_n[0], &k_exp_p,
-	   &const_scattering[0], &scattering_exp_m[0], &scattering_exp_n[0], &s_exp_p,
-	   &prop_temp_floor[0], &kappa_r_floor);	 
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+    for (MFIter mfi(kappa_r,true); mfi.isValid(); ++mfi) {
+	const Box& bx = mfi.growntilebox();
+	BL_FORT_PROC_CALL(CA_COMPUTE_POWERLAW_KAPPA_S, ca_compute_powerlaw_kappa_s)
+	    (bx.loVect(), bx.hiVect(), 
+	     BL_TO_FORTRAN(kappa_r[mfi]), BL_TO_FORTRAN(state[mfi]),
+	     &const_kappa_r[0], &kappa_r_exp_m[0], &kappa_r_exp_n[0], &k_exp_p,
+	     &const_scattering[0], &scattering_exp_m[0], &scattering_exp_n[0], &s_exp_p,
+	     &prop_temp_floor[0], &kappa_r_floor);	 
     }
   }
   else {
