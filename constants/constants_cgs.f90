08536738a360dca6ac78eeafc9de9ad8312c48ff
Max Katz <maxpkatz@gmail.com>
2015-07-12 01:15:20 -0400

    Add number of cm in an AU, and define the parsec in terms of that.

--- constants/constants_cgs.f90
+++ constants/constants_cgs.f90
@@ -3,7 +3,8 @@
 module fundamental_constants_module
 
   use bl_types
-
+  use bl_constants_module, only: M_PI
+  
   implicit none
 
   ! newton's gravitational constant
@@ -56,8 +57,12 @@ module fundamental_constants_module
   ! radiation constant
   real(kind=dp_t), parameter :: a_rad = 4.0_dp_t*sigma_SB/c_light
 
-  ! number of centimeters in a parsec
-  real(kind=dp_t), parameter :: parsec = 3.0857e18
+  ! Number of centimeters in a parsec and an AU.
+  ! Note that the length of an AU is defined exactly
+  ! by IAU convention, the length of a parsec is also
+  ! defined exactly as (6.48e5 / pi) AU.
+  real(kind=dp_t), parameter :: AU = 1.49597871e13
+  real(kind=dp_t), parameter :: parsec = (648000 / M_PI) * AU
 
   ! Hubble constant (in s^{-1}, converted from 100 (km/s)/Mpc by dividing by 3.08568025e19km/Mpc)
   real(kind=dp_t), parameter :: Hubble_const = 32.407764868e-19
