7fab51f79258aa4d7946afbff951b1223398b98e
Max Katz <maximilian.katz@stonybrook.edu>
2015-07-17 00:09:34 -0400

    Some cleanup of sum_integrated_quantities.cpp, and also make sure that we don't attempt to open a data log if we didn't enter any in the inputs file.

--- Source/sum_integrated_quantities.cpp
+++ Source/sum_integrated_quantities.cpp
@@ -16,11 +16,13 @@ Castro::sum_integrated_quantities ()
     Real time        = state[State_Type].curTime();
     Real mass        = 0.0;
     Real xmom        = 0.0;
+    Real ymom        = 0.0;
+    Real zmom        = 0.0;
+    Real rho_e       = 0.0;
+    Real rho_K       = 0.0;
     Real rho_E       = 0.0;
 #ifdef SGS
     Real dt_crse     = parent->dtLevel(0);
-    Real rho_e       = 0.0;
-    Real rho_K       = 0.0;
     Real Etot        = 0.0;
     Real delta_E     = 0.0;
     Real delta_K     = 0.0;
@@ -30,14 +32,8 @@ Castro::sum_integrated_quantities ()
     Real rms_mach    = 0.0;
 #endif
     Real com_xloc    = 0.0;
-#if (BL_SPACEDIM>=2)
-    Real ymom        = 0.0;
     Real com_yloc    = 0.0;
-#endif
-#if (BL_SPACEDIM==3)
-    Real zmom        = 0.0;
     Real com_zloc    = 0.0;
-#endif
 
     for (int lev = 0; lev <= finest_level; lev++)
     {
@@ -52,30 +48,29 @@ Castro::sum_integrated_quantities ()
 
        if (show_center_of_mass) 
 	   com_xloc += ca_lev.locWgtSum("density", time, 0, local_flag);
-#if (BL_SPACEDIM>=2)
        ymom     += ca_lev.volWgtSum("ymom", time, local_flag);
+
        if (show_center_of_mass) 
 	   com_yloc += ca_lev.locWgtSum("density", time, 1, local_flag);
-#endif
-#if (BL_SPACEDIM==3)
+
        zmom     += ca_lev.volWgtSum("zmom", time, local_flag);
+
        if (show_center_of_mass) 
 	   com_zloc += ca_lev.locWgtSum("density", time, 2, local_flag);
-#endif
+
+       rho_e    += ca_lev.volWgtSum("rho_e", time, local_flag);
+       rho_K    += ca_lev.volWgtSum("kineng", time, local_flag);
        rho_E    += ca_lev.volWgtSum("rho_E", time, local_flag);
 
 #ifdef SGS
         Real  cur_time = state[SGS_Type].curTime();
         Real prev_time = state[SGS_Type].prevTime();
 
-        rho_e    += ca_lev.volWgtSum("rho_e", time, local_flag);
-        rho_K    += ca_lev.volWgtSum("rho_K", time, local_flag);
-
         delta_E  += ca_lev.volWgtSum("rho_E", cur_time, local_flag);
         delta_E  -= ca_lev.volWgtSum("rho_E", prev_time, local_flag);
 
-        delta_K  += ca_lev.volWgtSum("rho_K", cur_time, local_flag);
-        delta_K  -= ca_lev.volWgtSum("rho_K", prev_time, local_flag);
+        delta_K  += ca_lev.volWgtSum("kineng", cur_time, local_flag);
+        delta_K  -= ca_lev.volWgtSum("kineng", prev_time, local_flag);
 
         rms_mach  += ca_lev.volWgtSquaredSum("MachNumber", time, local_flag);
 
@@ -93,12 +88,12 @@ Castro::sum_integrated_quantities ()
     if (verbose > 0)
     {
 #ifdef SGS
-	const int nfoo = 10+BL_SPACEDIM;
-	Real foo[nfoo] = {mass, D_DECL(xmom,ymom,zmom), rho_E, rho_K, Etot, delta_E, delta_K, 
+	const int nfoo = 11+BL_SPACEDIM;
+	Real foo[nfoo] = {mass, D_DECL(xmom,ymom,zmom), rho_e, rho_K, rho_E, Etot, delta_E, delta_K, 
 			  prod_sgs, diss_sgs, turb_src, rms_mach};
 #else
-	const int nfoo = 2+BL_SPACEDIM;
-	Real foo[nfoo] = {mass, D_DECL(xmom,ymom,zmom), rho_E};
+	const int nfoo = 7;
+	Real foo[nfoo] = {mass, xmom, ymom, zmom, rho_e, rho_K, rho_E};
 #endif
 
 	Real coms[BL_SPACEDIM] = {D_DECL(com_xloc, com_yloc, com_zloc)};
@@ -117,15 +112,12 @@ Castro::sum_integrated_quantities ()
 	    int i = 0;
 	    mass     = foo[i++];
 	    xmom     = foo[i++];
-#if (BL_SPACEDIM>=2)
             ymom     = foo[i++];
-#endif
-#if (BL_SPACEDIM==3)
             zmom     = foo[i++];
-#endif
+	    rho_e    = foo[i++];
             rho_E    = foo[i++];
-#ifdef SGS
 	    rho_K    = foo[i++];
+#ifdef SGS
 	    Etot     = foo[i++];
 	    delta_E  = foo[i++];
 	    delta_K  = foo[i++];
@@ -138,15 +130,12 @@ Castro::sum_integrated_quantities ()
 	    std::cout << '\n';
 	    std::cout << "TIME= " << time << " MASS        = "   << mass  << '\n';
 	    std::cout << "TIME= " << time << " XMOM        = "   << xmom     << '\n';
-#if (BL_SPACEDIM>=2)
 	    std::cout << "TIME= " << time << " YMOM        = "   << ymom     << '\n';
-#endif
-#if (BL_SPACEDIM==3)
 	    std::cout << "TIME= " << time << " ZMOM        = "   << zmom     << '\n';
-#endif
+	    std::cout << "TIME= " << time << " RHO*e       = "   << rho_e     << '\n';
+	    std::cout << "TIME= " << time << " RHO*K       = "   << rho_K     << '\n';
 	    std::cout << "TIME= " << time << " RHO*E       = "   << rho_E     << '\n';
 #ifdef SGS
-	    std::cout << "TIME= " << time << " RHO*K       = "   << rho_K     << '\n';
 	    Etot     = rho_E + rho_K;
 	    std::cout << "TIME= " << time << " TOTAL E     = "   << Etot      << '\n';
 	    std::cout << "TIME= " << time << " DELTA E     = "   << delta_E   << '\n';
@@ -156,44 +145,69 @@ Castro::sum_integrated_quantities ()
 	    std::cout << "TIME= " << time << " DISS_SGS    = "   << diss_sgs  << '\n';
 	    std::cout << "TIME= " << time << " TURB_SRC    = "   << turb_src  << '\n';
 	    std::cout << "TIME= " << time << " DE+DK-TURB_SRC = "   << delta_E+delta_K-turb_src  << '\n';
+#endif
+	    if (parent->NumDataLogs() > 0 ) {
 
-   	    std::ostream& data_log1 = parent->DataLog(0);
-	    
-	    if (time == 0.0) {
-		data_log1 << std::setw(14) <<  "      time    ";
-		data_log1 << std::setw(14) <<  "        rho_E ";
-		data_log1 << std::setw(14) <<  "        rho_K ";
-		data_log1 << std::setw(14) <<  "        rho_e ";
-		data_log1 << std::setw(16) <<  "  Etot-sum_turb  ";
-		data_log1 << std::setw(14) <<  "      rms_mach" << std::endl;
+	       std::ostream& data_log1 = parent->DataLog(0);
+
+	       if (data_log1.good()) {
+
+		  if (time == 0.0) {
+		      data_log1 << std::setw(14) <<  "      time    ";
+		      data_log1 << std::setw(14) <<  "         mass ";
+		      data_log1 << std::setw(14) <<  "         xmom ";
+		      data_log1 << std::setw(14) <<  "         ymom ";
+		      data_log1 << std::setw(14) <<  "         zmom ";
+		      data_log1 << std::setw(14) <<  "        rho_K ";
+		      data_log1 << std::setw(14) <<  "        rho_e ";
+		      data_log1 << std::setw(14) <<  "        rho_E " << std::endl;
+		  }
+
+		      // Write the quantities at this time
+		  data_log1 << std::setw(14) <<  time;
+		  data_log1 << std::setw(14) <<  std::setprecision(6) << mass;
+		  data_log1 << std::setw(14) <<  std::setprecision(6) << xmom;
+		  data_log1 << std::setw(14) <<  std::setprecision(6) << ymom;
+		  data_log1 << std::setw(14) <<  std::setprecision(6) << zmom;
+		  data_log1 << std::setw(14) <<  std::setprecision(6) << rho_K;
+		  data_log1 << std::setw(14) <<  std::setprecision(6) << rho_e;
+		  data_log1 << std::setw(14) <<  std::setprecision(6) << rho_E << std::endl;
+
+	       }
+
 	    }
 
-		// Write the quantities at this time
-	    data_log1 << std::setw(14) <<  time;
-	    data_log1 << std::setw(14) <<  std::setprecision(6) << rho_E;
-	    data_log1 << std::setw(14) <<  std::setprecision(6) << rho_K;
-	    data_log1 << std::setw(14) <<  std::setprecision(6) << rho_e;
-	    data_log1 << std::setw(16) <<  std::setprecision(10) << Etot-sum_turb_src;
-	    data_log1 << std::setw(14) <<  std::setprecision(6) << rms_mach << std::endl;
+#ifdef SGS
+	    if (parent->NumDataLogs() > 1) { 
 
-	    std::ostream& data_log2 = parent->DataLog(1);
+	       std::ostream& data_log2 = parent->DataLog(1);
 
-		// Write the quantities that represent changes from prev_time to cur_time
-	    if (time == 0.0) {
-		data_log2 << std::setw(14) <<  "      time    ";
-		data_log2 << std::setw(14) <<  "      delta_E ";
-		data_log2 << std::setw(14) <<  "      delta_K ";
-		data_log2 << std::setw(14) <<  "      prod_sgs";
-		data_log2 << std::setw(14) <<  "      diss_sgs";
-		data_log2 << std::setw(14) <<  "      turb_src" << std::endl;
+	       if (data_log2.good()) {
+
+		  // Write the quantities that represent changes from prev_time to cur_time
+		  if (time == 0.0) {
+		      data_log2 << std::setw(14) <<  "      time    ";
+		      data_log2 << std::setw(14) <<  "         Etot ";
+		      data_log2 << std::setw(16) <<  "  Etot-sum_turb  ";
+		      data_log2 << std::setw(14) <<  "      rms_mach";
+		      data_log2 << std::setw(14) <<  "      delta_E ";
+		      data_log2 << std::setw(14) <<  "      delta_K ";
+		      data_log2 << std::setw(14) <<  "      prod_sgs";
+		      data_log2 << std::setw(14) <<  "      diss_sgs";
+		      data_log2 << std::setw(14) <<  "      turb_src" << std::endl;
+		  }
+
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << time;
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << Etot;
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << delta_E;
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << delta_K;
+		  data_log2 << std::setw(16) <<  std::setprecision(10) << Etot-sum_turb_src;
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << rms_mach;
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << prod_sgs;
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << diss_sgs;
+		  data_log2 << std::setw(14) <<  std::setprecision(6) << turb_src << std::endl;
+	       }
 	    }
-	    
-	    data_log2 << std::setw(14) <<  std::setprecision(6) << time;
-	    data_log2 << std::setw(14) <<  std::setprecision(6) << delta_E;
-	    data_log2 << std::setw(14) <<  std::setprecision(6) << delta_K;
-	    data_log2 << std::setw(14) <<  std::setprecision(6) << prod_sgs;
-	    data_log2 << std::setw(14) <<  std::setprecision(6) << diss_sgs;
-	    data_log2 << std::setw(14) <<  std::setprecision(6) << turb_src << std::endl;
 #endif
 	    
 	    if (show_center_of_mass) {
