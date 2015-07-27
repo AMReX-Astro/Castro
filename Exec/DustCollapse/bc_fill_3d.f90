6ed6c33ce4e58183b2b1d108f26d65fd63514c3f
Weiqun Zhang <weiqunzhang@lbl.gov>
2015-07-27 13:26:37 -0700

    added subroutine ca_phigravfill for 3D DustCollapse

--- Exec/DustCollapse/bc_fill_3d.f90
+++ Exec/DustCollapse/bc_fill_3d.f90
@@ -300,3 +300,23 @@ subroutine ca_gravzfill(grav,grav_l1,grav_l2,grav_l3,grav_h1,grav_h2,grav_h3, &
   call filcc(grav,grav_l1,grav_l2,grav_l3,grav_h1,grav_h2,grav_h3,domlo,domhi,delta,xlo,bc)
   
 end subroutine ca_gravzfill
+
+! ::: -----------------------------------------------------------
+
+subroutine ca_phigravfill(phi,phi_l1,phi_l2,phi_l3, &
+                          phi_h1,phi_h2,phi_h3,domlo,domhi,delta,xlo,time,bc)
+
+  implicit none
+
+  include 'bc_types.fi'
+
+  integer          :: phi_l1,phi_l2,phi_l3,phi_h1,phi_h2,phi_h3
+  integer          :: bc(3,2,*)
+  integer          :: domlo(3), domhi(3)
+  double precision :: delta(3), xlo(3), time
+  double precision :: phi(phi_l1:phi_h1,phi_l2:phi_h2,phi_l3:phi_h3)
+
+  call filcc(phi,phi_l1,phi_l2,phi_l3,phi_h1,phi_h2,phi_h3, &
+             domlo,domhi,delta,xlo,bc)
+
+end subroutine ca_phigravfill
