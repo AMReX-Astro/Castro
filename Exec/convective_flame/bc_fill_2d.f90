377977671cda90d1b1448c0dc337d5f8ed1f84d7
Michael Zingale <michael.zingale@stonybrook.edu>
2015-09-25 17:19:03 -0400

    fix boundary condition UMZ

--- Exec/convective_flame/bc_fill_2d.f90
+++ Exec/convective_flame/bc_fill_2d.f90
@@ -4,7 +4,7 @@ subroutine ca_hypfill(adv,adv_l1,adv_l2,adv_h1,adv_h2,domlo,domhi,delta,xlo,time
 
   use probdata_module
   use prob_params_module, only: center
-  use meth_params_module, only: NVAR, URHO, UMX, UMY, UEDEN, UEINT, UFS, UTEMP
+  use meth_params_module, only: NVAR, URHO, UMX, UMY, UMZ, UEDEN, UEINT, UFS, UTEMP
   use interpolate_module
   use eos_module
   use eos_type_module
@@ -120,80 +120,17 @@ subroutine ca_hypfill(adv,adv_l1,adv_l2,adv_h1,adv_h2,domlo,domhi,delta,xlo,time
                 domlo,domhi,delta,xlo,bc(1,1,n))
   enddo
 
-  !        XLO
-  if ( bc(1,1,1).eq.EXT_DIR .and. adv_l1.lt.domlo(1)) then
-
-     do j=adv_l2,adv_h2
-        y = xlo(2) + delta(2)*(float(j-adv_l2) + 0.5d0)
-        do i=domlo(1)-1,adv_l1,-1
-
-           ! zero transverse momentum
-           adv(i,j,UMY) = 0.d0
-
-           if (boundary_type .eq. 1) then
-              ! extrapolate normal momentum
-              ! enforces pi=0 at boundary
-              adv(i,j,UMX) = adv(domlo(1),j,UMX)
-           else
-              ! zero normal momentum
-              ! permits pi to pass through boundary
-              adv(i,j,UMX) = 0.d0
-           end if
-
-           adv(i,j,URHO) = density(j)
-           adv(i,j,UFS) = adv(i,j,URHO)
-           adv(i,j,UEINT) = eint(j)*adv(i,j,URHO)
-           adv(i,j,UEDEN) = adv(i,j,UEINT) &
-                + 0.5d0*(adv(i,j,UMX)**2+adv(i,j,UMY)**2)/adv(i,j,URHO)
-           adv(i,j,UTEMP) = temp(j)
-
-        end do
-     end do
-
-  end if
-
-  !        XHI
-  if ( bc(1,2,1).eq.EXT_DIR .and. adv_h1.gt.domhi(1)) then
-
-     do j=adv_l2,adv_h2
-        y = xlo(2) + delta(2)*(float(j-adv_l2) + 0.5d0)
-        do i=domhi(1)+1,adv_h1
-
-           ! zero transverse momentum
-           adv(i,j,UMY) = 0.d0
-
-           if (boundary_type .eq. 1) then
-              ! extrapolate normal momentum
-              ! enforces pi=0 at boundary
-              adv(i,j,UMX) = adv(domhi(1),j,UMX)
-           else
-              ! zero normal momentum
-              ! permits pi to pass through boundary
-              adv(i,j,UMX) = 0.d0
-           end if
-
-           adv(i,j,URHO) = density(j)
-           adv(i,j,UFS) = adv(i,j,URHO)
-           adv(i,j,UEINT) = eint(j)*adv(i,j,URHO)
-           adv(i,j,UEDEN) = adv(i,j,UEINT) &
-                + 0.5d0*(adv(i,j,UMX)**2+adv(i,j,UMY)**2)/adv(i,j,URHO)
-           adv(i,j,UTEMP) = temp(j)
-
-        end do
-     end do
-
-  end if
-
-
   !        YLO
   if ( bc(2,1,1).eq.EXT_DIR .and. adv_l2.lt.domlo(2)) then
      ! this do loop counts backwards since we want to work downward
      do j=domlo(2)-1,adv_l2,-1
         y = xlo(2) + delta(2)*(float(j-adv_l2) + 0.5d0)
+
         do i=adv_l1,adv_h1
 
            ! zero transverse momentum
            adv(i,j,UMX) = 0.d0
+           adv(i,j,UMZ) = 0.d0
 
            if (boundary_type .eq. 1) then
               ! extrapolate normal momentum
@@ -209,7 +146,7 @@ subroutine ca_hypfill(adv,adv_l1,adv_l2,adv_h1,adv_h2,domlo,domhi,delta,xlo,time
            adv(i,j,UFS) = adv(i,j,URHO)
            adv(i,j,UEINT) = eint(j)*adv(i,j,URHO)
            adv(i,j,UEDEN) = adv(i,j,UEINT) &
-                + 0.5d0*(adv(i,j,UMX)**2+adv(i,j,UMY)**2)/adv(i,j,URHO)
+                + 0.5d0*(adv(i,j,UMX)**2+adv(i,j,UMY)**2+adv(i,j,UMZ)**2)/adv(i,j,URHO)
            adv(i,j,UTEMP) = temp(j)
 
         end do
@@ -224,6 +161,7 @@ subroutine ca_hypfill(adv,adv_l1,adv_l2,adv_h1,adv_h2,domlo,domhi,delta,xlo,time
 
            ! zero transverse momentum
            adv(i,j,UMX) = 0.d0
+           adv(i,j,UMZ) = 0.d0
 
            if (boundary_type .eq. 1) then
               ! extrapolate normal momentum
@@ -239,7 +177,7 @@ subroutine ca_hypfill(adv,adv_l1,adv_l2,adv_h1,adv_h2,domlo,domhi,delta,xlo,time
            adv(i,j,UFS) = adv(i,j,URHO)
            adv(i,j,UEINT) = eint(j)*adv(i,j,URHO)
            adv(i,j,UEDEN) = adv(i,j,UEINT) &
-                + 0.5d0*(adv(i,j,UMX)**2+adv(i,j,UMY)**2)/adv(i,j,URHO)
+                + 0.5d0*(adv(i,j,UMX)**2+adv(i,j,UMY)**2+adv(i,j,UMZ)**2)/adv(i,j,URHO)
            adv(i,j,UTEMP) = temp(j)
 
         end do
