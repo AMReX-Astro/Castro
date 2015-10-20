5b4074e6f7ef0cb7e649edbd499e8cee4782b9f4
Michael Zingale <michael.zingale@stonybrook.edu>
2015-10-20 15:26:32 -0400

    hook in the diffusion_cutoff_density -- below this density, we do not diffuse. This helps with the surface of a star

--- Source/Src_nd/EstDt_diffusion_nd.f90
+++ Source/Src_nd/EstDt_diffusion_nd.f90
@@ -8,7 +8,7 @@ subroutine ca_estdt_diffusion(lo,hi,state,s_lo,s_hi,dx,dt)
   use eos_module
   use eos_type_module
   use meth_params_module, only: NVAR, URHO, UEINT, UTEMP, UFS, UFX, &
-       allow_negative_energy
+       diffuse_cutoff_density
   use prob_params_module, only: dim
   use bl_constants_module
   use conductivity_module
@@ -30,39 +30,43 @@ subroutine ca_estdt_diffusion(lo,hi,state,s_lo,s_hi,dx,dt)
      do j = lo(2), hi(2)
         do i = lo(1), hi(1)
 
-           rho_inv = ONE/state(i,j,k,URHO)
+           if (state(i,j,k,URHO) > diffuse_cutoff_density) then
 
-           ! we need cv
-           eos_state % rho = state(i,j,k,URHO )
-           eos_state % T   = state(i,j,k,UTEMP)
-           eos_state % e   = state(i,j,k,UEINT) * rho_inv
+              rho_inv = ONE/state(i,j,k,URHO)
 
-           eos_state % xn  = state(i,j,k,UFS:UFS+nspec-1) * rho_inv
-           eos_state % aux = state(i,j,k,UFX:UFX+naux-1) * rho_inv
+              ! we need cv
+              eos_state % rho = state(i,j,k,URHO )
+              eos_state % T   = state(i,j,k,UTEMP)
+              eos_state % e   = state(i,j,k,UEINT) * rho_inv
 
-           call eos(eos_input_re, eos_state)
+              eos_state % xn  = state(i,j,k,UFS:UFS+nspec-1) * rho_inv
+              eos_state % aux = state(i,j,k,UFX:UFX+naux-1) * rho_inv
 
-           ! we also need the conductivity
-           call thermal_conductivity(eos_state, cond)
+              call eos(eos_input_re, eos_state)
 
-           ! maybe we should check (and take action) on negative cv here?
-           D = cond*rho_inv/eos_state%cv
+              ! we also need the conductivity
+              call thermal_conductivity(eos_state, cond)
+
+              ! maybe we should check (and take action) on negative cv here?
+              D = cond*rho_inv/eos_state%cv
            
-           dt1 = HALF*dx(1)**2/D
+              dt1 = HALF*dx(1)**2/D
 
-           if (dim >= 2) then
-              dt2 = HALF*dx(2)**2/D
-           else
-              dt2 = dt1
+              if (dim >= 2) then
+                 dt2 = HALF*dx(2)**2/D
+              else
+                 dt2 = dt1
+              endif
+
+              if (dim == 3) then
+                 dt3 = HALF*dx(3)**2/D
+              else
+                 dt3 = dt1
+              endif
+
+              dt  = min(dt,dt1,dt2,dt3)
+              
            endif
-
-           if (dim == 3) then
-              dt3 = HALF*dx(3)**2/D
-           else
-              dt3 = dt1
-           endif
-
-           dt  = min(dt,dt1,dt2,dt3)
 
         enddo
      enddo
