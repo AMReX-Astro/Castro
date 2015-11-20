910a7b8fe8f06a1d192f71614e459692541d39c9
Michael Zingale <michael.zingale@stonybrook.edu>
2015-11-20 13:55:32 -0500

    a 3-d version of the odd-even problem -- this looks great with the new HLLC solver, and awful with the default solvers

--- Exec/oddeven/Prob_3d.f90
+++ Exec/oddeven/Prob_3d.f90
@@ -4,12 +4,12 @@ subroutine PROBINIT (init,name,namlen,problo,probhi)
   use probdata_module
   use bl_constants_module
   use bl_error_module
-
+  
   implicit none
 
   integer init, namlen
   integer name(namlen)
-  double precision problo(2), probhi(2)
+  double precision problo(3), probhi(3)
 
   integer untin,i
 
@@ -25,13 +25,14 @@ subroutine PROBINIT (init,name,namlen,problo,probhi)
   do i = 1, namlen
      probin(i:i) = char(name(i))
   end do
-         
+
   ! set namelist defaults
 
   ! set center, domain extrema
   center(1) = (problo(1)+probhi(1))/2.d0
   center(2) = (problo(2)+probhi(2))/2.d0
-  
+  center(3) = (problo(3)+probhi(3))/2.d0
+
   ! Read namelists
   open(newunit=untin, file=probin(1:namlen), form='formatted', status='old')
   read(untin,fortin)
@@ -62,7 +63,8 @@ end subroutine PROBINIT
 ! :::		   ghost region).
 ! ::: -----------------------------------------------------------
 subroutine ca_initdata(level,time,lo,hi,nscal, &
-                       state,state_l1,state_l2,state_h1,state_h2, &
+                       state,state_l1,state_l2,state_l3, &
+                       state_h1,state_h2,state_h3, &
                        delta,xlo,xhi)
 
   use bl_constants_module
@@ -70,81 +72,85 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
   use eos_module
   use network, only: nspec
   use meth_params_module, only : NVAR, URHO, UMX, UMY, UMZ, UEDEN, UEINT, UFS, UTEMP
-  use prob_params_module, only: center
-  
+  use prob_params_module, only : center
+
   implicit none
 
   integer level, nscal
-  integer lo(2), hi(2)
-  integer state_l1,state_l2,state_h1,state_h2
-  double precision xlo(2), xhi(2), time, delta(2)
-  double precision state(state_l1:state_h1,state_l2:state_h2,NVAR)
+  integer lo(3), hi(3)
+  integer state_l1,state_l2,state_l3,state_h1,state_h2,state_h3
+  double precision xlo(3), xhi(3), time, delta(3)
+  double precision state(state_l1:state_h1,state_l2:state_h2, &
+                         state_l3:state_h3,NVAR)
 
-  double precision xcen, ycen
+  double precision xcen, ycen, zcen
   double precision dens, eint, xvel, X(nspec), temp
-  
-  integer i,j, icen, jcen
+  integer i,j,k, icen, jcen, kcen
 
   type (eos_t) :: eos_state
 
   ! compute the integer location of the center of the domain
   icen = center(1)/delta(1)
   jcen = center(2)/delta(2)
+  kcen = center(3)/delta(3)
+  
+  do k = lo(3), hi(3)
+     zcen = xlo(3) + delta(3)*(dble(k-lo(3)) + HALF)
+     
+     do j = lo(2), hi(2)
+        ycen = xlo(2) + delta(2)*(dble(j-lo(2)) + HALF)
 
-    
-  do j = lo(2), hi(2)
-     ycen = xlo(2) + delta(2)*(dble(j-lo(2)) + HALF)
+        do i = lo(1), hi(1)
+           xcen = xlo(1) + delta(1)*(dble(i-lo(1)) + HALF)
 
-     do i = lo(1), hi(1)
-        xcen = xlo(1) + delta(1)*(dble(i-lo(1)) + HALF)
+           if (i == icen .and. j == jcen .and. k == kcen) then
+              dens = dens_ambient*dens_pert_factor
+           else
+              dens = dens_ambient
+           endif
 
-        if (i == icen .and. j == jcen) then
-           dens = dens_ambient*dens_pert_factor
-        else
-           dens = dens_ambient
-        endif
+           state(i,j,k,URHO) = dens
 
-        state(i,j,URHO) = dens
+           ! velocity perturbation
+           if (xcen < center(1)) then 
+              xvel = vel_pert
+           else if (xcen > center(1)) then
+              xvel = -vel_pert
+           else
+              xvel = ZERO
+           endif
 
-        ! velocity perturbation
-        if (xcen < center(1)) then 
-           xvel = vel_pert
-        else if (xcen > center(1)) then
-           xvel = -vel_pert
-        else
-           xvel = ZERO
-        endif
-
-        state(i,j,UMX) = dens*xvel
-        state(i,j,UMY) = ZERO
-        state(i,j,UMZ) = ZERO
-
-        ! set the composition
-        X(:) = ZERO
-        X(1) = ONE
+           state(i,j,k,UMX) = dens*xvel
+           state(i,j,k,UMY) = ZERO
+           state(i,j,k,UMZ) = ZERO
+           
+           ! set the composition
+           X(:) = ZERO
+           X(1) = ONE
         
         
-        ! compute the internal energy and temperature
-        eos_state%T = ONE ! initial guess
-        eos_state%rho = dens
-        eos_state%p = p_ambient
-        eos_state%xn(:) = X
+           ! compute the internal energy and temperature
+           eos_state%T = ONE ! initial guess
+           eos_state%rho = dens
+           eos_state%p = p_ambient
+           eos_state%xn(:) = X
+           
+           call eos(eos_input_rp, eos_state)
 
-        call eos(eos_input_rp, eos_state)
+           temp = eos_state%T
+           eint = eos_state%e
+           
+           state(i,j,k,UEDEN) = dens*eint +  &
+                HALF*(state(i,j,k,UMX)**2/state(i,j,k,URHO) + &
+                      state(i,j,k,UMY)**2/state(i,j,k,URHO) + &
+                      state(i,j,k,UMZ)**2/state(i,j,k,URHO))
+           
+           state(i,j,k,UEINT) = dens*eint
+           state(i,j,k,UTEMP) = temp
 
-        temp = eos_state%T
-        eint = eos_state%e
-
-        state(i,j,UEDEN) = dens*eint +  &
-             HALF*(state(i,j,UMX)**2/state(i,j,URHO) + &
-                    state(i,j,UMY)**2/state(i,j,URHO) + &
-                    state(i,j,UMZ)**2/state(i,j,URHO))
-
-        state(i,j,UEINT) = dens*eint
-        state(i,j,UTEMP) = temp
-
-        state(i,j,UFS:UFS-1+nspec) = dens*X(:)
-
+           state(i,j,k,UFS:UFS-1+nspec) = dens*X(:)
+           
+        enddo
      enddo
   enddo
 
