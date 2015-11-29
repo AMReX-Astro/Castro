93bd3aa8098e50a1ad3e47d64ad268cd5df724fb
Michael Zingale <michael.zingale@stonybrook.edu>
2015-11-28 19:19:24 -0500

    new files

---
+++ Source/Src_nd/meth_params.f90
@@ -0,0 +1,94 @@
+! This module stores the runtime parameters and integer names for 
+! indexing arrays.
+!
+! These parameter are initialized in set_method_params() 
+
+module meth_params_module
+
+  implicit none
+
+  integer         , save :: iorder        ! used only in uslope 
+
+  ! number of ghost cells for the hyperbolic solver
+  integer, parameter     :: NHYP    = 4
+
+  ! NTHERM: number of thermodynamic variables
+  integer         , save :: NTHERM, NVAR
+  integer         , save :: URHO, UMX, UMY, UMZ, UEDEN, UEINT, UTEMP, UFA, UFS, UFX
+
+  ! QTHERM: number of primitive variables
+  integer         , save :: QTHERM, QVAR
+  integer         , save :: QRHO, QU, QV, QW, QPRES, QREINT, QTEMP
+  integer         , save :: QGAMC, QGAME
+  integer         , save :: QFA, QFS, QFX
+
+  ! These are only used when we use the SGS model.
+  integer         , save :: UESGS,QESGS
+
+  integer         , save :: nadv
+
+  integer, save :: npassive
+  integer, save, allocatable :: qpass_map(:), upass_map(:)
+
+
+  integer         , save :: numpts_1d
+
+  double precision, save, allocatable :: outflow_data_old(:,:)
+  double precision, save, allocatable :: outflow_data_new(:,:)
+  double precision, save :: outflow_data_old_time
+  double precision, save :: outflow_data_new_time
+  logical         , save :: outflow_data_allocated
+  double precision, save :: max_dist
+
+  double precision, save :: diffuse_cutoff_density
+
+  double precision, save :: const_grav
+
+  logical, save :: get_g_from_phi
+  
+  character(len=:), allocatable :: gravity_type
+  
+
+  double precision, save :: difmag
+  double precision, save :: small_dens
+  double precision, save :: small_temp
+  double precision, save :: small_pres
+  double precision, save :: small_ener
+  integer         , save :: do_grav
+  integer         , save :: do_rotation
+  integer         , save :: do_acc
+  double precision, save :: rot_period
+  double precision, save :: rot_period_dot
+  integer         , save :: rot_source_type
+  integer         , save :: rot_axis
+  logical         , save :: deterministic
+  integer         , save :: normalize_species
+  integer         , save :: fix_mass_flux
+  integer         , save :: allow_negative_energy
+  integer         , save :: ppm_type
+  integer         , save :: ppm_reference
+  integer         , save :: ppm_trace_sources
+  integer         , save :: ppm_temp_fix
+  integer         , save :: ppm_tau_in_tracing
+  integer         , save :: ppm_predict_gammae
+  integer         , save :: ppm_reference_edge_limit
+  integer         , save :: ppm_reference_eigenvectors
+  integer         , save :: hybrid_riemann
+  integer         , save :: use_colglaz
+  integer         , save :: riemann_solver
+  integer         , save :: cg_maxiter
+  double precision, save :: cg_tol
+  integer         , save :: use_flattening
+  integer         , save :: ppm_flatten_before_integrals
+  integer         , save :: transverse_use_eos
+  integer         , save :: transverse_reset_density
+  integer         , save :: transverse_reset_rhoe
+  logical         , save :: dual_energy_update_E_from_e
+  double precision, save :: dual_energy_eta1
+  double precision, save :: dual_energy_eta2
+  double precision, save :: dual_energy_eta3
+  integer         , save :: use_pslope
+  integer         , save :: grav_source_type
+  integer         , save :: do_sponge
+
+end module meth_params_module
---
+++ Source/Src_nd/set_castro_params.f90
@@ -0,0 +1,153 @@
+subroutine set_castro_method_params( &
+  difmag_in, small_dens_in, small_temp_in,  &
+  small_pres_in, small_ener_in, do_grav_in,  &
+  do_rotation_in, do_acc_in, rot_period_in,  &
+  rot_period_dot_in, rot_source_type_in, rot_axis_in,  &
+  deterministic_in, normalize_species_in, fix_mass_flux_in,  &
+  allow_negative_energy_in, ppm_type_in, ppm_reference_in,  &
+  ppm_trace_sources_in, ppm_temp_fix_in, ppm_tau_in_tracing_in,  &
+  ppm_predict_gammae_in, ppm_reference_edge_limit_in, ppm_reference_eigenvectors_in,  &
+  hybrid_riemann_in, use_colglaz_in, riemann_solver_in,  &
+  cg_maxiter_in, cg_tol_in, use_flattening_in,  &
+  ppm_flatten_before_integrals_in, transverse_use_eos_in, transverse_reset_density_in,  &
+  transverse_reset_rhoe_in, dual_energy_update_E_from_e_in, dual_energy_eta1_in,  &
+  dual_energy_eta2_in, dual_energy_eta3_in, use_pslope_in,  &
+  grav_source_type_in, do_sponge_in)
+
+  use meth_params_module
+  use network, only : nspec, naux
+  use eos_module
+  use parallel
+  use bl_error_module
+
+  implicit none
+
+  integer :: ioproc
+
+  double precision, intent(in) :: difmag_in
+  double precision, intent(in) :: small_dens_in
+  double precision, intent(in) :: small_temp_in
+  double precision, intent(in) :: small_pres_in
+  double precision, intent(in) :: small_ener_in
+  integer,          intent(in) :: do_grav_in
+  integer,          intent(in) :: do_rotation_in
+  integer,          intent(in) :: do_acc_in
+  double precision, intent(in) :: rot_period_in
+  double precision, intent(in) :: rot_period_dot_in
+  integer,          intent(in) :: rot_source_type_in
+  integer,          intent(in) :: rot_axis_in
+  integer,          intent(in) :: deterministic_in
+  integer,          intent(in) :: normalize_species_in
+  integer,          intent(in) :: fix_mass_flux_in
+  integer,          intent(in) :: allow_negative_energy_in
+  integer,          intent(in) :: ppm_type_in
+  integer,          intent(in) :: ppm_reference_in
+  integer,          intent(in) :: ppm_trace_sources_in
+  integer,          intent(in) :: ppm_temp_fix_in
+  integer,          intent(in) :: ppm_tau_in_tracing_in
+  integer,          intent(in) :: ppm_predict_gammae_in
+  integer,          intent(in) :: ppm_reference_edge_limit_in
+  integer,          intent(in) :: ppm_reference_eigenvectors_in
+  integer,          intent(in) :: hybrid_riemann_in
+  integer,          intent(in) :: use_colglaz_in
+  integer,          intent(in) :: riemann_solver_in
+  integer,          intent(in) :: cg_maxiter_in
+  double precision, intent(in) :: cg_tol_in
+  integer,          intent(in) :: use_flattening_in
+  integer,          intent(in) :: ppm_flatten_before_integrals_in
+  integer,          intent(in) :: transverse_use_eos_in
+  integer,          intent(in) :: transverse_reset_density_in
+  integer,          intent(in) :: transverse_reset_rhoe_in
+  integer,          intent(in) :: dual_energy_update_E_from_e_in
+  double precision, intent(in) :: dual_energy_eta1_in
+  double precision, intent(in) :: dual_energy_eta2_in
+  double precision, intent(in) :: dual_energy_eta3_in
+  integer,          intent(in) :: use_pslope_in
+  integer,          intent(in) :: grav_source_type_in
+  integer,          intent(in) :: do_sponge_in
+
+  difmag = difmag_in
+  small_dens = small_dens_in
+  small_temp = small_temp_in
+  small_pres = small_pres_in
+  small_ener = small_ener_in
+  do_grav = do_grav_in
+  do_rotation = do_rotation_in
+  do_acc = do_acc_in
+  rot_period = rot_period_in
+  rot_period_dot = rot_period_dot_in
+  rot_source_type = rot_source_type_in
+  rot_axis = rot_axis_in
+  deterministic = deterministic_in .ne. 0
+  normalize_species = normalize_species_in
+  fix_mass_flux = fix_mass_flux_in
+  allow_negative_energy = allow_negative_energy_in
+  ppm_type = ppm_type_in
+  ppm_reference = ppm_reference_in
+  ppm_trace_sources = ppm_trace_sources_in
+  ppm_temp_fix = ppm_temp_fix_in
+  ppm_tau_in_tracing = ppm_tau_in_tracing_in
+  ppm_predict_gammae = ppm_predict_gammae_in
+  ppm_reference_edge_limit = ppm_reference_edge_limit_in
+  ppm_reference_eigenvectors = ppm_reference_eigenvectors_in
+  hybrid_riemann = hybrid_riemann_in
+  use_colglaz = use_colglaz_in
+  riemann_solver = riemann_solver_in
+  cg_maxiter = cg_maxiter_in
+  cg_tol = cg_tol_in
+  use_flattening = use_flattening_in
+  ppm_flatten_before_integrals = ppm_flatten_before_integrals_in
+  transverse_use_eos = transverse_use_eos_in
+  transverse_reset_density = transverse_reset_density_in
+  transverse_reset_rhoe = transverse_reset_rhoe_in
+  dual_energy_update_E_from_e = dual_energy_update_E_from_e_in .ne. 0
+  dual_energy_eta1 = dual_energy_eta1_in
+  dual_energy_eta2 = dual_energy_eta2_in
+  dual_energy_eta3 = dual_energy_eta3_in
+  use_pslope = use_pslope_in
+  grav_source_type = grav_source_type_in
+  do_sponge = do_sponge_in
+
+  ! some checks
+  call bl_pd_is_ioproc(ioproc)
+
+  if (small_dens_in > 0.d0) then
+     small_dens = small_dens_in
+  else
+     if (ioproc == 1) then
+        call bl_warning("Warning:: small_dens has not been set, defaulting to 1.d-200.")
+     endif
+     small_dens = 1.d-200
+  endif
+
+  if (small_temp_in > 0.d0) then
+     small_temp = small_temp_in
+  else
+     if (ioproc == 1) then
+        call bl_warning("Warning:: small_temp has not been set, defaulting to 1.d-200.")
+     endif
+     small_temp = 1.d-200
+  endif
+
+  if (small_pres_in > 0.d0) then
+     small_pres = small_pres_in
+  else
+     small_pres = 1.d-200
+  endif
+  
+  if (small_ener_in > 0.d0) then
+     small_ener = small_ener_in
+  else
+     small_ener = 1.d-200
+  endif
+  
+  call eos_init(small_dens=small_dens, small_temp=small_temp)
+
+  ! The EOS might have modified our choices because of its
+  ! internal limitations, so let's get small_dens and small_temp
+  ! again just to make sure we're consistent with the EOS.
+  
+  call eos_get_small_dens(small_dens)
+  call eos_get_small_temp(small_temp)
+
+end subroutine set_castro_method_params
