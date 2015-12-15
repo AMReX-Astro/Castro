593d76f316f2b6d481937b2debc50d1af86cc878
Max Katz <maximilian.katz@stonybrook.edu>
2015-12-15 01:30:19 -0500

    Add the rotating torus problem of Papaloizou and Pringle (1984). This problem exercises the rotation, point-mass gravity, and polytrope EOS modules, and is written in dimension-agnostic fashion, though it is meant to be a 3D test.

---
+++ Exec/rotating_torus/probdata.f90
@@ -0,0 +1,13 @@
+module probdata_module
+
+  double precision, save :: inner_radius = 0.75d0
+  double precision, save :: outer_radius = 1.50d0
+
+  double precision, save :: density_maximum_radius
+
+  double precision, save :: torus_width
+  double precision, save :: torus_center
+
+  double precision, save :: ambient_density = 1.0d-8
+  
+end module probdata_module
