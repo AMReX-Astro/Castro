935329ac42e0dcc6ea11bbea8f8a4e90e4b5dbb7
Michael Zingale <michael.zingale@stonybrook.edu>
2016-01-25 09:14:27 -0500

    fix the pass

--- Src_1d/CastroRad_1d.f90
+++ Src_1d/CastroRad_1d.f90
@@ -153,8 +153,9 @@ subroutine ca_umdrv_rad(is_finest_level,time,&
   mass_added = 0.d0
   eint_added = 0.d0
   eden_added = 0.d0
+
   call enforce_minimum_density(uin,uin_l1,uin_h1,uout,uout_l1,uout_h1,lo,hi,&
-       mass_added,eint_added,eden_added,verbose)
+                               mass_added,eint_added,eden_added,verbose)
   
   ! Enforce that the species >= 0
   call ca_enforce_nonnegative_species(uout,uout_l1,uout_h1,lo,hi)
