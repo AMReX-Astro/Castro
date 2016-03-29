d19e9a2633c2bd059292b76330094b715179dff3
Michael Zingale <michael.zingale@stonybrook.edu>
2016-03-29 13:51:36 -0400

    we don't need eos_type_module

--- Networks/general_null/actual_burner.f90
+++ Networks/general_null/actual_burner.f90
@@ -3,9 +3,8 @@ module actual_burner_module
   use bl_types
   use bl_constants_module
   use network
-  use eos_type_module
   use burn_type_module
-  
+
 contains
 
   subroutine actual_burner_init()
@@ -13,7 +12,6 @@ contains
     ! Do nothing in this burner.
 
   end subroutine actual_burner_init
-
 
 
   subroutine actual_burner(state_in, state_out, dt, time)
@@ -25,7 +23,7 @@ contains
     double precision, intent(in) :: dt, time
 
     ! Do nothing in this burner.
-    
+
   end subroutine actual_burner
 
 end module actual_burner_module
