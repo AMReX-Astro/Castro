a0c2a6fbd708af2dbb3aed9504bf62126ba27667
Michael Zingale <michael.zingale@stonybrook.edu>
2016-07-18 15:44:36 -0400

    no need for enthalpy in burn_t

--- Microphysics/networks/burn_type.f90
+++ Microphysics/networks/burn_type.f90
@@ -35,7 +35,6 @@ module burn_type_module
     double precision :: rho              = init_num
     double precision :: T                = init_num
     double precision :: e                = init_num
-    double precision :: h                = init_num
     double precision :: xn(nspec)        = init_num
     double precision :: aux(naux)        = init_num
     double precision :: cv               = init_num
