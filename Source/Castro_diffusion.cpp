dc209f2a9b5e749c3a758b6f07804d3a769a2e97
Max Katz <maximilian.katz@stonybrook.edu>
2016-07-25 05:27:55 -0400

    Reorganize a little

--- Source/Castro_diffusion.cpp
+++ Source/Castro_diffusion.cpp
@@ -5,7 +5,6 @@
 
 using std::string;
 
-#ifdef DIFFUSION
 #include "Diffusion.H"
 
 void
@@ -276,4 +275,460 @@ Castro::time_center_viscous_term(MultiFab& S_new, MultiFab& OldViscousTermforMom
         }
 }
 #endif
+
+void
+Castro::getTempDiffusionTerm (Real time, MultiFab& TempDiffTerm)
+{
+    BL_PROFILE("Castro::getTempDiffusionTerm()");
+
+   MultiFab& S_old = get_old_data(State_Type);
+   if (verbose && ParallelDescriptor::IOProcessor())
+      std::cout << "Calculating diffusion term at time " << time << std::endl;
+
+#ifdef TAU
+   if (tau_diff == 0)
+     std::cerr << "ERROR:tau must be defined if USE_TAU = TRUE " << std::endl;
+#endif
+
+   // Fill coefficients at this level.
+   PArray<MultiFab> coeffs(BL_SPACEDIM,PArrayManage);
+   PArray<MultiFab> coeffs_temporary(3,PArrayManage); // This is what we pass to the dimension-agnostic Fortran
+   for (int dir = 0; dir < 3; dir++) {
+       if (dir < BL_SPACEDIM) {
+	 coeffs.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+	 coeffs_temporary.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+       } else {
+	 coeffs_temporary.set(dir,new MultiFab(grids, 1, 0, Fab_allocate));
+       }
+   }
+
+   // Fill temperature at this level.
+   MultiFab Temperature(grids,1,1,Fab_allocate);
+
+   {
+       FillPatchIterator fpi(*this,S_old,1,time,State_Type,0,NUM_STATE);
+       MultiFab& state_old = fpi.get_mf();
+
+       MultiFab::Copy(Temperature, state_old, Temp, 0, 1, 1);
+
+       for (MFIter mfi(state_old); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = grids[mfi.index()];
+
+	   ca_fill_temp_cond(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			     BL_TO_FORTRAN_3D(state_old[mfi]),
+#ifdef TAU
+			     BL_TO_FORTRAN_3D((*tau_diff)[mfi]),
+#endif
+			     BL_TO_FORTRAN_3D(coeffs_temporary[0][mfi]),
+			     BL_TO_FORTRAN_3D(coeffs_temporary[1][mfi]),
+			     BL_TO_FORTRAN_3D(coeffs_temporary[2][mfi]));
+       }
+   }
+
+   // Now copy the temporary array results back to the
+   // correctly dimensioned coeffs array.
+
+   for (int dir = 0; dir < BL_SPACEDIM; dir++)
+     MultiFab::Copy(coeffs[dir], coeffs_temporary[dir], 0, 0, 1, 0);
+
+   if (Geometry::isAnyPeriodic())
+     for (int d = 0; d < BL_SPACEDIM; d++)
+       geom.FillPeriodicBoundary(coeffs[d]);
+
+   MultiFab CrseTemp;
+   if (level > 0) {
+       // Fill temperature at next coarser level, if it exists.
+       const BoxArray& crse_grids = getLevel(level-1).boxArray();
+       CrseTemp.define(crse_grids,1,1,Fab_allocate);
+       FillPatch(getLevel(level-1),CrseTemp,1,time,State_Type,Temp,1);
+   }
+
+   diffusion->applyop(level,Temperature,CrseTemp,TempDiffTerm,coeffs);
+
+   // Extrapolate to ghost cells
+   if (TempDiffTerm.nGrow() > 0) {
+       for (MFIter mfi(TempDiffTerm); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = mfi.validbox();
+	   ca_tempdiffextrap(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			     BL_TO_FORTRAN_3D(TempDiffTerm[mfi]));
+       }
+   }
+}
+
+void
+Castro::getEnthDiffusionTerm (Real time, MultiFab& DiffTerm)
+{
+    BL_PROFILE("Castro::getEnthDiffusionTerm()");
+
+   MultiFab& S_old = get_old_data(State_Type);
+   if (verbose && ParallelDescriptor::IOProcessor())
+      std::cout << "Calculating diffusion term at time " << time << std::endl;
+
+   // Fill coefficients at this level.
+   PArray<MultiFab> coeffs(BL_SPACEDIM,PArrayManage);
+   PArray<MultiFab> coeffs_temporary(3,PArrayManage); // This is what we pass to the dimension-agnostic Fortran
+   for (int dir = 0; dir < 3; dir++) {
+       if (dir < BL_SPACEDIM) {
+	 coeffs.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+	 coeffs_temporary.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+       } else {
+	 coeffs_temporary.set(dir,new MultiFab(grids, 1, 0, Fab_allocate));
+       }
+   }
+
+   // Define enthalpy at this level.
+   MultiFab Enthalpy(grids,1,1,Fab_allocate);
+   {
+       FillPatchIterator fpi(*this,S_old,1,time,State_Type,0,NUM_STATE);
+       MultiFab& state_old = fpi.get_mf();
+
+       for (MFIter mfi(state_old); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = grids[mfi.index()];
+	   make_enthalpy(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+	                 BL_TO_FORTRAN_3D(state_old[mfi]),
+	                 BL_TO_FORTRAN_3D(Enthalpy[mfi]));
+
+	   ca_fill_enth_cond(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			     BL_TO_FORTRAN_3D(state_old[mfi]),
+			     BL_TO_FORTRAN_3D(coeffs_temporary[0][mfi]),
+			     BL_TO_FORTRAN_3D(coeffs_temporary[1][mfi]),
+			     BL_TO_FORTRAN_3D(coeffs_temporary[2][mfi]));
+       }
+   }
+
+   // Now copy the temporary array results back to the
+   // correctly dimensioned coeffs array.
+
+   for (int dir = 0; dir < BL_SPACEDIM; dir++)
+     MultiFab::Copy(coeffs[dir], coeffs_temporary[dir], 0, 0, 1, 0);
+
+   if (Geometry::isAnyPeriodic())
+     for (int d = 0; d < BL_SPACEDIM; d++)
+       geom.FillPeriodicBoundary(coeffs[d]);
+
+   MultiFab CrseEnth, CrseState;
+   if (level > 0) {
+       // Fill temperature at next coarser level, if it exists.
+       const BoxArray& crse_grids = getLevel(level-1).boxArray();
+       CrseEnth.define (crse_grids,1,1,Fab_allocate);
+       CrseState.define(crse_grids,NUM_STATE,1,Fab_allocate);
+       FillPatch(getLevel(level-1),CrseState,1,time,State_Type,Density,NUM_STATE);
+
+       for (MFIter mfi(CrseState); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = crse_grids[mfi.index()];
+	   make_enthalpy(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+	                 BL_TO_FORTRAN_3D(CrseState[mfi]),
+	                 BL_TO_FORTRAN_3D( CrseEnth[mfi]));
+       }
+   }
+
+   diffusion->applyop(level,Enthalpy,CrseEnth,DiffTerm,coeffs);
+
+   // Extrapolate to ghost cells
+   if (DiffTerm.nGrow() > 0) {
+       for (MFIter mfi(DiffTerm); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = mfi.validbox();
+	   ca_tempdiffextrap(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			     BL_TO_FORTRAN_3D(DiffTerm[mfi]));
+       }
+   }
+}
+
+void
+Castro::getSpecDiffusionTerm (Real time, MultiFab& SpecDiffTerm)
+{
+    BL_PROFILE("Castro::getSpecDiffusionTerm()");
+
+   MultiFab& S_old = get_old_data(State_Type);
+   if (verbose && ParallelDescriptor::IOProcessor())
+      std::cout << "Calculating species diffusion term at time " << time << std::endl;
+
+   // Fill coefficients at this level.
+   PArray<MultiFab> coeffs(BL_SPACEDIM,PArrayManage);
+   PArray<MultiFab> coeffs_temporary(3,PArrayManage); // This is what we pass to the dimension-agnostic Fortran
+   for (int dir = 0; dir < 3; dir++) {
+       if (dir < BL_SPACEDIM) {
+	 coeffs.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+	 coeffs_temporary.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+       } else {
+	 coeffs_temporary.set(dir,new MultiFab(grids, 1, 0, Fab_allocate));
+       }
+   }
+
+   FillPatchIterator fpi(*this,S_old,1,time,State_Type,0,NUM_STATE);
+   MultiFab& state_old = fpi.get_mf();
+
+   for (MFIter mfi(state_old); mfi.isValid(); ++mfi)
+   {
+       const Box& bx = grids[mfi.index()];
+
+       ca_fill_spec_coeff(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			  BL_TO_FORTRAN_3D(state_old[mfi]),
+			  BL_TO_FORTRAN_3D(coeffs_temporary[0][mfi]),
+			  BL_TO_FORTRAN_3D(coeffs_temporary[1][mfi]),
+			  BL_TO_FORTRAN_3D(coeffs_temporary[2][mfi]));
+   }
+
+   // Now copy the temporary array results back to the
+   // correctly dimensioned coeffs array.
+   for (int dir = 0; dir < BL_SPACEDIM; dir++)
+     MultiFab::Copy(coeffs[dir], coeffs_temporary[dir], 0, 0, 1, 0);
+
+   if (Geometry::isAnyPeriodic())
+     for (int d = 0; d < BL_SPACEDIM; d++)
+       geom.FillPeriodicBoundary(coeffs[d]);
+
+   // Create MultiFabs that only hold the data for one species at a time.
+   MultiFab Species(grids,1,1,Fab_allocate);
+   MultiFab     SDT(grids,SpecDiffTerm.nComp(),SpecDiffTerm.nGrow(),Fab_allocate);
+   SDT.setVal(0.);
+   MultiFab CrseSpec, CrseDen;
+   if (level > 0) {
+       const BoxArray& crse_grids = getLevel(level-1).boxArray();
+       CrseSpec.define(crse_grids,1,1,Fab_allocate);
+        CrseDen.define(crse_grids,1,1,Fab_allocate);
+   }
+
+   // Fill one species at a time at this level.
+   for (int ispec = 0; ispec < NumSpec; ispec++)
+   {
+       MultiFab::Copy  (Species, state_old, FirstSpec+ispec, 0, 1, 1);
+       MultiFab::Divide(Species, state_old, Density        , 0, 1, 1);
+
+       // Fill temperature at next coarser level, if it exists.
+       if (level > 0)
+       {
+           FillPatch(getLevel(level-1),CrseSpec,1,time,State_Type,FirstSpec+ispec,1);
+           FillPatch(getLevel(level-1),CrseDen ,1,time,State_Type,Density        ,1);
+           MultiFab::Divide(CrseSpec, CrseDen, 0, 0, 1, 1);
+       }
+
+       diffusion->applyop(level,Species,CrseSpec,SDT,coeffs);
+
+       // Extrapolate to ghost cells
+       if (SDT.nGrow() > 0) {
+           for (MFIter mfi(SDT); mfi.isValid(); ++mfi)
+           {
+	       const Box& bx = mfi.validbox();
+	       ca_tempdiffextrap(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+				 BL_TO_FORTRAN_3D(SDT[mfi]));
+           }
+       }
+       // Copy back into SpecDiffTerm from the temporary SDT
+       MultiFab::Copy(SpecDiffTerm, SDT, 0, ispec, 1, 1);
+   }
+}
+
+#if (BL_SPACEDIM == 1)
+// **********************************************
+// Note: this currently just gets the term that looks like div(2 mu grad(u)) which is
+//       only part of the viscous term.  We assume that the coefficient that is filled is "2 mu"
+// **********************************************
+void
+Castro::getViscousTerm (Real time, MultiFab& ViscousTermforMomentum, MultiFab& ViscousTermforEnergy)
+{
+    BL_PROFILE("Castro::getViscousTerm()");
+
+   if (verbose && ParallelDescriptor::IOProcessor())
+      std::cout << "Calculating viscous term at time " << time << std::endl;
+
+   getFirstViscousTerm(time,ViscousTermforMomentum);
+
+   MultiFab SecndTerm(grids,ViscousTermforMomentum.nComp(),ViscousTermforMomentum.nGrow(),Fab_allocate);
+   getSecndViscousTerm(time,SecndTerm);
+   MultiFab::Add(ViscousTermforMomentum, SecndTerm, 0, 0, ViscousTermforMomentum.nComp(), 0);
+
+   getViscousTermForEnergy(time,ViscousTermforEnergy);
+}
+
+void
+Castro::getFirstViscousTerm (Real time, MultiFab& ViscousTerm)
+{
+   MultiFab& S_old = get_old_data(State_Type);
+
+   // Fill coefficients at this level.
+   PArray<MultiFab> coeffs(BL_SPACEDIM,PArrayManage);
+   PArray<MultiFab> coeffs_temporary(3,PArrayManage); // This is what we pass to the dimension-agnostic Fortran
+   for (int dir = 0; dir < 3; dir++) {
+       if (dir < BL_SPACEDIM) {
+	 coeffs.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+	 coeffs_temporary.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+       } else {
+	 coeffs_temporary.set(dir,new MultiFab(grids, 1, 0, Fab_allocate));
+       }
+   }
+
+   // Fill velocity at this level.
+   MultiFab Vel(grids,1,1,Fab_allocate);
+
+   FillPatchIterator fpi(*this,S_old,1,time,State_Type,0,NUM_STATE);
+   MultiFab& state_old = fpi.get_mf();
+
+   // Remember this is just 1-d
+   MultiFab::Copy  (Vel, state_old, Xmom   , 0, 1, 1);
+   MultiFab::Divide(Vel, state_old, Density, 0, 1, 1);
+
+   for (MFIter mfi(state_old); mfi.isValid(); ++mfi)
+   {
+       const Box& bx = grids[mfi.index()];
+
+       ca_fill_first_visc_coeff(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+				BL_TO_FORTRAN_3D(state_old[mfi]),
+				BL_TO_FORTRAN_3D(coeffs_temporary[0][mfi]),
+				BL_TO_FORTRAN_3D(coeffs_temporary[1][mfi]),
+				BL_TO_FORTRAN_3D(coeffs_temporary[2][mfi]));
+   }
+
+   // Now copy the temporary array results back to the
+   // correctly dimensioned coeffs array.
+   for (int dir = 0; dir < BL_SPACEDIM; dir++)
+     MultiFab::Copy(coeffs[dir], coeffs_temporary[dir], 0, 0, 1, 0);
+
+   if (Geometry::isAnyPeriodic())
+     for (int d = 0; d < BL_SPACEDIM; d++)
+       geom.FillPeriodicBoundary(coeffs[d]);
+
+   MultiFab CrseVel, CrseDen;
+   if (level > 0) {
+       // Fill temperature at next coarser level, if it exists.
+       const BoxArray& crse_grids = getLevel(level-1).boxArray();
+       CrseVel.define(crse_grids,1,1,Fab_allocate);
+       CrseDen.define(crse_grids,1,1,Fab_allocate);
+       FillPatch(getLevel(level-1),CrseVel ,1,time,State_Type,Xmom   ,1);
+       FillPatch(getLevel(level-1),CrseDen ,1,time,State_Type,Density,1);
+       MultiFab::Divide(CrseVel, CrseDen, 0, 0, 1, 1);
+   }
+   diffusion->applyop(level,Vel,CrseVel,ViscousTerm,coeffs);
+
+   // Extrapolate to ghost cells
+   if (ViscousTerm.nGrow() > 0) {
+       for (MFIter mfi(ViscousTerm); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = mfi.validbox();
+	   ca_tempdiffextrap(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			     BL_TO_FORTRAN_3D(ViscousTerm[mfi]));
+       }
+   }
+}
+
+void
+Castro::getSecndViscousTerm (Real time, MultiFab& ViscousTerm)
+{
+   MultiFab& S_old = get_old_data(State_Type);
+
+   // Fill coefficients at this level.
+   PArray<MultiFab> coeffs(BL_SPACEDIM,PArrayManage);
+   PArray<MultiFab> coeffs_temporary(3,PArrayManage); // This is what we pass to the dimension-agnostic Fortran
+   for (int dir = 0; dir < 3; dir++) {
+       if (dir < BL_SPACEDIM) {
+	 coeffs.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+	 coeffs_temporary.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+       } else {
+	 coeffs_temporary.set(dir,new MultiFab(grids, 1, 0, Fab_allocate));
+       }
+   }
+
+   // Fill velocity at this level.
+   MultiFab Vel(grids,1,1,Fab_allocate);
+
+   FillPatchIterator fpi(*this,S_old,1,time,State_Type,0,NUM_STATE);
+   MultiFab& state_old = fpi.get_mf();
+
+   // Remember this is just 1-d
+   MultiFab::Copy  (Vel, state_old, Xmom   , 0, 1, 1);
+   MultiFab::Divide(Vel, state_old, Density, 0, 1, 1);
+
+   for (MFIter mfi(state_old); mfi.isValid(); ++mfi)
+   {
+       const Box& bx = grids[mfi.index()];
+
+       ca_fill_secnd_visc_coeff(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+				BL_TO_FORTRAN_3D(state_old[mfi]),
+				BL_TO_FORTRAN_3D(coeffs_temporary[0][mfi]),
+				BL_TO_FORTRAN_3D(coeffs_temporary[1][mfi]),
+				BL_TO_FORTRAN_3D(coeffs_temporary[2][mfi]));
+   }
+
+   // Now copy the temporary array results back to the
+   // correctly dimensioned coeffs array.
+   for (int dir = 0; dir < BL_SPACEDIM; dir++)
+     MultiFab::Copy(coeffs[dir], coeffs_temporary[dir], 0, 0, 1, 0);
+
+   if (Geometry::isAnyPeriodic())
+     for (int d = 0; d < BL_SPACEDIM; d++)
+       geom.FillPeriodicBoundary(coeffs[d]);
+
+   MultiFab CrseVel, CrseDen;
+   if (level > 0) {
+       // Fill temperature at next coarser level, if it exists.
+       const BoxArray& crse_grids = getLevel(level-1).boxArray();
+       CrseVel.define(crse_grids,1,1,Fab_allocate);
+       CrseDen.define(crse_grids,1,1,Fab_allocate);
+       FillPatch(getLevel(level-1),CrseVel ,1,time,State_Type,Xmom   ,1);
+       FillPatch(getLevel(level-1),CrseDen ,1,time,State_Type,Density,1);
+       MultiFab::Divide(CrseVel, CrseDen, 0, 0, 1, 1);
+   }
+   diffusion->applyViscOp(level,Vel,CrseVel,ViscousTerm,coeffs);
+
+   // Extrapolate to ghost cells
+   if (ViscousTerm.nGrow() > 0) {
+       for (MFIter mfi(ViscousTerm); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = mfi.validbox();
+	   ca_tempdiffextrap(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			     BL_TO_FORTRAN_3D(ViscousTerm[mfi]));
+       }
+   }
+}
+
+void
+Castro::getViscousTermForEnergy (Real time, MultiFab& ViscousTerm)
+{
+   MultiFab& S_old = get_old_data(State_Type);
+
+   // Fill coefficients at this level.
+   PArray<MultiFab> coeffs(BL_SPACEDIM,PArrayManage);
+   PArray<MultiFab> coeffs_temporary(3,PArrayManage); // This is what we pass to the dimension-agnostic Fortran
+   for (int dir = 0; dir < 3; dir++) {
+       if (dir < BL_SPACEDIM) {
+	 coeffs.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+	 coeffs_temporary.set(dir,new MultiFab(getEdgeBoxArray(dir), 1, 0, Fab_allocate));
+       } else {
+	 coeffs_temporary.set(dir,new MultiFab(grids, 1, 0, Fab_allocate));
+       }
+   }
+
+   FillPatchIterator fpi(*this,S_old,2,time,State_Type,0,NUM_STATE);
+   MultiFab& state_old = fpi.get_mf();
+
+   const Geometry& fine_geom = parent->Geom(parent->finestLevel());
+   const Real*       dx_fine = fine_geom.CellSize();
+
+   // Remember this is just 1-d
+   int coord_type = Geometry::Coord();
+   for (MFIter mfi(state_old); mfi.isValid(); ++mfi)
+   {
+       const Box& bx = grids[mfi.index()];
+
+       ca_compute_div_tau_u(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			    BL_TO_FORTRAN_3D(ViscousTerm[mfi]),
+			    BL_TO_FORTRAN_3D(state_old[mfi]),
+			    ZFILL(dx_fine),&coord_type);
+   }
+
+   // Extrapolate to ghost cells
+   if (ViscousTerm.nGrow() > 0) {
+       for (MFIter mfi(ViscousTerm); mfi.isValid(); ++mfi)
+       {
+	   const Box& bx = mfi.validbox();
+	   ca_tempdiffextrap(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+			     BL_TO_FORTRAN_3D(ViscousTerm[mfi]));
+       }
+   }
+}
 #endif
---
+++ Source/Castro_levelset.cpp
@@ -0,0 +1,268 @@
+#include "Castro.H"
+#include "Castro_F.H"
+
+void
+Castro::advance_levelset(Real time, Real dt)
+{
+    BL_PROFILE("Castro::advance_levelset()");
+
+    if (verbose && ParallelDescriptor::IOProcessor())
+        std::cout << "... update levelset\n";
+
+    MultiFab&  LS_new = get_new_data(LS_State_Type);
+    LS_new.copy(get_old_data(LS_State_Type));
+
+    for (MFIter mfi(LStype); mfi.isValid(); ++mfi)
+    {
+        IntFab& type = LStype[mfi];
+        IntFab& nband = LSnband[mfi];
+        IntFab& mine = LSmine[mfi];
+        const Box& box = mfi.validbox();
+        int nbandsize = nband.box().numPts();
+        int minesize = mine.box().numPts();
+        
+        // Set nband data based on type
+	BL_FORT_PROC_CALL(LS_NARROWBAND,ls_narrowband)
+	     (BL_TO_FORTRAN(type),
+              nband.dataPtr(), &nbandsize,
+              mine.dataPtr(), &minesize,
+              box.loVect(), box.hiVect());
+    }
+    
+    Real phidt = dt;
+    Real phit = dt;
+    
+    while(phit > 0) {
+        
+        int nGrowLS = 1;
+        int nCompLS = 1;
+        const Real* dx = geom.CellSize();
+        
+        phidt = phit;
+        
+	BoxLib::fill_boundary(LStype, 0, 1, geom);
+        
+        for (FillPatchIterator fpi(*this,LS_new,nGrowLS,state[LS_State_Type].curTime(),LS_State_Type,0,nCompLS);
+             fpi.isValid(); ++fpi)
+        {
+            
+            const FArrayBox& lsfab = fpi();  // Note that this is a temporary fab with nCompLS components filled by FPI
+            const Box& box = fpi.validbox();
+            IntFab& type = LStype[fpi];
+            
+            IntFab& nband = LSnband[fpi];
+            IntFab& mine = LSmine[fpi];
+            int nbandsize = nband.box().numPts();
+            int minesize = mine.box().numPts();
+            
+            const FArrayBox& uadv = u_gdnv[0][fpi];
+            const FArrayBox& vadv = u_gdnv[1][fpi];
+#if (BL_SPACEDIM == 3)
+            const FArrayBox& wadv = u_gdnv[2][fpi];
+#endif
+
+            Real lscfl;
+            BL_FORT_PROC_CALL(LS_CFL,ls_cfl)
+                  (&lscfl, 
+                   BL_TO_FORTRAN(lsfab), BL_TO_FORTRAN(uadv),  BL_TO_FORTRAN(vadv),
+#if (BL_SPACEDIM == 3)
+                   BL_TO_FORTRAN(wadv), 
+#endif
+                   nband.dataPtr(), &nbandsize, mine.dataPtr(), &minesize,
+                   box.loVect(), box.hiVect(), &phit, dx,
+                   BL_TO_FORTRAN(type));
+            phidt = std::min(phidt,lscfl);
+        }
+        ParallelDescriptor::ReduceRealMin(phidt);
+        
+        phit = phit - phidt;
+	if (verbose && ParallelDescriptor::IOProcessor())
+	  {
+	    std::cout<<"phit"<<phit<<"\n";
+	    std::cout<<"phidt"<<phidt<<"\n";
+	  }
+        bool reinit = false;
+        
+        for (FillPatchIterator fpi(*this,LS_new,nGrowLS,state[LS_State_Type].curTime(),LS_State_Type,0,nCompLS);
+             fpi.isValid(); ++fpi)
+        {
+            const FArrayBox& lsfab = fpi();  // Note that this is a temporary fab with nCompLS components filled by FPI
+            FArrayBox& phinew = LS_new[fpi];  // This is a fab of the actual state, with phi in the 0 component
+            const Box& box = fpi.validbox();
+            const FArrayBox& uadv = u_gdnv[0][fpi];
+            const FArrayBox& vadv = u_gdnv[1][fpi];
+#if (BL_SPACEDIM == 3)
+            const FArrayBox& wadv = u_gdnv[2][fpi];
+#endif
+
+            IntFab& type = LStype[fpi];
+            
+            IntFab& nband = LSnband[fpi];
+            IntFab& mine = LSmine[fpi];
+            int nbandsize = nband.box().numPts();
+            int minesize = mine.box().numPts();
+            
+            // Advance level set for this fab, result directly into state
+            int reinit_flag;
+	    BL_FORT_PROC_CALL(LS_PHIUPD,ls_phiupd)
+                  (&reinit_flag,BL_TO_FORTRAN(lsfab), BL_TO_FORTRAN(phinew), 
+                   BL_TO_FORTRAN(uadv), BL_TO_FORTRAN(vadv), 
+#if (BL_SPACEDIM == 3)
+                   BL_TO_FORTRAN(wadv), 
+#endif
+                   nband.dataPtr(), &nbandsize, mine.dataPtr(), &minesize,
+                   box.loVect(), box.hiVect(), &phidt, dx,
+                   BL_TO_FORTRAN(type)); 
+            reinit |= reinit_flag;
+        }
+        
+        ParallelDescriptor::ReduceBoolOr(reinit);
+        
+        if (reinit)
+        {
+            reinit_phi(time);
+        }
+    }
+    
+    delete [] u_gdnv;
+
+}
+
+void
+Castro::reinit_phi(Real time)
+{
+    BL_PROFILE("Castro::reinit_phi()");
+
+    if (verbose && ParallelDescriptor::IOProcessor())
+        std::cout << "...... reinitialzing levelset\n";
+
+    MultiFab& LS_new = get_new_data(LS_State_Type);
+    const Real* dx   = geom.CellSize();
+    int nGrowLS = 2;
+    int nCompLS = 1;
+
+    BoxLib::fill_boundary(LStype, 0, 1, geom);
+        
+    // Load valid region of phi
+    Array<int> intfacep, intfacen, heap, heaploc;
+    for (FillPatchIterator fpi(*this,LS_new,nGrowLS,state[LS_State_Type].curTime(),LS_State_Type,0,nCompLS);
+         fpi.isValid(); ++fpi)
+    {
+        const FArrayBox& lsfab = fpi();  // Note that this is a temporary fab with nCompLS components filled by FPI
+        FArrayBox& phinew = LS_new[fpi];  // This is a fab of the actual state, with phi in the 0 component
+        IntFab& type = LStype[fpi];
+        const Box& box = fpi.validbox();
+        
+        int intfacesize = box.numPts();
+        int intfacenump = 0;
+        int intfacenumn = 0;
+        intfacep.resize(BL_SPACEDIM*intfacesize);
+        intfacen.resize(BL_SPACEDIM*intfacesize);
+        
+        IntFab& nband = LSnband[fpi];
+        IntFab& mine = LSmine[fpi];
+        int nbandsize = nband.box().numPts();
+
+        int nbandnum = 0;
+        heap.resize(BL_SPACEDIM*nbandsize);
+        int  typesize = type.box().numPts();
+        heaploc.resize(typesize);
+
+        // Set type for all cells in band to "outside band"
+	BL_FORT_PROC_CALL(LS_RETYPIFY,ls_retypify)
+            (BL_TO_FORTRAN(type), nband.dataPtr(), &nbandsize);
+
+        // Set list of positive and negative narrowband points to be filled by FASTMARCH
+	BL_FORT_PROC_CALL(LS_FINDINTERFACE,ls_findinterface)
+            (BL_TO_FORTRAN(lsfab),  BL_TO_FORTRAN(phinew),  
+             BL_TO_FORTRAN(type),  
+             box.loVect(), box.hiVect(), dx, &intfacenump, &intfacenumn,
+             intfacep.dataPtr(), intfacen.dataPtr(),
+             nband.dataPtr(), &nbandsize, &intfacesize);
+
+        // Fill in narrow band (+ve/-ve sides), set mines
+        if(intfacenump > 0)
+        {
+            int positive = 1;
+	    BL_FORT_PROC_CALL(LS_FASTMARCH,ls_fastmarch)
+                 (BL_TO_FORTRAN(phinew),  BL_TO_FORTRAN(type),  
+                  box.loVect(), box.hiVect(), dx, &intfacenump, intfacep.dataPtr(),
+                  nband.dataPtr(), &nbandsize, &nbandnum,
+                  mine.dataPtr(), &positive,&intfacesize,
+                  heap.dataPtr(), heaploc.dataPtr());
+        }
+        
+        if(intfacenumn > 0)
+        {
+            int negative = -1;
+	    BL_FORT_PROC_CALL(LS_FASTMARCH,ls_fastmarch)
+                 (BL_TO_FORTRAN(phinew),  BL_TO_FORTRAN(type),  
+                  box.loVect(), box.hiVect(), dx,  &intfacenumn,  intfacen.dataPtr(),
+            	  nband.dataPtr(), &nbandsize, &nbandnum,
+            	  mine.dataPtr(), &negative,&intfacesize,
+            	  heap.dataPtr(), heaploc.dataPtr());
+        }
+    }
+
+    bool notdone = true;
+    bool redo = false;
+    
+    // Check grow region and see if anything changes due to neighboring grids
+    while (notdone)
+    {
+	BoxLib::fill_boundary(LS_new, geom);
+	BoxLib::fill_boundary(LStype, 0, 1, geom);
+        
+        for (MFIter mfi(LS_new); mfi.isValid(); ++mfi)
+        {
+            FArrayBox& phinew = LS_new[mfi];  // This is a fab of the actual state, with phi in the 0 component
+            IntFab& type = LStype[mfi];
+            const Box& box = mfi.validbox();
+            
+            IntFab& nband = LSnband[mfi];
+            int nbandsize = nband.box().numPts();
+            
+            int nbandnum = 0;
+            
+            int  numtemptype = type.box().numPts();            
+            heaploc.resize(numtemptype);
+            
+	    BL_FORT_PROC_CALL(LS_NBANDNUMIFY,ls_nbandnumify)
+               (nband.dataPtr(), &nbandsize,&nbandnum);
+            
+            int positive = 1;
+            int redo_flag;
+	    BL_FORT_PROC_CALL(LS_FASTMARCH2,ls_fastmarch2)
+                 (&redo_flag, BL_TO_FORTRAN(phinew),BL_TO_FORTRAN(type),
+                  box.loVect(), box.hiVect(), dx,
+                  nband.dataPtr(), &nbandsize, &nbandnum, &positive, heaploc.dataPtr());
+            redo |= redo_flag;
+            
+            int negative = -1;
+	    BL_FORT_PROC_CALL(LS_FASTMARCH2,ls_fastmarch2)
+                 (&redo_flag, BL_TO_FORTRAN(phinew),BL_TO_FORTRAN(type),
+                  box.loVect(), box.hiVect(), dx,
+                  nband.dataPtr(), &nbandsize, &nbandnum, &negative, heaploc.dataPtr());
+            redo |= redo_flag;
+        }
+        ParallelDescriptor::ReduceBoolOr(redo);
+        notdone = redo;
+        redo = false;
+    }
+
+    for (MFIter mfi(LStype); mfi.isValid(); ++mfi)
+    {
+        IntFab& type = LStype[mfi];
+        IntFab& nband = LSnband[mfi];
+        IntFab& mine = LSmine[mfi];
+        const Box& box = mfi.validbox();
+        
+        int nbandsize = nband.box().numPts();
+        int minesize = mine.box().numPts();
+        
+	BL_FORT_PROC_CALL(LS_MINE,ls_mine)
+             (BL_TO_FORTRAN(type),
+              nband.dataPtr(), &nbandsize, mine.dataPtr(), &minesize,
+              box.loVect(), box.hiVect());
+    }
+}
