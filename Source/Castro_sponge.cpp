91f1a2b20f99c82547bb08b69304484efa3c6d36
Max Katz <maximilian.katz@stonybrook.edu>
2016-07-29 05:08:05 -0400

    Include the sponge in the hydro sources

--- Source/Castro_sponge.cpp
+++ Source/Castro_sponge.cpp
@@ -11,6 +11,8 @@ Castro::construct_old_sponge_source(Real time, Real dt)
     old_sources[sponge_src].setVal(0.0, ng);
 
     if (!do_sponge) return;
+
+    add_force_to_sources(old_sources[sponge_src], *sources_for_hydro, (*Sborder));
 }
 
 void
@@ -36,46 +38,48 @@ Castro::construct_new_sponge_source(Real time, Real dt)
 #ifdef _OPENMP
 #pragma omp parallel reduction(+:E_added,xmom_added,ymom_added,zmom_added)
 #endif
-	for (MFIter mfi(S_new,true); mfi.isValid(); ++mfi)
-	{
-	    const Box& bx = mfi.tilebox();
+    for (MFIter mfi(S_new,true); mfi.isValid(); ++mfi)
+    {
+	const Box& bx = mfi.tilebox();
 
-	    Real mom_added[3] = { 0.0 };
+	Real mom_added[3] = { 0.0 };
 
-	    ca_sponge(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
-		      BL_TO_FORTRAN_3D(S_new[mfi]),
-		      BL_TO_FORTRAN_3D(new_sources[sponge_src][mfi]),
-		      BL_TO_FORTRAN_3D(volume[mfi]),
-		      ZFILL(dx), dt, &time,
-		      E_added,mom_added);
+	ca_sponge(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
+		  BL_TO_FORTRAN_3D(S_new[mfi]),
+		  BL_TO_FORTRAN_3D(new_sources[sponge_src][mfi]),
+		  BL_TO_FORTRAN_3D(volume[mfi]),
+		  ZFILL(dx), dt, &time,
+		  E_added,mom_added);
 
-	    xmom_added += mom_added[0];
-	    ymom_added += mom_added[1];
-	    zmom_added += mom_added[2];
+	xmom_added += mom_added[0];
+	ymom_added += mom_added[1];
+	zmom_added += mom_added[2];
 
-	}
+    }
 
-	if (print_energy_diagnostics)
-        {
-	    Real foo[4] = {E_added, xmom_added, ymom_added, zmom_added};
+    if (print_energy_diagnostics)
+    {
+	Real foo[4] = {E_added, xmom_added, ymom_added, zmom_added};
 #ifdef BL_LAZY
-            Lazy::QueueReduction( [=] () mutable {
+	Lazy::QueueReduction( [=] () mutable {
 #endif
-	    ParallelDescriptor::ReduceRealSum(foo, 4, ParallelDescriptor::IOProcessorNumber());
-	    if (ParallelDescriptor::IOProcessor()) {
-		E_added = foo[0];
-		xmom_added = foo[1],
-		ymom_added = foo[2],
-		zmom_added = foo[3];
+		ParallelDescriptor::ReduceRealSum(foo, 4, ParallelDescriptor::IOProcessorNumber());
+		if (ParallelDescriptor::IOProcessor()) {
+		    E_added = foo[0];
+		    xmom_added = foo[1];
+		    ymom_added = foo[2];
+		    zmom_added = foo[3];
 
-		std::cout << "(rho E) added from sponge                      : " << E_added << std::endl;
-		std::cout << "xmom added from sponge                         : " << xmom_added << std::endl;
-		std::cout << "ymom added from sponge                         : " << ymom_added << std::endl;
-		std::cout << "zmom added from sponge                         : " << zmom_added << std::endl;
-	    }
+		    std::cout << "(rho E) added from sponge                      : " << E_added << std::endl;
+		    std::cout << "xmom added from sponge                         : " << xmom_added << std::endl;
+		    std::cout << "ymom added from sponge                         : " << ymom_added << std::endl;
+		    std::cout << "zmom added from sponge                         : " << zmom_added << std::endl;
+		}
 #ifdef BL_LAZY
 	    });
 #endif
-        }
+    }
 
+    if (source_term_predictor == 1)
+	add_force_to_sources(new_sources[sponge_src], *sources_for_hydro, S_new);
 }
