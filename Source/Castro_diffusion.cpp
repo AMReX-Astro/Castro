a7aa92996061e83d019d92f61660a7d5cd0c8419
Max Katz <maximilian.katz@stonybrook.edu>
2016-08-05 11:43:55 -0400

    Clean up the diffusion code

--- Source/Castro_diffusion.cpp
+++ Source/Castro_diffusion.cpp
@@ -74,98 +74,7 @@ Castro::add_temp_diffusion_to_source (MultiFab& ext_src, MultiFab& DiffTerm, Rea
 
 // **********************************************************************************************
 
-void
-#ifdef TAU
-Castro::time_center_temp_diffusion(MultiFab& S_new, MultiFab& OldDiffTerm, Real cur_time, Real dt, MultiFab& tau_diff)
-#else
-Castro::time_center_temp_diffusion(MultiFab& S_new, MultiFab& OldDiffTerm, Real cur_time, Real dt)
-#endif
-{
-        // Correct the temperature update so that it will be time-centered.
-        MultiFab NewDiffTerm(grids,1,1);
-        NewDiffTerm.setVal(0.);
-        if (diffuse_temp == 1) {
-#ifdef TAU
-           getTempDiffusionTerm(cur_time,NewDiffTerm,&tau_diff);
-#else
-           getTempDiffusionTerm(cur_time,NewDiffTerm);
-#endif
-        } else if (diffuse_enth == 1) {
-           getEnthDiffusionTerm(cur_time,NewDiffTerm);
-        }
-        if (diffuse_temp == 1 or diffuse_enth == 1) {
-           NewDiffTerm.mult( 0.5*dt);
-           OldDiffTerm.mult(-0.5*dt);
-           // Subtract off half of the old source term, and add half of the new.
-           MultiFab::Add(S_new,OldDiffTerm,0,Eden,1,0);
-           MultiFab::Add(S_new,OldDiffTerm,0,Eint,1,0);
-           MultiFab::Add(S_new,NewDiffTerm,0,Eden,1,0);
-           MultiFab::Add(S_new,NewDiffTerm,0,Eint,1,0);
-           computeTemp(S_new);
-        }
-}
-
-// **********************************************************************************************
-
-void
-#ifdef TAU
-Castro::full_temp_diffusion_update (MultiFab& S_new, Real prev_time, Real cur_time, Real dt, MultiFab& tau_diff)
-#else
-Castro::full_temp_diffusion_update (MultiFab& S_new, Real prev_time, Real cur_time, Real dt)
-#endif
-{
-        MultiFab OldDiffTerm, NewDiffTerm;
-        if (diffuse_temp == 1 or diffuse_enth == 1) {
-           // Define an explicit temperature/enthalpy update.
-           OldDiffTerm.define(grids,1,1,Fab_allocate);
-           OldDiffTerm.setVal(0.);
-        }
-
-        if (diffuse_temp == 1) {
-#ifdef TAU
-           getTempDiffusionTerm(prev_time,OldDiffTerm,&tau_diff);
-#else
-           getTempDiffusionTerm(prev_time,OldDiffTerm);
-#endif
-        } else if (diffuse_enth == 1) {
-           getEnthDiffusionTerm(prev_time,OldDiffTerm);
-        }
-
-        if (diffuse_temp == 1 or diffuse_enth == 1) {
-           OldDiffTerm.mult(dt);
-           MultiFab::Add(S_new,OldDiffTerm,0,Eden,1,0);
-           MultiFab::Add(S_new,OldDiffTerm,0,Eint,1,0);
-           computeTemp(S_new);
-
-           // Correct the temperature update so that it will be time-centered.
-           NewDiffTerm.define(grids,1,1,Fab_allocate);
-           NewDiffTerm.setVal(0.);
-        }
-
-        if (diffuse_temp == 1) {
-#ifdef TAU
-           getTempDiffusionTerm(cur_time,NewDiffTerm,&tau_diff);
-#else
-           getTempDiffusionTerm(cur_time,NewDiffTerm);
-#endif
-        } else if (diffuse_enth == 1) {
-           getEnthDiffusionTerm(cur_time,NewDiffTerm);
-        }
-
-        if (diffuse_temp == 1 or diffuse_enth == 1) {
-           NewDiffTerm.mult( 0.5*dt);
-           OldDiffTerm.mult(-0.5*dt);
-           // Subtract off half of the old source term, and add half of the new.
-           MultiFab::Add(S_new,OldDiffTerm,0,Eden,1,0);
-           MultiFab::Add(S_new,OldDiffTerm,0,Eint,1,0);
-           MultiFab::Add(S_new,NewDiffTerm,0,Eden,1,0);
-           MultiFab::Add(S_new,NewDiffTerm,0,Eint,1,0);
-           computeTemp(S_new);
-        }
-}
-
-// **********************************************************************************************
-
+#if (BL_SPACEDIM == 1)
 void
 Castro::add_spec_diffusion_to_source (MultiFab& ext_src, MultiFab& SpecDiffTerm, Real t)
 {
@@ -177,56 +86,11 @@ Castro::add_spec_diffusion_to_source (MultiFab& ext_src, MultiFab& SpecDiffTerm,
        MultiFab::Add(ext_src,SpecDiffTerm,0,FirstSpec,NumSpec,ng);
     }
 }
+#endif
 
 // **********************************************************************************************
-
-void
-Castro::time_center_spec_diffusion(MultiFab& S_new, MultiFab& OldSpecDiffTerm, Real cur_time, Real dt)
-{
-        // Correct the species update so that it will be time-centered.
-        MultiFab NewSpecDiffTerm(grids,NumSpec,1);
-        NewSpecDiffTerm.setVal(0.);
-        if (diffuse_spec == 1) {
-           getSpecDiffusionTerm(cur_time,NewSpecDiffTerm);
-           NewSpecDiffTerm.mult( 0.5*dt);
-           OldSpecDiffTerm.mult(-0.5*dt);
-           // Subtract off half of the old source term, and add half of the new.
-           MultiFab::Add(S_new,OldSpecDiffTerm,0,FirstSpec,NumSpec,0);
-           MultiFab::Add(S_new,NewSpecDiffTerm,0,FirstSpec,NumSpec,0);
-           computeTemp(S_new);
-        }
-}
-
-// **********************************************************************************************
-
-void
-Castro::full_spec_diffusion_update (MultiFab& S_new, Real prev_time, Real cur_time, Real dt)
-{
-        if (diffuse_spec == 1) {
-           // Define an explicit species update.
-           MultiFab OldSpecDiffTerm(grids,NumSpec,1);
-           OldSpecDiffTerm.setVal(0.);
-           getSpecDiffusionTerm(prev_time,OldSpecDiffTerm);
-           OldSpecDiffTerm.mult(dt);
-           MultiFab::Add(S_new,OldSpecDiffTerm,0,FirstSpec,NumSpec,0);
-
-           // Correct the species update so that it will be time-centered.
-           MultiFab NewSpecDiffTerm(grids,NumSpec,1);
-           NewSpecDiffTerm.setVal(0.);
-           getSpecDiffusionTerm(cur_time,NewSpecDiffTerm);
-           NewSpecDiffTerm.mult( 0.5*dt);
-           OldSpecDiffTerm.mult(-0.5*dt);
-           // Subtract off half of the old source term, and add half of the new.
-           MultiFab::Add(S_new,OldSpecDiffTerm,0,FirstSpec,NumSpec,0);
-           MultiFab::Add(S_new,NewSpecDiffTerm,0,FirstSpec,NumSpec,0);
-
-           computeTemp(S_new);
-        }
-}
 
 #if (BL_SPACEDIM == 1)
-// **********************************************************************************************
-
 void
 Castro::add_viscous_term_to_source(MultiFab& ext_src, MultiFab& ViscousTermforMomentum, 
                                    MultiFab& ViscousTermforEnergy, Real t)
@@ -243,36 +107,9 @@ Castro::add_viscous_term_to_source(MultiFab& ext_src, MultiFab& ViscousTermforMo
        MultiFab::Add(ext_src,ViscousTermforEnergy  ,0,Eden,1,ng);
     }
 }
+#endif
 
 // **********************************************************************************************
-
-void
-Castro::time_center_viscous_term(MultiFab& S_new, MultiFab& OldViscousTermforMomentum,
-                                 MultiFab& OldViscousTermforEnergy, Real cur_time, Real dt)
-{
-        // Correct the species update so that it will be time-centered.
-        MultiFab NewViscousTermforMomentum(grids,BL_SPACEDIM,1);
-        MultiFab NewViscousTermforEnergy  (grids,1,1);
-        NewViscousTermforMomentum.setVal(0.);
-        NewViscousTermforEnergy.setVal(0.);
-        if (diffuse_vel == 1) {
-
-           getViscousTerm(cur_time,NewViscousTermforMomentum,NewViscousTermforEnergy);
-
-           NewViscousTermforMomentum.mult( 0.5*dt);
-           OldViscousTermforMomentum.mult(-0.5*dt);
-           MultiFab::Add(S_new,OldViscousTermforMomentum,0,Xmom,BL_SPACEDIM,0);
-           MultiFab::Add(S_new,NewViscousTermforMomentum,0,Xmom,BL_SPACEDIM,0);
-
-           NewViscousTermforEnergy.mult( 0.5*dt);
-           OldViscousTermforEnergy.mult(-0.5*dt);
-           MultiFab::Add(S_new,OldViscousTermforEnergy  ,0,Eden,1          ,0);
-           MultiFab::Add(S_new,NewViscousTermforEnergy  ,0,Eden,1          ,0);
-
-           computeTemp(S_new);
-        }
-}
-#endif
 
 void
 Castro::getTempDiffusionTerm (Real time, MultiFab& TempDiffTerm)
@@ -437,6 +274,7 @@ Castro::getEnthDiffusionTerm (Real time, MultiFab& DiffTerm)
    }
 }
 
+#if (BL_SPACEDIM == 1)
 void
 Castro::getSpecDiffusionTerm (Real time, MultiFab& SpecDiffTerm)
 {
@@ -521,6 +359,7 @@ Castro::getSpecDiffusionTerm (Real time, MultiFab& SpecDiffTerm)
        MultiFab::Copy(SpecDiffTerm, SDT, 0, ispec, 1, 1);
    }
 }
+#endif
 
 #if (BL_SPACEDIM == 1)
 // **********************************************
