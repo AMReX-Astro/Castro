22b1f535d549b1a407dd9c198cb4e3a87194f839
Michael Zingale <michael.zingale@stonybrook.edu>
2016-08-15 22:45:26 -0400

    remove ppm_flatten_before_integrals

--- Source/Src_nd/meth_params.F90
+++ Source/Src_nd/meth_params.F90
@@ -90,7 +90,6 @@ module meth_params_module
   double precision, save :: cg_tol
   integer         , save :: cg_blend
   integer         , save :: use_flattening
-  integer         , save :: ppm_flatten_before_integrals
   integer         , save :: transverse_use_eos
   integer         , save :: transverse_reset_density
   integer         , save :: transverse_reset_rhoe
@@ -142,21 +141,21 @@ module meth_params_module
   !$acc create(ppm_predict_gammae, ppm_reference_edge_limit, ppm_reference_eigenvectors) &
   !$acc create(plm_iorder, hybrid_riemann, riemann_solver) &
   !$acc create(cg_maxiter, cg_tol, cg_blend) &
-  !$acc create(use_flattening, ppm_flatten_before_integrals, transverse_use_eos) &
-  !$acc create(transverse_reset_density, transverse_reset_rhoe, dual_energy_update_E_from_e) &
-  !$acc create(dual_energy_eta1, dual_energy_eta2, dual_energy_eta3) &
-  !$acc create(use_pslope, fix_mass_flux, limit_fluxes_on_small_dens) &
-  !$acc create(density_reset_method, allow_negative_energy, allow_small_energy) &
-  !$acc create(do_sponge, sponge_implicit, cfl) &
-  !$acc create(dtnuc_e, dtnuc_X, dtnuc_mode) &
-  !$acc create(dxnuc, do_react, react_T_min) &
-  !$acc create(react_T_max, react_rho_min, react_rho_max) &
-  !$acc create(disable_shock_burning, do_grav, grav_source_type) &
-  !$acc create(do_rotation, rot_period, rot_period_dot) &
-  !$acc create(rotation_include_centrifugal, rotation_include_coriolis, rotation_include_domegadt) &
-  !$acc create(state_in_rotating_frame, rot_source_type, implicit_rotation_update) &
-  !$acc create(rot_axis, point_mass, point_mass_fix_solution) &
-  !$acc create(do_acc, track_grid_losses)
+  !$acc create(use_flattening, transverse_use_eos, transverse_reset_density) &
+  !$acc create(transverse_reset_rhoe, dual_energy_update_E_from_e, dual_energy_eta1) &
+  !$acc create(dual_energy_eta2, dual_energy_eta3, use_pslope) &
+  !$acc create(fix_mass_flux, limit_fluxes_on_small_dens, density_reset_method) &
+  !$acc create(allow_negative_energy, allow_small_energy, do_sponge) &
+  !$acc create(sponge_implicit, cfl, dtnuc_e) &
+  !$acc create(dtnuc_X, dtnuc_mode, dxnuc) &
+  !$acc create(do_react, react_T_min, react_T_max) &
+  !$acc create(react_rho_min, react_rho_max, disable_shock_burning) &
+  !$acc create(do_grav, grav_source_type, do_rotation) &
+  !$acc create(rot_period, rot_period_dot, rotation_include_centrifugal) &
+  !$acc create(rotation_include_coriolis, rotation_include_domegadt, state_in_rotating_frame) &
+  !$acc create(rot_source_type, implicit_rotation_update, rot_axis) &
+  !$acc create(point_mass, point_mass_fix_solution, do_acc) &
+  !$acc create(track_grid_losses)
 
   ! End the declarations of the ParmParse parameters
 
@@ -196,7 +195,6 @@ contains
     cg_tol = 1.0d-5;
     cg_blend = 2;
     use_flattening = 1;
-    ppm_flatten_before_integrals = 1;
     transverse_use_eos = 0;
     transverse_reset_density = 1;
     transverse_reset_rhoe = 0;
@@ -262,7 +260,6 @@ contains
     call pp%query("cg_tol", cg_tol)
     call pp%query("cg_blend", cg_blend)
     call pp%query("use_flattening", use_flattening)
-    call pp%query("ppm_flatten_before_integrals", ppm_flatten_before_integrals)
     call pp%query("transverse_use_eos", transverse_use_eos)
     call pp%query("transverse_reset_density", transverse_reset_density)
     call pp%query("transverse_reset_rhoe", transverse_reset_rhoe)
@@ -336,21 +333,21 @@ contains
     !$acc device(ppm_predict_gammae, ppm_reference_edge_limit, ppm_reference_eigenvectors) &
     !$acc device(plm_iorder, hybrid_riemann, riemann_solver) &
     !$acc device(cg_maxiter, cg_tol, cg_blend) &
-    !$acc device(use_flattening, ppm_flatten_before_integrals, transverse_use_eos) &
-    !$acc device(transverse_reset_density, transverse_reset_rhoe, dual_energy_update_E_from_e) &
-    !$acc device(dual_energy_eta1, dual_energy_eta2, dual_energy_eta3) &
-    !$acc device(use_pslope, fix_mass_flux, limit_fluxes_on_small_dens) &
-    !$acc device(density_reset_method, allow_negative_energy, allow_small_energy) &
-    !$acc device(do_sponge, sponge_implicit, cfl) &
-    !$acc device(dtnuc_e, dtnuc_X, dtnuc_mode) &
-    !$acc device(dxnuc, do_react, react_T_min) &
-    !$acc device(react_T_max, react_rho_min, react_rho_max) &
-    !$acc device(disable_shock_burning, do_grav, grav_source_type) &
-    !$acc device(do_rotation, rot_period, rot_period_dot) &
-    !$acc device(rotation_include_centrifugal, rotation_include_coriolis, rotation_include_domegadt) &
-    !$acc device(state_in_rotating_frame, rot_source_type, implicit_rotation_update) &
-    !$acc device(rot_axis, point_mass, point_mass_fix_solution) &
-    !$acc device(do_acc, track_grid_losses)
+    !$acc device(use_flattening, transverse_use_eos, transverse_reset_density) &
+    !$acc device(transverse_reset_rhoe, dual_energy_update_E_from_e, dual_energy_eta1) &
+    !$acc device(dual_energy_eta2, dual_energy_eta3, use_pslope) &
+    !$acc device(fix_mass_flux, limit_fluxes_on_small_dens, density_reset_method) &
+    !$acc device(allow_negative_energy, allow_small_energy, do_sponge) &
+    !$acc device(sponge_implicit, cfl, dtnuc_e) &
+    !$acc device(dtnuc_X, dtnuc_mode, dxnuc) &
+    !$acc device(do_react, react_T_min, react_T_max) &
+    !$acc device(react_rho_min, react_rho_max, disable_shock_burning) &
+    !$acc device(do_grav, grav_source_type, do_rotation) &
+    !$acc device(rot_period, rot_period_dot, rotation_include_centrifugal) &
+    !$acc device(rotation_include_coriolis, rotation_include_domegadt, state_in_rotating_frame) &
+    !$acc device(rot_source_type, implicit_rotation_update, rot_axis) &
+    !$acc device(point_mass, point_mass_fix_solution, do_acc) &
+    !$acc device(track_grid_losses)
 
     call parmparse_destroy(pp)
 
