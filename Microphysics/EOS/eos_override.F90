cf416773c2daad236b589e817d8f8ee4629f22b6
Max Katz <maximilian.katz@stonybrook.edu>
2016-08-20 21:19:06 -0400

    Add a hook to override the eos_t before EOS call

---
+++ Microphysics/EOS/eos_override.F90
@@ -0,0 +1,23 @@
+module eos_override_module
+
+  implicit none
+
+  public eos_override
+
+contains
+
+  ! This is a user hook to override the details of the EOS state.
+
+  subroutine eos_override(state)
+
+    !$acc routine seq
+
+    use eos_type_module, only: eos_t
+
+    implicit none
+
+    type (eos_t) :: state
+
+  end subroutine eos_override
+
+end module eos_override_module
