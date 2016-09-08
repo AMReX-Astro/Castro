95c005742d74a9af38085f10a120727a230293a9
Michael Zingale <michael.zingale@stonybrook.edu>
2016-09-08 16:07:45 -0400

    this is the version from wdmerger, which fixes ghost cell issues and the new class methods

--- Util/yt/vol-wd.py
+++ Util/yt/vol-wd.py
@@ -9,7 +9,6 @@ import yt
 import numpy as np
 from yt.visualization.volume_rendering.api import \
     Scene, \
-    Camera, \
     VolumeSource
 
 
@@ -20,21 +19,21 @@ def doit(plotfile):
     ds = yt.load(plotfile)
     ds.periodicity = (True, True, True)
 
-    cm = "coolwarm"
-
     field = ('boxlib', 'density')
     ds._get_field_info(field).take_log = True
-        
+
     sc = Scene()
 
 
     # add a volume: select a sphere
     vol = VolumeSource(ds, field=field)
+    vol.use_ghost_zones = True
+
     sc.add_source(vol)
 
 
     # transfer function
-    vals = [-1, 0, 1, 2, 4, 5, 6, 7]
+    vals = [-1, 0, 1, 2, 3, 4, 5, 6, 7]
     #vals = [0.1, 1.0, 10, 100., 1.e4, 1.e5, 1.e6, 1.e7]
     sigma = 0.1
 
@@ -44,7 +43,7 @@ def doit(plotfile):
     cm = "coolwarm"
     cm = "spectral"
     for v in vals:
-        if v < 4:
+        if v < 3:
             alpha = 0.1
         else:
             alpha = 0.5
@@ -52,11 +51,11 @@ def doit(plotfile):
 
     sc.get_source(0).transfer_function = tf
 
-        
-    cam = Camera(ds, lens_type="perspective")
-    cam.resolution = (1280, 720)
+
+    cam = sc.add_camera(ds, lens_type="perspective")
+    cam.resolution = (1920, 1080)
     cam.position = 1.5*ds.arr(np.array([0.0, 5.e9, 5.e9]), 'cm')
-    
+
     # look toward the center -- we are dealing with an octant
     center = 0.5*(ds.domain_left_edge + ds.domain_right_edge)
     normal = (center - cam.position)
@@ -66,21 +65,24 @@ def doit(plotfile):
                            north_vector=[0., 0., 1.])
     cam.set_width(ds.domain_width)
 
-    sc.camera = cam
     #sc.annotate_axes()
     #sc.annotate_domain(ds)
 
     pid = plotfile.split("plt")[1]
     sc.render()
-    sc.save("wdmerger_{}.png".format(pid), sigma_clip=6.0)
-    sc.save_annotated("wdmerger_annotated_{}.png".format(pid), 
-                      text_annotate=[[(0.05, 0.05), 
-                                      "t = {:.3f}".format(float(ds.current_time.d)),
+    sc.save("wdmerger_{}_new.png".format(pid), sigma_clip=6.0)
+    sc.save_annotated("wdmerger_annotated_{}_new.png".format(pid),
+                      text_annotate=[[(0.05, 0.05),
+                                      "t = {:.3f} s".format(float(ds.current_time.d)),
                                       dict(horizontalalignment="left")],
-                                     [(0.5,0.95), 
-                                      "Castro simulation of merging white dwarfs",
-                                      dict(color="y", fontsize="24",
-                                           horizontalalignment="center")]])
+                                     [(0.5,0.95),
+                                      "Castro simulation of merging white dwarfs (0.6 $M_\odot$ + 0.9 $M_\odot$)",
+                                      dict(color="y", fontsize="22",
+                                           horizontalalignment="center")],
+                                     [(0.95,0.05),
+                                      "M. Katz et al.",
+                                      dict(color="w", fontsize="16",
+                                           horizontalalignment="right")]])
 
 if __name__ == "__main__":
 
@@ -92,6 +94,3 @@ if __name__ == "__main__":
     except: sys.exit("ERROR: no plotfile specified")
 
     doit(plotfile)
-
-
-        
