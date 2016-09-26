13bade2269194a37db5438478b2ffc49d3a40c76
Michael Zingale <michael.zingale@stonybrook.edu>
2016-09-26 14:00:21 -0400

    fix compute_cfl -- QC lives in qaux

--- Source/Src_1d/Castro_1d.f90
+++ Source/Src_1d/Castro_1d.f90
@@ -105,7 +105,9 @@ subroutine ca_umdrv(is_finest_level,time,&
   dx = delta(1)
 
   ! Check if we have violated the CFL criterion.
-  call compute_cfl(q, q_lo_3D, q_hi_3D, lo_3D, hi_3D, dt, dx_3D, courno)
+  call compute_cfl(q, q_lo_3D, q_hi_3D, &
+                   qaux, [qa_l1, 0, 0], [qa_h1, 0, 0], &
+                   lo_3D, hi_3D, dt, dx_3D, courno)
 
   ! Compute flattening coefficient for slope calculations.
   if (use_flattening == 1) then
