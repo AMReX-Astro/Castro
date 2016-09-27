e652ef493385b7652038a1e997a4a2270c07d32d
Michael Zingale <michael.zingale@stonybrook.edu>
2016-09-26 21:11:43 -0400

    fix interface -- Er and lambda come into ctoprim

--- Source/Src_nd/advection_util_nd.F90
+++ Source/Src_nd/advection_util_nd.F90
@@ -401,6 +401,10 @@ contains
 
   subroutine ctoprim(lo, hi, &
                      uin, uin_lo, uin_hi, &
+#ifdef RADIATION
+                     Erin, Erin_lo, Erin_hi, &
+                     lam, lam_lo, lam_hi, &
+#endif
                      q,     q_lo,   q_hi, &
                      qaux, qa_lo,  qa_hi) bind(C, name = "ctoprim")
 
@@ -427,10 +431,19 @@ contains
 
     integer, intent(in) :: lo(3), hi(3)
     integer, intent(in) :: uin_lo(3), uin_hi(3)
+#ifdef RADIATION
+    integer, intent(in) :: Erin_lo(3), Erin_hi(3)
+    integer, intent(in) :: lam_lo(3), lam_hi(3)
+#endif
     integer, intent(in) :: q_lo(3), q_hi(3)
     integer, intent(in) :: qa_lo(3), qa_hi(3)
 
     double precision, intent(in   ) :: uin(uin_lo(1):uin_hi(1),uin_lo(2):uin_hi(2),uin_lo(3):uin_hi(3),NVAR)
+#ifdef RADIATION
+    double precision, intent(in   ) :: Erin(Erin_lo(1):Erin_hi(1),Erin_lo(2):Erin_hi(2),Erin_lo(3):Erin_hi(3),0:ngroups-1)
+    double precision, intent(in   ) :: lam(lam_lo(1):lam_hi(1),lam_lo(2):lam_hi(2),lam_lo(3):lam_hi(3),0:ngroups-1)
+#endif
+
     double precision, intent(inout) :: q(q_lo(1):q_hi(1),q_lo(2):q_hi(2),q_lo(3):q_hi(3),QVAR)
     double precision, intent(inout) :: qaux(qa_lo(1):qa_hi(1),qa_lo(2):qa_hi(2),qa_lo(3):qa_hi(3),NQAUX)
 
@@ -495,6 +508,9 @@ contains
 #endif
 
              q(i,j,k,QTEMP) = uin(i,j,k,UTEMP)
+#ifdef RADIATION
+             q(i,j,k,qrad:qradhi) = Erin(i,j,k,:)
+#endif
 
           enddo
        enddo
