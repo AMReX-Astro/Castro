5970240348ab1966478660d53889046d7b93fbdc
Michael Zingale <michael.zingale@stonybrook.edu>
2016-12-21 17:35:08 -0500

    QVAR -> NQ

--- Source/Src_3d/Castro_advection_3d.F90
+++ Source/Src_3d/Castro_advection_3d.F90
@@ -463,7 +463,7 @@ contains
           ! Compute all slopes at kc (k3d)
           call uslope(q,flatn,qd_lo,qd_hi, &
                       dqx,dqy,dqz,qt_lo,qt_hi, &
-                      lo(1),lo(2),hi(1),hi(2),kc,k3d,QVAR)
+                      lo(1),lo(2),hi(1),hi(2),kc,k3d,NQ)
 
           if (use_pslope .eq. 1) &
                call pslope(q(:,:,:,QPRES),q(:,:,:,QRHO), &
