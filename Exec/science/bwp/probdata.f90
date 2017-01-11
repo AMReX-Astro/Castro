c36ddc81d07c903fc4075b4f2ab5afa04bc94c13
Michael Zingale <michael.zingale@stonybrook.edu>
2017-01-11 18:16:12 -0500

    rmeove -- redundant with rad

--- Exec/science/bwp/probdata.f90
+++
@@ -1,7 +0,0 @@
-module probdata_module
-
-!     These determine the refinement criteria
-      use bl_fort_module, only : rt => c_real
-      character (len=80), save  :: model_name
-
-end module probdata_module
