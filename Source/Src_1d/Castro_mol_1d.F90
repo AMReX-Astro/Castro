2974e0d8a059cb60acee87e85f814e76613eaf43
Michael Zingale <michael.zingale@stonybrook.edu>
2017-03-22 14:52:53 -0400

    first cut

---
+++ Source/Src_1d/Castro_mol_1d.F90
@@ -0,0 +1,299 @@
+subroutine ca_mol_single_stage(time, &
+                               lo, hi, domlo, domhi, &
+                               uin, uin_l1, uin_h1, &
+                               uout, uout_l1, uout_h1, &
+                               q, q_l1, q_h1, &
+                               qaux, qa_l1, qa_h1, &
+                               srcU, srU_l1, srU_h1, &
+                               update, updt_l1, updt_h1, &
+                               delta, dt, &
+                               flux, flux_l1, flux_h1, &
+                               pradial, p_l1, p_h1, &
+                               area, area_l1, area_h1, &
+                               dloga, dloga_l1, dloga_h1, &
+                               vol, vol_l1, vol_h1, &
+                               courno, verbose) bind(C, name="ca_mol_single_stage")
+
+  use meth_params_module, only : NQ, QVAR, QU, QV, QW, QPRES, &
+                                 UMX, UMY, UMZ, UTEMP, USHK, UEINT, &
+                                 NQAUX, NVAR, NHYP, use_flattening, &
+                                 NGDNV, GDU, GDPRES, first_order_hydro, difmag, &
+                                 hybrid_riemann
+  use advection_util_module, only : compute_cfl
+  use bl_constants_module, only : ZERO, HALF, ONE
+  use flatten_module, only : uflaten
+  use prob_params_module, only : coord_type
+  use riemann_module, only: cmpflx, shock
+  use ppm_module, only : ppm_reconstruct
+  use advection_util_1d_module, only : normalize_species_fluxes
+  use bl_fort_module, only : rt => c_real
+
+  implicit none
+
+  integer, intent(in) :: lo(1), hi(1), verbose
+  integer, intent(in) :: domlo(1), domhi(1)
+  integer, intent(in) :: uin_l1, uin_h1
+  integer, intent(in) :: uout_l1, uout_h1
+  integer, intent(in) :: q_l1, q_h1
+  integer, intent(in) :: qa_l1, qa_h1
+  integer, intent(in) :: srU_l1, srU_h1
+  integer, intent(in) :: updt_l1, updt_h1
+  integer, intent(in) :: flux_l1, flux_h1
+  integer, intent(in) :: p_l1, p_h1
+  integer, intent(in) :: area_l1, area_h1
+  integer, intent(in) :: dloga_l1, dloga_h1
+  integer, intent(in) :: vol_l1, vol_h1
+
+  real(rt)        , intent(in) ::      uin(  uin_l1:  uin_h1,NVAR)
+  real(rt)        , intent(inout) ::  uout( uout_l1: uout_h1,NVAR)
+  real(rt)        , intent(inout) ::     q(    q_l1:    q_h1,NQ)
+  real(rt)        , intent(in) ::     qaux(   qa_l1:   qa_h1,NQAUX)
+  real(rt)        , intent(in) ::     srcU(  srU_l1:  srU_h1,NVAR)
+  real(rt)        , intent(inout) :: update(updt_l1: updt_h1,NVAR)
+  real(rt)        , intent(inout) ::  flux( flux_l1: flux_h1,NVAR)
+  real(rt)        , intent(inout) :: pradial(  p_l1:   p_h1)
+  real(rt)        , intent(in) :: area( area_l1: area_h1     )
+  real(rt)        , intent(in) :: dloga(dloga_l1:dloga_h1     )
+  real(rt)        , intent(in) ::   vol(  vol_l1: vol_h1      )
+  real(rt)        , intent(in) :: delta(1), dt, time
+  real(rt)        , intent(inout) :: courno
+
+  ! Automatic arrays for workspace
+  real(rt)        , allocatable :: flatn(:)
+  real(rt)        , allocatable :: div(:)
+  real(rt)        , allocatable :: pdivu(:)
+
+  ! Edge-centered primitive variables (Riemann state)
+  real(rt)        , allocatable :: q1(:,:)
+
+  ! radiation fluxes (needthese to get things to compile)
+  real(rt)        , allocatable :: rflx(:,:)
+
+  real(rt)        , allocatable :: shk(:)
+
+  ! temporary interface values of the parabola
+  real(rt), allocatable :: sxm(:), sxp(:)
+
+  real(rt), allocatable :: qxm(:,:), qxp(:,:)
+
+  real(rt)         :: dx
+
+  integer i, n, ngf
+
+  integer ::  uin_lo(1),  uin_hi(1)
+  integer :: uout_lo(1), uout_hi(1)
+  integer :: q_lo(1), q_hi(1)
+
+  integer :: lo_3D(3), hi_3D(3)
+  integer :: q_lo_3D(3), q_hi_3D(3)
+  integer :: uin_lo_3D(3), uin_hi_3D(3)
+  real(rt)         :: dx_3D(3)
+
+  real(rt) :: div1
+
+  uin_lo  = [uin_l1]
+  uin_hi  = [uin_h1]
+
+  uout_lo = [uout_l1]
+  uout_hi = [uout_h1]
+
+  ngf = 1
+
+  q_lo = [q_l1]
+  q_hi = [q_h1]
+
+  lo_3D   = [lo(1), 0, 0]
+  hi_3D   = [hi(1), 0, 0]
+
+  q_lo_3D = [q_l1, 0, 0]
+  q_hi_3D = [q_h1, 0, 0]
+
+  uin_lo_3D = [uin_l1, 0, 0]
+  uin_hi_3D = [uin_h1, 0, 0]
+
+  dx_3D   = [delta(1), ZERO, ZERO]
+
+  allocate( flatn(q_l1:q_h1))
+
+  allocate(    q1(flux_l1-1:flux_h1+1, NGDNV))
+
+  ! when we do radiation, these would be passed out
+  allocate(rflx(flux_l1-1:flux_h1+1, NGDNV))
+
+  allocate( pdivu(lo(1):hi(1)  ))
+  allocate( shk(lo(1)-1:hi(1)+1))
+
+  dx = delta(1)
+
+
+#ifdef SHOCK_VAR
+    uout(lo(1):hi(1),USHK) = ZERO
+
+    call shock(q,q_l1,q_h1,shk,lo(1)-1,hi(1)+1,lo(1),hi(1),dx)
+
+    ! Store the shock data for future use in the burning step.
+    do i = lo(1), hi(1)
+       uout(i,USHK) = shk(i)
+    enddo
+
+    ! Discard it locally if we don't need it in the hydro update.
+    if (hybrid_riemann /= 1) then
+       shk(:) = ZERO
+    endif
+#else
+    ! multidimensional shock detection -- this will be used to do the
+    ! hybrid Riemann solver
+    if (hybrid_riemann == 1) then
+       call shock(q,q_l1,q_h1,shk,lo(1)-1,hi(1)+1,lo(1),hi(1),dx)
+    else
+       shk(:) = ZERO
+    endif
+#endif
+
+
+  ! Check if we have violated the CFL criterion.
+  call compute_cfl(q, q_lo_3D, q_hi_3D, &
+                   qaux, [qa_l1, 0, 0], [qa_h1, 0, 0], &
+                   lo_3D, hi_3D, dt, dx_3D, courno)
+
+  ! Compute flattening coefficient for slope calculations.
+  if (first_order_hydro == 1) then
+     flatn = ZERO
+
+  else if (use_flattening == 1) then
+     call uflaten([lo(1) - ngf, 0, 0], [hi(1) + ngf, 0, 0], &
+                  q(:,QPRES), q(:,QU), q(:,QV), q(:,QW), &
+                  flatn, [q_l1, 0, 0], [q_h1, 0, 0])
+  else
+     flatn = ONE
+  endif
+
+
+  ! sm and sp are the minus and plus parts of the parabola -- they are
+  ! defined for a single zone, so for zone i, sm is the left value of
+  ! the parabola and sp is the right value of the parabola
+  allocate(sxm(q_l1:q_h1))
+  allocate(sxp(q_l1:q_h1))
+
+  ! qm and qp are the left and right states for an interface -- they 
+  ! are defined for a particular interface, with the convention that
+  ! qm(i) and qp(i) correspond to the i-1/2 interface
+  allocate ( qxm(lo(1)-1:hi(1)+2,NQ) ) 
+  allocate ( qxp(lo(1)-1:hi(1)+2,NQ) ) 
+  
+  ! Do PPM reconstruction
+  do n = 1, QVAR
+     call ppm_reconstruct(q(:,n), q_l1, q_h1, &
+                          flatn, &
+                          sxm, sxp, &
+                          lo(1), hi(1), dx)
+
+     ! Construct the interface states -- this is essentially just a
+     ! reshuffling of interface states from zone-center indexing to
+     ! edge-centered indexing
+     do i = lo(1)-1, hi(1)+1
+        qxm(i,n) = sxp(i-1)
+        qxp(i,n) = sxm(i)
+     enddo
+  enddo
+
+  deallocate(sxm, sxp)
+
+
+  ! Get the fluxes from the Riemann solver
+  call cmpflx(lo, hi, domlo, domhi, &
+              qxm, qxp, lo(1)-1, hi(1)+1, &
+              flux, flux_l1, flux_h1, &
+              q1, flux_l1-1, flux_h1+1, &
+#ifdef RADIATION
+              rflux, rfd_l1,rfd_h1, &
+#endif
+              qaux, qa_l1, qa_h1, lo(1), hi(1))
+
+  deallocate(qxm, qxp)
+
+  ! construct p div(U)
+  do i = lo(1), hi(1)
+     pdivu(i) = HALF * &
+          (q1(i+1,GDPRES) + q1(i,GDPRES))* &
+          (q1(i+1,GDU)*area(i+1) - q1(i,GDU)*area(i)) / vol(i)
+  end do
+
+  ! Compute the artifical viscosity
+  allocate(div(lo(1):hi(1)+1))
+
+  do i = lo(1), hi(1)+1
+     div(i) = (q(i,QU) - q(i-1,QU)) / dx
+  enddo
+
+  do n = 1, NVAR
+     if ( n == UTEMP ) then
+        flux(lo(1):hi(1)+1,n) = ZERO
+#ifdef SHOCK_VAR
+     else if ( n == USHK) then
+        flux(lo(1):hi(1)+1,n) = ZERO
+#endif
+     else if ( n == UMY ) then
+        flux(lo(1):hi(1)+1,n) = ZERO
+     else if ( n == UMZ ) then
+        flux(lo(1):hi(1)+1,n) = ZERO
+     else
+        ! add the artifical viscosity
+        do i = lo(1),hi(1)+1
+           div1 = difmag*min(ZERO,div(i))
+           flux(i,n) = flux(i,n) + dx*div1*(uin(i,n) - uin(i-1,n))
+        enddo
+     endif
+  enddo
+
+  ! Normalize the species fluxes
+  call normalize_species_fluxes(flux,flux_l1,flux_h1,lo,hi)
+
+
+  ! Make the update for this state
+
+  ! For hydro, we will create an update source term that is
+  ! essentially the flux divergence.  This can be added with dt to
+  ! get the update
+
+  do n = 1, NVAR
+     do i = lo(1), hi(1)
+        update(i,n) = update(i,n) + ( flux(i,n) * area(i) - &
+                                      flux(i+1,n) * area(i+1) ) / vol(i)
+
+        ! Add p div(u) source term to (rho e)
+        if (n == UEINT) then
+           update(i,n) = update(i,n) - pdivu(i)
+        else if (n == UMX) then
+           update(i,UMX) = update(i,UMX) - ( q1(i+1,GDPRES) - q1(i,GDPRES) ) / dx        
+        endif
+
+        ! include source terms
+        update(i,n) = update(i,n) + srcU(i,n)
+
+     enddo
+  enddo
+
+  ! Scale the fluxes for the form we expect later in refluxing.
+
+  do n = 1, NVAR
+     do i = lo(1), hi(1)+1
+
+        flux(i,n) = dt * area(i) * flux(i,n)
+
+        ! Correct the momentum flux with the grad p part.
+        if (coord_type .eq. 0 .and. n == UMX) then
+           flux(i,n) = flux(i,n) + dt * area(i) * q1(i,GDPRES)
+        endif
+
+     enddo
+  enddo
+
+
+  if (coord_type .gt. 0) then
+     pradial(lo(1):hi(1)+1) = q1(lo(1):hi(1)+1,GDPRES) * dt
+  end if
+
+  deallocate(flatn,div,pdivu,q1)
+
+end subroutine ca_mol_single_stage
