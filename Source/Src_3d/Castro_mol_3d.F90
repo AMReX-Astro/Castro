119b0c10124ffd28fc46b0eff3f4d396b3bf3c34
Michael Zingale <michael.zingale@stonybrook.edu>
2017-03-23 15:33:56 -0400

    work in progress

---
+++ Source/Src_3d/Castro_mol_3d.F90
@@ -0,0 +1,388 @@
+! advection routines in support of method of lines integration
+
+subroutine ca_mol_single_stage(time, &
+                               lo, hi, domlo, domhi, &
+                               uin, uin_l1, uin_l2, uin_l3, uin_h1, uin_h2, uin_h3, &
+                               uout, uout_l1, uout_l2, uout_l3, uout_h1, uout_h2, uout_h3, &
+                               q, q_l1, q_l2, q_l3, q_h1, q_h2, q_h3, &
+                               qaux, qa_l1, qa_l2, qa_l3, qa_h1, qa_h2, qa_h3, &
+                               srcU, srU_l1, srU_l2, srU_l3, srU_h1, srU_h2, srU_h3, &
+                               update, updt_l1, updt_l2, updt_l3, updt_h1, updt_h2, updt_h3, &
+                               delta, dt, &
+                               flux1, flux1_l1, flux1_l2, flux1_l3, flux1_h1, flux1_h2, flux1_h3, &
+                               flux2, flux2_l1, flux2_l2, flux2_l3, flux2_h1, flux2_h2, flux2_h3, &
+                               flux3, flux3_l1, flux3_l2, flux3_l3, flux3_h1, flux3_h2, flux3_h3, &
+                               area1, area1_l1, area1_l2, area1_l3, area1_h1, area1_h2, area1_h3, &
+                               area2, area2_l1, area2_l2, area2_l3, area2_h1, area2_h2, area2_h3, &
+                               area3, area3_l1, area3_l2, area3_l3, area3_h1, area3_h2, area3_h3, &
+                               vol, vol_l1, vol_l2, vol_l3, vol_h1, vol_h2, vol_h3, &
+                               courno, verbose) bind(C, name="ca_mol_single_stage")
+
+  use mempool_module, only : bl_allocate, bl_deallocate
+  use meth_params_module, only : NQ, QVAR, NVAR, NGDNV, GDPRES, &
+                                 UTEMP, UEINT, USHK, UMX, GDU, GDV, &
+                                 use_flattening, QU, QV, QW, QPRES, NQAUX, &
+                                 first_order_hydro, difmag, hybrid_riemann
+  use advection_util_3d_module, only : divu, normalize_species_fluxes
+  use advection_util_module, only : compute_cfl
+  use bl_constants_module, only : ZERO, HALF, ONE
+  use flatten_module, only: uflaten
+  use riemann_module, only: cmpflx, shock
+  use ppm_module, only : ppm_reconstruct
+  use bl_fort_module, only : rt => c_real
+
+  implicit none
+
+  integer, intent(in) :: lo(3), hi(3), verbose
+  integer, intent(in) ::  domlo(3), domhi(3)
+  integer, intent(in) :: uin_l1, uin_l2, uin_l3, uin_h1, uin_h2, uin_h3
+  integer, intent(in) :: uout_l1, uout_l2, uout_l3, uout_h1, uout_h2, uout_h3
+  integer, intent(in) :: q_l1, q_l2, q_l3, q_h1, q_h2, q_h3
+  integer, intent(in) :: qa_l1, qa_l2, qa_l3, qa_h1, qa_h2, qa_h3
+  integer, intent(in) :: srQ_l1, srQ_l2, srQ_l3, srQ_h1, srQ_h2, srQ_h3
+  integer, intent(in) :: updt_l1, updt_l2, updt_l3, updt_h1, updt_h2, updt_h3
+  integer, intent(in) :: flux1_l1, flux1_l2, flux1_l3, flux1_h1, flux1_h2, flux1_h3
+  integer, intent(in) :: flux2_l1, flux2_l2, flux2_l3, flux2_h1, flux2_h2, flux2_h3
+  integer, intent(in) :: flux3_l1, flux3_l2, flux3_l3, flux3_h1, flux3_h2, flux3_h3
+  integer, intent(in) :: area1_l1, area1_l2, area1_l3, area1_h1, area1_h2, area1_h3
+  integer, intent(in) :: area2_l1, area2_l2, area2_l3, area2_h1, area2_h2, area2_h3
+  integer, intent(in) :: area3_l1, area3_l2, area3_l3, area3_h1, area3_h2, area3_h3
+  integer, intent(in) :: vol_l1, vol_l2, vol_l3, vol_h1, vol_h2, vol_h3
+
+  real(rt)        , intent(in) :: uin(uin_l1:uin_h1, uin_l2:uin_h2, uin_l3:uin_h3, NVAR)
+  real(rt)        , intent(inout) :: uout(uout_l1:uout_h1, uout_l2:uout_h2, uout_l3:uout_h3, NVAR)
+  real(rt)        , intent(inout) :: q(q_l1:q_h1, q_l2:q_h2, q_l3:q_h3, NQ)
+  real(rt)        , intent(inout) :: qaux(qa_l1:qa_h1, qa_l2:qa_h2, qa_l3:qa_h3, NQAUX)
+  real(rt)        , intent(in) :: srcQ(srQ_l1:srQ_h1, srQ_l2:srQ_h2, srQ_l3:srQ_h3, QVAR)
+  real(rt)        , intent(inout) :: update(updt_l1:updt_h1, updt_l2:updt_h2, updt_l3:updt_h3, NVAR)
+  real(rt)        , intent(inout) :: flux1(flux1_l1:flux1_h1, flux1_l2:flux1_h2, flux1_l3:flux1_h3, NVAR)
+  real(rt)        , intent(inout) :: flux2(flux2_l1:flux2_h1, flux2_l2:flux2_h2, flux2_l3:flux2_h3, NVAR)
+  real(rt)        , intent(inout) :: flux3(flux3_l1:flux3_h1, flux3_l2:flux3_h2, flux3_l3:flux3_h3, NVAR)
+  real(rt)        , intent(in) :: area1(area1_l1:area1_h1, area1_l2:area1_h2, area1_l3:area1_h3)
+  real(rt)        , intent(in) :: area2(area2_l1:area2_h1, area2_l2:area2_h2, area2_l3:area2_h3)
+  real(rt)        , intent(in) :: area3(area3_l1:area3_h1, area3_l2:area3_h2, area3_l3:area3_h3)
+  real(rt)        , intent(in) :: vol(vol_l1:vol_h1, vol_l2:vol_h2, vol_l3:vol_h3)
+  real(rt)        , intent(in) :: delta(3), dt, time
+  real(rt)        , intent(inout) :: courno
+
+  ! Automatic arrays for workspace
+  real(rt)        , pointer:: flatn(:,:,:)
+  real(rt)        , pointer:: div(:,:,:)
+  real(rt)        , pointer:: pdivu(:,:,:)
+
+  ! Edge-centered primitive variables (Riemann state)
+  real(rt)        , pointer:: q1(:,:,:,:)
+  real(rt)        , pointer:: q2(:,:,:,:)
+  real(rt)        , pointer:: q3(:,:,:,:)
+
+  ! radiation fluxes (need these to get things to compile)
+  real(rt)        , pointer:: rflx(:,:,:,:)
+  real(rt)        , pointer:: rfly(:,:,:,:)
+  real(rt)        , pointer:: rflz(:,:,:,:)
+
+  real(rt)        , pointer:: shk(:,:,:,:)
+
+  ! temporary interface values of the parabola
+  real(rt)        , pointer :: sxm(:,:,:), sym(:,:,:), szm(:,:,:)
+  real(rt)        , pointer :: sxp(:,:,:), syp(:,:,:), szp(:,:,:)
+
+  integer :: ngq, ngf
+  integer :: uin_lo(3), uin_hi(3)
+  integer :: uout_lo(3), uout_hi(3)
+  integer :: updt_lo(3), updt_hi(3)
+  integer :: flux1_lo(3), flux1_hi(3)
+  integer :: flux2_lo(3), flux2_hi(3)
+  integer :: flux3_lo(3), flux3_hi(3)
+  integer :: area1_lo(3), area1_hi(3)
+  integer :: area2_lo(3), area2_hi(3)
+  integer :: area3_lo(3), area3_hi(3)
+  integer :: vol_lo(3), vol_hi(3)
+  integer :: q_lo(3), q_hi(3)
+  integer :: qa_lo(3), qa_hi(3)
+  integer :: srQ_lo(3), srQ_hi(3)
+
+  ngf = 1
+
+  q_lo = [ q_l1, q_l2, q_l3 ]
+  q_hi = [ q_h1, q_h2, q_h3 ]
+
+  qa_lo = [ qa_l1, qa_l2, qa_l3 ]
+  qa_hi = [ qa_h1, qa_h2, qa_h3 ]
+
+  srQ_lo = [ srQ_l1, srQ_l2, srQ_l3 ]
+  srQ_hi = [ srQ_h1, srQ_h2, srQ_h3 ]
+
+  uin_lo = [ uin_l1, uin_l2, uin_l3 ]
+  uin_hi = [ uin_h1, uin_h2, uin_h3 ]
+
+  uout_lo = [ uout_l1, uout_l2, uout_l3 ]
+  uout_hi = [ uout_h1, uout_h2, uout_h3 ]
+
+  updt_lo = [ updt_l1, updt_l2, updt_l3 ]
+  updt_hi = [ updt_h1, updt_h2, updt_h3 ]
+
+  flux1_lo = [ flux1_l1, flux1_l2, flux1_l3 ]
+  flux1_hi = [ flux1_h1, flux1_h2, flux1_h3 ]
+
+  flux2_lo = [ flux2_l1, flux2_l2, flux2_l3 ]
+  flux2_hi = [ flux2_h1, flux2_h2, flux2_h3 ]
+
+  flux3_lo = [ flux3_l1, flux3_l2, flux3_l3 ]
+  flux3_hi = [ flux3_h1, flux3_h2, flux3_h3 ]
+
+  area1_lo = [ area1_l1, area1_l2, area1_l3 ]
+  area1_hi = [ area1_h1, area1_h2, area1_h3 ]
+
+  area2_lo = [ area2_l1, area2_l2, area2_l3 ]
+  area2_hi = [ area2_h1, area2_h2, area2_h3 ]
+
+  area3_lo = [ area3_l1, area3_l2, area3_l3 ]
+  area3_hi = [ area3_h1, area3_h2, area3_h3 ]
+
+  vol_lo = [ vol_l1, vol_l2, vol_l3 ]
+  vol_hi = [ vol_h1, vol_h2, vol_h3 ]
+
+  It_lo = [lo(1) - 1, lo(2) - 1, 1]
+  It_hi = [hi(1) + 1, hi(2) + 1, 2]
+
+  call bl_allocate(   div, lo(1), hi(1)+1, lo(2), hi(2)+1, lo(3), hi(3)+1)
+  call bl_allocate( pdivu, lo(1), hi(1)  , lo(2), hi(2)  , lo(3), hi(3)  )
+
+  call bl_allocate(q1, flux1_lo, flux1_hi, NGDNV)
+  call bl_allocate(q2, flux2_lo, flux2_hi, NGDNV)
+  call bl_allocate(q3, flux3_lo, flux3_hi, NGDNV)
+
+  call bl_allocate(sxm, It_lo, It_hi)
+  call bl_allocate(sxp, It_lo, It_hi)
+  call bl_allocate(sym, It_lo, It_hi)
+  call bl_allocate(syp, It_lo, It_hi)
+  call bl_allocate(szm, It_lo, It_hi)
+  call bl_allocate(szp, It_lo, It_hi)
+
+  call bl_allocate ( qxm, qt_lo, qt_hi, NQ)
+  call bl_allocate ( qxp, qt_lo, qt_hi, NQ)
+
+  call bl_allocate ( qym, qt_lo, qt_hi, NQ)
+  call bl_allocate ( qyp, qt_lo, qt_hi, NQ)
+
+  call bl_allocate ( qzm, qt_lo, qt_hi, NQ)
+  call bl_allocate ( qzp, qt_lo, qt_hi, NQ)
+
+#ifdef SHOCK_VAR
+    uout(lo(1):hi(1),lo(2):hi(2),lo(3):hi(3),USHK) = ZERO
+
+    call shock(q,qd_lo,qd_hi,shk,shk_lo,shk_hi,lo,hi,dx)
+
+    ! Store the shock data for future use in the burning step.
+
+    do k3d = lo(3), hi(3)
+       do j = lo(2), hi(2)
+          do i = lo(1), hi(1)
+             uout(i,j,k3d,USHK) = shk(i,j,k3d)
+          enddo
+       enddo
+    enddo
+
+    ! Discard it locally if we don't need it in the hydro update.
+
+    if (hybrid_riemann /= 1) then
+       shk(:,:,:) = ZERO
+    endif
+#else
+    ! multidimensional shock detection -- this will be used to do the
+    ! hybrid Riemann solver
+    if (hybrid_riemann == 1) then
+       call shock(q,qd_lo,qd_hi,shk,shk_lo,shk_hi,lo,hi,dx)
+    else
+       shk(:,:,:) = ZERO
+    endif
+#endif
+
+  ! Check if we have violated the CFL criterion.
+  call compute_cfl(q, q_lo, q_hi, &
+                   qaux, qa_lo, qa_hi, &
+                   lo, hi, dt, delta, courno)
+
+  ! Compute flattening coefficient for slope calculations.
+  call bl_allocate( flatn, q_lo, q_hi)
+
+  if (first_order_hydro == 1) then
+     flatn = ZERO
+  elseif (use_flattening == 1) then
+     call uflaten(lo - ngf, hi + ngf, &
+                  q(:,:,:,QPRES), q(:,:,:,QU), q(:,:,:,QV), q(:,:,:,QW), &
+                  flatn, q_lo, q_hi)
+  else
+     flatn = ONE
+  endif
+
+  ! We come into this routine with a 3-d box of data, but we operate
+  ! on it locally by considering 2 planes that encompass all of the
+  ! x, y indices of the original box, but each plane corresponds to
+  ! a single z index.
+  !
+  ! In the notation below, k3d will always been the index into the
+  ! original 3-d box.  kc will be the z-index in the local "planar"
+  ! data and km will be the previously used index in the local
+  ! planar data.
+  !
+  ! With each loop in the k direction, we will overwrite the old
+  ! data in the planar arrays.
+
+  ! Initialize kc (current k-level) and km (previous k-level)
+  kc = 1
+  km = 2
+
+  do k3d = lo(3)-1, hi(3)+1
+
+     ! Swap pointers to levels
+     kt = km
+     km = kc
+     kc = kt
+
+     do n = 1, NQ
+        call ppm_reconstruct(q(:,:,:,n  ), qd_lo, qd_hi, &
+                             flatn, qd_lo, qd_hi, &
+                             sxm, sxp, sym, syp, szm, szp, It_lo, It_hi, &
+                             lo(1), lo(2), hi(1), hi(2), dx, k3d, kc)
+
+        ! Construct the interface states -- this is essentially just a
+        ! reshuffling of interface states from zone-center indexing to
+        ! edge-centered indexing
+        if (k3d >= lo(3)) then
+           do j = lo(2)-1, hi(2)+1
+              do i = lo(1)-1, hi(1)+1
+
+                 ! x-edges
+
+                 ! left state at i-1/2 interface
+                 qxm(i,j,kc,n) = sxp(i-1,j,kc)
+
+                 ! right state at i-1/2 interface
+                 qxp(i,j,kc,n) = sxm(i,j,kc)
+
+                 ! y-edges
+
+                 ! left state at j-1/2 interface
+                 qym(i,j,kc,n) = syp(i,j-1,kc)
+
+                 ! right state at j-1/2 interface
+                 qyp(i,j,kc,n) = sym(i,j,kc)
+
+                 ! z-edges
+
+                 ! left state at k3d-1/2 interface
+                 qzm(i,j,kc,n) = szp(i,j,km)
+
+                 ! right state at k3d-1/2 interface
+                 qzp(i,j,kc,n) = szm(i,j,kc)
+
+              enddo
+           enddo
+
+           ! Compute F^x at kc (k3d)
+           call cmpflx(qxm, qxp, qt_lo, qt_hi, &
+                       flux1, fd1_lo, fd1_hi, &
+                       qint, qt_lo, qt_hi, &  ! temporary
+#ifdef RADIATION
+                       rflux1, rfd1_lo, rfd1_hi, &
+#endif
+                       qaux, qa_lo, qa_hi, &
+                       shk, shk_lo, shk_hi, &
+                       1, lo(1), hi(1)+1, lo(2), hi(2), kc, k3d, k3d, domlo, domhi)
+
+           do j = lo(2)-1, hi(2)+1
+              do i = lo(1)-1, hi(1)+2
+                 q1(i,j,k3d,:) = qgdnvxf(i,j,kc,:)
+              enddo
+           enddo
+
+
+           ! Compute F^y at kc (k3d)
+           call cmpflx(qym, qyp, qt_lo, qt_hi, &
+                       flux2, fd2_lo, fd2_hi, &
+                       qint, qt_lo, qt_hi, &  ! temporary
+#ifdef RADIATION
+                       rflux2, rfd2_lo, rfd2_hi, &
+#endif
+                       qaux, qa_lo, qa_hi, &
+                       shk, shk_lo, shk_hi, &
+                       2, lo(1), hi(1), lo(2), hi(2)+1, kc, k3d, k3d, domlo, domhi)
+
+           do j = lo(2)-1, hi(2)+2
+              do i = lo(1)-1, hi(1)+1
+                 q2(i,j,k3d,:) = qint(i,j,kc,:)
+              enddo
+           enddo
+           
+           ! Compute F^z at kc (k3d)
+           call cmpflx(qzm, qzp, qt_lo, qt_hi, &
+                       flux3, fd3_lo, fd3_hi, &
+                       qint, qt_lo, qt_hi, &
+#ifdef RADIATION
+                       rflux3, rfd3_lo, rfd3_hi, &
+#endif
+                       qaux, qa_lo, qa_hi, &
+                       shk, shk_lo, shk_hi, &
+                       3, lo(1), hi(1), lo(2), hi(2), kc, k3d, k3d, domlo, domhi)
+
+           do j=lo(2)-1,hi(2)+1
+              do i=lo(1)-1,hi(1)+1
+                 q3(i,j,k3d,:) = qint(i,j,kc,:)
+              enddo
+           enddo
+
+        endif
+
+     enddo
+  enddo
+
+
+  call bl_deallocate(flatn)
+
+  call bl_deallocate(sxm)
+  call bl_deallocate(sxp)
+  call bl_deallocate(sym)
+  call bl_deallocate(syp)
+  call bl_deallocate(szm)
+  call bl_deallocate(szp)
+
+  ! Compute divergence of velocity field (on surroundingNodes(lo,hi))
+  call divu(lo,hi,q,q_lo,q_hi,delta,div,lo,hi+1)
+
+  ! Conservative update
+  call consup(uin ,  uin_lo , uin_hi, &
+              q, q_lo, q_hi, &
+              uout, uout_lo, uout_hi, &
+              update, updt_lo, updt_hi, &
+              flux1, flux1_lo, flux1_hi, &
+              flux2, flux2_lo, flux2_hi, &
+              flux3, flux3_lo, flux3_hi, &
+#ifdef RADIATION
+              Erin, Erin_lo, Erin_hi, &
+              Erout, Erout_lo, Erout_hi, &
+              radflux1, radflux1_lo, radflux1_hi, &
+              radflux2, radflux2_lo, radflux2_hi, &
+              radflux3, radflux3_lo, radflux3_hi, &
+              nstep_fsp, &
+#endif
+              q1, q1_lo, q1_hi, &
+              q2, q2_lo, q2_hi, &
+              q3, q3_lo, q3_hi, &
+              area1, area1_lo, area1_hi, &
+              area2, area2_lo, area2_hi, &
+              area3, area3_lo, area3_hi, &
+              vol, vol_lo, vol_hi, &
+              div,pdivu,lo,hi,delta,dt, &
+              mass_lost,xmom_lost,ymom_lost,zmom_lost, &
+              eden_lost,xang_lost,yang_lost,zang_lost, &
+              verbose)
+
+  call bl_deallocate(   div)
+  call bl_deallocate( pdivu)
+
+  call bl_deallocate(    q1)
+  call bl_deallocate(    q2)
+  call bl_deallocate(    q3)
+
+end subroutine ca_ctu_update
