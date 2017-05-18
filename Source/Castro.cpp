e455dedd6e7d7d4cbd43db6d084ebb37c47733be
Michael Zingale <michael.zingale@stonybrook.edu>
2017-05-18 12:28:02 -0400

    add intents

--- Source/Castro.cpp
+++ Source/Castro.cpp
@@ -2493,7 +2493,8 @@ Castro::enforce_consistent_e (MultiFab& S)
         const int* lo      = box.loVect();
         const int* hi      = box.hiVect();
 
-        ca_enforce_consistent_e(ARLIM_3D(lo), ARLIM_3D(hi), BL_TO_FORTRAN_3D(S[mfi]));
+	const int idx      = mfi.uniqueIndex();
+        ca_enforce_consistent_e(ARLIM_3D(lo), ARLIM_3D(hi), BL_TO_FORTRAN_3D(S[mfi]), &idx);
     }
 }
 
