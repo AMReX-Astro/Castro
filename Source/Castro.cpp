ce815217ffc658cce190b1761e08445813f1cf24
Max Katz <maximilian.katz@stonybrook.edu>
2017-05-19 11:54:10 -0400

    uniqueIndex -> tileIndex

--- Source/Castro.cpp
+++ Source/Castro.cpp
@@ -2493,7 +2493,7 @@ Castro::enforce_consistent_e (MultiFab& S)
         const int* lo      = box.loVect();
         const int* hi      = box.hiVect();
 
-	const int idx      = mfi.uniqueIndex();
+	const int idx      = mfi.tileIndex();
         ca_enforce_consistent_e(ARLIM_3D(lo), ARLIM_3D(hi), BL_TO_FORTRAN_3D(S[mfi]), &idx);
     }
 }
