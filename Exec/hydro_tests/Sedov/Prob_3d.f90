3853262466a51a34a96f55c2c04aff33b7b8089f
Michael Zingale <michael.zingale@stonybrook.edu>
2017-07-21 13:04:30 -0400

    switch to the EOS instead of relying on gamma_const -- this allows other EOS-es to be used

--- Exec/hydro_tests/Sedov/Prob_3d.f90
+++ Exec/hydro_tests/Sedov/Prob_3d.f90
@@ -14,7 +14,7 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
 
   namelist /fortin/ probtype, p_ambient, dens_ambient, exp_energy, &
        r_init, nsub
-  
+
   ! Build "probin" filename -- the name of file containing fortin namelist.
   integer, parameter :: maxlen = 256
   character :: probin*(maxlen)
@@ -26,7 +26,7 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
   do i = 1, namlen
      probin(i:i) = char(name(i))
   end do
-         
+
   ! set namelist defaults
 
   p_ambient = 1.e-5_rt        ! ambient pressure (in erg/cc)
@@ -45,22 +45,22 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
   center(1) = (problo(1)+probhi(1))/2.e0_rt
   center(2) = (problo(2)+probhi(2))/2.e0_rt
   center(3) = (problo(3)+probhi(3))/2.e0_rt
-  
+
 end subroutine amrex_probinit
 
 
 ! ::: -----------------------------------------------------------
 ! ::: This routine is called at problem setup time and is used
-! ::: to initialize data on each grid.  
-! ::: 
+! ::: to initialize data on each grid.
+! :::
 ! ::: NOTE:  all arrays have one cell of ghost zones surrounding
 ! :::        the grid interior.  Values in these cells need not
 ! :::        be set here.
-! ::: 
+! :::
 ! ::: INPUTS/OUTPUTS:
-! ::: 
+! :::
 ! ::: level     => amr level of grid
-! ::: time      => time at which to init data             
+! ::: time      => time at which to init data
 ! ::: lo,hi     => index limits of grid interior (cell centered)
 ! ::: nstate    => number of state components.  You should know
 ! :::		   this already!
@@ -75,13 +75,16 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
                        delta,xlo,xhi)
 
   use probdata_module
-  use actual_eos_module, only: gamma_const
-  use bl_constants_module, only: M_PI, FOUR3RD
+  use bl_constants_module, only: M_PI, FOUR3RD, ZERO, ONE
   use meth_params_module , only: NVAR, URHO, UMX, UMY, UMZ, UEDEN, UEINT, UFS
   use prob_params_module, only : center
   use amrex_fort_module, only : rt => amrex_real
+  use network, only : nspec
+  use eos_module, only : eos
+  use eos_type_module, only : eos_t, eos_input_rp, eos_input_re
 
   implicit none
+
   integer :: level, nscal
   integer :: lo(3), hi(3)
   integer :: state_l1,state_l2,state_l3,state_h1,state_h2,state_h3
@@ -98,54 +101,76 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
 
   integer :: i,j,k, ii, jj, kk
   integer :: npert, nambient
-  
+  real(rt) :: xn_zone(nspec), e_zone
+  type(eos_t) :: eos_state
+
+  xn_zone(:) = ZERO
+  xn_zone(1) = ONE
+
   if (probtype .eq. 32) then
 
      ! set explosion pressure -- we will convert the point-explosion
      ! energy into a corresponding pressure distributed throughout the
      ! perturbed volume
      vctr  = M_PI*r_init**2
-     p_exp = (gamma_const - 1.e0_rt)*exp_energy/vctr
-     
+
+     e_zone = exp_energy/vctr/dens_ambient
+
+     eos_state % e = e_zone
+     eos_state % rho = dens_ambient
+     eos_state % xn(:) = xn_zone(:)
+     eos_state % T = 100.0  ! initial guess
+
+     call eos(eos_input_re, eos_state)
+
+     p_exp = eos_state % p
+
      do k = lo(3), hi(3)
-        zmin = xlo(3) + delta(3)*dble(k-lo(3)) 
-        
+        zmin = xlo(3) + delta(3)*dble(k-lo(3))
+
         do j = lo(2), hi(2)
            ymin = xlo(2) + delta(2)*dble(j-lo(2))
-           
+
            do i = lo(1), hi(1)
               xmin = xlo(1) + delta(1)*dble(i-lo(1))
-              
+
               npert = 0
               nambient = 0
-              
+
               do jj = 0, nsub-1
                  yy = ymin + (delta(2)/dble(nsub))*(jj + 0.5e0_rt)
-                 
+
                  do ii = 0, nsub-1
                     xx = xmin + (delta(1)/dble(nsub))*(ii + 0.5e0_rt)
-                    
+
                     dist = (center(1)-xx)**2 + (center(2)-yy)**2
-                    
+
                     if(dist <= r_init**2) then
                        npert = npert + 1
                     else
                        nambient = nambient + 1
                     endif
-                    
+
                  enddo
               enddo
-              
+
               p_zone = (dble(npert)*p_exp + dble(nambient)*p_ambient) / &
                        (dble(npert) + dble(nambient))
 
-              eint = p_zone/(gamma_const - 1.e0_rt)
+
+              eos_state % p = p_zone
+              eos_state % rho = dens_ambient
+              eos_state % xn(:) = xn_zone(:)
+
+              call eos(eos_input_rp, eos_state)
+
+              eint = dens_ambient * eos_state % e
 
               state(i,j,k,URHO) = dens_ambient
               state(i,j,k,UMX) = 0.e0_rt
               state(i,j,k,UMY) = 0.e0_rt
               state(i,j,k,UMZ) = 0.e0_rt
-              
+
               state(i,j,k,UEDEN) = eint +  &
                    0.5e0_rt*(state(i,j,k,UMX)**2/state(i,j,k,URHO) + &
                           state(i,j,k,UMY)**2/state(i,j,k,URHO) + &
@@ -158,20 +183,30 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
            enddo
         enddo
      enddo
-     
+
   else if (probtype .eq. 33) then
 
      ! set explosion pressure -- we will convert the point-explosion energy into
      ! a corresponding pressure distributed throughout the perturbed volume
      vctr  = FOUR3RD*M_PI*r_init**3
-     p_exp = (gamma_const - 1.e0_rt)*exp_energy/vctr
+
+     e_zone = exp_energy/vctr/dens_ambient
+
+     eos_state % e = e_zone
+     eos_state % rho = dens_ambient
+     eos_state % xn(:) = xn_zone(:)
+     eos_state % T = 100.0  ! initial guess
+
+     call eos(eos_input_re, eos_state)
+
+     p_exp = eos_state % p
 
      do k = lo(3), hi(3)
-        zmin = xlo(3) + delta(3)*dble(k-lo(3)) 
+        zmin = xlo(3) + delta(3)*dble(k-lo(3))
 
         do j = lo(2), hi(2)
            ymin = xlo(2) + delta(2)*dble(j-lo(2))
-           
+
            do i = lo(1), hi(1)
               xmin = xlo(1) + delta(1)*dble(i-lo(1))
 
@@ -180,35 +215,41 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
 
               do kk = 0, nsub-1
                  zz = zmin + (delta(3)/dble(nsub))*(kk + 0.5e0_rt)
-                 
+
                  do jj = 0, nsub-1
                     yy = ymin + (delta(2)/dble(nsub))*(jj + 0.5e0_rt)
-                    
+
                     do ii = 0, nsub-1
                        xx = xmin + (delta(1)/dble(nsub))*(ii + 0.5e0_rt)
-                       
+
                        dist = (center(1)-xx)**2 + (center(2)-yy)**2 + (center(3)-zz)**2
-                       
+
                        if(dist <= r_init**2) then
                           npert = npert + 1
                        else
                           nambient = nambient + 1
                        endif
-                       
+
                     enddo
                  enddo
               enddo
-              
+
               p_zone = (dble(npert)*p_exp + dble(nambient)*p_ambient)/  &
                    dble(nsub*nsub*nsub)
 
-              eint = p_zone/(gamma_const - 1.e0_rt)
+              eos_state % p = p_zone
+              eos_state % rho = dens_ambient
+              eos_state % xn(:) = xn_zone(:)
+
+              call eos(eos_input_rp, eos_state)
+
+              eint = dens_ambient * eos_state % e
 
               state(i,j,k,URHO) = dens_ambient
               state(i,j,k,UMX) = 0.e0_rt
               state(i,j,k,UMY) = 0.e0_rt
               state(i,j,k,UMZ) = 0.e0_rt
-              
+
               state(i,j,k,UEDEN) = eint + &
                    0.5e0_rt*(state(i,j,k,UMX)**2/state(i,j,k,URHO) + &
                           state(i,j,k,UMY)**2/state(i,j,k,URHO) + &
@@ -221,12 +262,11 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
            enddo
         enddo
      enddo
-     
-  else 
+
+  else
 
      call bl_error('Dont know this probtype in initdata')
 
   end if
-  
-end subroutine ca_initdata
 
+end subroutine ca_initdata
