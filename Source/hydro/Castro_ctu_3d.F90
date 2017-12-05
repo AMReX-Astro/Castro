8a8bbde368a311939fc09a0efd75d98ed552aa00
Michael Zingale <michael.zingale@stonybrook.edu>
2017-12-05 15:37:36 -0500

    start of de-dimensionalizing

--- Source/hydro/Castro_ctu_3d.F90
+++ Source/hydro/Castro_ctu_3d.F90
@@ -1,3 +1,5 @@
+! advection routines in support of the CTU unsplit advection scheme
+
 subroutine ca_ctu_update(is_finest_level, time, &
                          lo, hi, domlo, domhi, &
                          uin, uin_lo, uin_hi, &
@@ -13,15 +15,25 @@ subroutine ca_ctu_update(is_finest_level, time, &
                          delta, dt, &
                          flux1, flux1_lo, flux1_hi, &
                          flux2, flux2_lo, flux2_hi, &
+#if BL_SPACEDIM == 3
                          flux3, flux3_lo, flux3_hi, &
+#endif
 #ifdef RADIATION
                          radflux1, radflux1_lo, radflux1_hi, &
                          radflux2, radflux2_lo, radflux2_hi, &
+#if BL_SPACEDIM == 3
                          radflux3, radflux3_lo, radflux3_hi, &
+#endif
 #endif
                          area1, area1_lo, area1_hi, &
                          area2, area2_lo, area2_hi, &
+#if BL_SPACEDIM == 3
                          area3, area3_lo, area3_hi, &
+#endif
+#if BL_SPACEDIM <= 2
+                         pradial, p_lo, p_hi, &
+                         dloga, dloga_lo, dloga_hi, &
+#endif
                          vol, vol_lo, vol_hi, &
                          verbose, &
 #ifdef RADIATION
@@ -40,11 +52,12 @@ subroutine ca_ctu_update(is_finest_level, time, &
   use advection_util_module, only : divu
   use bl_constants_module, only : ZERO, ONE
   use flatten_module, only: uflatten
+  use prob_params_module, only : mom_flux_has_p
 #ifdef RADIATION
   use rad_params_module, only : ngroups
   use flatten_module, only : rad_flatten
 #endif
-  use ctu_advection_module, only : umeth3d, consup
+  use ctu_advection_module, only : umeth, consup
 
   use amrex_fort_module, only : rt => amrex_real
   implicit none
@@ -67,16 +80,26 @@ subroutine ca_ctu_update(is_finest_level, time, &
   integer, intent(in) :: updt_lo(3), updt_hi(3)
   integer, intent(in) :: flux1_lo(3), flux1_hi(3)
   integer, intent(in) :: flux2_lo(3), flux2_hi(3)
+#if BL_SPACEDIM == 3
   integer, intent(in) :: flux3_lo(3), flux3_hi(3)
+#endif
 #ifdef RADIATION
   integer, intent(in) :: radflux1_lo(3), radflux1_hi(3)
   integer, intent(in) :: radflux2_lo(3), radflux2_hi(3)
+#if BL_SPACEDIM == 3
   integer, intent(in) :: radflux3_lo(3), radflux3_hi(3)
+#endif
 #endif
   integer, intent(in) :: area1_lo(3), area1_hi(3)
   integer, intent(in) :: area2_lo(3), area2_hi(3)
+#if BL_SPACEDIM == 3
   integer, intent(in) :: area3_lo(3), area3_hi(3)
+#endif
   integer, intent(in) :: vol_lo(3), vol_hi(3)
+#if BL_SPACEDIM <= 2
+  integer, intent(in) :: p_lo(3), p_hi(3)
+  integer, intent(in) :: dloga_lo(3), dloga_hi(3)
+#endif
 
   real(rt)        , intent(in) :: uin(uin_lo(1):uin_hi(1), uin_lo(2):uin_hi(2), uin_lo(3):uin_hi(3), NVAR)
   real(rt)        , intent(inout) :: uout(uout_lo(1):uout_hi(1), uout_lo(2):uout_hi(2), uout_lo(3):uout_hi(3), NVAR)
@@ -90,19 +113,29 @@ subroutine ca_ctu_update(is_finest_level, time, &
   real(rt)        , intent(inout) :: update(updt_lo(1):updt_hi(1), updt_lo(2):updt_hi(2), updt_lo(3):updt_hi(3), NVAR)
   real(rt)        , intent(inout) :: flux1(flux1_lo(1):flux1_hi(1), flux1_lo(2):flux1_hi(2), flux1_lo(3):flux1_hi(3), NVAR)
   real(rt)        , intent(inout) :: flux2(flux2_lo(1):flux2_hi(1), flux2_lo(2):flux2_hi(2), flux2_lo(3):flux2_hi(3), NVAR)
+#if BL_SPACEDIM == 3
   real(rt)        , intent(inout) :: flux3(flux3_lo(1):flux3_hi(1), flux3_lo(2):flux3_hi(2), flux3_lo(3):flux3_hi(3), NVAR)
+#endif
 #ifdef RADIATION
   real(rt)        , intent(inout) :: radflux1(radflux1_lo(1):radflux1_hi(1), radflux1_lo(2):radflux1_hi(2), &
                                               radflux1_lo(3):radflux1_hi(3), 0:ngroups-1)
   real(rt)        , intent(inout) :: radflux2(radflux2_lo(1):radflux2_hi(1), radflux2_lo(2):radflux2_hi(2), &
                                               radflux2_lo(3):radflux2_hi(3), 0:ngroups-1)
+#if BL_SPACEDIM == 3
   real(rt)        , intent(inout) :: radflux3(radflux3_lo(1):radflux3_hi(1), radflux3_lo(2):radflux3_hi(2), &
                                               radflux3_lo(3):radflux3_hi(3), 0:ngroups-1)
+#endif
 #endif
   real(rt)        , intent(in) :: area1(area1_lo(1):area1_hi(1), area1_lo(2):area1_hi(2), area1_lo(3):area1_hi(3))
   real(rt)        , intent(in) :: area2(area2_lo(1):area2_hi(1), area2_lo(2):area2_hi(2), area2_lo(3):area2_hi(3))
   real(rt)        , intent(in) :: area3(area3_lo(1):area3_hi(1), area3_lo(2):area3_hi(2), area3_lo(3):area3_hi(3))
   real(rt)        , intent(in) :: vol(vol_lo(1):vol_hi(1), vol_lo(2):vol_hi(2), vol_lo(3):vol_hi(3))
+
+#if BL_SPACEDIM <= 3
+  real(rt)        , intent(in) :: dloga(dloga_lo(1):dloga_hi(1),dloga_lo(2):dloga_hi(2),dloga_lo(3):dloga_hi(3))
+  real(rt)        , intent(inout) :: pradial(p_lo(1):p_hi(1),p_lo(2):p_hi(2),p_lo(3):p_hi(3))
+#endif
+
   real(rt)        , intent(in) :: delta(3), dt, time
 
   real(rt)        , intent(inout) :: mass_lost, xmom_lost, ymom_lost, zmom_lost
@@ -123,14 +156,14 @@ subroutine ca_ctu_update(is_finest_level, time, &
   ngq = NHYP
   ngf = 1
 
-  call bl_allocate(   div, lo(1), hi(1)+1, lo(2), hi(2)+1, lo(3), hi(3)+1)
+  call bl_allocate(   div, lo, hi+dg)
 
   q1_lo = flux1_lo - 1
   q1_hi = flux1_hi + 1
-  q2_lo = flux2_lo - 1
-  q2_hi = flux2_hi + 1
-  q3_lo = flux3_lo - 1
-  q3_hi = flux3_hi + 1
+  q2_lo = flux2_lo - dg(2)
+  q2_hi = flux2_hi + dg(2)
+  q3_lo = flux3_lo - dg(3)
+  q3_hi = flux3_hi + dg(3)
 
   call bl_allocate(q1, q1_lo, q1_hi, NGDNV)
   call bl_allocate(q2, q2_lo, q2_hi, NGDNV)
@@ -143,11 +176,10 @@ subroutine ca_ctu_update(is_finest_level, time, &
      flatn = ZERO
   elseif (use_flattening == 1) then
 #ifdef RADIATION
-     call rad_flatten([lo(1)-ngf, lo(2)-ngf, lo(3)-ngf], &
-                      [hi(1)+ngf, hi(2)+ngf, hi(3)+ngf], &
+     call rad_flatten(lo-dg*ngf, hi+dg*ngf,
                       q, flatn, q_lo, q_hi)
 #else
-     call uflatten(lo - ngf, hi + ngf, &
+     call uflatten(lo-dg*ngf, hi+dg*ngf, &
                    q, flatn, q_lo, q_hi, QPRES)
 #endif
   else
@@ -155,24 +187,28 @@ subroutine ca_ctu_update(is_finest_level, time, &
   endif
 
   ! Compute hyperbolic fluxes using unsplit Godunov
-  call umeth3d(q, q_lo, q_hi, &
-               flatn, &
-               qaux, qa_lo, qa_hi, &
-               srcQ, srQ_lo, srQ_hi, &
-               lo, hi, delta, dt, &
-               uout, uout_lo, uout_hi, &
-               flux1, flux1_lo, flux1_hi, &
-               flux2, flux2_lo, flux2_hi, &
-               flux3, flux3_lo, flux3_hi, &
+  call umeth(q, q_lo, q_hi, &
+             flatn, &
+             qaux, qa_lo, qa_hi, &
+             srcQ, srQ_lo, srQ_hi, &
+             lo, hi, delta, dt, &
+             uout, uout_lo, uout_hi, &
+             flux1, flux1_lo, flux1_hi, &
+             flux2, flux2_lo, flux2_hi, &
+#if BL_SPACEDIM == 3
+             flux3, flux3_lo, flux3_hi, &
+#endig
 #ifdef RADIATION
-               radflux1, radflux1_lo, radflux1_hi, &
-               radflux2, radflux2_lo, radflux2_hi, &
-               radflux3, radflux3_lo, radflux3_hi, &
+             radflux1, radflux1_lo, radflux1_hi, &
+             radflux2, radflux2_lo, radflux2_hi, &
+#if BL_SPACEDIM == 3
+             radflux3, radflux3_lo, radflux3_hi, &
 #endif
-               q1, q1_lo, q1_hi, &
-               q2, q2_lo, q2_hi, &
-               q3, q3_lo, q3_hi, &
-               domlo, domhi)
+#endif
+             q1, q1_lo, q1_hi, &
+             q2, q2_lo, q2_hi, &
+             q3, q3_lo, q3_hi, &
+             domlo, domhi)
 
 
   call bl_deallocate( flatn)
