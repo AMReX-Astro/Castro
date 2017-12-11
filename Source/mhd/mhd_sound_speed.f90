a2ecc3d73bd5b6e0b9180b09f64b20257b6e87d9
Michael Zingale <michael.zingale@stonybrook.edu>
2017-12-11 17:28:47 -0500

    pass T into MHD soundspeed to have good initial guess

--- Source/mhd/mhd_sound_speed.f90
+++ Source/mhd/mhd_sound_speed.f90
@@ -1,5 +1,5 @@
 !======= Sound speed calc for ideal MHD ================
-  subroutine eos_soundspeed_mhd(c, R, e, bx, by, bz, bd, compo)
+  subroutine eos_soundspeed_mhd(c, rho, e, temp, bx, by, bz, bd, compo)
 
      use amrex_fort_module, only : rt => amrex_real
      use meth_params_module
@@ -9,7 +9,7 @@
 
      implicit none
      ! In/out variables
-     real(rt), intent(in   ) :: R, e, bx , by, bz, bd !density, internal energy, magnetic fields, directional mag field
+     real(rt), intent(in   ) :: rho, e, temp, bx , by, bz, bd !density, internal energy, magnetic fields, directional mag field
      real(rt), intent(in   ) :: compo(nspec)
      real(rt), intent(  out) :: c
 
@@ -20,19 +20,21 @@
 
      type(eos_t) :: eos_state
 
-     eos_state % rho = R
+     eos_state % rho = rho
      eos_state % e   = e
+     eos_state % T   = temp
      eos_state % xn  = compo
      
      call eos(eos_input_re, eos_state)
   
      P = eos_state % p
-     as = eos_state % gam1 * P/R
-     ca = (bx**2 + by**2 + bz**2)/R
+     as = eos_state % cs
+     ca = (bx**2 + by**2 + bz**2)/rho
      !Fast Magneto-Sonic Wave
+     ! TODO: double check this expression
      c = eos_state % gam1 * P + (bx**2 + by**2 + bx**2) + &
          sqrt((eos_state % gam1 * P + (bx**2+by**2+bz**2))**2 - 4.d0 * eos_state % gam1 *P*(bd))
-     c = 0.5d0*c/R
+     c = 0.5d0*c/rho
      c = sqrt(c)
 
   end subroutine eos_soundspeed_mhd
