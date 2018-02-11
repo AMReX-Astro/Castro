af9e6de8013c38e6193580bd858e751a1a6d3d67
Michael Zingale <michael.zingale@stonybrook.edu>
2018-02-11 17:16:14 -0500

    script to run and assess convergence

---
+++ Exec/hydro_tests/acoustic_pulse/convergence.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.64 castro.fourth_order=1 castro.mol_order=4
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.128 castro.fourth_order=1 castro.mol_order=4
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.256 castro.fourth_order=1 castro.mol_order=4
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.512 castro.fourth_order=1 castro.mol_order=4
+
+RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_64_plt00081 mediFile=acoustic_pulse_128_plt00161 fineFile=acoustic_pulse_256_plt00321 mediError=med.out coarError=coar.out
+
