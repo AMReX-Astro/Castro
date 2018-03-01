6eeb49e9ba8ecf1c6d5695fd36a1ebf726ac32a2
Michael Zingale <michael.zingale@stonybrook.edu>
2018-03-01 13:54:40 -0500

    nice default plot for this sim

---
+++ Exec/science/convective_flame/slice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import yt
+
+plotfile = sys.argv[1]
+
+# slice plot of temperature
+ds = yt.load(plotfile)
+sp = yt.SlicePlot(ds, "theta", "Temp")
+sp.set_zlim("Temp", 1.e-3, 100)
+
+# now do a contour of density
+sp.annotate_contour("density", ncont=2, clim=(0.01, 1.0), 
+                    plot_args={"colors": "red", "linewidths": 2})
+
+sp.save("slice.png")
