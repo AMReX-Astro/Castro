b509778d871efd7827c85a3dbf8b460070c0d92c
Michael Zingale <michael.zingale@stonybrook.edu>
2018-03-21 15:28:51 -0400

    add more testing scripts

---
+++ Exec/hydro_tests/acoustic_pulse/convergence_sdc4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.64 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.128 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.256 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.512 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
+
+RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_128_plt00161 mediFile=acoustic_pulse_256_plt00321 fineFile=acoustic_pulse_512_plt00641 mediError=med.out coarError=coar.out
+
