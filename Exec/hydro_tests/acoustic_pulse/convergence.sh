dba74c55e1fd01bb497f1d7647f6acf333f9015c
Michael Zingale <michael.zingale@stonybrook.edu>
2018-04-25 14:11:36 -0400

    redirect out

--- Exec/hydro_tests/acoustic_pulse/convergence.sh
+++ Exec/hydro_tests/acoustic_pulse/convergence.sh
@@ -5,5 +5,6 @@ mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.128 castro.fourth_order=1 castro.mo
 mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.256 castro.fourth_order=1 castro.mol_order=4 castro.time_integration_method=1
 mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.512 castro.fourth_order=1 castro.mol_order=4 castro.time_integration_method=1
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_128_plt00161 mediFile=acoustic_pulse_256_plt00321 fineFile=acoustic_pulse_512_plt00641 mediError=med.out coarError=coar.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_128_plt00161 mediFile=acoustic_pulse_256_plt00321 fineFile=acoustic_pulse_512_plt00641 mediError=med.out coarError=coar.out > convergence.out
+
 
