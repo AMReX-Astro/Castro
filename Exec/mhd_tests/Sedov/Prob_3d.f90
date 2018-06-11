45f20dcf4fd01e6f8b9658ded2cdb87830e083f3
guadabsb15 <guadabsb15@gmail.com>
2018-06-11 15:37:40 -0400

    more fix B initialization

--- Exec/mhd_tests/Sedov/Prob_3d.f90
+++ Exec/mhd_tests/Sedov/Prob_3d.f90
@@ -257,12 +257,26 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)
            do i = lo(1), hi(1)+1
                  mag_x(i,j,k,1) = B_x
-                 mag_y(i,j,k,1) = B_y
-                 mag_z(i,j,k,1) = B_z
            enddo
         enddo
      enddo
 
+   do k = lo(3), hi(3)
+        do j = lo(2), hi(2)+1
+           do i = lo(1), hi(1)
+              mag_y(i,j,k,1) = B_y
+           enddo
+        enddo
+     enddo
+
+    do k = lo(3), hi(3)+1
+        do j = lo(2), hi(2)
+           do i = lo(1), hi(1)
+              mag_z(i,j,k,1) = B_z
+           enddo
+        enddo
+    enddo
+   
 
   end subroutine ca_initmag
 
