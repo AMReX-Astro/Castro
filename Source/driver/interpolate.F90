d05a39bf8877081941c32918aae19bb2c86a9673
dwillcox <eugene.willcox@gmail.com>
2018-06-13 17:28:02 -0400

    Add device subroutine versions of interpolate and locate.

--- Source/driver/interpolate.F90
+++ Source/driver/interpolate.F90
@@ -5,10 +5,10 @@ module interpolate_module
 
   contains
 
-      function interpolate(r, npts_model, model_r, model_var, iloc)
+    function interpolate(r, npts_model, model_r, model_var, iloc)
       
 !     given the array of model coordinates (model_r), and variable (model_var),
-!     find the value of model_var at point r using linear interpolation.
+!     find the value of model_var at point r (var_r) using linear interpolation.
 !     Eventually, we can do something fancier here.
       
       use amrex_fort_module, only : rt => amrex_real
@@ -80,6 +80,84 @@ module interpolate_module
       endif
 
     end function interpolate
+
+
+    AMREX_DEVICE subroutine interpolate_sub(r, npts_model, model_r, model_var, iloc, var_r)
+      
+!     given the array of model coordinates (model_r), and variable (model_var),
+!     find the value of model_var at point r (var_r) using linear interpolation.
+!     Eventually, we can do something fancier here.
+      
+      use amrex_fort_module, only : rt => amrex_real
+      real(rt)        , intent(in   ) :: r
+      integer         , intent(in   ) :: npts_model
+      real(rt)        , intent(in   ) :: model_r(npts_model), model_var(npts_model)
+      integer, intent(in), optional   :: iloc
+      real(rt)                        :: interpolate
+
+      ! Local variables
+      integer                         :: id
+      real(rt)                        :: slope,minvar,maxvar
+      
+!     find the location in the coordinate array where we want to interpolate
+      if (present(iloc)) then
+         id = iloc
+      else
+         call locate_sub(r, npts_model, model_r, id)
+      end if
+      
+      if (id .eq. 1) then
+
+         slope = (model_var(id+1) - model_var(id))/(model_r(id+1) - model_r(id))
+         interpolate = slope*(r - model_r(id)) + model_var(id)
+
+         ! safety check to make sure interpolate lies within the bounding points
+         minvar = min(model_var(id+1),model_var(id))
+         maxvar = max(model_var(id+1),model_var(id))
+         interpolate = max(interpolate,minvar)
+         interpolate = min(interpolate,maxvar)
+         
+      else if (id .eq. npts_model) then
+
+         slope = (model_var(id) - model_var(id-1))/(model_r(id) - model_r(id-1))
+         interpolate = slope*(r - model_r(id)) + model_var(id)
+
+         ! safety check to make sure interpolate lies within the bounding points
+         minvar = min(model_var(id),model_var(id-1))
+         maxvar = max(model_var(id),model_var(id-1))
+         interpolate = max(interpolate,minvar)
+         interpolate = min(interpolate,maxvar)
+
+      else
+
+         if (r .ge. model_r(id)) then
+
+            slope = (model_var(id+1) - model_var(id))/(model_r(id+1) - model_r(id))
+            interpolate = slope*(r - model_r(id)) + model_var(id)
+
+            ! ! safety check to make sure interpolate lies within the bounding points
+            ! minvar = min(model_var(id+1),model_var(id))
+            ! maxvar = max(model_var(id+1),model_var(id))
+            ! interpolate = max(interpolate,minvar)
+            ! interpolate = min(interpolate,maxvar)
+
+         else
+
+            slope = (model_var(id) - model_var(id-1))/(model_r(id) - model_r(id-1))
+            interpolate = slope*(r - model_r(id)) + model_var(id)
+
+            ! ! safety check to make sure interpolate lies within the bounding points
+            ! minvar = min(model_var(id),model_var(id-1))
+            ! maxvar = max(model_var(id),model_var(id-1))
+            ! interpolate = max(interpolate,minvar)
+            ! interpolate = min(interpolate,maxvar)
+
+          end if
+
+      endif
+
+    end subroutine interpolate_sub
+
 
     subroutine tri_interpolate(x, y, z, npts_x, npts_y, npts_z, &
                                model_x, model_y, model_z, model_var, &
@@ -192,4 +270,37 @@ module interpolate_module
 
     end function locate
 
+
+    AMREX_DEVICE subroutine locate_sub(x, n, xs, loc)
+      use amrex_fort_module, only : rt => amrex_real
+      integer, intent(in) :: n
+      real(rt)        , intent(in) :: x, xs(n)
+      integer :: loc
+
+      integer :: ilo, ihi, imid
+
+      if (x .le. xs(1)) then
+         loc = 1
+      else if (x .gt. xs(n-1)) then
+         loc = n
+      else
+
+         ilo = 1
+         ihi = n-1
+
+         do while (ilo+1 .ne. ihi)
+            imid = (ilo+ihi)/2
+            if (x .le. xs(imid)) then
+               ihi = imid
+            else 
+               ilo = imid
+            end if
+         end do
+         
+         loc = ihi
+
+      end if
+
+    end subroutine locate_sub
+    
 end module interpolate_module
