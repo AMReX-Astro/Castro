c9c21deb3a39698fcbcb63fe035625f88b82755a
Max Katz <maxpkatz@gmail.com>
2018-06-17 10:38:10 -0700

    Fix macro

--- Source/driver/Castro.cpp
+++ Source/driver/Castro.cpp
@@ -2593,7 +2593,7 @@ Castro::normalize_species (MultiFab& S_new)
        const Box& bx = mfi.growntilebox(ng);
 
 #pragma gpu
-       ca_normalize_species(AMREX_ARLIM_ARG(bx.loVect()), AMREX_ARLIM_3D(bx.hiVect()), 
+       ca_normalize_species(AMREX_ARLIM_ARG(bx.loVect()), AMREX_ARLIM_ARG(bx.hiVect()), 
                                      BL_TO_FORTRAN_3D(S_new[mfi]));
     }
 }
