4d80abd2fb95708275a79fe692afad55d5da917d
Max Katz <maxpkatz@gmail.com>
2018-06-16 23:15:49 -0400

    Move more variables to the device

--- Source/driver/meth_params.F90
+++ Source/driver/meth_params.F90
@@ -26,18 +26,18 @@ module meth_params_module
   integer, allocatable, save :: USHK
 
   ! primitive variables
-  integer, save :: QVAR
-  integer, save :: QRHO, QU, QV, QW, QPRES, QREINT, QTEMP, QGAME
-  integer, save :: NQAUX, QGAMC, QC, QDPDR, QDPDE
+  integer, allocatable, save :: QVAR
+  integer, allocatable, save :: QRHO, QU, QV, QW, QPRES, QREINT, QTEMP, QGAME
+  integer, allocatable, save :: NQAUX, QGAMC, QC, QDPDR, QDPDE
 #ifdef RADIATION
-  integer, save :: QGAMCG, QCG, QLAMS
+  integer, allocatable, save :: QGAMCG, QCG, QLAMS
 #endif
-  integer, save :: QFA, QFS, QFX
+  integer, allocatable, save :: QFA, QFS, QFX
 
-  integer, save :: nadv
+  integer, allocatable, save :: nadv
 
   ! NQ will be the total number of primitive variables, hydro + radiation
-  integer, save :: NQ         
+  integer, allocatable, save :: NQ         
 
 #ifdef RADIATION
   integer, save :: QRAD, QRADHI, QPTOT, QREITOT
@@ -48,15 +48,15 @@ module meth_params_module
   real(rt)        , save :: flatten_pp_threshold = -1.e0_rt
 #endif
 
-  integer, save :: npassive
+  integer, save, allocatable :: npassive
   integer, save, allocatable :: qpass_map(:), upass_map(:)
 
   ! These are used for the Godunov state
   ! Note that the velocity indices here are picked to be the same value
   ! as in the primitive variable array
-  integer, save :: NGDNV, GDRHO, GDU, GDV, GDW, GDPRES, GDGAME
+  integer, save, allocatable :: NGDNV, GDRHO, GDU, GDV, GDW, GDPRES, GDGAME
 #ifdef RADIATION
-  integer, save :: GDLAMS, GDERADS
+  integer, save, allocatable :: GDLAMS, GDERADS
 #endif
 
   integer         , save :: numpts_1d
@@ -75,7 +75,7 @@ module meth_params_module
   integer, parameter :: EXT_HSE = 1
   integer, parameter :: EXT_INTERP = 2 
   
-  integer, save :: xl_ext, yl_ext, zl_ext, xr_ext, yr_ext, zr_ext
+  integer, allocatable, save :: xl_ext, yl_ext, zl_ext, xr_ext, yr_ext, zr_ext
 
   ! Create versions of these variables on the GPU
   ! the device update is then done in Castro_nd.f90
@@ -84,6 +84,22 @@ module meth_params_module
   attributes(managed) :: NVAR
   attributes(managed) :: URHO, UMX, UMY, UMZ, UMR, UML, UMP, UEDEN, UEINT, UTEMP, UFA, UFS, UFX
   attributes(managed) :: USHK
+  attributes(managed) :: QVAR
+  attributes(managed) :: QRHO, QU, QV, QW, QPRES, QREINT, QTEMP, QGAME
+  attributes(managed) :: NQAUX, QGAMC, QC, QDPDR, QDPDE
+#ifdef RADIATION
+  attributes(managed) :: QGAMCG, QCG, QLAMS
+#endif
+  attributes(managed) :: QFA, QFS, QFX
+  attributes(managed) :: nadv
+  attributes(managed) :: NQ
+  attributes(managed) :: npassive
+  attributes(managed) :: qpass_map, upass_map
+  attributes(managed) :: NGDNV, GDRHO, GDU, GDV, GDW, GDPRES, GDGAME
+#ifdef RADIATION
+  attributes(managed) :: GDLAMS, GDERADS
+#endif
+  attributes(managed) :: xl_ext, yl_ext, zl_ext, xr_ext, yr_ext, zr_ext
 #endif
 
   !$acc declare &
@@ -316,6 +332,21 @@ contains
     allocate(NVAR)
     allocate(URHO, UMX, UMY, UMZ, UMR, UML, UMP, UEDEN, UEINT, UTEMP, UFA, UFS, UFX)
     allocate(USHK)
+    allocate(QVAR)
+    allocate(QRHO, QU, QV, QW, QPRES, QREINT, QTEMP, QGAME)
+    allocate(NQAUX, QGAMC, QC, QDPDR, QDPDE)
+#ifdef RADIATION
+    allocate(QGAMCG, QCG, QLAMS)
+#endif
+    allocate(QFA, QFS, QFX)
+    allocate(nadv)
+    allocate(NQ)
+    allocate(npassive)
+    allocate(NGDNV, GDRHO, GDU, GDV, GDW, GDPRES, GDGAME)
+#ifdef RADIATION
+    allocate(GDLAMS, GDERADS)
+#endif
+    allocate(xl_ext, yl_ext, zl_ext, xr_ext, yr_ext, zr_ext)
 
     allocate(const_grav)
     const_grav = 0.0d0;
@@ -333,34 +364,6 @@ contains
     diffuse_cutoff_density = -1.d200;
     allocate(diffuse_cond_scale_fac)
     diffuse_cond_scale_fac = 1.0d0;
-#endif
-#ifdef ROTATION
-    allocate(rot_period)
-    rot_period = -1.d200;
-    allocate(rot_period_dot)
-    rot_period_dot = 0.0d0;
-    allocate(rotation_include_centrifugal)
-    rotation_include_centrifugal = 1;
-    allocate(rotation_include_coriolis)
-    rotation_include_coriolis = 1;
-    allocate(rotation_include_domegadt)
-    rotation_include_domegadt = 1;
-    allocate(state_in_rotating_frame)
-    state_in_rotating_frame = 1;
-    allocate(rot_source_type)
-    rot_source_type = 4;
-    allocate(implicit_rotation_update)
-    implicit_rotation_update = 1;
-    allocate(rot_axis)
-    rot_axis = 3;
-#endif
-#ifdef POINTMASS
-    allocate(use_point_mass)
-    use_point_mass = 1;
-    allocate(point_mass)
-    point_mass = 0.0d0;
-    allocate(point_mass_fix_solution)
-    point_mass_fix_solution = 0;
 #endif
     allocate(difmag)
     difmag = 0.1d0;
@@ -492,27 +495,39 @@ contains
     grown_factor = 1;
     allocate(track_grid_losses)
     track_grid_losses = 0;
+#ifdef POINTMASS
+    allocate(use_point_mass)
+    use_point_mass = 1;
+    allocate(point_mass)
+    point_mass = 0.0d0;
+    allocate(point_mass_fix_solution)
+    point_mass_fix_solution = 0;
+#endif
+#ifdef ROTATION
+    allocate(rot_period)
+    rot_period = -1.d200;
+    allocate(rot_period_dot)
+    rot_period_dot = 0.0d0;
+    allocate(rotation_include_centrifugal)
+    rotation_include_centrifugal = 1;
+    allocate(rotation_include_coriolis)
+    rotation_include_coriolis = 1;
+    allocate(rotation_include_domegadt)
+    rotation_include_domegadt = 1;
+    allocate(state_in_rotating_frame)
+    state_in_rotating_frame = 1;
+    allocate(rot_source_type)
+    rot_source_type = 4;
+    allocate(implicit_rotation_update)
+    implicit_rotation_update = 1;
+    allocate(rot_axis)
+    rot_axis = 3;
+#endif
 
     call amrex_parmparse_build(pp, "castro")
 #ifdef DIFFUSION
     call pp%query("diffuse_cutoff_density", diffuse_cutoff_density)
     call pp%query("diffuse_cond_scale_fac", diffuse_cond_scale_fac)
-#endif
-#ifdef ROTATION
-    call pp%query("rotational_period", rot_period)
-    call pp%query("rotational_dPdt", rot_period_dot)
-    call pp%query("rotation_include_centrifugal", rotation_include_centrifugal)
-    call pp%query("rotation_include_coriolis", rotation_include_coriolis)
-    call pp%query("rotation_include_domegadt", rotation_include_domegadt)
-    call pp%query("state_in_rotating_frame", state_in_rotating_frame)
-    call pp%query("rot_source_type", rot_source_type)
-    call pp%query("implicit_rotation_update", implicit_rotation_update)
-    call pp%query("rot_axis", rot_axis)
-#endif
-#ifdef POINTMASS
-    call pp%query("use_point_mass", use_point_mass)
-    call pp%query("point_mass", point_mass)
-    call pp%query("point_mass_fix_solution", point_mass_fix_solution)
 #endif
     call pp%query("difmag", difmag)
     call pp%query("small_dens", small_dens)
@@ -579,6 +594,22 @@ contains
     call pp%query("do_acc", do_acc)
     call pp%query("grown_factor", grown_factor)
     call pp%query("track_grid_losses", track_grid_losses)
+#ifdef POINTMASS
+    call pp%query("use_point_mass", use_point_mass)
+    call pp%query("point_mass", point_mass)
+    call pp%query("point_mass_fix_solution", point_mass_fix_solution)
+#endif
+#ifdef ROTATION
+    call pp%query("rotational_period", rot_period)
+    call pp%query("rotational_dPdt", rot_period_dot)
+    call pp%query("rotation_include_centrifugal", rotation_include_centrifugal)
+    call pp%query("rotation_include_coriolis", rotation_include_coriolis)
+    call pp%query("rotation_include_domegadt", rotation_include_domegadt)
+    call pp%query("state_in_rotating_frame", state_in_rotating_frame)
+    call pp%query("rot_source_type", rot_source_type)
+    call pp%query("implicit_rotation_update", implicit_rotation_update)
+    call pp%query("rot_axis", rot_axis)
+#endif
     call amrex_parmparse_destroy(pp)
 
 
@@ -678,6 +709,21 @@ contains
     deallocate(NVAR)
     deallocate(URHO, UMX, UMY, UMZ, UMR, UML, UMP, UEDEN, UEINT, UTEMP, UFA, UFS, UFX)
     deallocate(USHK)
+    deallocate(QVAR)
+    deallocate(QRHO, QU, QV, QW, QPRES, QREINT, QTEMP, QGAME)
+    deallocate(NQAUX, QGAMC, QC, QDPDR, QDPDE)
+#ifdef RADIATION
+    deallocate(QGAMCG, QCG, QLAMS)
+#endif
+    deallocate(QFA, QFS, QFX)
+    deallocate(nadv)
+    deallocate(NQ)
+    deallocate(npassive)
+    deallocate(NGDNV, GDRHO, GDU, GDV, GDW, GDPRES, GDGAME)
+#ifdef RADIATION
+    deallocate(GDLAMS, GDERADS)
+    deallocate(xl_ext, yl_ext, zl_ext, xr_ext, yr_ext, zr_ext)
+#endif
 
     if (allocated(difmag)) then
         deallocate(difmag)
