305a911942677afb6ce1086fdfcf913b96588090
Max Katz <maxpkatz@gmail.com>
2018-06-21 06:46:08 -0700

    Add the sources_for_hydro term on the GPU

--- Source/hydro/Castro_hydro.cpp
+++ Source/hydro/Castro_hydro.cpp
@@ -545,6 +545,7 @@ Castro::construct_mol_hydro_source(Real time, Real dt)
            BL_TO_FORTRAN_ANYD(area[1][mfi]),
            BL_TO_FORTRAN_ANYD(area[2][mfi]),
            BL_TO_FORTRAN_ANYD(volume[mfi]),
+           BL_TO_FORTRAN_ANYD(sources_for_hydro[mfi]),
            BL_TO_FORTRAN_ANYD(k_stage[mfi]));
 
   } // MFIter loop
