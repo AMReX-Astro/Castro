e8305a233748481d6aa2bc7f08a30962b4ac1569
dwillcox <eugene.willcox@gmail.com>
2018-06-21 22:05:00 -0400

    Update ca_react_state for GPU.

--- Source/reactions/Castro_react.cpp
+++ Source/reactions/Castro_react.cpp
@@ -255,12 +255,13 @@ Castro::react_state(MultiFab& s, MultiFab& r, const iMultiFab& mask, MultiFab& w
 	const Box& bx = mfi.growntilebox(ngrow);
 
 	// Note that box is *not* necessarily just the valid region!
+#pragma gpu
 	ca_react_state(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
-		       BL_TO_FORTRAN_3D(s[mfi]),
-		       BL_TO_FORTRAN_3D(r[mfi]),
-		       BL_TO_FORTRAN_3D(w[mfi]),
-		       BL_TO_FORTRAN_3D(mask[mfi]),
-		       time, dt_react, strang_half);
+				   BL_TO_FORTRAN_3D(s[mfi]),
+				   BL_TO_FORTRAN_3D(r[mfi]),
+				   BL_TO_FORTRAN_3D(w[mfi]),
+				   BL_TO_FORTRAN_3D(mask[mfi]),
+				   &time, &dt_react, &strang_half);
 
     }
 
