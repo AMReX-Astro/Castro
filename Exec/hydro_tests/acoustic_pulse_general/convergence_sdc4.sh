d15bcc7e12221fedfcce646bcc600f580e9e9dd1
Michael Zingale <michael.zingale@stonybrook.edu>
2018-07-09 20:44:11 -0400

    fix file #s

--- Exec/hydro_tests/acoustic_pulse_general/convergence_sdc4.sh
+++ Exec/hydro_tests/acoustic_pulse_general/convergence_sdc4.sh
@@ -4,9 +4,9 @@ mpiexec -n 16 ./Castro2d.gnu.MPI.ex inputs.64 castro.sdc_order=4 castro.time_int
 mpiexec -n 16 ./Castro2d.gnu.MPI.ex inputs.128 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
 mpiexec -n 16 ./Castro2d.gnu.MPI.ex inputs.256 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_64_plt00201 mediFile=acoustic_pulse_128_plt00401 fineFile=acoustic_pulse_256_plt00801 mediError=med.out coarError=coar.out > convergence_sdc4.1.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_64_plt00101 mediFile=acoustic_pulse_128_plt00201 fineFile=acoustic_pulse_256_plt00401 mediError=med.out coarError=coar.out > convergence_sdc4.1.out
 
 mpiexec -n 16 ./Castro2d.gnu.MPI.ex inputs.512 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_128_plt00401 mediFile=acoustic_pulse_256_plt00801 fineFile=acoustic_pulse_512_plt01601 mediError=med.out coarError=coar.out > convergence_sdc4.2.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_128_plt00201 mediFile=acoustic_pulse_256_plt00401 fineFile=acoustic_pulse_512_plt00801 mediError=med.out coarError=coar.out > convergence_sdc4.2.out
 
