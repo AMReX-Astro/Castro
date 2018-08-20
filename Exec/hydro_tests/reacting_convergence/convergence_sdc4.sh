eb1a7c3d283bd8e1197e957ea14394b85baf0206
Michael Zingale <michael.zingale@stonybrook.edu>
2018-08-20 12:52:50 -0400

    slightly longer still + get the sdc scripts in sync

--- Exec/hydro_tests/reacting_convergence/convergence_sdc4.sh
+++ Exec/hydro_tests/reacting_convergence/convergence_sdc4.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.64 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.dual_energy_update_E_from_e=0 castro.use_reconstructed_gamma1=1
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.128 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.dual_energy_update_E_from_e=0 castro.use_reconstructed_gamma1=1
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.256 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.dual_energy_update_E_from_e=0 castro.use_reconstructed_gamma1=1
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.64 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.use_reconstructed_gamma1=1
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.128 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.use_reconstructed_gamma1=1
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.256 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.use_reconstructed_gamma1=1
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_64_plt00101 mediFile=acoustic_pulse_128_plt00201 fineFile=acoustic_pulse_256_plt00401 mediError=med.out coarError=coar.out > convergence_sdc4.1.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=react_converge_64_plt00301 mediFile=react_converge_128_plt00601 fineFile=react_converge_256_plt01201 mediError=med.out coarError=coar.out > convergence.1.sdc4.out
 
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.512 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.dual_energy_update_E_from_e=0 castro.use_reconstructed_gamma1=1
+mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.512 castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 castro.limit_fourth_order=1 castro.use_reconstructed_gamma1=1
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_128_plt00201 mediFile=acoustic_pulse_256_plt00401 fineFile=acoustic_pulse_512_plt00801 mediError=med.out coarError=coar.out > convergence_sdc4.2.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=react_converge_128_plt00601 mediFile=react_converge_256_plt01201 fineFile=react_converge_512_plt02401 mediError=med.out coarError=coar.out > convergence.2.sdc2.out
 
 
 
--- Exec/hydro_tests/reacting_convergence/convergence_strang.sh
+++ Exec/hydro_tests/reacting_convergence/convergence_strang.sh
@@ -5,7 +5,7 @@ mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.128 castro.time_integration_method=0
 mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.256 castro.time_integration_method=0
 mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.512 castro.time_integration_method=0
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=react_converge_64_plt00201 mediFile=react_converge_128_plt00401 fineFile=react_converge_256_plt00801 mediError=med.out coarError=coar.out > convergence.1.out
-RichardsonConvergenceTest2d.gnu.ex coarFile=react_converge_128_plt00401 mediFile=react_converge_256_plt00801 fineFile=react_converge_512_plt01601 mediError=med.out coarError=coar.out > convergence.2.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=react_converge_64_plt00301 mediFile=react_converge_128_plt00601 fineFile=react_converge_256_plt01201 mediError=med.out coarError=coar.out > convergence.1.strang.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=react_converge_128_plt00601 mediFile=react_converge_256_plt01201 fineFile=react_converge_512_plt02401 mediError=med.out coarError=coar.out > convergence.2.strang.out
 
 
