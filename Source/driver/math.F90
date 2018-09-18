8e7840d6da2bdd88e01fb501cf3ca44fd85387f7
Alice Harpole <ah1e14@soton.ac.uk>
2018-09-18 11:24:56 -0400

    math.f90 -> math.F90

--- Source/driver/math.F90
+++ Source/driver/math.F90
@@ -14,6 +14,8 @@ contains
     real(rt)         :: A(3), B(3)
     real(rt)         :: C(3)
 
+    !$gpu
+
     C(1) = A(2)*B(3) - A(3)*B(2)
     C(2) = A(3)*B(1) - A(1)*B(3)
     C(3) = A(1)*B(2) - A(2)*B(1)
