4d89b24946783bbaa86ebc78a46b548a5278f88d
Max Katz <maxpkatz@gmail.com>
2018-09-19 11:55:17 -0400

    Move amrinfo, etc. to the GPU

--- Source/driver/Castro.cpp
+++ Source/driver/Castro.cpp
@@ -211,6 +211,7 @@ Castro::variableCleanUp ()
 #ifdef SPONGE
     sponge_finalize();
 #endif
+    amrinfo_finalize();
 }
 
 void
@@ -347,13 +348,13 @@ Castro::read_params ()
     if (!do_ctu && use_retry)
         amrex::Error("Method of lines integration is incompatible with the timestep retry mechanism.");
 
-#ifdef AMREX_USE_CUDA     
+#ifdef AMREX_USE_CUDA
     // not use ctu if using gpu
     if (do_ctu == 1)
       {
 	 amrex::Error("Running with CUDA requires do_ctu = 0");
       }
-#endif    
+#endif
 
     // fourth order implies do_ctu=0
     if (fourth_order == 1 && do_ctu == 1)
@@ -924,6 +925,7 @@ Castro::initData ()
          AmrLevel::FillPatch(*this, Sborder, NUM_GROW, cur_time, State_Type, 0, NUM_STATE);
 
          // note: this cannot be tiled
+
          for (MFIter mfi(S_new); mfi.isValid(); ++mfi)
            {
              const Box& box     = mfi.validbox();
@@ -2995,6 +2997,18 @@ Castro::derive (const std::string& name,
 #endif
 
     AmrLevel::derive(name,time,mf,dcomp);
+}
+
+void
+Castro::amrinfo_init ()
+{
+   ca_amrinfo_init();
+}
+
+void
+Castro::amrinfo_finalize()
+{
+   ca_amrinfo_finalize();
 }
 
 void
