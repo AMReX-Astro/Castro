2dec37c1aa4f09885c14d64d67f6c8ec188e16d4
Michael Zingale <michael.zingale@stonybrook.edu>
2018-11-17 21:52:52 -0500

    make pslope also gpu friendly

--- Source/hydro/slope_nd.F90
+++ Source/hydro/slope_nd.F90
@@ -248,12 +248,8 @@ contains
     real(rt) :: dm, dp, dc, dl, dfm, dfp
 
     !     Local arrays
-    real(rt), pointer :: dsgn(:,:,:), dlim(:,:,:), df(:,:,:), dcen(:,:,:)
-
-    call bl_allocate(dsgn, lo-2*dg, hi+2*dg)
-    call bl_allocate(dlim, lo-2*dg, hi+2*dg)
-    call bl_allocate(  df, lo-2*dg, hi+2*dg)
-    call bl_allocate(dcen, lo-2*dg, hi+2*dg)
+    real(rt) :: dsgn, dlim, dcen
+    real(rt) :: dfp1, dfm1, dlftp1, drgtp1, dlftm1, drgtm1
 
     if (plm_iorder == 1) then
 
@@ -275,34 +271,51 @@ contains
        ! Compute slopes in first coordinate direction
        do k = lo(3)-dg(3), hi(3)+dg(3)
           do j = lo(2)-dg(2), hi(2)+dg(2)
+             do i = lo(1)-1, hi(1)+1
 
-             ! First compute Fromm slopes
-             do i = lo(1)-2, hi(1)+2
+                ! First compute Fromm slopes
 
-                dlft = q(i  ,j,k,QPRES) - q(i-1,j,k,QPRES)
-                drgt = q(i+1,j,k,QPRES) - q(i  ,j,k,QPRES)
+                ! df at i+1
+                dlftp1 = q(i+1,j,k,QPRES) - q(i,j,k,QPRES)
+                drgtp1 = q(i+2,j,k,QPRES) - q(i+1,j,k,QPRES)
 
                 ! Subtract off (rho * acceleration) so as not to limit that part of the slope
-                dlft = dlft - FOURTH * &
-                     (q(i,j,k,QRHO) + q(i-1,j,k,QRHO))*(src(i,j,k,QU)+src(i-1,j,k,QU))*dx(1)
-                drgt = drgt - FOURTH * &
-                     (q(i,j,k,QRHO) + q(i+1,j,k,QRHO))*(src(i,j,k,QU)+src(i+1,j,k,QU))*dx(1)
+                dlftp1 = dlftp1 - FOURTH * &
+                     (q(i+1,j,k,QRHO) + q(i,j,k,QRHO)) * (src(i+1,j,k,QU)+src(i,j,k,QU))*dx(1)
+                drgtp1 = drgtp1 - FOURTH * &
+                     (q(i+1,j,k,QRHO) + q(i+2,j,k,QRHO)) * (src(i+1,j,k,QU)+src(i+2,j,k,QU))*dx(1)
 
-                dcen(i,j,k) = HALF*(dlft+drgt)
-                dsgn(i,j,k) = sign(ONE, dcen(i,j,k))
-                if (dlft*drgt .ge. ZERO) then
-                   dlim(i,j,k) = TWO * min( abs(dlft), abs(drgt) )
-                else
-                   dlim(i,j,k) = ZERO
-                endif
-                df(i,j,k) = dsgn(i,j,k)*min( dlim(i,j,k), abs(dcen(i,j,k)) )
-             end do
+                dcen = HALF*(dlftp1 + drgtp1)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlftp1), abs(drgtp1)), ZERO, dlftp1*drgtp1 >= ZERO)
+                dfp1 = dsgn*min(dlim, abs(dcen))
 
-             ! Now limited fourth order slopes
-             do i = lo(1)-1, hi(1)+1
-                dp1         = FOUR3RD*dcen(i,j,k) - SIXTH*(df(i+1,j,k) + df(i-1,j,k))
-                dqx(i,j,k,QPRES) = flatn(i,j,k)*dsgn(i,j,k)*min(dlim(i,j,k),abs(dp1))
+                ! df at i-1
+                dlftm1 = q(i-1,j,k,QPRES) - q(i-2,j,k,QPRES)
+                drgtm1 = q(i,j,k,QPRES) - q(i-1,j,k,QPRES)
+
+                ! Subtract off (rho * acceleration) so as not to limit that part of the slope
+                dlftm1 = dlftm1 - FOURTH * &
+                     (q(i-1,j,k,QRHO) + q(i-2,j,k,QRHO)) * (src(i-1,j,k,QU)+src(i-2,j,k,QU))*dx(1)
+                drgtm1 = drgtm1 - FOURTH * &
+                     (q(i-1,j,k,QRHO) + q(i,j,k,QRHO)) * (src(i-1,j,k,QU)+src(i,j,k,QU))*dx(1)
+
+                dcen = HALF*(dlftm1 + drgtm1)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlftm1), abs(drgtm1)), ZERO, dlftm1*drgtm1 >= ZERO)
+                dfm1 = dsgn*min(dlim, abs(dcen))
+
+                ! Now limited fourth order slopes at i
+                dlft = drgtm1
+                drgt = dlftp1
+                dcen = HALF*(dlft + drgt)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlft), abs(drgt)), ZERO, dlft*drgt >= ZERO)
+
+                dp1 = FOUR3RD*dcen - SIXTH*(dfp1 + dfm1)
+                dqx(i,j,k,QPRES) = flatn(i,j,k)*dsgn*min(dlim, abs(dp1))
                 dqx(i,j,k,QPRES) = dqx(i,j,k,QPRES) + q(i,j,k,QRHO)*src(i,j,k,QU)*dx(1)
+
              end do
           end do
        end do
@@ -310,38 +323,51 @@ contains
 #if AMREX_SPACEDIM >= 2
        ! Compute slopes in second coordinate direction
        do k = lo(3)-dg(3), hi(3)+dg(3)
-          do j = lo(2)-2, hi(2)+2
-             do i = lo(1)-1, hi(1)+1
-
-                ! First compute Fromm slopes
-                dlft = q(i,j  ,k,QPRES) - q(i,j-1,k,QPRES)
-                drgt = q(i,j+1,k,QPRES) - q(i,j  ,k,QPRES)
-
-                ! Subtract off (rho * acceleration) so as not to limit that part of the slope
-                dlft = dlft - FOURTH * &
-                     (q(i,j,k,QRHO) + q(i,j-1,k,QRHO))*(src(i,j,k,QV)+src(i,j-1,k,QV))*dx(2)
-                drgt = drgt - FOURTH * &
-                     (q(i,j,k,QRHO) + q(i,j+1,k,QRHO))*(src(i,j,k,QV)+src(i,j+1,k,QV))*dx(2)
-
-                dcen(i,j,k) = HALF*(dlft+drgt)
-                dsgn(i,j,k) = sign( ONE, dcen(i,j,k) )
-                if (dlft*drgt .ge. ZERO) then
-                   dlim(i,j,k) = TWO * min( abs(dlft), abs(drgt) )
-                else
-                   dlim(i,j,k) = ZERO
-                endif
-                df(i,j,k) = dsgn(i,j,k)*min( dlim(i,j,k), abs(dcen(i,j,k)) )
-             end do
-          end do
-       end do
-
-       do k = lo(3)-dg(3), hi(3)+dg(3)
           do j = lo(2)-1, hi(2)+1
              do i = lo(1)-1, hi(1)+1
 
-                ! Now limited fourth order slopes
-                dp1 = FOUR3RD*dcen(i,j,k) - SIXTH*( df(i,j+1,k) + df(i,j-1,k) )
-                dqy(i,j,k,QPRES) = flatn(i,j,k)*dsgn(i,j,k)*min(dlim(i,j,k),abs(dp1))
+                ! First compute Fromm slopes
+
+                ! dt at j+1
+                dlftp1 = q(i,j+1,k,QPRES) - q(i,j,k,QPRES)
+                drgtp1 = q(i,j+2,k,QPRES) - q(i,j+1,k,QPRES)
+
+                ! Subtract off (rho * acceleration) so as not to limit that part of the slope
+                dlftp1 = dlftp1 - FOURTH * &
+                     (q(i,j+1,k,QRHO) + q(i,j,k,QRHO)) * (src(i,j+1,k,QV)+src(i,j,k,QV))*dx(2)
+                drgtp1 = drgtp1 - FOURTH * &
+                     (q(i,j+1,k,QRHO) + q(i,j+2,k,QRHO)) * (src(i,j+1,k,QV)+src(i,j+2,k,QV))*dx(2)
+
+                dcen = HALF*(dlftp1 + drgtp1)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlftp1), abs(drgtp1)), ZERO, dlftp1*drgtp1 >= ZERO)
+                dfp1 = dsgn*min(dlim, abs(dcen))
+
+                ! df at j-1
+                dlftm1 = q(i,j-1,k,QPRES) - q(i,j-2,k,QPRES)
+                drgtm1 = q(i,j,k,QPRES) - q(i,j-1,k,QPRES)
+
+                ! Subtract off (rho * acceleration) so as not to limit that part of the slope
+                dlftm1 = dlftm1 - FOURTH * &
+                     (q(i,j-1,k,QRHO) + q(i,j-2,k,QRHO)) * (src(i,j-1,k,QV)+src(i,j-2,k,QV))*dx(2)
+                drgtm1 = drgtm1 - FOURTH * &
+                     (q(i,j-1,k,QRHO) + q(i,j,k,QRHO)) * (src(i,j-1,k,QV)+src(i,j,k,QV))*dx(2)
+
+                dcen = HALF*(dlftm1 + drgtm1)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlftm1), abs(drgtm1)), ZERO, dlftm1*drgtm1 >= ZERO)
+                dfm1 = dsgn*min(dlim, abs(dcen))
+
+                ! Now limited fourth order slopes at j
+                dlft = drgtm1
+                drgt = dlftp1
+
+                dcen = HALF*(dlft+drgt)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlft), abs(drgt)), ZERO, dlft*drgt >= ZERO)
+
+                dp1 = FOUR3RD*dcen - SIXTH*(dfp1 + dfm1)
+                dqy(i,j,k,QPRES) = flatn(i,j,k)*dsgn*min(dlim, abs(dp1))
                 dqy(i,j,k,QPRES) = dqy(i,j,k,QPRES) + q(i,j,k,QRHO)*src(i,j,k,QV)*dx(2)
              end do
           end do
@@ -350,39 +376,52 @@ contains
 
 #if AMREX_SPACEDIM == 3
        ! Compute slopes in third coordinate direction
-       do k = lo(3)-2, hi(3)+2
-          do j = lo(2)-1, hi(2)+1
-             do i = lo(1)-1, hi(1)+1
-
-                ! First compute Fromm slopes
-                dlft = q(i,j,k  ,QPRES) - q(i,j,k-1,QPRES)
-                drgt = q(i,j,k+1,QPRES) - q(i,j,k  ,QPRES)
-
-                ! Subtract off (rho * acceleration)
-                dlft = dlft - FOURTH * &
-                     (q(i,j,k,QRHO) + q(i,j,k-1,QRHO))*(src(i,j,k,QW)+src(i,j,k-1,QW))*dx(3)
-                drgt = drgt - FOURTH * &
-                     (q(i,j,k,QRHO) + q(i,j,k+1,QRHO))*(src(i,j,k,QW)+src(i,j,k+1,QW))*dx(3)
-
-                dcen(i,j,k) = HALF*(dlft+drgt)
-                dsgn(i,j,k) = sign(ONE, dcen(i,j,k))
-                if (dlft*drgt .ge. ZERO) then
-                   dlim(i,j,k) = TWO * min( abs(dlft), abs(drgt) )
-                else
-                   dlim(i,j,k) = ZERO
-                endif
-                df(i,j,k) = dsgn(i,j,k)*min( dlim(i,j,k), abs(dcen(i,j,k)) )
-             end do
-          end do
-       end do
-
        do k = lo(3)-1, hi(3)+1
           do j = lo(2)-1, hi(2)+1
              do i = lo(1)-1, hi(1)+1
 
-                ! now limited fourth order slopes
-                dp1 = FOUR3RD*dcen(i,j,k) - SIXTH*(df(i,j,k+1) + df(i,j,k-1))
-                dqz(i,j,k,QPRES) = flatn(i,j,k)*dsgn(i,j,k)*min(dlim(i,j,k),abs(dp1))
+                ! First compute Fromm slopes
+
+                ! df at k+1
+                dlftp1 = q(i,j,k+1,QPRES) - q(i,j,k,QPRES)
+                drgtp1 = q(i,j,k+2,QPRES) - q(i,j,k+1,QPRES)
+
+                ! Subtract off (rho * acceleration)
+                dlftp1 = dlftp1 - FOURTH * &
+                     (q(i,j,k+1,QRHO) + q(i,j,k,QRHO)) * (src(i,j,k+1,QW)+src(i,j,k,QW))*dx(3)
+                drgtp1 = drgtp1 - FOURTH * &
+                     (q(i,j,k+1,QRHO) + q(i,j,k+2,QRHO)) * (src(i,j,k+1,QW)+src(i,j,k+2,QW))*dx(3)
+
+                dcen = HALF*(dlftp1 + drgtp1)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlftp1), abs(drgtp1)), ZERO, dlftp1*drgtp1 >= ZERO)
+                dfp1 = dsgn*min(dlim, abs(dcen))
+
+                ! df at k-1
+                dlftm1 = q(i,j,k-1,QPRES) - q(i,j,k-2,QPRES)
+                drgtm1 = q(i,j,k,QPRES) - q(i,j,k-1,QPRES)
+
+                ! Subtract off (rho * acceleration)
+                dlftm1 = dlftm1 - FOURTH * &
+                     (q(i,j,k-1,QRHO) + q(i,j,k-2,QRHO)) * (src(i,j,k-1,QW)+src(i,j,k-2,QW))*dx(3)
+                drgtm1 = drgtm1 - FOURTH * &
+                     (q(i,j,k-1,QRHO) + q(i,j,k,QRHO)) * (src(i,j,k-1,QW)+src(i,j,k,QW))*dx(3)
+
+                dcen = HALF*(dlftm1 + drgtm1)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min( abs(dlftm1), abs(drgtm1) ), ZERO, dlftm1*drgtm1 >= ZERO)
+                dfm1 = dsgn*min(dlim, abs(dcen))
+
+                ! now limited fourth order slopes at k
+                dlft = drgtm1
+                drgt = dlftp1
+
+                dcen = HALF*(dlft+drgt)
+                dsgn = sign(ONE, dcen)
+                dlim = merge(TWO * min(abs(dlft), abs(drgt)), ZERO, dlft*drgt >= ZERO)
+
+                dp1 = FOUR3RD*dcen - SIXTH*(dfp1 + dfm1)
+                dqz(i,j,k,QPRES) = flatn(i,j,k)*dsgn*min(dlim, abs(dp1))
                 dqz(i,j,k,QPRES) = dqz(i,j,k,QPRES) + q(i,j,k,QRHO)*src(i,j,k,QW)*dx(3)
              end do
           end do
@@ -390,11 +429,6 @@ contains
 #endif
 
     endif
-
-    call bl_deallocate(dsgn)
-    call bl_deallocate(dlim)
-    call bl_deallocate(  df)
-    call bl_deallocate(dcen)
 
   end subroutine pslope
 
