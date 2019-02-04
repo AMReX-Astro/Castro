2f442cd4f6cc387abeb0cf9b5ca3f346bacf1b67
Michael Zingale <michael.zingale@stonybrook.edu>
2019-02-04 16:08:33 -0500

    get a version of the SDC comparison for old Sod

--- Exec/hydro_tests/Sod/compare_methods.sh
+++ Exec/hydro_tests/Sod/compare_methods.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-./Castro2d.gnu.ex inputs-sod-x amr.plot_file=sod_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
-./Castro2d.gnu.ex inputs-sod-x amr.plot_file=sod_ppm_plt
+./Castro2d.gnu.ex inputs-sod-x amr.plot_file=sod_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 amr.n_cell=128 8 8 geometry.prob_hi=1 0.0625 0.0625 amr.max_level=0
+./Castro2d.gnu.ex inputs-sod-x amr.plot_file=sod_ppm_plt amr.n_cell=128 8 8 geometry.prob_hi=1 0.0625 0.0625 amr.max_level=0
 
 ./compare_sdc.py --ppm sod_ppm_plt????? --sdc sod_sdc_plt????? -o sod_sdc.png
 
-./Castro2d.gnu.ex inputs-test1-x amr.plot_file=test1_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
-./Castro2d.gnu.ex inputs-test1-helm amr.plot_file=test1_ppm_plt
 
-./compare_sdc.py --ppm test1_ppm_plt????? --sdc test1_sdc_plt????? -o test1_sdc.png
-
-./Castro2d.gnu.ex inputs-test2-x amr.plot_file=test2_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
-./Castro2d.gnu.ex inputs-test2-x amr.plot_file=test2_ppm_plt
+./Castro2d.gnu.ex inputs-test2-x amr.plot_file=test2_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1  amr.n_cell=128 8 8 geometry.prob_hi=1 0.0625 0.0625 amr.max_level=0
+./Castro2d.gnu.ex inputs-test2-x amr.plot_file=test2_ppm_plt  amr.n_cell=128 8 8 geometry.prob_hi=1 0.0625 0.0625 amr.max_level=0
 
 ./compare_sdc.py --ppm test2_ppm_plt????? --sdc test2_sdc_plt????? -o test2_sdc.png
+
+
+./Castro2d.gnu.ex inputs-test3-x amr.plot_file=test3_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1 amr.n_cell=128 8 8 geometry.prob_hi=1 0.0625 0.0625 amr.max_level=0
+./Castro2d.gnu.ex inputs-test3-x amr.plot_file=test3_ppm_plt amr.n_cell=128 8 8 geometry.prob_hi=1 0.0625 0.0625 amr.max_level=0
+
+./compare_sdc.py --ppm test3_ppm_plt????? --sdc test3_sdc_plt????? -o test3_sdc.png
+
 
 
 
