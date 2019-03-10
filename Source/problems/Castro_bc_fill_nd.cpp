de3fcbc44733f9c5de6e43d45f635b494c811d35
Max Katz <maximilian.katz@stonybrook.edu>
2019-03-10 05:01:34 -0400

    Ensure that we have enough threads per block for BCs

--- Source/problems/Castro_bc_fill_nd.cpp
+++ Source/problems/Castro_bc_fill_nd.cpp
@@ -10,6 +10,50 @@ extern "C"
 {
 #endif
 
+#ifdef AMREX_USE_CUDA
+    void set_bc_launch_config(const int* lo, const int* hi, const int* domlo, const int* domhi)
+    {
+        // Ensure that our threadblock size is such that it is
+        // evenly divisible by the number of zones in the box,
+        // and is at least one larger than the number of ghost zones.
+        // This ensures that the corners plus one interior zone
+        // are all on the same threadblock.
+
+        int left[3] = {0, 0, 0};
+        int rght[3] = {0, 0, 0};
+        int sz[3] = {0, 0, 0};
+
+        int ng[3] = {0, 0, 0};
+
+        for (int n = 0; n < AMREX_SPACEDIM; ++n) {
+            left[n] = domlo[n] - lo[n];
+            rght[n] = hi[n] - domhi[n];
+            sz[n] = hi[n] - lo[n] + 1;
+            ng[n] = std::max(0, std::max(left[n], rght[n]));
+        }
+
+        int numThreadsMin[3] = {ng[0] + 1, ng[1] + 1, ng[2] + 1};
+
+        for (int n = 0; n < AMREX_SPACEDIM; ++n) {
+            while (sz[n] % numThreadsMin[n] != 0) {
+                ++numThreadsMin[n];
+            }
+        }
+
+        if (std::min({numThreadsMin[0], numThreadsMin[1], numThreadsMin[2]}) < 1) {
+            amrex::Error("Minimum number of CUDA threads must be positive.");
+        }
+
+        Gpu::Device::setNumThreadsMin(numThreadsMin[0], numThreadsMin[1], numThreadsMin[2]);
+
+    }
+
+    void clean_bc_launch_config()
+    {
+        Gpu::Device::setNumThreadsMin(1, 1, 1);
+    }
+#endif
+
     // Note that these are called with dimension agnostic macros like
     // AMREX_ZFILL and AMREX_INT_ANYD already, so we should expect that
     // everything has three entries, not AMREX_SPACEDIM entries.
@@ -30,11 +74,19 @@ extern "C"
 
         prepare_nvar_bc(bc);
 
+#ifdef AMREX_USE_CUDA
+        set_bc_launch_config(lo, hi, domlo, domhi);
+#endif
+
 #pragma gpu
         hypfill(AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
                 adv, AMREX_INT_ANYD(adv_lo), AMREX_INT_ANYD(adv_hi),
                 AMREX_INT_ANYD(domlo), AMREX_INT_ANYD(domhi),
                 AMREX_REAL_ANYD(dx), AMREX_REAL_ANYD(xlo), *time);
+
+#ifdef AMREX_USE_CUDA
+        clean_bc_launch_config();
+#endif
     }
 
     void ca_denfill(Real* adv, const int* adv_lo, const int* adv_hi,
@@ -51,11 +103,19 @@ extern "C"
 
         prepare_bc(bc);
 
+#ifdef AMREX_USE_CUDA
+        set_bc_launch_config(lo, hi, domlo, domhi);
+#endif
+
 #pragma gpu
         denfill(AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
                 adv, AMREX_INT_ANYD(adv_lo), AMREX_INT_ANYD(adv_hi),
                 AMREX_INT_ANYD(domlo), AMREX_INT_ANYD(domhi),
                 AMREX_REAL_ANYD(dx), AMREX_REAL_ANYD(xlo), *time);
+
+#ifdef AMREX_USE_CUDA
+        clean_bc_launch_config();
+#endif
     }
 
 #ifdef GRAVITY
