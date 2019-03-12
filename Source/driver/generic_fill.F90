c19b6f9f592cd25e273294dca7449bfac8453585
Max Katz <maxpkatz@gmail.com>
2019-03-12 06:28:50 -0700

    Overhaul dimension agnostic BCs for GPU compatibility (#546)
    This PR includes quite a few changes centered around making physical boundary conditions compatible for running on GPUs. Summary:
    
    The dimension agnostic ca_*fill interfaces have been moved up to C++, leaving the actual routines that do the work (*fill) in Fortran.
    
    The Fortran routines now take (lo, hi) as their first two arguments. They also now accept time by value rather than by reference.
    
    The declarations have been split into separate headers (some in driver/, some in problems/).
    
    We no longer do CUDA profiling on the initialization.
    
    For CUDA, the number of registers when running on the GPU is capped to 128 when doing BCs. This is because amrex_filccn needs to call syncthreads() as it works to ensure synchronization, and because of the way it is designed, can need as many as 8 threads per dimension == 512 total, which is too many registers required if 255 registers per thread are allowed. This requires some custom GNU Make rules in Make.cuda_rules

--- Source/driver/generic_fill.F90
+++ Source/driver/generic_fill.F90
@@ -1,7 +1,6 @@
 module generic_fill_module
 
   use amrex_fort_module, only: rt => amrex_real
-  use prob_params_module, only: dim
   use meth_params_module, only: NVAR
 
   implicit none
@@ -9,71 +8,44 @@ module generic_fill_module
 contains
 
   ! Used for a generic fill of any StateData.
-  subroutine generic_single_fill(s_lo, s_hi, state, domlo, domhi, delta, xlo, bc)
+  subroutine generic_single_fill(lo, hi, state, s_lo, s_hi, domlo, domhi, delta, xlo, bc) bind(C, name="generic_single_fill")
 
     use amrex_filcc_module, only: amrex_filccn
 
     implicit none
 
+    integer,  intent(in   ) :: lo(3), hi(3)
     integer,  intent(in   ) :: s_lo(3), s_hi(3)
-    integer,  intent(in   ) :: bc(dim,2)
     integer,  intent(in   ) :: domlo(3), domhi(3)
-    real(rt), intent(in   ) :: delta(dim), xlo(dim)
+    integer,  intent(in   ) :: bc(AMREX_SPACEDIM, 2)
+    real(rt), intent(in   ) :: delta(3), xlo(3)
     real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3))
 
-    call amrex_filccn(s_lo, s_hi, state, s_lo, s_hi, 1, domlo, domhi, delta, xlo, bc)
+    !$gpu
+
+    call amrex_filccn(lo, hi, state, s_lo, s_hi, 1, domlo, domhi, delta, xlo, bc)
 
   end subroutine generic_single_fill
 
 
-  subroutine ca_generic_single_fill(state, s_lo, s_hi, &
-                                    domlo, domhi, delta, xlo, time, bc) &
-                                    bind(C, name="ca_generic_single_fill")
 
-    implicit none
-
-    integer,  intent(in   ) :: s_lo(3), s_hi(3)
-    integer,  intent(in   ) :: bc(dim,2)
-    integer,  intent(in   ) :: domlo(3), domhi(3)
-    real(rt), intent(in   ) :: delta(dim), xlo(dim), time
-    real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3))
-
-    call generic_single_fill(s_lo, s_hi, state, domlo, domhi, delta, xlo, bc)
-
-  end subroutine ca_generic_single_fill
-
-
-  subroutine generic_multi_fill(s_lo, s_hi, state, domlo, domhi, delta, xlo, bc)
+  subroutine generic_multi_fill(lo, hi, state, s_lo, s_hi, domlo, domhi, delta, xlo, bc) bind(C, name="generic_multi_fill")
 
     use amrex_filcc_module, only: amrex_filccn
 
     implicit none
 
+    integer,  intent(in   ) :: lo(3), hi(3)
     integer,  intent(in   ) :: s_lo(3), s_hi(3)
-    integer,  intent(in   ) :: bc(dim,2,NVAR)
     integer,  intent(in   ) :: domlo(3), domhi(3)
-    real(rt), intent(in   ) :: delta(dim), xlo(dim)
+    integer,  intent(in   ) :: bc(AMREX_SPACEDIM, 2, NVAR)
+    real(rt), intent(in   ) :: delta(3), xlo(3)
     real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3),NVAR)
 
-    call amrex_filccn(s_lo, s_hi, state, s_lo, s_hi, NVAR, domlo, domhi, delta, xlo, bc)
+    !$gpu
+
+    call amrex_filccn(lo, hi, state, s_lo, s_hi, NVAR, domlo, domhi, delta, xlo, bc)
 
   end subroutine generic_multi_fill
-
-
-  subroutine ca_generic_multi_fill(state, s_lo, s_hi, &
-                                   domlo, domhi, delta, xlo, time, bc) &
-                                   bind(C, name="ca_generic_multi_fill")
-
-    implicit none
-
-    integer,  intent(in   ) :: s_lo(3), s_hi(3)
-    integer,  intent(in   ) :: bc(dim,2,NVAR)
-    integer,  intent(in   ) :: domlo(3), domhi(3)
-    real(rt), intent(in   ) :: delta(dim), xlo(dim), time
-    real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3),NVAR)
-
-    call generic_multi_fill(s_lo, s_hi, state, domlo, domhi, delta, xlo, bc)
-
-  end subroutine ca_generic_multi_fill
 
 end module generic_fill_module
