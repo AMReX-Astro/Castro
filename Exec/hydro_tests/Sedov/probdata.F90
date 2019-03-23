AMReX-Astro/Castro

commit ea93ddb2943cd285d132cf450759dc0a1f8380d7

Corresponding to the new DIMENSION_AGNOSTIC makefile variable, permit dimension agnostic versions of bc_fill_nd.f90 and Prob_nd.f90. Also, add stub Prob_*d.f90 files that will throw an error at runtime if you haven't defined a local copy.

Exec/Detonation/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_${DIM}d.f90	probdata.f90
+f90EXE_sources += probdata.f90

Exec/DustCollapse/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_$(DIM)d.f90	probdata.f90
+f90EXE_sources += probdata.f90

Exec/HCBubble/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_${DIM}d.f90	probdata.f90
+f90EXE_sources += probdata.f90

Exec/KH/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_${DIM}d.f90	probdata.f90
+f90EXE_sources += probdata.f90

Exec/RT/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_${DIM}d.f90	probdata.f90
+f90EXE_sources += probdata.f90

Exec/RT_particles/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_${DIM}d.f90	probdata.f90
+f90EXE_sources += probdata.f90

Exec/Sedov/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_${DIM}d.f90	probdata.f90
+f90EXE_sources += probdata.f90

Exec/Sod/Make.package
@@ -1,1 +1,1 @@
-f90EXE_sources += Prob_${DIM}d.f90	probdata.f90
+f90EXE_sources += probdata.f90

commit dd6608ffb878fb2a25db102dc23b054515750ea1

Add capability for GPU-compatible problem initialization
When we're running a problem on GPUs, we're typically only running 1 MPI rank per
GPU. On a system like Summit, this means 6 MPI ranks per node, each with a fairly
large box size (typically we'll want something like 128^3). But this single CPU
core isn't enough to do CPU-side operations quickly, so problem initialization
becomes a bottleneck (especially given that we don't currently have OpenMP support
when we're using CUDA, so we can't currently thread the initialization). This
commit adds the option for a problem to opt-in to a version of ca_initdata that
is GPU friendly. The interface is slightly different, and supports the GPU pragma.
The user has the responsibility to ensure that all of the variables accessed in
ca_initdata are GPU friendly, so this will not necessarily make sense for every problem.

This also fixes a bug introduced in commit ea93ddb in August 2015, where we were
giving incorrect bounds to ca_initdata after a restart for grown problems. Thus
this fixes #566.

Exec/Make.Castro
@@ -69,6 +69,20 @@ ifeq ($(USE_CUDA),TRUE)
   DEFINES += -DCUDA
 endif
 
+# Allow a problem to specify that we want to initialize
+# on the GPU if we're using CUDA. This is a subset of
+# dimension agnostic problems, and it requires a different
+# interface to initdata, so we want to define this even
+# if we're not using CUDA.
+
+ifeq ($(GPU_COMPATIBLE_INITIALIZATION),TRUE)
+  ifeq ($(DIMENSION_AGNOSTIC),TRUE)
+    DEFINES += -DGPU_COMPATIBLE_INITIALIZATION
+  else
+    $(error GPU compatible initialization is only supported if DIMENSION_AGNOSTIC=TRUE)
+  endif
+endif
+
 include $(AMREX_HOME)/Tools/GNUMake/Make.defs
 
 all: build_status $(executable)

Source/driver/Castro.cpp
@@ -1001,29 +1001,50 @@ Castro::initData ()
 #ifdef AMREX_USE_CUDA
        for (MFIter mfi(S_new); mfi.isValid(); ++mfi)
        {
+#ifdef GPU_COMPATIBLE_INITIALIZATION
+           // Prefetch data to the device to avoid page faults while we're initializing.
+           S_new.prefetchToDevice(mfi);
+#else
            // Prefetch data to the host (and then back to the device at the end)
            // to avoid expensive page faults while the initialization is done.
            S_new.prefetchToHost(mfi);
+#endif
        }
 #endif
 
        for (MFIter mfi(S_new); mfi.isValid(); ++mfi)
        {
 	  RealBox gridloc = RealBox(grids[mfi.index()],geom.CellSize(),geom.ProbLo());
+          const Real* prob_lo = geom.ProbLo();
           const Box& box     = mfi.validbox();
           const int* lo      = box.loVect();
           const int* hi      = box.hiVect();
 
 #ifdef AMREX_DIMENSION_AGNOSTIC
+
+#ifdef GPU_COMPATIBLE_INITIALIZATION
+
+#pragma gpu
+          ca_initdata(AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
+                      BL_TO_FORTRAN_ANYD(S_new[mfi]),
+                      AMREX_REAL_ANYD(dx), AMREX_REAL_ANYD(prob_lo));
+
+#else
+
           BL_FORT_PROC_CALL(CA_INITDATA,ca_initdata)
           (level, cur_time, ARLIM_3D(lo), ARLIM_3D(hi), ns,
   	   BL_TO_FORTRAN_ANYD(S_new[mfi]), ZFILL(dx),
   	   ZFILL(gridloc.lo()), ZFILL(gridloc.hi()));
+
+#endif
+
 #else
+
           BL_FORT_PROC_CALL(CA_INITDATA,ca_initdata)
   	  (level, cur_time, lo, hi, ns,
   	   BL_TO_FORTRAN(S_new[mfi]), dx,
   	   gridloc.lo(), gridloc.hi());
+
 #endif
 
 	  // Generate the initial hybrid momenta based on this user data.
@@ -1032,19 +1053,28 @@ Castro::initData ()
 	  ca_init_hybrid_momentum(lo, hi, BL_TO_FORTRAN_ANYD(S_new[mfi]));
 #endif
 
-          // Verify that the sum of (rho X)_i = rho at every cell
-
-          ca_check_initial_species(AMREX_ARLIM_3D(lo), AMREX_ARLIM_3D(hi),
-				   BL_TO_FORTRAN_ANYD(S_new[mfi]));
-
        }
-       enforce_consistent_e(S_new);
 
 #ifdef AMREX_USE_CUDA
+#ifndef GPU_COMPATIBLE_INITIALIZATION
        for (MFIter mfi(S_new); mfi.isValid(); ++mfi) {
            S_new.prefetchToDevice(mfi);
        }
 #endif
+#endif
+
+       // Verify that the sum of (rho X)_i = rho at every cell
+
+       for (MFIter mfi(S_new); mfi.isValid(); ++mfi) {
+           const Box& bx = mfi.validbox();
+#pragma gpu
+           ca_check_initial_species(AMREX_INT_ANYD(bx.loVect()), AMREX_INT_ANYD(bx.hiVect()),
+                                    BL_TO_FORTRAN_ANYD(S_new[mfi]));
+       }
+
+       // Enforce that the total and internal energies are consistent.
+
+       enforce_consistent_e(S_new);
 
        // thus far, we assume that all initialization has worked on cell-centers
        // (to second-order, these are cell-averages, so we're done in that case).
@@ -2810,7 +2840,8 @@ Castro::enforce_consistent_e (MultiFab& S)
         const int* lo      = box.loVect();
         const int* hi      = box.hiVect();
 
-        ca_enforce_consistent_e(ARLIM_3D(lo), ARLIM_3D(hi), BL_TO_FORTRAN_ANYD(S[mfi]));
+#pragma gpu
+        ca_enforce_consistent_e(AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi), BL_TO_FORTRAN_ANYD(S[mfi]));
     }
 }
 

Source/driver/Castro_F.H
@@ -132,6 +132,14 @@ extern "C"
 #endif
 #endif
 
+#ifdef AMREX_DIMENSION_AGNOSTIC
+#ifdef GPU_COMPATIBLE_INITIALIZATION
+  void ca_initdata(const int* lo, const int* hi,
+                   BL_FORT_FAB_ARG_3D(state),
+                   const amrex::Real* dx, const amrex::Real* problo);
+#endif
+#endif
+
   void ca_apply_metric
     (const int* lo, const int* hi,
      const int* xlo, const int* xhi,
@@ -499,12 +507,14 @@ extern "C"
 
 #ifdef AMREX_DIMENSION_AGNOSTIC
 
+#ifndef GPU_COMPATIBLE_INITIALIZATION
 BL_FORT_PROC_DECL(CA_INITDATA,ca_initdata)
     (const int& level, const amrex::Real& time,
      const int* lo, const int* hi,
      const int& num_state,
      BL_FORT_FAB_ARG_3D(state),
      const amrex::Real* dx, const amrex::Real* xlo, const amrex::Real* xhi);
+#endif
 
 #else
 

Source/driver/Castro_io.cpp
@@ -421,6 +421,8 @@ Castro::restart (Amr&     papa,
 
        for (MFIter mfi(S_new); mfi.isValid(); ++mfi)
        {
+           RealBox gridloc = RealBox(grids[mfi.index()],geom.CellSize(),geom.ProbLo());
+           const Real* prob_lo = geom.ProbLo();
            const Box& bx      = mfi.validbox();
            const int* lo      = bx.loVect();
            const int* hi      = bx.hiVect();
@@ -428,15 +430,30 @@ Castro::restart (Amr&     papa,
            if (! orig_domain.contains(bx)) {
 
 #ifdef AMREX_DIMENSION_AGNOSTIC
+
+#ifdef GPU_COMPATIBLE_INITIALIZATION
+
+#pragma gpu
+              ca_initdata(AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
+                          BL_TO_FORTRAN_ANYD(S_new[mfi]),
+                          AMREX_REAL_ANYD(dx), AMREX_REAL_ANYD(prob_lo));
+
+#else
+
               BL_FORT_PROC_CALL(CA_INITDATA,ca_initdata)
                 (level, cur_time, ARLIM_3D(lo), ARLIM_3D(hi), ns,
 		 BL_TO_FORTRAN_ANYD(S_new[mfi]), ZFILL(dx),
-		 ZFILL(geom.ProbLo()), ZFILL(geom.ProbHi()));
+		 ZFILL(gridloc.lo()), ZFILL(gridloc.hi()));
+
+#endif
+
 #else
+
 	      BL_FORT_PROC_CALL(CA_INITDATA,ca_initdata)
 		(level, cur_time, lo, hi, ns,
 		 BL_TO_FORTRAN(S_new[mfi]), dx,
-		 geom.ProbLo(), geom.ProbHi());
+		 gridloc.lo(), gridloc.hi());
+
 #endif
 
            }

Source/driver/Castro_util.F90
@@ -104,6 +104,8 @@ contains
     integer  :: i,j,k
     real(rt) :: u, v, w, rhoInv
 
+    !$gpu
+
     !
     ! Enforces (rho E) = (rho e) + 1/2 rho (u^2 + v^2 + w^2)
     !
@@ -382,9 +384,11 @@ contains
 
     use network           , only: nspec
     use meth_params_module, only: NVAR, URHO, UFS
-
-    use amrex_error_module
+#ifndef AMREX_USE_CUDA
+    use amrex_error_module, only: amrex_error
+#endif
     use amrex_fort_module, only: rt => amrex_real
+
     implicit none
 
     integer, intent(in) :: lo(3), hi(3)
@@ -394,6 +398,8 @@ contains
     ! Local variables
     integer  :: i, j, k
     real(rt) :: spec_sum
+
+    !$gpu
 
     do k = lo(3), hi(3)
        do j = lo(2), hi(2)

Exec/hydro_tests/Sedov/GNUmakefile
@@ -11,6 +11,7 @@ USE_MPI    = TRUE
 USE_OMP    = FALSE
 
 DIMENSION_AGNOSTIC = TRUE
+GPU_COMPATIBLE_INITIALIZATION = TRUE
 
 # define the location of the CASTRO top directory
 CASTRO_HOME  := ../../..

Exec/hydro_tests/Sedov/Make.package
@@ -1,1 +1,1 @@
-ca_f90EXE_sources += probdata.f90
+ca_F90EXE_sources += probdata.F90

Exec/hydro_tests/Sedov/Prob_nd.F90
@@ -1,17 +1,19 @@
 subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
 
-  use amrex_constants_module
-  use probdata_module
-  use prob_params_module, only : center, coord_type
-  use amrex_error_module
+  use amrex_fort_module, only: rt => amrex_real
+  use amrex_constants_module, only: ZERO, HALF, ONE, FOUR3RD, M_PI
+  use probdata_module, only: p_ambient, dens_ambient, exp_energy, temp_ambient, e_ambient, &
+                             xn_zone, r_init, nsub, e_exp
+  use prob_params_module, only: center, coord_type
+  use amrex_error_module, only: amrex_error
   use eos_type_module, only: eos_t, eos_input_rt, eos_input_rp
   use eos_module, only: eos
-  use amrex_fort_module, only : rt => amrex_real
+  use network, only: nspec
 
   implicit none
 
-  integer, intent(in) :: init, namlen
-  integer, intent(in) :: name(namlen)
+  integer,  intent(in) :: init, namlen
+  integer,  intent(in) :: name(namlen)
   real(rt), intent(in) :: problo(3), probhi(3)
 
   integer :: untin, i
@@ -19,7 +21,7 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
   type(eos_t) :: eos_state
 
   namelist /fortin/ p_ambient, dens_ambient, exp_energy, &
-       r_init, nsub, temp_ambient
+                    r_init, nsub, temp_ambient
 
   real(rt) :: vctr
 
@@ -34,6 +36,16 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
   do i = 1, namlen
      probin(i:i) = char(name(i))
   end do
+
+  allocate(p_ambient)
+  allocate(dens_ambient)
+  allocate(exp_energy)
+  allocate(temp_ambient)
+  allocate(e_ambient)
+  allocate(xn_zone(1:nspec))
+  allocate(r_init)
+  allocate(nsub)
+  allocate(e_exp)
 
   ! set namelist defaults
 
@@ -139,50 +151,27 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
 end subroutine amrex_probinit
 
 
-! ::: -----------------------------------------------------------
-! ::: This routine is called at problem setup time and is used
-! ::: to initialize data on each grid.
-! :::
-! ::: NOTE:  all arrays have one cell of ghost zones surrounding
-! :::        the grid interior.  Values in these cells need not
-! :::        be set here.
-! :::
-! ::: INPUTS/OUTPUTS:
-! :::
-! ::: level     => amr level of grid
-! ::: time      => time at which to init data
-! ::: lo,hi     => index limits of grid interior (cell centered)
-! ::: nstate    => number of state components.  You should know
-! :::		   this already!
-! ::: state     <=  Scalar array
-! ::: delta     => cell size
-! ::: xlo,xhi   => physical locations of lower left and upper
-! :::              right hand corner of grid.  (does not include
-! :::		   ghost region).
-! ::: -----------------------------------------------------------
-subroutine ca_initdata(level,time,lo,hi,nscal, &
-                       state,state_lo,state_hi, &
-                       delta,xlo,xhi)
 
-  use probdata_module
+subroutine ca_initdata(lo, hi, &
+                       state, s_lo, s_hi, &
+                       dx, problo) bind(C, name='ca_initdata')
+
+  use amrex_fort_module, only: rt => amrex_real
   use amrex_constants_module, only: ZERO, ONE, HALF, FOUR3RD, M_PI
   use meth_params_module , only: NVAR, URHO, UMX, UMY, UMZ, UTEMP, UEDEN, UEINT, UFS
-  use prob_params_module, only : center, coord_type, dim
-  use amrex_fort_module, only : rt => amrex_real
-  use network, only : nspec
+  use probdata_module, only: p_ambient, dens_ambient, exp_energy, temp_ambient, e_ambient, &
+                             xn_zone, r_init, nsub, e_exp
+  use prob_params_module, only: center, coord_type, dim
   use eos_module, only : eos
-  use eos_type_module, only : eos_t, eos_input_rp, eos_input_re
-  use amrex_error_module, only: amrex_error
+  use eos_type_module, only: eos_t, eos_input_rp, eos_input_re
 
   implicit none
 
-  integer :: level, nscal
-  integer :: lo(3), hi(3)
-  integer :: state_lo(3), state_hi(3)
-  real(rt) :: xlo(3), xhi(3), time, delta(3)
-  real(rt) :: state(state_lo(1):state_hi(1), &
-                    state_lo(2):state_hi(2), &
-                    state_lo(3):state_hi(3),NVAR)
+  integer,  intent(in   ) :: lo(3), hi(3)
+  integer,  intent(in   ) :: s_lo(3), s_hi(3)
+  real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3),NVAR)
+  real(rt), intent(in   ) :: dx(3), problo(3)
+
   real(rt) :: xmin,ymin,zmin
   real(rt) :: xx, yy, zz
   real(rt) :: dist
@@ -194,6 +183,8 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
   real(rt) :: ds(3), xl, xr
   real(rt) :: vol_pert, vol_ambient
   logical :: pert
+
+  !$gpu
 
   if (dim == 1) then
      nsubx = nsub
@@ -209,18 +200,18 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
      nsubz = nsub
   end if
 
-  ds(1) = delta(1) / nsubx
-  ds(2) = delta(2) / nsuby
-  ds(3) = delta(3) / nsubz
+  ds(1) = dx(1) / nsubx
+  ds(2) = dx(2) / nsuby
+  ds(3) = dx(3) / nsubz
 
   do k = lo(3), hi(3)
-     zmin = xlo(3) + delta(3)*dble(k-lo(3))
+     zmin = problo(3) + dx(3) * dble(k)
 
      do j = lo(2), hi(2)
-        ymin = xlo(2) + delta(2)*dble(j-lo(2))
+        ymin = problo(2) + dx(2) * dble(j)
 
         do i = lo(1), hi(1)
-           xmin = xlo(1) + delta(1)*dble(i-lo(1))
+           xmin = problo(1) + dx(1) * dble(i)
 
            vol_pert = ZERO
            vol_ambient = ZERO

Exec/hydro_tests/Sedov/probdata.F90
@@ -1,0 +1,17 @@
+module probdata_module
+
+  use amrex_fort_module, only: rt => amrex_real
+
+  real(rt), allocatable :: p_ambient, dens_ambient, exp_energy, temp_ambient, e_ambient
+  real(rt), allocatable :: xn_zone(:)
+  real(rt), allocatable :: r_init
+  integer,  allocatable :: nsub
+
+  real(rt), allocatable :: e_exp ! Explosion energy per zone
+
+#ifdef AMREX_USE_CUDA
+  attributes(managed) :: p_ambient, dens_ambient, exp_energy, temp_ambient, e_ambient
+  attributes(managed) :: xn_zone, r_init, nsub, e_exp
+#endif
+
+end module probdata_module

Exec/hydro_tests/Sedov/probdata.f90
@@ -1,13 +1,0 @@
-module probdata_module
-
-  use network, only: nspec
-  use amrex_fort_module, only : rt => amrex_real
-
-  real(rt), save :: p_ambient, dens_ambient, exp_energy, temp_ambient, e_ambient
-  real(rt), save :: xn_zone(nspec)
-  real(rt), save :: r_init
-  integer, save ::  nsub
-
-  real(rt), save :: e_exp ! Explosion energy per zone
-
-end module probdata_module