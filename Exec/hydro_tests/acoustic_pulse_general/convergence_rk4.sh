8962d66f21c2561e3a64b0c9d38f69d6364dcc2c
Michael Zingale <michael.zingale@stonybrook.edu>
2019-04-22 15:29:30 -0400

    fix convergence check times

--- Exec/hydro_tests/acoustic_pulse_general/convergence_rk4.sh
+++ Exec/hydro_tests/acoustic_pulse_general/convergence_rk4.sh
@@ -13,7 +13,7 @@ mpiexec -n 4 ${EXEC} inputs.128 ${RUNPARAMS} amr.plot_file=acoustic_pulse_128_rk
 mpiexec -n 4 ${EXEC} inputs.256 ${RUNPARAMS} amr.plot_file=acoustic_pulse_256_rk4_plt &> 256.out
 mpiexec -n 4 ${EXEC} inputs.512 ${RUNPARAMS} amr.plot_file=acoustic_pulse_512_rk4_plt &> 512.out
 
-RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_64_rk4_plt00201 mediFile=acoustic_pulse_128_rk4_plt00401 fineFile=acoustic_pulse_256_rk4_plt00801 > convergence.${DIM}d.lo.rk4.out
-RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_128_rk4_plt00401 mediFile=acoustic_pulse_256_rk4_plt00801 fineFile=acoustic_pulse_512_rk4_plt01601 > convergence.${DIM}d.hi.rk4.out
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_64_rk4_plt00101 mediFile=acoustic_pulse_128_rk4_plt00201 fineFile=acoustic_pulse_256_rk4_plt00401 > convergence.${DIM}d.lo.rk4.out
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_128_rk4_plt00201 mediFile=acoustic_pulse_256_rk4_plt00401 fineFile=acoustic_pulse_512_rk4_plt00801 > convergence.${DIM}d.hi.rk4.out
 
 
--- Exec/hydro_tests/acoustic_pulse_general/convergence_sdc.sh
+++ Exec/hydro_tests/acoustic_pulse_general/convergence_sdc.sh
@@ -13,6 +13,6 @@ mpiexec -n 8 ${EXEC} inputs.128 ${RUNPARAMS} amr.plot_file=acoustic_pulse_128_sd
 mpiexec -n 8 ${EXEC} inputs.256 ${RUNPARAMS} amr.plot_file=acoustic_pulse_256_sdc &> 256.out
 mpiexec -n 8 ${EXEC} inputs.512 ${RUNPARAMS} amr.plot_file=acoustic_pulse_512_sdc &> 512.out
 
-RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_64_sdc_plt00201 mediFile=acoustic_pulse_128_sdc_plt00401 fineFile=acoustic_pulse_256_sdc_plt00801 > convergence.${DIM}d.lo.sdc.out
-RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_128_sdc_plt00401 mediFile=acoustic_pulse_256_sdc_plt00801 fineFile=acoustic_pulse_512_sdc_plt01601 > convergence.${DIM}d.hi.sdc.out
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_64_sdc_plt00101 mediFile=acoustic_pulse_128_sdc_plt00201 fineFile=acoustic_pulse_256_sdc_plt00401 > convergence.${DIM}d.lo.sdc.out
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_128_sdc_plt00201 mediFile=acoustic_pulse_256_sdc_plt00401 fineFile=acoustic_pulse_512_sdc_plt00801 > convergence.${DIM}d.hi.sdc.out
 
