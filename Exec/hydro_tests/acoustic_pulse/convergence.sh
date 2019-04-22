1abca1f414f2192cfb2f2960d0d3985d38578bd6
Michael Zingale <michael.zingale@stonybrook.edu>
2019-04-22 09:52:44 -0400

    rename and update the rk4 convergence

--- Exec/hydro_tests/acoustic_pulse/convergence.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.64 castro.fourth_order=1 castro.mol_order=4 castro.time_integration_method=1
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.128 castro.fourth_order=1 castro.mol_order=4 castro.time_integration_method=1
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.256 castro.fourth_order=1 castro.mol_order=4 castro.time_integration_method=1
-mpiexec -n 8 ./Castro2d.gnu.MPI.ex inputs.2d.512 castro.fourth_order=1 castro.mol_order=4 castro.time_integration_method=1
-
-RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_64_plt00081 mediFile=acoustic_pulse_128_plt00161 fineFile=acoustic_pulse_256_plt00321 mediError=med.out coarError=coar.out > convergence.1.out
-RichardsonConvergenceTest2d.gnu.ex coarFile=acoustic_pulse_128_plt00161 mediFile=acoustic_pulse_256_plt00321 fineFile=acoustic_pulse_512_plt00641 mediError=med.out coarError=coar.out > convergence.2.out
