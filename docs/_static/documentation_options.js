aa5afeb7334119e397bcb791a480e2cf4c1b2e37
Travis CI <>
2019-04-28 01:55:12 +0000

    Deploy to GitHub Pages: 6dc1580a2901e93de28edfae18e7303fc351c7c3

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'actual_flame_wave',
+    VERSION: '19.04',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
