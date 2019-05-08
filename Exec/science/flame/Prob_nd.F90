2084b4089851a0515b8fe32781efc789a125a86b
Michael Zingale <michael.zingale@stonybrook.edu>
2019-05-08 13:01:00 -0400

    keep mass flux constant across the flame initially

--- Exec/science/flame/Prob_nd.F90
+++ Exec/science/flame/Prob_nd.F90
@@ -157,6 +157,9 @@ subroutine amrex_probinit(init, name, namlen, problo, probhi) bind(C, name="amre
   rho_ash = eos_state % rho
   e_ash = eos_state % e
 
+  ! mass flux will be constant across the flame
+  mass_flux = rho_fuel * v_inflow
+
 end subroutine amrex_probinit
 
 
@@ -228,9 +231,9 @@ subroutine ca_initdata(level, time, lo, hi, nscal, &
 
            state(i,j,k,URHO ) = eos_state % rho
            state(i,j,k,UMX:UMZ) = ZERO
-           state(i,j,k,UMX) = eos_state % rho * v_inflow
+           state(i,j,k,UMX) = mass_flux
            state(i,j,k,UEDEN) = eos_state % rho * eos_state % e + &
-                HALF * eos_state % rho * v_inflow**2
+                HALF * sum(state(i,j,k,UMX:UMZ)**2)/state(i,j,k,URHO)
            state(i,j,k,UEINT) = eos_state % rho * eos_state % e
            state(i,j,k,UTEMP) = eos_state % T
            state(i,j,k,UFS:UFS-1+nspec) = eos_state % rho * eos_state % xn(:)
