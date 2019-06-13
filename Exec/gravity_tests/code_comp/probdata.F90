52575c2e4bed90d400d26c0b6f80e465db64d6b8
Alice Harpole <aliceharpole@gmail.com>
2019-06-13 10:46:17 -0400

    code comparison problem

---
+++ Exec/gravity_tests/code_comp/probdata.F90
@@ -0,0 +1,7 @@
+module probdata_module
+
+!     These determine the refinement criteria
+      use amrex_fort_module, only : rt => amrex_real
+      character (len=80), save  :: model_name
+
+end module probdata_module
