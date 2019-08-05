58607d113498bc8be0195c79a1a4262eee30edd2
Michael Zingale <michael.zingale@stonybrook.edu>
2019-08-05 14:16:47 -0400

    don't do slipwall

--- Exec/hydro_tests/Sod_stellar/compare_methods.sh
+++ Exec/hydro_tests/Sod_stellar/compare_methods.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-./Castro2d.gnu.ex inputs-test1-helm amr.plot_file=test1_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
+./Castro2d.gnu.ex inputs-test1-helm amr.plot_file=test1_sdc_plt castro.sdc_order=4 castro.time_integration_method=2
 ./Castro2d.gnu.ex inputs-test1-helm amr.plot_file=test1_ppm_plt
 
-./compare_sdc.py --ppm test1_ppm_plt????? --sdc test1_sdc_plt????? -o test1_sdc.png --exact Verification/test1.exact.128.out
+./compare_sdc.py --ppm test1_ppm_plt????? --sdc test1_sdc_plt????? -o test1_sdc.pdf --exact Verification/test1.exact.128.out
 
-./Castro2d.gnu.ex inputs-test2-helm amr.plot_file=test2_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
+./Castro2d.gnu.ex inputs-test2-helm amr.plot_file=test2_sdc_plt castro.sdc_order=4 castro.time_integration_method=2
 ./Castro2d.gnu.ex inputs-test2-helm amr.plot_file=test2_ppm_plt
 
-./compare_sdc.py --ppm test2_ppm_plt????? --sdc test2_sdc_plt????? -o test2_sdc.png --exact Verification/test2.exact.128.out
+./compare_sdc.py --ppm test2_ppm_plt????? --sdc test2_sdc_plt????? -o test2_sdc.pdf --exact Verification/test2.exact.128.out
 
-./Castro2d.gnu.ex inputs-test3-helm amr.plot_file=test3_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
+./Castro2d.gnu.ex inputs-test3-helm amr.plot_file=test3_sdc_plt castro.sdc_order=4 castro.time_integration_method=2
 ./Castro2d.gnu.ex inputs-test3-helm amr.plot_file=test3_ppm_plt
 
-./compare_sdc.py --ppm test3_ppm_plt????? --sdc test3_sdc_plt????? -o test3_sdc.png --exact Verification/test3.exact.128.out
+./compare_sdc.py --ppm test3_ppm_plt????? --sdc test3_sdc_plt????? -o test3_sdc.pdf --exact Verification/test3.exact.128.out
 
 #./Castro2d.gnu.ex inputs-test4-helm amr.plot_file=test4_sdc_plt castro.sdc_order=4 castro.time_integration_method=2 castro.fourth_order=1
 #./Castro2d.gnu.ex inputs-test4-helm amr.plot_file=test4_ppm_plt
