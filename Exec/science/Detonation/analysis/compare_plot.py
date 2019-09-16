2cd10da3f2065ab07220f99594a0c394fb10a40a
Michael Zingale <michael.zingale@stonybrook.edu>
2019-09-16 14:05:29 -0400

    updated version of the comparison with the different quadrature types

--- Exec/science/Detonation/analysis/compare_plot.py
+++ Exec/science/Detonation/analysis/compare_plot.py
@@ -24,47 +24,66 @@ def get_Te_profile(plotfile):
     x_coord = np.array(ad['x'][srt])
     temp = np.array(ad['Temp'][srt])
     enuc = np.array(ad['enuc'][srt])
+    rho_He = np.array(ad['rho_he4'][srt])
 
-    return time, x_coord, temp, enuc
+    return time, x_coord, temp, enuc, rho_He
 
 
-def doit(plotfiles):
+def doit(plotfiles, xmin, xmax):
 
     f = plt.figure()
     f.set_size_inches(7.0, 9.0)
 
-    ax_T = f.add_subplot(211)
-    ax_e = f.add_subplot(212)
-    
+    ax_T = f.add_subplot(311)
+    ax_e = f.add_subplot(312)
+    ax_he = f.add_subplot(313)
+
     for p in plotfiles:
         if plotfiles[p] is None:
             continue
 
-        time, x, T, enuc = get_Te_profile(plotfiles[p])
+        time, x, T, enuc, rho_he4 = get_Te_profile(plotfiles[p])
 
         ax_T.plot(x, T, label=p)
         ax_e.plot(x, enuc)
-        
+        ax_he.plot(x, rho_he4)
+
     ax_T.legend(frameon=False)
 
+    ax_T.set_ylabel(r"$T$ (K)")
+    ax_e.set_ylabel(r"$H_\mathrm{nuc}$ (erg/g/s)")
+    ax_he.set_ylabel(r"$\rho X({}^4\mathrm{He})$ (g/cm${}^3$)")
+    ax_he.set_xlabel("x (cm)")
+
     ax_e.set_yscale("log")
-    
+    ax_he.set_yscale("log")
+
+    ax_T.set_xlim(left=xmin, right=xmax)
+    ax_e.set_xlim(left=xmin, right=xmax)
+    ax_he.set_xlim(left=xmin, right=xmax)
+
+    f.tight_layout()
+
     f.savefig("det.png")
-    
+
 
 if __name__ == "__main__":
 
     p = argparse.ArgumentParser()
 
-    p.add_argument("--strang", help="plotfile with Strang splitting", type=str, default=None)
-    p.add_argument("--sdc", help="plotfile with SDC", type=str, default=None)
-    p.add_argument("--mol", help="plotfile with MOL integration", type=str, default=None)
+    p.add_argument("--ctu", help="plotfile with Strang CTU splitting", type=str, default=None)
+    p.add_argument("--lobatto2", help="plotfile with Lobatto SDC-2", type=str, default=None)
+    p.add_argument("--lobatto4", help="plotfile with Lobatto SDC-4", type=str, default=None)
+    p.add_argument("--radau4", help="plotfile with Radau SDC-4", type=str, default=None)
+    p.add_argument("-x", help="minimum x value", type=float, default=0.0)
+    p.add_argument("-X", help="maximum x value", type=float, default=None)
 
     args = p.parse_args()
 
     pfiles = {}
-    pfiles["Strang"] = args.strang
-    pfiles["SDC"] = args.sdc
-    pfiles["MOL"] = args.mol
-    
-    doit(pfiles)
+    pfiles["Strang CTU"] = args.ctu
+    pfiles["Gauss-Lobatto SDC-2"] = args.lobatto2
+    pfiles["Gauss-Lobatto SDC-4"] = args.lobatto4
+    pfiles["Radau SDC-4"] = args.radau4
+
+    doit(pfiles, args.x, args.X)
