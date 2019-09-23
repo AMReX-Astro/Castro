bcc581c6d2ee0e57e226b8e77f270baf898e1c89
Michael Zingale <michael.zingale@stonybrook.edu>
2019-09-23 19:48:00 -0400

    switch to the auto prob params

--- Exec/gravity_tests/StarGrav/Prob_nd.F90
+++ Exec/gravity_tests/StarGrav/Prob_nd.F90
@@ -12,29 +12,7 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
   integer, intent(in) :: name(namlen)
   real(rt), intent(in) :: problo(3), probhi(3)
 
-  integer :: untin, i
-
-  namelist /fortin/ &
-       model_name
-
-  ! Build "probin" filename -- the name of file containing fortin namelist.
-  integer, parameter :: maxlen = 127
-  character probin*(maxlen)
-
-  if (namlen .gt. maxlen) then
-     call castro_error("probin file name too long")
-  end if
-
-  do i = 1, namlen
-     probin(i:i) = char(name(i))
-  end do
-
-  ! set namelist defaults
-
-  ! Read namelists
-  open(newunit=untin, file=probin(1:namlen), form='formatted', status='old')
-  read(untin,fortin)
-  close(unit=untin)
+  call probdata_init(name, namlen)
 
   ! read initial model
   call read_model_file(model_name)
