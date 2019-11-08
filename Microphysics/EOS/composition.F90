4fc7eef2bf073816940806f7bbf8d265c4b50d81
Michael Zingale <michael.zingale@stonybrook.edu>
2019-11-08 18:50:34 -0500

    define the composition stuff

--- Microphysics/EOS/composition.F90
+++ Microphysics/EOS/composition.F90
@@ -6,6 +6,12 @@ module eos_composition_module
 
   implicit none
 
+  type :: eos_xderivs_t
+    real(rt) :: dedX(nspec)
+    real(rt) :: dpdX(nspec)
+    real(rt) :: dhdX(nspec)
+ end type eos_xderivs_t
+
 contains
 
   ! Given a set of mass fractions, calculate quantities that depend
@@ -35,5 +41,42 @@ contains
     state % zbar = state % abar / state % mu_e
 
   end subroutine composition
+
+
+  ! Compute thermodynamic derivatives with respect to xn(:)
+
+  subroutine composition_derivatives(state, state_xderivs)
+
+    use amrex_constants_module, only: ZERO
+    use network, only: aion, aion_inv, zion
+
+    implicit none
+
+    type (eos_t), intent(in) :: state
+    type (eos_xderivs_t), intent(out) :: state_xderivs
+
+    !$gpu
+
+#ifdef EXTRA_THERMO
+    state_xderivs % dpdX(:) = state % dpdA * (state % abar * aion_inv(:))   &
+                                        * (aion(:) - state % abar) &
+                            + state % dpdZ * (state % abar * aion_inv(:))   &
+                                        * (zion(:) - state % zbar)
+
+    state_xderivs % dEdX(:) = state % dedA * (state % abar * aion_inv(:))   &
+                                        * (aion(:) - state % abar) &
+                            + state % dedZ * (state % abar * aion_inv(:))   &
+                                        * (zion(:) - state % zbar)
+
+    if (state % dPdr .ne. ZERO) then
+
+       state_xderivs % dhdX(:) = state_xderivs % dedX(:) &
+            + (state % p / state % rho**2 - state % dedr) &
+            *  state_xderivs % dPdX(:) / state % dPdr
+
+    endif
+#endif
+
+  end subroutine composition_derivatives
 
 end module eos_composition_module
