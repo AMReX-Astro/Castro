cbc22dfb65f06c63e36636645cc86cc803555f9b
Max Katz <maxpkatz@gmail.com>
2019-11-11 21:36:33 -0800

    Don't tile y dimension in 2D on GPUs

--- Source/driver/Castro.cpp
+++ Source/driver/Castro.cpp
@@ -176,7 +176,7 @@ IntVect      Castro::no_tile_size(1024);
 #ifndef AMREX_USE_CUDA
 IntVect      Castro::hydro_tile_size(1024,16);
 #else
-IntVect      Castro::hydro_tile_size(1024,64);
+IntVect      Castro::hydro_tile_size(1024,1024);
 #endif
 IntVect      Castro::no_tile_size(1024,1024);
 #else
