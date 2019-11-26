4ba0aa42f53aa50fb7b89cef49a4a4d90177e24c
guadabsb15 <guadabsb15@gmail.com>
2019-11-26 17:03:22 -0500

    Sedov and species compile and run now. BrioWu compiles but is not running

--- Exec/mhd_tests/BrioWu/Prob_nd.F90
+++ Exec/mhd_tests/BrioWu/Prob_nd.F90
@@ -1,74 +1,35 @@
-subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
+subroutine amrex_probinit(init, name, namlen, problo, probhi) bind(c)
 
-  use amrex_constants_module
+  use eos_module
+  use eos_type_module
+  use castro_error_module
+  use network
   use probdata_module
-  use prob_params_module, only : center
-  use amrex_error_module
-  use eos_type_module, only: eos_t, eos_input_rt, eos_input_rp
-  use eos_module, only: eos
-  use amrex_fort_module, only : rt => amrex_real
 
+  use amrex_fort_module, only : rt => amrex_real
   implicit none
 
-  integer, intent(in) :: init, namlen
-  integer, intent(in) :: name(namlen)
-  real(rt), intent(in) :: problo(3), probhi(3)
+  integer init, namlen
+  integer name(namlen)
+  real(rt)         problo(3), probhi(3)
+  real(rt)         xn(nspec)
 
-  integer :: untin, i
+  type (eos_t) :: eos_state
 
-  type(eos_t) :: eos_state
+  call probdata_init(name, namlen)
 
-  namelist /fortin/ p_l, u_l, rho_l, p_r, u_r, rho_r, frac, &
-       B_x_l, B_y_l, B_z_l, B_x_r, B_y_r, B_z_r, idir
-
-  ! Build "probin" filename -- the name of file containing fortin namelist.
-  integer, parameter :: maxlen = 256
-  character :: probin*(maxlen)
-
-  if (namlen .gt. maxlen) then
-     call amrex_error('probin file name too long')
-  end if
-
-  do i = 1, namlen
-     probin(i:i) = char(name(i))
-  end do
-
-  ! set namelist defaults
-  p_l = 1.0               ! left pressure (erg/cc)
-  u_l = 0.0               ! left velocity (cm/s)
-  rho_l = 1.0             ! left density (g/cc)
-  B_x_l = 0.75
-  B_y_l = 0.
-  B_z_l = 1.
-
-  p_r = 0.1               ! right pressure (erg/cc)
-  u_r = 0.0               ! right velocity (cm/s)
-  rho_r = 0.125           ! right density (g/cc)
-  B_x_r = 0.75
-  B_y_r = 0.
-  B_z_r = -1.
-
-  idir = 1                ! direction across which to jump
-  frac = 0.5              ! fraction of the domain for the interface
-
-  ! set local variable defaults
-  center(1) = HALF*(problo(1) + probhi(1))
-  center(2) = HALF*(problo(2) + probhi(2))
-  center(3) = HALF*(problo(3) + probhi(3))
-
-  ! Read namelists
-  open(newunit=untin, file=probin(1:namlen), form='formatted', status='old')
-  read(untin,fortin)
-  close(unit=untin)
-
-  xn_zone(:) = ZERO
-  xn_zone(1) = ONE
+  split(1) = frac*(problo(1)+probhi(1))
+  split(2) = frac*(problo(2)+probhi(2))
+  split(3) = frac*(problo(3)+probhi(3))
 
   ! compute the internal energy (erg/cc) for the left and right state
+  xn(:) = 0.0e0_rt
+  xn(1) = 1.0e0_rt
+
   eos_state%rho = rho_l
   eos_state%p = p_l
   eos_state%T = 100000.e0_rt  ! initial guess
-  eos_state%xn(:) = xn_zone(:)
+  eos_state%xn(:) = xn(:)
 
   call eos(eos_input_rp, eos_state)
 
@@ -78,77 +39,54 @@ subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c)
   eos_state%rho = rho_r
   eos_state%p = p_r
   eos_state%T = 100000.e0_rt  ! initial guess
-  eos_state%xn(:) = xn_zone(:)
+  eos_state%xn(:) = xn(:)
 
   call eos(eos_input_rp, eos_state)
 
   rhoe_r = rho_r*eos_state%e
   T_r = eos_state%T
 
-  end subroutine amrex_probinit
+end subroutine amrex_probinit
 
 
-! ::: -----------------------------------------------------------
-! ::: This routine is called at problem setup time and is used
-! ::: to initialize data on each grid.
-! :::
-! ::: NOTE:  all arrays have one cell of ghost zones surrounding
-! :::        the grid interior.  Values in these cells need not
-! :::        be set here.
-! :::
-! ::: INPUTS/OUTPUTS:
-! :::
-! ::: level     => amr level of grid
-! ::: time      => time at which to init data
-! ::: lo,hi     => index limits of grid interior (cell centered)
-! ::: nstate    => number of state components.  You should know
-! :::		   this already!
-! ::: state     <=  Scalar array
-! ::: delta     => cell size
-! ::: xlo,xhi   => physical locations of lower left and upper
-! :::              right hand corner of grid.  (does not include
-! :::		   ghost region).
-! ::: -----------------------------------------------------------
-subroutine ca_initdata(level,time,lo,hi,nscal, &
-                       state,state_l1,state_l2,state_l3,state_h1,state_h2,state_h3, &
-                       delta,xlo,xhi)
+subroutine ca_initdata(level, time, lo, hi, nscal, &
+                       state, s_lo, s_hi, &
+                       dx, xlo, xhi)
 
+  use castro_error_module, only: castro_error
+  use amrex_fort_module, only: rt => amrex_real
+  use network, only: nspec
   use probdata_module
-  use meth_params_module , only: NVAR, URHO, UMX, UMY, UMZ, UEDEN, UEINT, UFS, UTEMP
-  use prob_params_module, only : center
-  use amrex_fort_module, only : rt => amrex_real
-  use network, only : nspec
+  use meth_params_module, only : NVAR, URHO, UMX, UMY, UMZ, UEDEN, UEINT, UTEMP, UFS
+  use prob_params_module, only : problo
 
   implicit none
 
-  integer :: level, nscal
-  integer :: lo(3), hi(3)
-  integer :: state_l1,state_l2,state_l3,state_h1,state_h2,state_h3
-  real(rt) :: xlo(3), xhi(3), time, delta(3)
-  real(rt) :: state(state_l1:state_h1, &
-                    state_l2:state_h2, &
-                    state_l3:state_h3,NVAR)
+  integer,  intent(in   ) :: level, nscal
+  integer,  intent(in   ) :: lo(3), hi(3)
+  integer,  intent(in   ) :: s_lo(3), s_hi(3)
+  real(rt), intent(in   ) :: xlo(3), xhi(3), time, dx(3)
+  real(rt), intent(inout) :: state(s_lo(1):s_hi(1), s_lo(2):s_hi(2), s_lo(3):s_hi(3), NVAR)
 
-  
-  real(rt) :: xcen, ycen, zcen
-  integer :: i,j,k
+  real(rt) :: x, y, z
+  integer  :: i, j, k
 
   do k = lo(3), hi(3)
-     zcen = xlo(3) + delta(3)*(float(k-lo(3)) + 0.5e0_rt)
+     z = problo(3) + dx(3)*(dble(k) + 0.5e0_rt)
 
      do j = lo(2), hi(2)
-        ycen = xlo(2) + delta(2)*(float(j-lo(2)) + 0.5e0_rt)
+        y = problo(2) + dx(2)*(dble(j) + 0.5e0_rt)
 
         do i = lo(1), hi(1)
-           xcen = xlo(1) + delta(1)*(float(i-lo(1)) + 0.5e0_rt)
+           x = problo(1) + dx(1)*(dble(i) + 0.5e0_rt)
 
            if (idir == 1) then
-              if (xcen <= center(1)) then
+              if (x <= split(1)) then
                  state(i,j,k,URHO) = rho_l
                  state(i,j,k,UMX) = rho_l*u_l
                  state(i,j,k,UMY) = 0.e0_rt
                  state(i,j,k,UMZ) = 0.e0_rt
-                 state(i,j,k,UEDEN) = rhoe_l + 0.5e0_rt*rho_l*u_l*u_l + 0.5e0_rt * (B_x_l**2 + B_y_l**2 + B_z_l**2)
+                 state(i,j,k,UEDEN) = rhoe_l + 0.5*rho_l*u_l*u_l + 0.5e0_rt * (B_x_l**2 + B_y_l**2 + B_z_l**2)
                  state(i,j,k,UEINT) = rhoe_l
                  state(i,j,k,UTEMP) = T_l
               else
@@ -156,20 +94,18 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
                  state(i,j,k,UMX) = rho_r*u_r
                  state(i,j,k,UMY) = 0.e0_rt
                  state(i,j,k,UMZ) = 0.e0_rt
-                 state(i,j,k,UEDEN) = rhoe_r + 0.5e0_rt*rho_r*u_r*u_r + 0.5e0_rt *(B_x_r**2 + B_y_r**2 + B_z_r**2)
+                 state(i,j,k,UEDEN) = rhoe_r + 0.5*rho_r*u_r*u_r + 0.5e0_rt *(B_x_r**2 + B_y_r**2 + B_z_r**2)
                  state(i,j,k,UEINT) = rhoe_r
                  state(i,j,k,UTEMP) = T_r
               endif
-           endif
 
-
-           if (idir == 2) then
-              if (ycen <= center(2)) then
+           else if (idir == 2) then
+              if (y <= split(2)) then
                  state(i,j,k,URHO) = rho_l
                  state(i,j,k,UMX) = 0.e0_rt
                  state(i,j,k,UMY) = rho_l*u_l
                  state(i,j,k,UMZ) = 0.e0_rt
-                 state(i,j,k,UEDEN) = rhoe_l + 0.5e0_rt*rho_l*u_l*u_l + 0.5e0_rt * (B_x_l**2 + B_y_l**2 + B_z_l**2)
+                 state(i,j,k,UEDEN) = rhoe_l + 0.5*rho_l*u_l*u_l + 0.5e0_rt * (B_x_l**2 + B_y_l**2 + B_z_l**2)
                  state(i,j,k,UEINT) = rhoe_l
                  state(i,j,k,UTEMP) = T_l
               else
@@ -177,20 +113,18 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
                  state(i,j,k,UMX) = 0.e0_rt
                  state(i,j,k,UMY) = rho_r*u_r
                  state(i,j,k,UMZ) = 0.e0_rt
-                 state(i,j,k,UEDEN) = rhoe_r + 0.5e0_rt*rho_r*u_r*u_r + 0.5e0_rt *(B_x_r**2 + B_y_r**2 + B_z_r**2)
+                 state(i,j,k,UEDEN) = rhoe_r + 0.5*rho_r*u_r*u_r + 0.5e0_rt *(B_x_r**2 + B_y_r**2 + B_z_r**2)
                  state(i,j,k,UEINT) = rhoe_r
                  state(i,j,k,UTEMP) = T_r
               endif
-           endif
 
-
-           if (idir == 3) then
-              if (zcen <= center(3)) then
+           else if (idir == 3) then
+              if (z <= split(3)) then
                  state(i,j,k,URHO) = rho_l
                  state(i,j,k,UMX) = 0.e0_rt
                  state(i,j,k,UMY) = 0.e0_rt
                  state(i,j,k,UMZ) = rho_l*u_l
-                 state(i,j,k,UEDEN) = rhoe_l + 0.5e0_rt*rho_l*u_l*u_l + 0.5e0_rt * (B_x_l**2 + B_y_l**2 + B_z_l**2)
+                 state(i,j,k,UEDEN) = rhoe_l + 0.5*rho_l*u_l*u_l + 0.5e0_rt * (B_x_l**2 + B_y_l**2 + B_z_l**2)
                  state(i,j,k,UEINT) = rhoe_l
                  state(i,j,k,UTEMP) = T_l
               else
@@ -198,24 +132,28 @@ subroutine ca_initdata(level,time,lo,hi,nscal, &
                  state(i,j,k,UMX) = 0.e0_rt
                  state(i,j,k,UMY) = 0.e0_rt
                  state(i,j,k,UMZ) = rho_r*u_r
-                 state(i,j,k,UEDEN) = rhoe_r + 0.5e0_rt*rho_r*u_r*u_r + 0.5e0_rt *(B_x_r**2 + B_y_r**2 + B_z_r**2)
+                 state(i,j,k,UEDEN) = rhoe_r + 0.5*rho_r*u_r*u_r + 0.5e0_rt *(B_x_r**2 + B_y_r**2 + B_z_r**2)
                  state(i,j,k,UEINT) = rhoe_r
                  state(i,j,k,UTEMP) = T_r
               endif
+
+           else
+              call castro_error('invalid idir')
            endif
-!              state(i,j,k,UFS:UFS-1+nspec) = 0.0e0_rt
-              state(i,j,k,UFS  ) = state(i,j,k,URHO)
+
+           state(i,j,k,UFS:UFS-1+nspec) = 0.0e0_rt
+           state(i,j,k,UFS  ) = state(i,j,k,URHO)
+
 
         enddo
      enddo
   enddo
 
- end subroutine ca_initdata
-
+end subroutine ca_initdata
 
 ! :::
 ! ::: --------------------------------------------------------------------
-! ::: 
+! :::
 subroutine ca_initmag(level, time, lo, hi, &
                       nbx, mag_x, bx_lo, bx_hi, &
                       nby, mag_y, by_lo, by_hi, &
@@ -223,11 +161,11 @@ subroutine ca_initmag(level, time, lo, hi, &
                       delta, xlo, xhi)
 
   use probdata_module
-  use prob_params_module, only : center
+  use prob_params_module
   use amrex_fort_module, only : rt => amrex_real
-  
+
   implicit none
-  
+
   integer :: level, nbx, nby, nbz
   integer :: lo(3), hi(3)
   integer :: bx_lo(3), bx_hi(3)
@@ -241,7 +179,7 @@ subroutine ca_initmag(level, time, lo, hi, &
 
   real(rt) :: xcen, ycen, zcen
   integer  :: i, j, k
-  
+
   print *, "Initializing magnetic field!!"
 
   if (idir .eq. 1) then
@@ -249,7 +187,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)
            do i = lo(1), hi(1)+1
               xcen = xlo(1) + delta(1)*(float(i-lo(1)) + 0.5d0)
-              if (xcen <= center(1)+1) then
+              if (xcen <= split(1)+1) then
                  mag_x(i,j,k,1) = B_x_l
               else
                  mag_x(i,j,k,1) = B_x_r
@@ -262,7 +200,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)+1
            do i = lo(1), hi(1)
               xcen = xlo(1) + delta(1)*(float(i-lo(1)) + 0.5d0)
-              if (xcen <= center(1)) then
+              if (xcen <= split(1)) then
                  mag_y(i,j,k,1) = B_y_l
               else
                  mag_y(i,j,k,1) = B_y_r
@@ -275,7 +213,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)
            do i = lo(1), hi(1)
               xcen = xlo(1) + delta(1)*(float(i-lo(1)) + 0.5d0)
-              if (xcen <= center(1)) then
+              if (xcen <= split(1)) then
                  mag_z(i,j,k,1) = B_z_l
               else
                  mag_z(i,j,k,1) = B_z_r
@@ -285,16 +223,13 @@ subroutine ca_initmag(level, time, lo, hi, &
      enddo
 
   endif
-
-
-
 
   if (idir .eq. 2) then
      do k = lo(3), hi(3)
         do j = lo(2), hi(2)
            ycen = xlo(2) + delta(2)*(float(j-lo(2)) + 0.5d0)
            do i = lo(1), hi(1)+1
-              if (ycen <= center(2)) then
+              if (ycen <= split(2)) then
                  mag_x(i,j,k,1) = B_x_l
               else
                  mag_x(i,j,k,1) = B_x_r
@@ -307,7 +242,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)+1
            ycen = xlo(2) + delta(2)*(float(j-lo(2)) + 0.5d0)
            do i = lo(1), hi(1)
-              if (ycen <= center(2)+1) then
+              if (ycen <= split(2)+1) then
                  mag_y(i,j,k,1) = B_y_l
               else
                  mag_y(i,j,k,1) = B_y_r
@@ -320,7 +255,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)
            ycen = xlo(2) + delta(2)*(float(j-lo(2)) + 0.5d0)
            do i = lo(1), hi(1)
-              if (ycen <= center(2)) then
+              if (ycen <= split(2)) then
                  mag_z(i,j,k,1) = B_z_l
               else
                  mag_z(i,j,k,1) = B_z_r
@@ -331,14 +266,12 @@ subroutine ca_initmag(level, time, lo, hi, &
 
   endif
 
-
-
   if (idir .eq. 3) then
      do k = lo(3), hi(3)
         zcen = xlo(3) + delta(3)*(float(k-lo(3)) + 0.5d0)
         do j = lo(2), hi(2)
            do i = lo(1), hi(1)+1
-              if (zcen <= center(3)) then
+              if (zcen <= split(3)) then
                  mag_x(i,j,k,1) = B_x_l
               else
                  mag_x(i,j,k,1) = B_x_r
@@ -351,7 +284,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         zcen = xlo(3) + delta(3)*(float(k-lo(3)) + 0.5d0)
         do j = lo(2), hi(2)+1
            do i = lo(1), hi(1)
-              if (zcen <= center(3)) then
+              if (zcen <= split(3)) then
                  mag_y(i,j,k,1) = B_y_l
               else
                  mag_y(i,j,k,1) = B_y_r
@@ -364,7 +297,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         zcen = xlo(3) + delta(3)*(float(k-lo(3)) + 0.5d0)
         do j = lo(2), hi(2)
            do i = lo(1), hi(1)
-              if (zcen <= center(3)+1) then
+              if (zcen <= split(3)+1) then
                  mag_z(i,j,k,1) = B_z_l
               else
                  mag_z(i,j,k,1) = B_z_r
@@ -376,3 +309,4 @@ subroutine ca_initmag(level, time, lo, hi, &
   endif
 
 end subroutine ca_initmag
+
