cc40a8e0f95bf501211e5852126a2e2b2c485e25
Michael Zingale <michael.zingale@stonybrook.edu>
2020-01-23 11:23:45 -0500

    rename to avoid .o name clash

--- Source/driver/Castro_grid_util.cpp
+++ Source/driver/Castro_grid_util.cpp
@@ -16,40 +16,44 @@ Castro::position_c(const int i, const int j, const int k,
   loc.resize(3);
 
   const int* domain_lo = geom.Domain().loVect();
-  int* domain_hi = geom.Domain().hiVect();
+  const int* domain_hi = geom.Domain().hiVect();
 
   const Real *dx = geom.CellSize();
 
   const Real* prob_lo = geom.ProbLo();
+  const Real* prob_hi = geom.ProbHi();
 
-  amrex::Vector<Real> offset = {prob_lo[0]. prob_lo[1], prob_lo[2]};
+  const int* lo_bc = phys_bc.lo();
+  const int* hi_bc = phys_bc.hi();
 
   amrex::Vector<int> idx = {i, j, k};
 
   for (int idir = 0; idir < 3; idir++) {
+    int correction = 0;
+    Real offset = prob_lo[idir];
+
     if ((idir == 0 && ccx) || (idir == 1 && ccy) || (idir == 2 && ccz)) {
 
       // If we're cell-centered, we want to be in the middle of the zone.
-      offset[dir] = offset[dir] + 0.5 * dx[dir];
+      offset += 0.5 * dx[idir];
 
     } else {
       // Take care of the fact that for edge-centered indexing,
       // we actually range from (domlo, domhi+1).
-      domain_hi[idir] += 1;
-    }
-  }
-
-  // Be careful when using periodic boundary conditions. In that case,
-  // we need to loop around to the other side of the domain.
-
-  for {int idir = 0; idir < 3; idir++) {
-    if (physbc_lo[idir] == Interior && idx(dir) < domain_lo[idir]) {
-      offset[dir] += (probhi[dir] - problo[dir]);
-    } else if (physbc_hi[dir] == Interior && idx[dir] > domain_hi[idir]) {
-      offset(dir) = offset(dir) + (problo(dir) - probhi(dir));
+      correction = 1;
     }
 
-    loc[idir] = offset[idir] + static_cast<double> (idx[idir]) * dx[idir];
+    // Be careful when using periodic boundary conditions. In that case,
+    // we need to loop around to the other side of the domain.
+
+    if (lo_bc[idir] == Interior && idx[idir] < domain_lo[idir]) {
+      offset += (prob_hi[idir] - prob_lo[idir]);
+
+    } else if (hi_bc[idir] == Interior && idx[idir] > domain_hi[idir] + correction) {
+      offset += (prob_lo[idir] - prob_hi[idir]);
+    }
+
+    loc[idir] = offset + static_cast<double> (idx[idir]) * dx[idir];
   }
 
   return loc;
