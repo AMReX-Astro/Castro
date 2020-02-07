291fbdc11c5819021f12b92c7bd600bf25abd7fc
Michael Zingale <michael.zingale@stonybrook.edu>
2020-02-06 20:09:42 -0500

    get SDC compiling

--- Source/hydro/Castro_mol_hydro.cpp
+++ Source/hydro/Castro_mol_hydro.cpp
@@ -91,16 +91,13 @@ Castro::construct_mol_hydro_source(Real time, Real dt, MultiFab& A_update)
         flatn.resize(obx, 1);
         Elixir elix_flatn = flatn.elixir();
 
+        Array4<Real const> const q_arr = q.array(mfi);
         Array4<Real> const flatn_arr = flatn.array();
 
         if (first_order_hydro == 1) {
           AMREX_PARALLEL_FOR_3D(obx, i, j, k, { flatn_arr(i,j,k) = 0.0; });
         } else if (use_flattening == 1) {
-#pragma gpu box(obx)
-          ca_uflatten
-            (AMREX_INT_ANYD(obx.loVect()), AMREX_INT_ANYD(obx.hiVect()),
-             BL_TO_FORTRAN_ANYD(q[mfi]),
-             BL_TO_FORTRAN_ANYD(flatn), QPRES+1);
+          uflatten(obx, q_arr, flatn_arr, QPRES);
         } else {
           AMREX_PARALLEL_FOR_3D(obx, i, j, k, { flatn_arr(i,j,k) = 1.0; });
         }
@@ -381,10 +378,10 @@ Castro::construct_mol_hydro_source(Real time, Real dt, MultiFab& A_update)
               Array4<Real const> const avis_arr = avis.array();
 
               AMREX_PARALLEL_FOR_4D(nbx, NUM_STATE, i, j, k, n, {
-                  if (n == Temp) {
+                  if (n == UTEMP) {
                     flux_arr(i,j,k,n) = 0.0;
 #ifdef SHOCK_VAR
-                  } else if (n == Shock) {
+                  } else if (n == USHK) {
                     flux_arr(i,j,k,n) == 0.0;
 #endif
                   } else {
@@ -502,16 +499,11 @@ Castro::construct_mol_hydro_source(Real time, Real dt, MultiFab& A_update)
 
               // set UTEMP and USHK fluxes to zero
               Array4<Real> const flux_arr = (flux[idir]).array();
-              const int temp_comp = Temp;
-#ifdef SHOCK_VAR
-              const int shk_comp = Shock;
-#endif
-
               AMREX_PARALLEL_FOR_3D(nbx, i, j, k,
                                     {
-                                      flux_arr(i,j,k,temp_comp) = 0.e0;
+                                      flux_arr(i,j,k,UTEMP) = 0.e0;
 #ifdef SHOCK_VAR
-                                      flux_arr(i,j,k,shk_comp) = 0.e0;
+                                      flux_arr(i,j,k,USHK) = 0.e0;
 #endif
                                     });
 
@@ -678,7 +670,7 @@ Castro::construct_mol_hydro_source(Real time, Real dt, MultiFab& A_update)
 #endif
 
 #if AMREX_SPACEDIM == 2
-            if (!mom_flux_has_p[0][0]) {
+            if (!momx_flux_has_p[0]) {
               AMREX_PARALLEL_FOR_3D(nbx, i, j, k,
                                     {
                                       pradial_fab(i,j,k) = qex_fab(i,j,k,prescomp) * dt;
