53aca4dc830693353125052b93510fe6bee4187f
Michael Zingale <michael.zingale@stonybrook.edu>
2020-02-07 22:00:46 -0500

    get hydro compiling

--- Source/hydro/Castro_ctu_hydro.cpp
+++ Source/hydro/Castro_ctu_hydro.cpp
@@ -193,14 +193,14 @@ Castro::construct_ctu_hydro_source(Real time, Real dt)
 
         AMREX_PARALLEL_FOR_3D(obx, i, j, k,
         {
-          flatn(i,j,k) = flatn(i,j,k) * flatg(i,j,k);
+          flatn_arr(i,j,k) = flatn_arr(i,j,k) * flatg_arr(i,j,k);
 
-          if (flatten_pp_threshold > ZERO) {
-            if ( q(i-1,j,k,QU) + q(i,j-1,k,QV) + q(i,j,k-1,QW) > &
-                 q(i+1,j,k,QU) + q(i,j+1,k,QV) + q(i,j,k+1,QW) ) {
+          if (Radiation::flatten_pp_threshold > 0.0) {
+            if ( q_arr(i-1,j,k,QU) + q_arr(i,j-1,k,QV) + q_arr(i,j,k-1,QW) >
+                 q_arr(i+1,j,k,QU) + q_arr(i,j+1,k,QV) + q_arr(i,j,k+1,QW) ) {
 
-              if (q(i,j,k,QPRES) < flatten_pp_threshold * q(i,j,k,QPTOT)) {
-                flatn(i,j,k) = 0.0;
+              if (q_arr(i,j,k,QPRES) < Radiation::flatten_pp_threshold * q_arr(i,j,k,QPTOT)) {
+                flatn_arr(i,j,k) = 0.0;
               }
             }
           }
