c31d50cf54b32c9b09cd3b9b249052e2d1be4aee
Jean M. Sexton <jmsexton@lbl.gov>
2020-02-20 10:47:26 -0800

    Update sdc convergence test scripts to use TRUESDC in executable name

---
+++ Exec/hydro_tests/acoustic_pulse/convergence_rk4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# echo the commands
+set -x
+
+DIM=2
+EXEC=./Castro${DIM}d.gnu.MPI.TRUESDC.ex
+
+RUNPARAMS="castro.fourth_order=1 castro.mol_order=4 castro.time_integration_method=1"
+
+mpiexec -n 4 ${EXEC} inputs.2d.64 ${RUNPARAMS} amr.plot_file=acoustic_pulse_64_rk4_plt &> 64.out
+mpiexec -n 4 ${EXEC} inputs.2d.128 ${RUNPARAMS} amr.plot_file=acoustic_pulse_128_rk4_plt &> 128.out
+mpiexec -n 4 ${EXEC} inputs.2d.256 ${RUNPARAMS} amr.plot_file=acoustic_pulse_256_rk4_plt &> 256.out
+mpiexec -n 4 ${EXEC} inputs.2d.512 ${RUNPARAMS} amr.plot_file=acoustic_pulse_512_rk4_plt &> 512.out
+
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_64_rk4_plt00081 mediFile=acoustic_pulse_128_rk4_plt00161 fineFile=acoustic_pulse_256_rk4_plt00321 > convergence.${DIM}d.lo.rk4.out
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=acoustic_pulse_128_rk4_plt00161 mediFile=acoustic_pulse_256_rk4_plt00321 fineFile=acoustic_pulse_512_rk4_plt00641 > convergence.${DIM}d.hi.rk4.out
--- Exec/reacting_tests/reacting_convergence/convergence_sdc.sh
+++ Exec/reacting_tests/reacting_convergence/convergence_sdc.sh
@@ -4,7 +4,7 @@
 set -x
 
 DIM=1
-EXEC=./Castro${DIM}d.gnu.MPI.ex
+EXEC=./Castro${DIM}d.gnu.MPI.TRUESDC.ex
 
 RUNPARAMS="castro.sdc_order=2 castro.time_integration_method=2 castro.ppm_type=0 castro.sdc_solve_for_rhoe=1 castro.sdc_solver_tol_dens=1.e-10 castro.sdc_solver_tol_spec=1.e-10 castro.sdc_solver_tol_ener=1.e-10 castro.sdc_solver=1 "
 
