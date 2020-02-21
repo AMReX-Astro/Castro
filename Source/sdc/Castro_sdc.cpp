9a177d4f870c88c48b8f87d4ca4618a6c0b1ee76
Jean M. Sexton <jmsexton@lbl.gov>
2020-02-20 18:10:23 -0800

    Convert update_advection_o4

--- Source/sdc/Castro_sdc.cpp
+++ Source/sdc/Castro_sdc.cpp
@@ -256,25 +256,18 @@ Castro::do_sdc_update(int m_start, int m_end, Real dt) {
       }
 
     } else {
-      if (sdc_quadrature == 0) {
-        ca_sdc_update_advection_o4_lobatto(BL_TO_FORTRAN_BOX(bx), &dt_m, &dt,
-                                           BL_TO_FORTRAN_3D((*k_new[m_start])[mfi]),
-                                           BL_TO_FORTRAN_3D((*k_new[m_end])[mfi]),
-                                           BL_TO_FORTRAN_3D((*A_new[m_start])[mfi]),
-                                           BL_TO_FORTRAN_3D((*A_old[0])[mfi]),
-                                           BL_TO_FORTRAN_3D((*A_old[1])[mfi]),
-                                           BL_TO_FORTRAN_3D((*A_old[2])[mfi]),
-                                           &m_start);
+        amrex::Array4<const amrex::Real> const& A_old_2_arr=(A_old[2])->array(mfi);
+        if (sdc_quadrature == 0) {
+            ca_sdc_update_advection_o4_lobatto(bx, dt_m, dt, k_new_m_start_arr, k_new_m_end_arr,
+                                               A_new_arr, A_old_0_arr, A_old_1_arr, A_old_2_arr,
+                                               m_start);
+
       } else {
-        ca_sdc_update_advection_o4_radau(BL_TO_FORTRAN_BOX(bx), &dt_m, &dt,
-                                         BL_TO_FORTRAN_3D((*k_new[m_start])[mfi]),
-                                         BL_TO_FORTRAN_3D((*k_new[m_end])[mfi]),
-                                         BL_TO_FORTRAN_3D((*A_new[m_start])[mfi]),
-                                         BL_TO_FORTRAN_3D((*A_old[0])[mfi]),
-                                         BL_TO_FORTRAN_3D((*A_old[1])[mfi]),
-                                         BL_TO_FORTRAN_3D((*A_old[2])[mfi]),
-                                         BL_TO_FORTRAN_3D((*A_old[3])[mfi]),
-                                         &m_start);
+            amrex::Array4<const amrex::Real> const& A_old_3_arr=(A_old[3])->array(mfi);
+            ca_sdc_update_advection_o4_radau(bx, dt_m, dt, k_new_m_start_arr, k_new_m_end_arr,
+                                             A_new_arr, A_old_0_arr, A_old_1_arr, A_old_2_arr,
+                                             A_old_3_arr, m_start);
+
       }
 
     }
@@ -415,6 +408,103 @@ Castro::ca_sdc_update_advection_o2_radau(const amrex::Box& bx,
         });
     }
 }
+
+void
+Castro::ca_sdc_update_advection_o4_lobatto(const amrex::Box& bx,
+                                         amrex::Real dt_m, amrex::Real dt,
+                                         amrex::Array4<const amrex::Real> const& k_m,
+                                         amrex::Array4<amrex::Real> const& k_n,
+                                         amrex::Array4<const amrex::Real> const& A_m,
+                                         amrex::Array4<const amrex::Real> const& A_0_old,
+                                         amrex::Array4<const amrex::Real> const& A_1_old,
+                                         amrex::Array4<const amrex::Real> const& A_2_old,
+                                         int m_start)
+{
+    // update k_m to k_n via advection -- this is a fourth order accurate update
+
+    // here, dt_m is the update for this stage, from one time node to the next
+    // dt is the update over the whole timestep, n to n+1
+
+    // Gauss-Lobatto (Simpsons)
+
+    if (m_start == 0)
+    {
+        AMREX_PARALLEL_FOR_4D(bx, k_n.nComp(), i, j, k, n,
+        {
+            k_n(i,j,k,n) = k_m(i,j,k,n) +
+                dt_m * (A_m(i,j,k,n) - A_0_old(i,j,k,n)) +
+                dt/24.0_rt * (5.0_rt*A_0_old(i,j,k,n) + 8.0_rt*A_1_old(i,j,k,n) - A_2_old(i,j,k,n));
+        });
+    }
+    else if (m_start == 1)
+    {
+        AMREX_PARALLEL_FOR_4D(bx, k_n.nComp(), i, j, k, n,
+        {
+            k_n(i,j,k,n) = k_m(i,j,k,n) +
+                dt_m * (A_m(i,j,k,n) - A_1_old(i,j,k,n)) +
+                dt/24.0_rt * (-A_0_old(i,j,k,n) + 8.0_rt*A_1_old(i,j,k,n) + 5.0_rt*A_2_old(i,j,k,n));
+        });
+    }
+    else
+        amrex::Abort("error in ca_sdc_update_advection_o4_lobatto -- should not be here");
+}
+
+void
+Castro::ca_sdc_update_advection_o4_radau(const amrex::Box& bx,
+                                         amrex::Real dt_m, amrex::Real dt,
+                                         amrex::Array4<const amrex::Real> const& k_m,
+                                         amrex::Array4<amrex::Real> const& k_n,
+                                         amrex::Array4<const amrex::Real> const& A_m,
+                                         amrex::Array4<const amrex::Real> const& A_0_old,
+                                         amrex::Array4<const amrex::Real> const& A_1_old,
+                                         amrex::Array4<const amrex::Real> const& A_2_old,
+                                         amrex::Array4<const amrex::Real> const& A_3_old,
+                                         int m_start)
+{
+    // update k_m to k_n via advection -- this is a fourth-order accurate update
+
+    // here, dt_m is the update for this stage, from one time node to the next
+    // dt is the update over the whole timestep, n to n+1
+
+    // Gauss-Lobatto (Simpsons)
+
+    if (m_start == 0)
+    {
+        AMREX_PARALLEL_FOR_4D(bx, k_n.nComp(), i, j, k, n,
+        {
+            k_n(i,j,k,n) = k_m(i,j,k,n) +
+                dt_m * (A_m(i,j,k,n) - A_0_old(i,j,k,n)) +
+                dt/1800.0_rt * ((-35.0_rt*sqrt(6.0_rt) + 440.0_rt)*A_1_old(i,j,k,n) +
+                                (-169.0_rt*sqrt(6.0_rt) + 296.0_rt)*A_2_old(i,j,k,n) +
+                                (-16.0_rt + 24.0_rt*sqrt(6.0_rt))*A_3_old(i,j,k,n));
+        });
+    }
+    else if (m_start == 1)
+    {
+        AMREX_PARALLEL_FOR_4D(bx, k_n.nComp(), i, j, k, n,
+        {
+            k_n(i,j,k,n) = k_m(i,j,k,n) +
+                dt_m * (A_m(i,j,k,n) - A_1_old(i,j,k,n)) +
+                dt/150.0_rt * ((-12.0_rt + 17.0_rt*sqrt(6.0_rt))*A_1_old(i,j,k,n) +
+                               (12.0_rt + 17.0_rt*sqrt(6.0_rt))*A_2_old(i,j,k,n) +
+                               (-4.0_rt*sqrt(6.0_rt))*A_3_old(i,j,k,n));
+        });
+    }
+    else if (m_start == 2)
+    {
+        AMREX_PARALLEL_FOR_4D(bx, k_n.nComp(), i, j, k, n,
+        {
+            k_n(i,j,k,n) = k_m(i,j,k,n) +
+                dt_m * (A_m(i,j,k,n) - A_2_old(i,j,k,n)) +
+                dt/600.0_rt * ((168.0_rt - 73.0_rt*sqrt(6.0_rt))*A_1_old(i,j,k,n) +
+                               (120.0_rt + 5.0_rt*sqrt(6.0_rt))*A_2_old(i,j,k,n) +
+                               (72.0_rt + 8.0_rt*sqrt(6.0_rt))*A_3_old(i,j,k,n));
+        });
+    }
+    else
+        amrex::Abort("error in ca_sdc_update_advection_o4_radau -- should not be here");
+}
+
 
 
 
