33dfe143ae6d9092f32636addffe45302465baa4
Michael Zingale <michael.zingale@stonybrook.edu>
2020-03-01 10:56:25 -0500

    save current state

--- Source/hydro/riemann_nd.F90
+++ Source/hydro/riemann_nd.F90
@@ -98,52 +98,111 @@ contains
 
     !$gpu
 
-    ! Solve Riemann problem to get the fluxes
-    if (riemann_solver == 0 .or. riemann_solver == 1) then
-       ! Colella, Glaz, & Ferguson solver
+    ! do we want to force the flux to zero at the boundary?
+    special_bnd_lo = (physbc_lo(idir) == Symmetry &
+         .or.         physbc_lo(idir) == SlipWall &
+         .or.         physbc_lo(idir) == NoSlipWall)
+    special_bnd_hi = (physbc_hi(idir) == Symmetry &
+         .or.         physbc_hi(idir) == SlipWall &
+         .or.         physbc_hi(idir) == NoSlipWall)
 
-       call riemann_state(lo, hi, &
-                          qm, qm_lo, qm_hi, &
+    do k = lo(3), hi(3)
+       do j = lo(2), hi(2)
+          do i = lo(1), hi(1)
+
+             call interface_input_states(i, j, k, &
+                                         qm, qm_lo, qm_hi, &
+                                         qp, qp_lo, qp_hi, &
+                                         qaux, qa_lo, qa_hi, &
+                                         idir, compute_gammas, &
+                                         qleft, qright, &
+                                         gamcl, gamcr, &
+#ifdef RADIATION
+                                         laml, lamr, &
+                                         gamcgl, gamcgr, &
+#endif
+                                         csmall, cavg, bnd_fac, &
+                                         special_bnd_lo, special_bnd_hi, &
+                                         domlo, domhi)
+
+             if (riemann_solver == 0) then
+                ! Colella, Glaz, & Ferguson solver
+
+                call riemannus(qleft, qright, &
+                               gamcl, gamcr, &
+#ifdef RADIATION
+                               laml, lamr, &
+                               gamcgl, gamcgr, &
+#endif
+                               csmall, cavg, &
+                               bnd_fac, &
+                               qint_zone, &
+#ifdef RADIATION
+                               lambda_int_zone, &
+#endif
+                               idir)
+
+                qint(i,j,k,:) = qint_zone(:)
+#ifdef RADIATION
+                lambda_int(i,j,k,:) = lambda_int_zone(:)
+#endif
+
+                call compute_flux_q(lo, hi, &
+                                    qint, q_lo, q_hi, &
+                                    flx, flx_lo, flx_hi, &
+#ifdef RADIATION
+                                    lambda_int, q_lo, q_hi, &
+                                    rflx, rflx_lo, rflx_hi, &
+#endif
+                                    idir, 0)
+
+
+             else if (riemann_solver == 1) then
+                ! Colella & Glaz solver
+
+#ifndef RADIATION
+                call riemanncg(qleft, qright, &
+                               gamcl, gamcr, &
+                               csmall, cavg, &
+                               bnd_fac, &
+                               qint_zone_zone, &
+                               idir)
+
+                qint(i,j,k,:) = qint_zone(:)
+#ifdef RADIATION
+                lambda_int(i,j,k,:) = lambda_int_zone(:)
+#endif
+
+                call compute_flux_q(lo, hi, &
+                                    qint, q_lo, q_hi, &
+                                    flx, flx_lo, flx_hi, &
+#ifdef RADIATION
+                                    lambda_int, q_lo, q_hi, &
+                                    rflx, rflx_lo, rflx_hi, &
+#endif
+                                    idir, 0)
+
+
+             else if (riemann_solver == 2) then
+
+                ! HLLC
+                call HLLC(qm, qm_lo, qm_hi, &
                           qp, qp_lo, qp_hi, &
-                          qint, q_lo, q_hi, &
-#ifdef RADIATION
-                          lambda_int, q_lo, q_hi, &
-#endif
                           qaux, qa_lo, qa_hi, &
-                          idir, 0, &
+                          flx, flx_lo, flx_hi, &
+                          qint, q_lo, q_hi, &
+                          idir, lo, hi, &
                           domlo, domhi)
-
-       call compute_flux_q(lo, hi, &
-                           qint, q_lo, q_hi, &
-                           flx, flx_lo, flx_hi, &
-#ifdef RADIATION
-                           lambda_int, q_lo, q_hi, &
-                           rflx, rflx_lo, rflx_hi, &
-#endif
-                           idir, 0)
-
-    elseif (riemann_solver == 2) then
-       ! HLLC
-       call HLLC(qm, qm_lo, qm_hi, &
-                 qp, qp_lo, qp_hi, &
-                 qaux, qa_lo, qa_hi, &
-                 flx, flx_lo, flx_hi, &
-                 qint, q_lo, q_hi, &
-                 idir, lo, hi, &
-                 domlo, domhi)
 #ifndef AMREX_USE_CUDA
-    else
-       call castro_error("ERROR: invalid value of riemann_solver")
+             else
+                call castro_error("ERROR: invalid value of riemann_solver")
 #endif
-    endif
+             endif
 
 
-    if (hybrid_riemann == 1) then
-       ! correct the fluxes using an HLL scheme if we are in a shock
-       ! and doing the hybrid approach
-       do k = lo(3), hi(3)
-          do j = lo(2), hi(2)
-             do i = lo(1), hi(1)
+             if (hybrid_riemann == 1) then
+                ! correct the fluxes using an HLL scheme if we are in a shock
+                ! and doing the hybrid approach
 
                 is_shock = 0
 
@@ -170,129 +229,33 @@ contains
                       cr = qaux(i,j,k,QC)
                    end select
 
-                   ql_zone(:) = qm(i,j,k,:)
-                   qr_zone(:) = qp(i,j,k,:)
-                   call HLL(ql_zone, qr_zone, cl, cr, idir, flx_zone)
+                   call HLL(qleft, qright, cl, cr, idir, flx_zone)
                    flx(i,j,k,:) = flx_zone(:)
-                endif
+                end if
 
-             end do
+             end if
+
           end do
        end do
-
-    endif
-
-    call ca_store_godunov_state(lo, hi, &
-                                qint, q_lo, q_hi, &
-#ifdef RADIATION
-                                lambda_int, li_lo, li_hi, &
-#endif
-                                qgdnv, qg_lo, qg_hi)
+    end do
 
   end subroutine cmpflx_plus_godunov
 
 
-  subroutine riemann_state(lo, hi, &
-                           qm, qm_lo, qm_hi, &
-                           qp, qp_lo, qp_hi, &
-                           qint, q_lo, q_hi, &
+  subroutine interface_input_states(i, j, k, &
+                                    qm, qm_lo, qm_hi, &
+                                    qp, qp_lo, qp_hi, &
+                                    qaux, qa_lo, qa_hi, &
+                                    idir, compute_gammas, &
+                                    qleft, qright, &
+                                    gamcl, gamcr, &
 #ifdef RADIATION
-                           lambda_int, l_lo, l_hi, &
+                                    laml, lamr, &
+                                    gamcgl, gamcgr, &
 #endif
-                           qaux, qa_lo, qa_hi, &
-                           idir, compute_gammas, &
-                           domlo, domhi) bind(C, name="riemann_state")
-
-    ! just compute the hydrodynamic state on the interfaces
-    ! don't compute the fluxes
-
-    use eos_module, only: eos
-    use eos_type_module, only: eos_t, eos_input_re, eos_input_rp
-    use network, only: nspec, naux
-    use castro_error_module
-    use amrex_fort_module, only : rt => amrex_real
-    use meth_params_module, only : hybrid_riemann, ppm_temp_fix, riemann_solver, &
-                                   T_guess
-    use prob_params_module, only : physbc_lo, physbc_hi, &
-         Symmetry, SlipWall, NoSlipWall
-
-    implicit none
-
-    integer, intent(in) :: qm_lo(3), qm_hi(3)
-    integer, intent(in) :: qp_lo(3), qp_hi(3)
-    integer, intent(in) :: q_lo(3), q_hi(3)
-    integer, intent(in) :: qa_lo(3), qa_hi(3)
-
-    integer, intent(in), value :: idir, compute_gammas
-    ! note: lo, hi are not necessarily the limits of the valid (no
-    ! ghost cells) domain, but could be hi+1 in some dimensions.  We
-    ! rely on the caller to specific the interfaces over which to
-    ! solve the Riemann problems
-    integer, intent(in) :: lo(3), hi(3)
-    integer, intent(in) :: domlo(3), domhi(3)
-
-    real(rt), intent(inout) :: qm(qm_lo(1):qm_hi(1),qm_lo(2):qm_hi(2),qm_lo(3):qm_hi(3),NQ)
-    real(rt), intent(inout) :: qp(qp_lo(1):qp_hi(1),qp_lo(2):qp_hi(2),qp_lo(3):qp_hi(3),NQ)
-
-    real(rt), intent(inout) :: qint(q_lo(1):q_hi(1),q_lo(2):q_hi(2),q_lo(3):q_hi(3),NQ)
-#ifdef RADIATION
-    integer, intent(in) :: l_lo(3), l_hi(3)
-    real(rt), intent(inout) :: lambda_int(l_lo(1):l_hi(1),l_lo(2):l_hi(2),l_lo(3):l_hi(3),0:ngroups-1)
-#endif
-
-    real(rt), intent(in) :: qaux(qa_lo(1):qa_hi(1),qa_lo(2):qa_hi(2),qa_lo(3):qa_hi(3),NQAUX)
-
-    ! local variables
-
-    integer i, j, k
-
-    type (eos_t) :: eos_state
-
-    real(rt) :: qleft(NQ), qright(NQ)
-    real(rt) :: qint_zone(NQ)
-    real(rt) :: lambda_int_zone(0:ngroups-1)
-    real(rt) :: bnd_fac
-    real(rt) :: csmall, cavg
-    real(rt) :: gamcl, gamcr
-#ifdef RADIATION
-    real(rt) :: gamcgl, gamcgr
-#endif
-
-    logical :: special_bnd_lo, special_bnd_hi
-
-    !$gpu
-
-#ifdef RADIATION
-#ifndef AMREX_USE_CUDA
-    if (hybrid_riemann == 1) then
-       call castro_error("ERROR: hybrid Riemann not supported for radiation")
-    endif
-
-    if (riemann_solver > 0) then
-       call castro_error("ERROR: only the CGF Riemann solver is supported for radiation")
-    endif
-#endif
-#endif
-
-#if AMREX_SPACEDIM == 1
-#ifndef AMREX_USE_CUDA
-    if (riemann_solver > 1) then
-       call castro_error("ERROR: HLLC not implemented for 1-d")
-    endif
-#endif
-#endif
-
-    ! do we want to force the flux to zero at the boundary?
-    special_bnd_lo = (physbc_lo(idir) == Symmetry &
-         .or.         physbc_lo(idir) == SlipWall &
-         .or.         physbc_lo(idir) == NoSlipWall)
-    special_bnd_hi = (physbc_hi(idir) == Symmetry &
-         .or.         physbc_hi(idir) == SlipWall &
-         .or.         physbc_hi(idir) == NoSlipWall)
-
-    do k = lo(3), hi(3)
-       do j = lo(2), hi(2)
-          do i = lo(1), hi(1)
+                                    csmall, cavg, bnd_fac, &
+                                    special_bnd_lo, special_bnd_hi, &
+                                    domlo, domhi) bind(C, name="riemann_state")
 
              ! Extract this zone's interface values
 
@@ -430,18 +393,138 @@ contains
              end if
 
 
+  end subroutine interface_input_states
+
+
+  subroutine riemann_state(lo, hi, &
+                           qm, qm_lo, qm_hi, &
+                           qp, qp_lo, qp_hi, &
+                           qint, q_lo, q_hi, &
+#ifdef RADIATION
+                           lambda_int, l_lo, l_hi, &
+#endif
+                           qaux, qa_lo, qa_hi, &
+                           idir, compute_gammas, &
+                           domlo, domhi) bind(C, name="riemann_state")
+
+    ! just compute the hydrodynamic state on the interfaces
+    ! don't compute the fluxes
+
+    use eos_module, only: eos
+    use eos_type_module, only: eos_t, eos_input_re, eos_input_rp
+    use network, only: nspec, naux
+    use castro_error_module
+    use amrex_fort_module, only : rt => amrex_real
+    use meth_params_module, only : hybrid_riemann, ppm_temp_fix, riemann_solver, &
+                                   T_guess
+    use prob_params_module, only : physbc_lo, physbc_hi, &
+         Symmetry, SlipWall, NoSlipWall
+
+    implicit none
+
+    integer, intent(in) :: qm_lo(3), qm_hi(3)
+    integer, intent(in) :: qp_lo(3), qp_hi(3)
+    integer, intent(in) :: q_lo(3), q_hi(3)
+    integer, intent(in) :: qa_lo(3), qa_hi(3)
+
+    integer, intent(in), value :: idir, compute_gammas
+    ! note: lo, hi are not necessarily the limits of the valid (no
+    ! ghost cells) domain, but could be hi+1 in some dimensions.  We
+    ! rely on the caller to specific the interfaces over which to
+    ! solve the Riemann problems
+    integer, intent(in) :: lo(3), hi(3)
+    integer, intent(in) :: domlo(3), domhi(3)
+
+    real(rt), intent(inout) :: qm(qm_lo(1):qm_hi(1),qm_lo(2):qm_hi(2),qm_lo(3):qm_hi(3),NQ)
+    real(rt), intent(inout) :: qp(qp_lo(1):qp_hi(1),qp_lo(2):qp_hi(2),qp_lo(3):qp_hi(3),NQ)
+
+    real(rt), intent(inout) :: qint(q_lo(1):q_hi(1),q_lo(2):q_hi(2),q_lo(3):q_hi(3),NQ)
+#ifdef RADIATION
+    integer, intent(in) :: l_lo(3), l_hi(3)
+    real(rt), intent(inout) :: lambda_int(l_lo(1):l_hi(1),l_lo(2):l_hi(2),l_lo(3):l_hi(3),0:ngroups-1)
+#endif
+
+    real(rt), intent(in) :: qaux(qa_lo(1):qa_hi(1),qa_lo(2):qa_hi(2),qa_lo(3):qa_hi(3),NQAUX)
+
+    ! local variables
+
+    integer i, j, k
+
+    type (eos_t) :: eos_state
+
+    real(rt) :: qleft(NQ), qright(NQ)
+    real(rt) :: qint_zone(NQ)
+    real(rt) :: lambda_int_zone(0:ngroups-1)
+    real(rt) :: bnd_fac
+    real(rt) :: csmall, cavg
+    real(rt) :: gamcl, gamcr
+#ifdef RADIATION
+    real(rt) :: gamcgl, gamcgr
+#endif
+
+    logical :: special_bnd_lo, special_bnd_hi
+
+    !$gpu
+
+#ifdef RADIATION
+#ifndef AMREX_USE_CUDA
+    if (hybrid_riemann == 1) then
+       call castro_error("ERROR: hybrid Riemann not supported for radiation")
+    endif
+
+    if (riemann_solver > 0) then
+       call castro_error("ERROR: only the CGF Riemann solver is supported for radiation")
+    endif
+#endif
+#endif
+
+#if AMREX_SPACEDIM == 1
+#ifndef AMREX_USE_CUDA
+    if (riemann_solver > 1) then
+       call castro_error("ERROR: HLLC not implemented for 1-d")
+    endif
+#endif
+#endif
+
+    ! do we want to force the flux to zero at the boundary?
+    special_bnd_lo = (physbc_lo(idir) == Symmetry &
+         .or.         physbc_lo(idir) == SlipWall &
+         .or.         physbc_lo(idir) == NoSlipWall)
+    special_bnd_hi = (physbc_hi(idir) == Symmetry &
+         .or.         physbc_hi(idir) == SlipWall &
+         .or.         physbc_hi(idir) == NoSlipWall)
+
+    do k = lo(3), hi(3)
+       do j = lo(2), hi(2)
+          do i = lo(1), hi(1)
+
+             call interface_input_states(i, j, k, &
+                                         qm, qm_lo, qm_hi, &
+                                         qp, qp_lo, qp_hi, &
+                                         qaux, qa_lo, qa_hi, &
+                                         idir, compute_gammas, &
+                                         qleft, qright, &
+                                         gamcl, gamcr, &
+#ifdef RADIATION
+                                         laml, lamr, &
+                                         gamcgl, gamcgr, &
+#endif
+                                         csmall, cavg, bnd_fac, &
+                                         special_bnd_lo, special_bnd_hi, &
+                                         domlo, domhi)
+
              if (riemann_solver == 0) then
                 ! Colella, Glaz, & Ferguson solver
 
                 call riemannus(qleft, qright, &
                                gamcl, gamcr, &
 #ifdef RADIATION
+                               laml, lamr, &
                                gamcgl, gamcgr, &
 #endif
                                csmall, cavg, &
                                bnd_fac, &
                                qint_zone, &
-
 #ifdef RADIATION
                                lambda_int_zone, &
 #endif
@@ -474,6 +557,7 @@ contains
 #ifdef RADIATION
              lambda_int(i,j,k,:) = lambda_int_zone(:)
 #endif
+
           end do
        end do
     end do
