a43c3fc7c4fc3a6eba7dc17f6daaec824bdf318e
Michael Zingale <michael.zingale@stonybrook.edu>
2020-03-06 20:11:09 -0500

    convert ctoprim to C++

--- Source/hydro/advection_util.cpp
+++ Source/hydro/advection_util.cpp
@@ -6,7 +6,162 @@
 #include "Radiation.H"
 #endif
 
+#include "eos.H"
+
 using namespace amrex;
+
+
+void
+Castro::ctoprim(const Box& bx,
+                Array4<Real const> const uin,
+#ifdef RADIATION
+                Array4<Real const> const Erin,
+                Array4<Real const> const lam,
+#endif
+                Array4<Real> const q_arr,
+                Array4<Real> const qaux_arr) {
+
+
+  GpuArray<int, npassive> upass_map_p;
+  GpuArray<int, npassive> qpass_map_p;
+  for (int n = 0; n < npassive; ++n) {
+    upass_map_p[n] = upass_map[n];
+    qpass_map_p[n] = qpass_map[n];
+  }
+
+  Real lsmall_dens = small_dens;
+
+  AMREX_PARALLEL_FOR_3D(bx, i, j, k,
+  {
+
+
+#ifndef AMREX_USE_CUDA
+    if (uin(i,j,k,URHO) <= 0.0_rt) {
+      std::cout << std::endl;
+      std::cout << ">>> Error: advection_util_nd.F90::ctoprim " << i << " " << j << " " << k << std::endl;
+      std::cout << ">>> ... negative density " << uin(i,j,k,URHO) << std::endl;
+      amrex::Error("Error:: advection_util_nd.f90 :: ctoprim");
+    } else if (uin(i,j,k,URHO) < lsmall_dens) {
+      std::cout << std::endl;
+      std::cout << ">>> Error: advection_util_nd.F90::ctoprim " << i << " " << j << " " << k << std::endl;
+      std::cout << ">>> ... small density " << uin(i,j,k,URHO) << std::endl;
+      amrex::Error("Error:: advection_util_nd.f90 :: ctoprim");
+    }
+#endif
+
+    q_arr(i,j,k,QRHO) = uin(i,j,k,URHO);
+    Real rhoinv = 1.0_rt/q_arr(i,j,k,QRHO);
+
+    q_arr(i,j,k,QU) = uin(i,j,k,UMX) * rhoinv;
+    q_arr(i,j,k,QV) = uin(i,j,k,UMY) * rhoinv;
+    q_arr(i,j,k,QW) = uin(i,j,k,UMZ) * rhoinv;
+
+    // Get the internal energy, which we'll use for
+    // determining the pressure.  We use a dual energy
+    // formalism. If (E - K) < eta1 and eta1 is suitably
+    // small, then we risk serious numerical truncation error
+    // in the internal energy.  Therefore we'll use the result
+    // of the separately updated internal energy equation.
+    // Otherwise, we'll set e = E - K.
+
+    Real kineng = 0.5_rt * q_arr(i,j,k,QRHO) * (q_arr(i,j,k,QU)*q_arr(i,j,k,QU) +
+                                                q_arr(i,j,k,QV)*q_arr(i,j,k,QV) +
+                                                q_arr(i,j,k,QW)*q_arr(i,j,k,QW));
+
+    if ((uin(i,j,k,UEDEN) - kineng) > dual_energy_eta1*uin(i,j,k,UEDEN)) {
+      q_arr(i,j,k,QREINT) = (uin(i,j,k,UEDEN) - kineng) * rhoinv;
+    } else {
+      q_arr(i,j,k,QREINT) = uin(i,j,k,UEINT) * rhoinv;
+    }
+
+    // If we're advecting in the rotating reference frame,
+    // then subtract off the rotation component here.
+
+#ifdef ROTATION
+    if (do_rotation == 1 && state_in_rotating_frame != 1) {
+      Real vel[3];
+      for (int n = 0; n < 3; n++) {
+        vel[n] = uin(i,j,k,UMX+n) * rhoinv;
+      }
+
+      call inertial_to_rotational_velocity([i, j, k], amr_time, vel);
+      q_arr(i,j,k,QU) = vel[0];
+      q_arr(i,j,k,QV) = vel[1];
+      q_arr(i,j,k,QW) = vel[2];
+    }
+#endif
+
+    q_arr(i,j,k,QTEMP) = uin(i,j,k,UTEMP);
+#ifdef RADIATION
+    for (int g = 0; g < NGROUPS; g++) {
+      q_arr(i,j,k,QRAD+g) = Erin(i,j,k,g);
+    }
+#endif
+
+    // Load passively advected quatities into q
+    for (int ipassive = 0; ipassive < npassive; ipassive++) {
+      int n  = upass_map_p[ipassive];
+      int iq = qpass_map_p[ipassive];
+      q_arr(i,j,k,iq) = uin(i,j,k,n) * rhoinv;
+    }
+
+    // get gamc, p, T, c, csml using q state
+    eos_t eos_state;
+    eos_state.T = q_arr(i,j,k,QTEMP);
+    eos_state.rho = q_arr(i,j,k,QRHO);
+    eos_state.e = q_arr(i,j,k,QREINT);
+    for (int n = 0; n < NumSpec; n++) {
+      eos_state.xn[n]  = q_arr(i,j,k,QFS+n);
+    }
+    for (int n = 0; n < NumAux; n++) {
+      eos_state.aux[n] = q_arr(i,j,k,QFX+n);
+    }
+
+    eos(eos_input_re, eos_state);
+
+    q_arr(i,j,k,QTEMP) = eos_state.T;
+    q_arr(i,j,k,QREINT) = eos_state.e * q_arr(i,j,k,QRHO);
+    q_arr(i,j,k,QPRES) = eos_state.p;
+    q_arr(i,j,k,QGC) = eos_state.gam1;
+
+    qaux_arr(i,j,k,QDPDR) = eos_state.dpdr_e;
+    qaux_arr(i,j,k,QDPDE) = eos_state.dpde;
+
+#ifdef RADIATION
+    qaux_arr(i,j,k,QGAMCG) = eos_state.gam1;
+    qaux_arr(i,j,k,QCG) = eos_state.cs;
+
+    Real lams[NGROUPS];
+    for (int g = 0; g < NGROUPS; g++) {
+      lams[g] = lam(i,j,k,g);
+    }
+    Real qs[NQ];
+    for (int n = 0; g < NQ; g++) {
+      qs[n] = q_arr(i,j,k,n);
+    }
+    Real ptot;
+    Real ctot;
+    Real gamc_tot;
+    compute_ptot_ctot(lams, qs, qaux_arr(i,j,k,QCG), ptot, ctot, gamc_tot);
+
+    q_arr(i,j,k,QPTOT) = ptot;
+
+    qaux_arr(i,j,k,QC) = ctot;
+    qaux_arr(i,j,k,QGAMC) = gamc_tot;
+
+    q_arr(i,j,k,qreitot) = q_arr(i,j,k,QREINT);
+    for (int g = 0; g < NGROUPS; g++) {
+      qaux_arr(i,j,k,QLAMS+g) = lam(i,j,k,g);
+      q_arr(i,j,k,qreitot) += q_arr(i,j,k,QRAD+g);
+    }
+
+#else
+    qaux_arr(i,j,k,QGAMC) = eos_state.gam1;
+    qaux_arr(i,j,k,QC) = eos_state.cs;
+#endif
+
+  });
+}
 
 
 void
