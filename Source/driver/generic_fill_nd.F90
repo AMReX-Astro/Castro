bc7c2e66c3b901d772b6db028c467611a88d5579
Max Katz <maxpkatz@gmail.com>
2020-03-22 17:28:31 -0700

    Switch over to the new Fab interface for BC fills (#819)
    This is in preparation to use the new fab_filcc in AMReX.
    
    generic_single_fill and generic_multi_fill are merged into one function, generic_fill, since the Fab interface has no notion of separate single and group interfaces. Similarly, the calls to denfill and hypfill are now both done in one function (though the functions are not merged, to preserve user-facing code).
    
    The source fill routines are also merged into the generic fill routines.
    
    Finally, instead of locally overriding the BCs in the boundary fill routine, we now globally override any inflow boundary conditions (for the non-State_Type data) to become FOEXTRAP. So this is the same result but with code that is a little simpler and will be easier to link up to AMReX later.

--- Source/driver/generic_fill_nd.F90
+++ Source/driver/generic_fill_nd.F90
@@ -1,7 +1,6 @@
 module generic_fill_module
 
   use amrex_fort_module, only: rt => amrex_real
-  use meth_params_module, only: NVAR
 
   implicit none
 
@@ -9,9 +8,7 @@ module generic_fill_module
 
 contains
 
-
-  subroutine generic_single_fill(lo, hi, state, s_lo, s_hi, domlo, domhi, delta, xlo, bc) bind(C, name="generic_single_fill")
-    ! Used for a generic fill of any StateData.
+  subroutine generic_fill(lo, hi, state, s_lo, s_hi, numcomp, domlo, domhi, delta, xlo, bc) bind(C, name="generic_fill")
 
     use amrex_filcc_module, only: amrex_filccn
 
@@ -20,69 +17,15 @@ contains
     integer,  intent(in   ) :: lo(3), hi(3)
     integer,  intent(in   ) :: s_lo(3), s_hi(3)
     integer,  intent(in   ) :: domlo(3), domhi(3)
-    integer,  intent(in   ) :: bc(AMREX_SPACEDIM, 2)
+    integer,  intent(in   ) :: bc(AMREX_SPACEDIM, 2, numcomp)
     real(rt), intent(in   ) :: delta(3), xlo(3)
-    real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3))
-
-    integer :: d
-    integer :: bc_temp(AMREX_SPACEDIM,2)
+    real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3),numcomp)
+    integer,  intent(in   ), value :: numcomp
 
     !$gpu
 
-    ! handle an external BC via extrapolation here -- user's can override as needed
-    bc_temp(:,:) = bc(:,:)
+    call amrex_filccn(lo, hi, state, s_lo, s_hi, numcomp, domlo, domhi, delta, xlo, bc)
 
-    do d = 1, AMREX_SPACEDIM
-       if (bc(d,1) == EXT_DIR .and. s_lo(d) < domlo(d)) then
-          bc_temp(d,1) = FOEXTRAP
-       end if
-
-       if (bc(d,2) == EXT_DIR .and. s_hi(d) > domhi(d)) then
-          bc_temp(d,2) = FOEXTRAP
-       end if
-    end do
-
-    call amrex_filccn(lo, hi, state, s_lo, s_hi, 1, domlo, domhi, delta, xlo, bc_temp)
-
-  end subroutine generic_single_fill
-
-
-
-  subroutine generic_multi_fill(lo, hi, state, s_lo, s_hi, domlo, domhi, delta, xlo, bc) bind(C, name="generic_multi_fill")
-
-    use amrex_filcc_module, only: amrex_filccn
-
-    implicit none
-
-    integer,  intent(in   ) :: lo(3), hi(3)
-    integer,  intent(in   ) :: s_lo(3), s_hi(3)
-    integer,  intent(in   ) :: domlo(3), domhi(3)
-    integer,  intent(in   ) :: bc(AMREX_SPACEDIM, 2, NVAR)
-    real(rt), intent(in   ) :: delta(3), xlo(3)
-    real(rt), intent(inout) :: state(s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3),NVAR)
-
-    integer :: d, n
-    integer :: bc_temp(AMREX_SPACEDIM,2,NVAR)
-
-    !$gpu
-
-    ! handle an external BC via extrapolation here -- user's can override as needed
-    bc_temp(:,:,:) = bc(:,:,:)
-
-    do n = 1, NVAR
-       do d = 1, AMREX_SPACEDIM
-          if (bc(d,1,n) == EXT_DIR .and. s_lo(d) < domlo(d)) then
-             bc_temp(d,1,n) = FOEXTRAP
-          end if
-
-          if (bc(d,2,n) == EXT_DIR .and. s_hi(d) > domhi(d)) then
-             bc_temp(d,2,n) = FOEXTRAP
-          end if
-       end do
-    end do
-
-    call amrex_filccn(lo, hi, state, s_lo, s_hi, NVAR, domlo, domhi, delta, xlo, bc_temp)
-
-  end subroutine generic_multi_fill
+  end subroutine generic_fill
 
 end module generic_fill_module
