52bf7393b4f347c97fb1ffc81563ca7017acaac9
Michael Zingale <michael.zingale@stonybrook.edu>
2020-03-29 16:00:46 -0400

    a more automated convergence test

--- Exec/gravity_tests/hse_convergence/convergence_ppm.sh
+++ Exec/gravity_tests/hse_convergence/convergence_ppm.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-./Castro1d.gnu.MPI.ex inputs.ppm.64 >& 64.out
-./Castro1d.gnu.MPI.ex inputs.ppm.128 >& 128.out
-./Castro1d.gnu.MPI.ex inputs.ppm.256 >& 256.out
-./Castro1d.gnu.MPI.ex inputs.ppm.512 >& 512.out
+ofile=ppm.converge.out
 
-fextrema.gnu.ex -v magvel hse_64_plt00667 > ppm.converge.out
-fextrema.gnu.ex -v magvel hse_128_plt01334 >> ppm.converge.out
-fextrema.gnu.ex -v magvel hse_256_plt02667 >> ppm.converge.out
-fextrema.gnu.ex -v magvel hse_512_plt05334 >> ppm.converge.out
+./Castro1d.gnu.MPI.ex inputs.ppm.64 >& 64.out
+pfile=`ls -t | grep -i hse_64_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} > ${ofile}
+
+./Castro1d.gnu.MPI.ex inputs.ppm.128 >& 128.out
+pfile=`ls -t | grep -i hse_128_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} >> ${ofile}
+
+./Castro1d.gnu.MPI.ex inputs.ppm.256 >& 256.out
+pfile=`ls -t | grep -i hse_256_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} >> ${ofile}
+
+./Castro1d.gnu.MPI.ex inputs.ppm.512 >& 512.out
+pfile=`ls -t | grep -i hse_512_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} >> ${ofile}
+
+
 
