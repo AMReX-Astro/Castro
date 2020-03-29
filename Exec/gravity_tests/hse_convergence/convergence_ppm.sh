b15b3f479a0c651cfc2f6edc00e3eb9cb81657df
Michael Zingale <michael.zingale@stonybrook.edu>
2020-03-29 15:44:42 -0400

    update the convergence

--- Exec/gravity_tests/hse_convergence/convergence_ppm.sh
+++ Exec/gravity_tests/hse_convergence/convergence_ppm.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-nohup ./Castro1d.gnu.MPI.ex inputs.ppm.64 >& 64.out &
-nohup ./Castro1d.gnu.MPI.ex inputs.ppm.128 >& 128.out &
-nohup ./Castro1d.gnu.MPI.ex inputs.ppm.256 >& 256.out &
-nohup ./Castro1d.gnu.MPI.ex inputs.ppm.512 >& 512.out &
+./Castro1d.gnu.MPI.ex inputs.ppm.64 >& 64.out
+./Castro1d.gnu.MPI.ex inputs.ppm.128 >& 128.out
+./Castro1d.gnu.MPI.ex inputs.ppm.256 >& 256.out
+./Castro1d.gnu.MPI.ex inputs.ppm.512 >& 512.out
+
+fextrema.gnu.ex -v magvel hse_64_plt00667 > ppm.converge.out
+fextrema.gnu.ex -v magvel hse_128_plt01334 >> ppm.converge.out
+fextrema.gnu.ex -v magvel hse_256_plt02667 >> ppm.converge.out
+fextrema.gnu.ex -v magvel hse_512_plt05334 >> ppm.converge.out
+
