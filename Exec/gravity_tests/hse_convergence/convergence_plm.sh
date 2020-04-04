941cb535eb87282d4718828359fd4f0a05f29574
Michael Zingale <michael.zingale@stonybrook.edu>
2020-04-04 13:40:23 -0400

    add a no pslope hse-reflect case

--- Exec/gravity_tests/hse_convergence/convergence_plm.sh
+++ Exec/gravity_tests/hse_convergence/convergence_plm.sh
@@ -31,10 +31,38 @@ fextrema.gnu.ex -v magvel ${pfile} | grep -i magvel >> ${ofile}
 
 ## plm + hse reflect
 
-ofile=plm-hsereflect.out
+ofile=plm-hsereflect.converge.out
 
 RUNPARAMS="
 castro.ppm_type=0
+castro.hse_interp_temp=1
+castro.hse_reflect_vels=1
+"""
+
+${EXEC} inputs.ppm.64 ${RUNPARAMS} >& 64.out
+pfile=`ls -t | grep -i hse_64_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} | grep -i magvel > ${ofile}
+
+${EXEC} inputs.ppm.128 ${RUNPARAMS} >& 128.out
+pfile=`ls -t | grep -i hse_128_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} | grep -i magvel >> ${ofile}
+
+${EXEC} inputs.ppm.256 ${RUNPARAMS} >& 256.out
+pfile=`ls -t | grep -i hse_256_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} | grep -i magvel >> ${ofile}
+
+${EXEC} inputs.ppm.512 ${RUNPARAMS} >& 512.out
+pfile=`ls -t | grep -i hse_512_plt | head -1` 
+fextrema.gnu.ex -v magvel ${pfile} | grep -i magvel >> ${ofile}
+
+
+## plm + hse reflect + no pslope
+
+ofile=plm-hsereflect-nopslope.converge.out
+
+RUNPARAMS="
+castro.ppm_type=0
+castro.use_pslope=0
 castro.hse_interp_temp=1
 castro.hse_reflect_vels=1
 """
