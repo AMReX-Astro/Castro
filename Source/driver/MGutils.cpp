3430aecb60fcee5133ffb0d39f54294429d415b3
Michael Zingale <michael.zingale@stonybrook.edu>
2020-04-13 13:24:23 -0400

    convert all of the weighting to C++

--- Source/driver/MGutils.cpp
+++ Source/driver/MGutils.cpp
@@ -1,20 +1,18 @@
+#include <AMReX_Geometry.H>
+#include <AMReX_FArrayBox.H>
+
+using namespace amrex;
+
 void
-Castro::apply_metric(const Box& bx,
-                     const Box& xbx,
+apply_metric(const Box& bx,
+             Array4<Real> const rhs, const Box& rbx,
+             Array4<Real> const ecx, const Box& xbx,
 #if AMREX_SPACEDIM >= 2
-                     const Box& ybx,
+             Array4<Real> const ecy, const Box& ybx,
 #endif
-                     Array4<Real> const rhs, const Box& rbx,
-                     Array4<Real> const ecx,
-#if AMREX_SPACEDIM >= 2
-                     Array4<Real> const ecy
-#endif
-                     )
+             const Real* dx,
+             const int coord_type)
 {
-
-
-
-  int coord_type = geom.Coord();
 
   // r-z
   if (coord_type == 1) {
@@ -58,156 +56,101 @@ Castro::apply_metric(const Box& bx,
 }
 
 
-  subroutine ca_weight_cc(lo, hi, &
-                          cc, clo, chi, &
-                          dx, coord_type) bind(C, name="ca_weight_cc")
+void
+do_weight_cc(const Box& bx,
+             Array4<Real> const cc,
+             const Real* dx,
+             const int coord_type)
+{
 
-    use amrex_fort_module, only: rt => amrex_real
+  // r-z
+  if (coord_type == 1) {
 
-    implicit none
+    // At centers
+    AMREX_PARALLEL_FOR_3D(bx, i, j, k,
+    {
 
-    integer,  intent(in   ) :: lo(3), hi(3)
-    integer,  intent(in   ) :: clo(3), chi(3)
-    real(rt), intent(inout) :: cc(clo(1):chi(1),clo(2):chi(2),clo(3):chi(3))
-    real(rt), intent(in   ) :: dx(3)
-    integer,  intent(in   ), value :: coord_type
-
-    real(rt) :: r
-    integer  :: i, j, k
-
-    !$gpu
-
-    ! r-z
-    if (coord_type == 1) then
-
-       ! At centers
-       do i = lo(1), hi(1)
-          r = (dble(i) + 0.5e0_rt) * dx(1)
-          do k = lo(3), hi(3)
-             do j = lo(2), hi(2)
-                cc(i,j,k) = cc(i,j,k) * r
-             end do
-          end do
-       end do
+      Real r = (static_cast<Real>(i) + 0.5_rt) * dx[0];
+      cc(i,j,k) *= r;
+    });
 
 #ifndef AMREX_USE_CUDA
-    else
+  } else {
 
-       print *,'Bogus coord_type in weight_cc ' ,coord_type
-       call castro_error("Error:: MGutils_2d.f90 :: ca_weight_cc")
+    amrex::Print() << "Bogus coord_type in weight_cc " << coord_type << std::endl;
+    amrex::Error("Error:: MGutils.cpp :: ca_weight_cc");
 #endif
-
-    end if
-
-  end subroutine ca_weight_cc
+  }
+}
 
 
+void
+do_unweight_cc(const Box& bx,
+               Array4<Real> const cc,
+               const Real* dx,
+               const int coord_type)
+{
 
-  subroutine ca_unweight_cc(lo, hi, &
-                            cc, clo, chi, &
-                            dx, coord_type) bind(C, name="ca_unweight_cc")
+  // r-z
+  if (coord_type == 1) {
 
-    use amrex_fort_module, only: rt => amrex_real
+    // At centers
+    AMREX_PARALLEL_FOR_3D(bx, i, j, k,
+    {
 
-    implicit none
-
-    integer,  intent(in   ) :: lo(3), hi(3)
-    integer,  intent(in   ) :: clo(3), chi(3)
-    real(rt), intent(inout) :: cc(clo(1):chi(1),clo(2):chi(2),clo(3):chi(3))
-    real(rt), intent(in   ) :: dx(3)
-    integer,  intent(in   ), value :: coord_type
-
-    real(rt) :: r
-    integer  :: i, j, k
-
-    !$gpu
-
-    ! r-z
-    if (coord_type == 1) then
-
-       ! At centers
-       do i = lo(1), hi(1)
-          r = (dble(i) + 0.5e0_rt) * dx(1)
-          do k = lo(3), hi(3)
-             do j = lo(2), hi(2)
-                cc(i,j,k) = cc(i,j,k) / r
-             end do
-          end do
-       end do
+      Real r = (static_cast<Real>(i) + 0.5_rt) * dx[0];
+      cc(i,j,k) /= r;
+    });
 
 #ifndef AMREX_USE_CUDA
-    else
+  } else {
 
-       print *,'Bogus coord_type in unweight_cc ' ,coord_type
-       call castro_error("Error:: MGutils_2d.f90 :: ca_unweight_cc")
+    amrex::Print() << "Bogus coord_type in unweight_cc " << coord_type << std::endl;
+    amrex::Error("Error:: MGutils.cpp :: ca_unweight_cc");
 #endif
-
-    end if
-
-  end subroutine ca_unweight_cc
+  }
+}
 
 
+void
+do_unweight_edges(const Box& bx,
+                  Array4<Real> const ec,
+                  const int idir,
+                  const Real* dx,
+                  const int coord_type)
+{
 
-  subroutine ca_unweight_edges(lo, hi, &
-                               ec, eclo, echi, &
-                               dx, coord_type, idir) &
-                               bind(C, name="ca_unweight_edges")
+  // r-z
+  if (coord_type == 1) {
 
-    use amrex_fort_module, only: rt => amrex_real
+    if (idir == 0) {
 
-    implicit none
+      // On x-edges
+      AMREX_PARALLEL_FOR_3D(bx, i, j, k,
+      {
 
-    integer,  intent(in   ) :: lo(3), hi(3)
-    integer,  intent(in   ) :: eclo(3), echi(3)
-    real(rt), intent(inout) :: ec(eclo(1):echi(1),eclo(2):echi(2),eclo(3):echi(3))
-    real(rt), intent(in   ) :: dx(3)
-    integer,  intent(in   ), value :: coord_type, idir
+        if (i != 0) {
+          Real r = static_cast<Real>(i) * dx[0];
+          ec(i,j,k) /= r;
+        }
+      });
 
-    real(rt) :: r
-    integer  :: i, j, k
+    } else {
 
-    !$gpu
-
-    ! r-z
-    if (coord_type == 1) then
-
-       if (idir == 0) then
-
-          ! On x-edges
-          do i = lo(1), hi(1)
-             if (i /= 0) then
-                r = dble(i) * dx(1)
-                do k = lo(3), hi(3)
-                   do j = lo(2), hi(2)
-                      ec(i,j,k) = ec(i,j,k) / r
-                   end do
-                end do
-             end if
-          end do
-
-       else
-
-          ! On y-edges
-          do i = lo(1), hi(1)
-             r = (dble(i) + 0.5e0_rt) * dx(1)
-             do k = lo(3), hi(3)
-                do j = lo(2), hi(2)
-                   ec(i,j,k) = ec(i,j,k) / r
-                end do
-             end do
-          end do
-       end if
+      // On y-edges
+      AMREX_PARALLEL_FOR_3D(bx, i, j, k,
+      {
+        Real r = (static_cast<Real>(i) + 0.5_rt) * dx[0];
+        ec(i,j,k) /= r;
+      });
+    }
 
 #ifndef AMREX_USE_CUDA
-    else
+  } else {
 
-       print *,'Bogus coord_type in unweight_edges ' ,coord_type
-       call castro_error("Error:: MGutils_2d.f90 :: ca_unweight_edges")
+    amrex::Print() << "Bogus coord_type in unweight_edges " << coord_type << std::endl;
+    amrex::Error("Error:: MGutils.cpp :: ca_unweight_edges");
 #endif
-
-    end if
-
-  end subroutine ca_unweight_edges
-
-end module MGutils_2D_module
+  }
+}
 
