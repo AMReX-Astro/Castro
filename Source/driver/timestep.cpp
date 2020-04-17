199925b8e7ad7357721fd4a159c1335a6dd83055
Michael Zingale <michael.zingale@stonybrook.edu>
2020-04-17 10:19:22 -0400

    fix GPU compilation

--- Source/driver/timestep.cpp
+++ Source/driver/timestep.cpp
@@ -29,6 +29,12 @@ Castro::estdt_cfl(const Real time)
 
   const MultiFab& stateMF = get_new_data(State_Type);
 
+  const int ltime_integration_method = time_integration_method;
+#ifdef ROTATION
+  const int ldo_rotation = do_rotation;
+  const int lstate_in_rotating_frame = state_in_rotating_frame;
+#endif
+
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
@@ -63,7 +69,7 @@ Castro::estdt_cfl(const Real time)
       Real uz = u(i,j,k,UMZ) * rhoInv;
 
 #ifdef ROTATION
-      if (do_rotation == 1 && state_in_rotating_frame != 1) {
+      if (ldo_rotation == 1 && lstate_in_rotating_frame != 1) {
         Real vel[3];
         vel[0] = ux;
         vel[1] = uy;
@@ -101,7 +107,7 @@ Castro::estdt_cfl(const Real time)
       // The CTU method has a less restrictive timestep than MOL-based
       // schemes (including the true SDC).  Since the simplified SDC
       // solver is based on CTU, we can use its timestep.
-      if (time_integration_method == 0 || time_integration_method == 3) {
+      if (ltime_integration_method == 0 || ltime_integration_method == 3) {
         return {amrex::min(dt1, dt2, dt3)};
 
       } else {
@@ -147,6 +153,10 @@ Castro::estdt_temp_diffusion(void)
 
   const MultiFab& stateMF = get_new_data(State_Type);
 
+  const Real ldiffuse_cutoff_density = diffuse_cutoff_density;
+  const Real lmax_dt = max_dt;
+  const Real lcfl = cfl;
+
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
@@ -156,53 +166,53 @@ Castro::estdt_temp_diffusion(void)
     auto ustate = stateMF.array(mfi);
 
     reduce_op.eval(box, reduce_data,
-    [=] AMREX_GPU_HOST_DEVICE (int i, int j, int k) noexcept -> ReduceTuple
-    {
+                   [=] AMREX_GPU_HOST_DEVICE (int i, int j, int k) noexcept -> ReduceTuple
+                   {
 
-      if (ustate(i,j,k,URHO) > diffuse_cutoff_density) {
+                     if (ustate(i,j,k,URHO) > ldiffuse_cutoff_density) {
 
-        Real rho_inv = 1.0_rt/ustate(i,j,k,URHO);
+                       Real rho_inv = 1.0_rt/ustate(i,j,k,URHO);
 
-        // we need cv
-        eos_t eos_state;
-        eos_state.rho = ustate(i,j,k,URHO);
-        eos_state.T = ustate(i,j,k,UTEMP);
-        eos_state.e = ustate(i,j,k,UEINT) * rho_inv;
-        for (int n = 0; n < NumSpec; n++) {
-          eos_state.xn[n]  = ustate(i,j,k,UFS+n) * rho_inv;
-        }
-        for (int n = 0; n < NumAux; n++) {
-          eos_state.aux[n] = ustate(i,j,k,UFX+n) * rho_inv;
-        }
+                       // we need cv
+                       eos_t eos_state;
+                       eos_state.rho = ustate(i,j,k,URHO);
+                       eos_state.T = ustate(i,j,k,UTEMP);
+                       eos_state.e = ustate(i,j,k,UEINT) * rho_inv;
+                       for (int n = 0; n < NumSpec; n++) {
+                         eos_state.xn[n]  = ustate(i,j,k,UFS+n) * rho_inv;
+                       }
+                       for (int n = 0; n < NumAux; n++) {
+                         eos_state.aux[n] = ustate(i,j,k,UFX+n) * rho_inv;
+                       }
 
-        eos(eos_input_re, eos_state);
+                       eos(eos_input_re, eos_state);
 
-        // we also need the conductivity
-        conductivity(eos_state);
+                       // we also need the conductivity
+                       conductivity(eos_state);
 
-        // maybe we should check (and take action) on negative cv here?
-        Real D = eos_state.conductivity * rho_inv / eos_state.cv;
+                       // maybe we should check (and take action) on negative cv here?
+                       Real D = eos_state.conductivity * rho_inv / eos_state.cv;
 
-        Real dt1 = 0.5_rt * dx[0]*dx[0] / D;
+                       Real dt1 = 0.5_rt * dx[0]*dx[0] / D;
 
-        Real dt2;
+                       Real dt2;
 #if AMREX_SPACEDIM >= 2
-        dt2 = 0.5_rt * dx[1]*dx[1] / D;
+                       dt2 = 0.5_rt * dx[1]*dx[1] / D;
 #else
-        dt2 = dt1;
+                       dt2 = dt1;
 #endif
 
-        Real dt3;
+                       Real dt3;
 #if AMREX_SPACEDIM >= 3
-        dt3 = 0.5_rt * dx[2]*dx[2] / D;
+                       dt3 = 0.5_rt * dx[2]*dx[2] / D;
 #else
-        dt3 = dt1;
+                       dt3 = dt1;
 #endif
 
-        return {amrex::min(dt1, dt2, dt3)};
+                       return {amrex::min(dt1, dt2, dt3)};
 
-      } else {
-        return max_dt/cfl;
+                     } else {
+                       return lmax_dt/lcfl;
       }
     });
   }
