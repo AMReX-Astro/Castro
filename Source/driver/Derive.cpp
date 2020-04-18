351597861e46f5c068db523020ef7e8cf9b4f529
Michael Zingale <michael.zingale@stonybrook.edu>
2020-04-18 16:53:32 -0400

    move all of the EOS-specific derives to C++

--- Source/driver/Derive.cpp
+++ Source/driver/Derive.cpp
@@ -18,27 +18,35 @@ extern "C"
     // These routines are called in an MFIter loop, so we do not
     // need to explicitly synchronize after GPU kernels.
 
-    void ca_derpres(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                    const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                    const int* lo, const int* hi,
-                    const int* domain_lo, const int* domain_hi,
-                    const Real* delta, const Real* xlo,
-                    const Real* time, const Real* dt, const int* bcrec, 
-                    const int* level, const int* grid_no)
+    void ca_derpres(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                    const FArrayBox& datfab, const Geometry& geomdata,
+                    Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
 
-#pragma gpu box(bx)
-        derpres(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                AMREX_REAL_ANYD(delta));
+        Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
 
+        eos_t eos_state;
+        eos_state.rho  = dat(i,j,k,URHO);
+        eos_state.T = dat(i,j,k,UTEMP);
+        eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+        for (int n = 0; n < NumSpec; n++) {
+          eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+        }
+        for (int n = 0; n < NumAux; n++) {
+          eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+        }
+
+        eos(eos_input_re, eos_state);
+
+        der(i,j,k,0) = eos_state.p;
+      });
     }
 
     void ca_dereint1(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
@@ -94,142 +102,201 @@ extern "C"
       });
     }
 
-    void ca_deruplusc(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                      const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                      const int* lo, const int* hi,
-                      const int* domain_lo, const int* domain_hi,
-                      const Real* delta, const Real* xlo,
-                      const Real* time, const Real* dt, const int* bcrec, 
-                      const int* level, const int* grid_no)
+    void ca_deruplusc(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                      const FArrayBox& datfab, const Geometry& geomdata,
+                      Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
 
-#pragma gpu box(bx)
-        deruplusc(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                  data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                  AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                  AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                  AMREX_REAL_ANYD(delta));
 
+        Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
+
+        eos_t eos_state;
+        eos_state.rho  = dat(i,j,k,URHO);
+        eos_state.T = dat(i,j,k,UTEMP);
+        eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+        for (int n = 0; n < NumSpec; n++) {
+          eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+        }
+        for (int n = 0; n < NumAux; n++) {
+          eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+        }
+
+        eos(eos_input_re, eos_state);
+
+        der(i,j,k,0) = dat(i,j,k,UMX) / dat(i,j,k,URHO) + eos_state.cs;
+
+      });
     }
 
-    void ca_deruminusc(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                       const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                       const int* lo, const int* hi,
-                       const int* domain_lo, const int* domain_hi,
-                       const Real* delta, const Real* xlo,
-                       const Real* time, const Real* dt, const int* bcrec, 
-                       const int* level, const int* grid_no)
+    void ca_deruminusc(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                       const FArrayBox& datfab, const Geometry& geomdata,
+                       Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
 
-#pragma gpu box(bx)
-        deruminusc(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                   data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                   AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                   AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                   AMREX_REAL_ANYD(delta));
 
+        Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
+
+        eos_t eos_state;
+        eos_state.rho  = dat(i,j,k,URHO);
+        eos_state.T = dat(i,j,k,UTEMP);
+        eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+        for (int n = 0; n < NumSpec; n++) {
+          eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+        }
+        for (int n = 0; n < NumAux; n++) {
+          eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+        }
+
+        eos(eos_input_re, eos_state);
+
+        der(i,j,k,0) = dat(i,j,k,UMX) / dat(i,j,k,URHO) - eos_state.cs;
+
+      });
     }
 
-    void ca_dersoundspeed(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                          const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                          const int* lo, const int* hi,
-                          const int* domain_lo, const int* domain_hi,
-                          const Real* delta, const Real* xlo,
-                          const Real* time, const Real* dt, const int* bcrec, 
-                          const int* level, const int* grid_no)
+    void ca_dersoundspeed(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                          const FArrayBox& datfab, const Geometry& geomdata,
+                          Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
 
-#pragma gpu box(bx)
-        dersoundspeed(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                      data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                      AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                      AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                      AMREX_REAL_ANYD(delta));
 
+        Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
+
+        eos_t eos_state;
+        eos_state.rho  = dat(i,j,k,URHO);
+        eos_state.T = dat(i,j,k,UTEMP);
+        eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+        for (int n = 0; n < NumSpec; n++) {
+          eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+        }
+        for (int n = 0; n < NumAux; n++) {
+          eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+        }
+
+        eos(eos_input_re, eos_state);
+
+        der(i,j,k,0) = eos_state.cs;
+
+      });
     }
 
-    void ca_dergamma1(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                      const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                      const int* lo, const int* hi,
-                      const int* domain_lo, const int* domain_hi,
-                      const Real* delta, const Real* xlo,
-                      const Real* time, const Real* dt, const int* bcrec, 
-                      const int* level, const int* grid_no)
+
+    void ca_dergamma1(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                      const FArrayBox& datfab, const Geometry& geomdata,
+                      Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
 
-#pragma gpu box(bx)
-        dergamma1(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                  data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                  AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                  AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                  AMREX_REAL_ANYD(delta));
 
+        Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
+
+        eos_t eos_state;
+        eos_state.rho  = dat(i,j,k,URHO);
+        eos_state.T = dat(i,j,k,UTEMP);
+        eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+        for (int n = 0; n < NumSpec; n++) {
+          eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+        }
+        for (int n = 0; n < NumAux; n++) {
+          eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+        }
+
+        eos(eos_input_re, eos_state);
+
+        der(i,j,k,0) = eos_state.gam1;
+
+      });
     }
 
-    void ca_dermachnumber(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                          const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                          const int* lo, const int* hi,
-                          const int* domain_lo, const int* domain_hi,
-                          const Real* delta, const Real* xlo,
-                          const Real* time, const Real* dt, const int* bcrec, 
-                          const int* level, const int* grid_no)
+    void ca_dermachnumber(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                          const FArrayBox& datfab, const Geometry& geomdata,
+                          Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
 
-#pragma gpu box(bx)
-        dermachnumber(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                      data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                      AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                      AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                      AMREX_REAL_ANYD(delta));
 
+        Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
+
+        eos_t eos_state;
+        eos_state.rho  = dat(i,j,k,URHO);
+        eos_state.T = dat(i,j,k,UTEMP);
+        eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+        for (int n = 0; n < NumSpec; n++) {
+          eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+        }
+        for (int n = 0; n < NumAux; n++) {
+          eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+        }
+
+        der(i,j,k,0) = std::sqrt(dat(i,j,k,UMX)*dat(i,j,k,UMX) +
+                                 dat(i,j,k,UMY)*dat(i,j,k,UMY) +
+                                 dat(i,j,k,UMZ)*dat(i,j,k,UMZ)) /
+          (dat(i,j,k,URHO) * eos_state.cs);
+
+      });
     }
 
-    void ca_derentropy(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                       const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                       const int* lo, const int* hi,
-                       const int* domain_lo, const int* domain_hi,
-                       const Real* delta, const Real* xlo,
-                       const Real* time, const Real* dt, const int* bcrec, 
-                       const int* level, const int* grid_no)
+    void ca_derentropy(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                          const FArrayBox& datfab, const Geometry& geomdata,
+                          Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
 
-#pragma gpu box(bx)
-        derentropy(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                   data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                   AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                   AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                   AMREX_REAL_ANYD(delta));
 
+        Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
+
+        eos_t eos_state;
+        eos_state.rho  = dat(i,j,k,URHO);
+        eos_state.T = dat(i,j,k,UTEMP);
+        eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+        for (int n = 0; n < NumSpec; n++) {
+          eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+        }
+        for (int n = 0; n < NumAux; n++) {
+          eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+        }
+
+        der(i,j,k,0) = eos_state.s;
+      });
     }
 
 #ifdef DIFFUSION
@@ -356,27 +423,64 @@ extern "C"
 #endif
 
 #ifdef REACTIONS
-    void ca_derenuctimescale(Real* der, const int* der_lo, const int* der_hi, const int* nvar,
-                             const Real* data, const int* data_lo, const int* data_hi, const int* ncomp,
-                             const int* lo, const int* hi,
-                             const int* domain_lo, const int* domain_hi,
-                             const Real* delta, const Real* xlo,
-                             const Real* time, const Real* dt, const int* bcrec, 
-                             const int* level, const int* grid_no)
+    void ca_derenuctimescale(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
+                             const FArrayBox& datfab, const Geometry& geomdata,
+                             Real /*time*/, const int* /*bcrec*/, int /*level*/)
     {
 
-        IntVect ilo(D_DECL(lo[0], lo[1], lo[2]));
-        IntVect ihi(D_DECL(hi[0], hi[1], hi[2]));
+      auto const dat = datfab.array();
+      auto const der = derfab.array();
 
-        Box bx(ilo, ihi);
+      auto dx = geomdata.CellSizeArray();
 
-#pragma gpu box(bx)
-        derenuctimescale(der, AMREX_INT_ANYD(der_lo), AMREX_INT_ANYD(der_hi), *nvar,
-                         data, AMREX_INT_ANYD(data_lo), AMREX_INT_ANYD(data_hi), *ncomp,
-                         AMREX_INT_ANYD(lo), AMREX_INT_ANYD(hi),
-                         AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
-                         AMREX_REAL_ANYD(delta));
+      Real dd = 0.0_rt;
+#if AMREX_SPACEDIM == 1
+      dd = dx[0];
+#elif AMREX_SPACEDIM == 2
+      dd = amrex::min(dx[0], dx[1]);
+#else
+      dd = amrex::min(dx[0], dx[1], dx[2]);
+#endif
 
+      int enuc_comp = datfab.nComp()-1;
+
+      amrex::ParallelFor(bx,
+      [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      {
+
+        // the nuclear energy (rho H_nuc) is tacked onto the end of
+        // the input state, after the NUM_STATE conserved state
+        // quantities
+        Real enuc = std::abs(dat(i,j,k,enuc_comp));
+
+        if (enuc > 1.e-100_rt) {
+
+          Real rhoInv = 1.0_rt / dat(i,j,k,URHO);
+
+          // calculate the sound speed
+          eos_t eos_state;
+          eos_state.rho  = dat(i,j,k,URHO);
+          eos_state.T = dat(i,j,k,UTEMP);
+          eos_state.e = dat(i,j,k,UEINT) * rhoInv;
+          for (int n = 0; n < NumSpec; n++) {
+            eos_state.xn[n] = dat(i,j,k,UFS+n) * rhoInv;
+          }
+          for (int n = 0; n < NumAux; n++) {
+            eos_state.aux[n] = dat(i,j,k,UFX+n) * rhoInv;
+          }
+
+          eos(eos_input_re, eos_state);
+
+          Real t_e = eos_state.e / enuc;
+          Real t_s = dd / eos_state.cs;
+
+          der(i,j,k,0) = t_s/t_e;
+
+        } else {
+          der(i,j,k,0) = 0.0_rt;
+        }
+
+      });
     }
 #endif
 
