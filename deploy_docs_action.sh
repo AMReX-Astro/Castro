9efb689af3984cea54f4a392e6d5d9b3256bb6a0
Alice Harpole <harpolea@users.noreply.github.com>
2020-05-03 09:31:01 -0400

    Github action for building docs (#899)
    This defines a github action for building the documentation. It is triggered by pushes to the master and development branches

---
+++ deploy_docs_action.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+# Build the documentation from the MASTER_BRANCH or DEV_BRANCH
+# and push it to TARGET_BRANCH.
+MASTER_BRANCH="master"
+DEV_BRANCH="development"
+TARGET_BRANCH="gh-pages"
+
+mkdir out
+
+# if on the dev branch, use the dev_layout.html template to get the 
+# links correct
+if [ "$GITHUB_BRANCH" = "$DEV_BRANCH" ]; then
+    mv Docs/source/_templates/dev_layout.html Docs/source/_templates/layout.html 
+fi
+
+# Build the Sphinx documentation
+cd Docs
+make html
+cd ../
+
+mkdir -p out/docs/
+if [ "$GITHUB_BRANCH" = "$MASTER_BRANCH" ]; then
+    mkdir -p out/docs
+    mv Docs/build/html/* out/docs
+else 
+    mkdir -p out/docs/dev/
+    mv Docs/build/html/* out/docs/dev
+fi
