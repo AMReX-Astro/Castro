1257241a2011c4998d773f9590ae61282addee5d
Michael Zingale <michael.zingale@stonybrook.edu>
2020-05-05 08:28:56 -0400

    split out the MHD Initialization so we can run MHD on any problem (#909)
    This gives us a separate Prob_mhd_nd.F90 that initiializes the magnetic field to 0. Problems can implement their own version of this. By providing this default we can run the MHD solver on any problem setup.

---
+++ Exec/mhd_tests/Alfven/Prob_mhd_nd.F90
@@ -0,0 +1,58 @@
+subroutine ca_initmag(level, time, lo, hi, &
+                      nbx, mag_x, bx_lo, bx_hi, &
+                      nby, mag_y, by_lo, by_hi, &
+                      nbz, mag_z, bz_lo, bz_hi, &
+                      delta, xlo, xhi)
+
+  use probdata_module
+  use prob_params_module
+  use amrex_fort_module, only : rt => amrex_real
+  use amrex_constants_module, only : TWO, M_PI, ZERO, ONE, M_SQRT_2, HALF
+
+  implicit none
+
+  integer :: level, nbx, nby, nbz
+  integer :: lo(3), hi(3)
+  integer :: bx_lo(3), bx_hi(3)
+  integer :: by_lo(3), by_hi(3)
+  integer :: bz_lo(3), bz_hi(3)
+  real(rt) :: xlo(3), xhi(3), time, delta(3)
+
+  real(rt) :: mag_x(bx_lo(1):bx_hi(1), bx_lo(2):bx_hi(2), bx_lo(3):bx_hi(3), nbx)
+  real(rt) :: mag_y(by_lo(1):by_hi(1), by_lo(2):by_hi(2), by_lo(3):by_hi(3), nby)
+  real(rt) :: mag_z(bz_lo(1):bz_hi(1), bz_lo(2):bz_hi(2), bz_lo(3):bz_hi(3), nbz)
+
+  real(rt) :: xcen, ycen, zcen
+  integer  :: i, j, k
+
+  print *, "Initializing magnetic field!!"
+
+  if (idir .eq. 1) then
+     do k = lo(3), hi(3)
+        do j = lo(2), hi(2)
+           do i = lo(1), hi(1)+1
+                 mag_x(i,j,k,1) = B_x
+           enddo
+        enddo
+     enddo
+
+     do k = lo(3), hi(3)
+        do j = lo(2), hi(2)+1
+           do i = lo(1), hi(1)
+                 mag_y(i,j,k,1) = B_y
+           enddo
+        enddo
+     enddo
+
+     do k = lo(3), hi(3)+1
+        do j = lo(2), hi(2)
+           do i = lo(1), hi(1)
+              xcen = xlo(1) + delta(1)*(float(i-lo(1)) + 0.5d0)
+              mag_z(i,j,k,1) = 1.0e-5_rt*sin(TWO*M_PI*xcen)
+           enddo
+        enddo
+     enddo
+
+  endif
+
+ end subroutine ca_initmag
