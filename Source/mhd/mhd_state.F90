11e7ee2c179455261144a771fb909878e40d9317
Michael Zingale <michael.zingale@stonybrook.edu>
2020-05-14 19:37:04 -0400

    new file

---
+++ Source/mhd/mhd_state.F90
@@ -0,0 +1,12 @@
+module mhd_state_module
+
+  use meth_params_module, only: NVAR
+
+  implicit none
+
+  integer, parameter :: UMAGX = NVAR+1
+  integer, parameter :: UMAGY = NVAR+2
+  integer, parameter :: UMAGZ = NVAR+3
+
+end module mhd_state_module
+
