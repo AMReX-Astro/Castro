483666b0a81d9c13757dd5859348993ae7f0a461
Michael Zingale <michael.zingale@stonybrook.edu>
2020-05-18 11:10:08 -0400

    implement and epsilon function

--- Source/mhd/mhd_state.F90
+++ Source/mhd/mhd_state.F90
@@ -1,6 +1,7 @@
 module mhd_state_module
 
   use meth_params_module, only: NVAR
+  use amrex_fort_module, only : rt => amrex_real
 
   implicit none
 
@@ -10,7 +11,22 @@ module mhd_state_module
 
 contains
 
-  pure function epsilon_ijk(
+  pure function epsilon_ijk(i, j, k) result (val)
+
+    integer, intent(in) :: i, j, k
+    real(rt) :: val
+
+    if (i == j .or. j == k .or. i == k) then
+       val = 0.0_rt
+    else if ((i == 1 .and. j == 2 .and. k == 3) .or. &
+             (i == 2 .and. j == 3 .and. k == 1) .or. &
+             (i == 3 .and. j == 1 .and. k == 2)) then
+       val = 1.0_rt
+    else
+       val = -1.0_rt
+    end if
+
+  end function epsilon_ijk
 
 end module mhd_state_module
 
