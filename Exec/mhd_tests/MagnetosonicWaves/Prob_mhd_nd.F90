0013b1aebd0da38d125075649455998d412b0cad
guadabsb15 <guadabsb15@gmail.com>
2020-05-24 15:19:05 -0400

    mhd tests - alfven and mhd waves (#992)
    alfven and magnetosonic waves test as in MM section 3.1

--- Exec/mhd_tests/MagnetosonicWaves/Prob_mhd_nd.F90
+++ Exec/mhd_tests/MagnetosonicWaves/Prob_mhd_nd.F90
@@ -22,32 +22,38 @@ subroutine ca_initmag(level, time, lo, hi, &
   real(rt) :: mag_y(by_lo(1):by_hi(1), by_lo(2):by_hi(2), by_lo(3):by_hi(3), nby)
   real(rt) :: mag_z(bz_lo(1):bz_hi(1), bz_lo(2):bz_hi(2), bz_lo(3):bz_hi(3), nbz)
 
-  real(rt) :: x, y
+  real(rt) :: x, y, pert
   integer  :: i, j, k
 
 
      do k = lo(3), hi(3)
         do j = lo(2), hi(2)
+           y = problo(2) + dx(2)*(dble(j) + 0.5e0_rt)
            do i = lo(1), hi(1)+1
-                 mag_x(i,j,k,1) = B_x
+              x = problo(1) + dx(1)*(dble(i) + 0.5e0_rt)
+             
+              pert = 1.0e-5_rt*sin(TWO*M_PI*(k_x*x + k_y*y)) 
+              mag_x(i,j,k,1) = (B_0 * u_bx) - M_SQRT_2*B_0*(cw*cw-c_s)*u_ky*pert
            enddo
         enddo
      enddo
 
      do k = lo(3), hi(3)
         do j = lo(2), hi(2)+1
+           y = problo(2) + dx(2)*(dble(j) + 0.5e0_rt)
            do i = lo(1), hi(1)
-                 mag_y(i,j,k,1) = B_y
+              x = problo(1) + dx(1)*(dble(i) + 0.5e0_rt)
+              
+              pert = 1.0e-5_rt*sin(TWO*M_PI*(k_x*x + k_y*y))
+              mag_y(i,j,k,1) = (B_0 * u_by) + M_SQRT_2*B_0*(cw*cw-c_s)*u_kx*pert
            enddo
         enddo
      enddo
 
      do k = lo(3), hi(3)+1
         do j = lo(2), hi(2)
-           y = problo(2) + dx(2)*(dble(j) + 0.5e0_rt)
            do i = lo(1), hi(1)
-              x = problo(1) + dx(1)*(dble(i) + 0.5e0_rt)
-              mag_z(i,j,k,1) = 1.0e-5_rt*sin(TWO*M_PI*(k_x*x + k_y*y))
+              mag_z(i,j,k,1) = ZERO
            enddo
         enddo
      enddo
