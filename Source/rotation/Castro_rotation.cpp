7ab1b77b92ea3174c0719933258aa5329939c135
Michael Zingale <michael.zingale@stonybrook.edu>
2020-06-01 14:42:51 -0400

    more compilation work

--- Source/rotation/Castro_rotation.cpp
+++ Source/rotation/Castro_rotation.cpp
@@ -39,15 +39,14 @@ Castro::construct_old_rotation_source(MultiFab& source, MultiFab& state_in, Real
     for (MFIter mfi(state_in, TilingIfNotGPU()); mfi.isValid(); ++mfi)
     {
         const Box& bx = mfi.tilebox();
-#pragma gpu box(bx)
-        ca_rsrc(AMREX_INT_ANYD(bx.loVect()), AMREX_INT_ANYD(bx.hiVect()),
-                AMREX_INT_ANYD(domlo), AMREX_INT_ANYD(domhi),
-                BL_TO_FORTRAN_ANYD(phirot_old[mfi]),
-                BL_TO_FORTRAN_ANYD(rot_old[mfi]),
-                BL_TO_FORTRAN_ANYD(state_in[mfi]),
-                BL_TO_FORTRAN_ANYD(source[mfi]),
-                BL_TO_FORTRAN_ANYD(volume[mfi]),
-                AMREX_REAL_ANYD(dx),dt,time);
+
+        rsrc(bx,
+             phirot_old.array(mfi),
+             rot_old.array(mfi),
+             state_in.array(mfi),
+             source.array(mfi),
+             volume.array(mfi),
+             dt, time);
 
     }
 
@@ -165,10 +164,8 @@ void Castro::fill_rotation_field(MultiFab& phi, MultiFab& rot, MultiFab& state_i
     {
 
         const Box& bx = mfi.growntilebox(ng);
-#pragma gpu box(bx)
-        ca_fill_rotational_potential(AMREX_INT_ANYD(bx.loVect()), AMREX_INT_ANYD(bx.hiVect()),
-                                     BL_TO_FORTRAN_ANYD(phi[mfi]),
-                                     AMREX_REAL_ANYD(dx),time);
+
+        fill_rotational_potential(bx, phi.array(mfi), time);
 
     }
 
@@ -186,11 +183,8 @@ void Castro::fill_rotation_field(MultiFab& phi, MultiFab& rot, MultiFab& state_i
     {
 
         const Box& bx = mfi.growntilebox(ng);
-#pragma gpu box(bx)
-        ca_fill_rotational_acceleration(AMREX_INT_ANYD(bx.loVect()), AMREX_INT_ANYD(bx.hiVect()),
-                                        BL_TO_FORTRAN_ANYD(rot[mfi]),
-                                        BL_TO_FORTRAN_ANYD(state_in[mfi]),
-                                        AMREX_REAL_ANYD(dx),time);
+
+        fill_rotational_acceleration(bx, rot.array(mfi), state_in.array(mfi), time);
 
     }
 
