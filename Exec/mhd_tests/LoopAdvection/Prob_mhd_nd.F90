6b568c0ffa81edd2e8417c289cf2ded5be22980e
guadabsb15 <guadabsb15@gmail.com>
2020-07-01 22:04:45 -0400

    initialization looks good now

--- Exec/mhd_tests/LoopAdvection/Prob_mhd_nd.F90
+++ Exec/mhd_tests/LoopAdvection/Prob_mhd_nd.F90
@@ -6,9 +6,10 @@ subroutine ca_initmag(level, time, lo, hi, &
 
   use probdata_module
   use prob_params_module
+!  use castro_util_module,only: position
   use amrex_fort_module, only : rt => amrex_real
   use amrex_constants_module, only : M_PI, TWO, FOUR
-
+  
 
   implicit none
 
@@ -23,24 +24,27 @@ subroutine ca_initmag(level, time, lo, hi, &
   real(rt) :: mag_y(by_lo(1):by_hi(1), by_lo(2):by_hi(2), by_lo(3):by_hi(3), nby)
   real(rt) :: mag_z(bz_lo(1):bz_hi(1), bz_lo(2):bz_hi(2), bz_lo(3):bz_hi(3), nbz)
 
-  real(rt) :: A_z(lo(1)-1:hi(1)+2, lo(2)-1:hi(2)+2, lo(3):hi(3))
+  real(rt) :: A_z(lo(1):hi(1)+1, lo(2):hi(2)+1, lo(3):hi(3))
   real(rt) :: x, y, z, r
   integer  :: i, j, k
 
 
     !get the vector potential A
-    A_z = 0.0e0_rt
+
 
     do k = lo(3), hi(3)
-        do j = lo(2)-1, hi(2)+2
+        do j = lo(2), hi(2)+1
            y = problo(2) + dx(2)*(dble(j))
-           do i = lo(1)-1, hi(1)+2
+           do i = lo(1), hi(1)+1
               x = problo(1) + dx(1)*(dble(i))
-             
-              r = sqrt(x**2+y**2)
-              if ( r .lt. 0.3 ) then
+
+              r = sqrt(x**2 + y**2)
+
+              if ( r .le. 0.3 ) then
                  A_z(i,j,k) = B_0 * (0.3 - r)
-              endif 
+              else
+                 A_z(i,j,k) = 0.0e0_rt
+              endif
            
            enddo
         enddo
@@ -52,7 +56,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)
            do i = lo(1), hi(1)+1
 
-                 mag_x(i,j,k,1) = 0.5e0_rt*(A_z(i,j+1,k) - A_z(i,j-1,k))/dx(2)
+                 mag_x(i,j,k,1) = (A_z(i,j+1,k) - A_z(i,j,k))/dx(2)
            
            enddo
         enddo
@@ -62,7 +66,7 @@ subroutine ca_initmag(level, time, lo, hi, &
         do j = lo(2), hi(2)+1
            do i = lo(1), hi(1)
 
-                 mag_y(i,j,k,1) = 0.5e0_rt*(A_z(i-1,j,k) - A_z(i+1,j,k))/dx(1)
+                 mag_y(i,j,k,1) = (A_z(i,j,k) - A_z(i+1,j,k))/dx(1)
            enddo
         enddo
      enddo
