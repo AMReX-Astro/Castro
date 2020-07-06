77f903c95ddb3879114ac605c6e40d9ff282b4f8
Michael Zingale <michael.zingale@stonybrook.edu>
2020-07-06 08:55:05 -0400

    don't try to pass logical prob params to C++

--- Util/scripts/write_probdata.py
+++ Util/scripts/write_probdata.py
@@ -359,6 +359,8 @@ def write_probin(probin_template, param_file, out_file, cxx_prefix):
                 for p in params:
                     if not p.in_namelist:
                         continue
+                    if p.dtype == "logical":
+                        continue
 
                     if p.dtype == "character":
                         fout.write("{}subroutine get_f90_{}_len(slen) bind(C, name=\"get_f90_{}_len\")\n".format(
@@ -378,19 +380,6 @@ def write_probin(probin_template, param_file, out_file, cxx_prefix):
                         fout.write("{}   {}_in(len(trim({}))+1) = char(0)\n".format(indent, p.var, p.var))
                         fout.write("{}end subroutine get_f90_{}\n\n".format(indent, p.var))
 
-                    elif p.dtype == "logical":
-                        # F90 logicals are integers in C++
-                        fout.write("{}subroutine get_f90_{}({}_in) bind(C, name=\"get_f90_{}\")\n".format(
-                            indent, p.var, p.var, p.var))
-                        fout.write("{}   integer, intent(inout) :: {}_in\n".format(
-                            indent, p.var))
-                        fout.write("{}   {}_in = 0\n".format(indent, p.var))
-                        fout.write("{}   if ({}) then\n".format(indent, p.var))
-                        fout.write("{}      {}_in = 1\n".format(indent, p.var))
-                        fout.write("{}   endif\n".format(indent))
-                        fout.write("{}end subroutine get_f90_{}\n\n".format(
-                            indent, p.var))
-
                     else:
                         fout.write("{}subroutine get_f90_{}({}_in) bind(C, name=\"get_f90_{}\")\n".format(
                             indent, p.var, p.var, p.var))
@@ -408,6 +397,8 @@ def write_probin(probin_template, param_file, out_file, cxx_prefix):
 
                 for p in params:
                     if not p.in_namelist:
+                        continue
+                    if p.dtype == "logical":
                         continue
 
                     if p.dtype == "character":
@@ -427,19 +418,6 @@ def write_probin(probin_template, param_file, out_file, cxx_prefix):
                         fout.write("{}   end do\n".format(indent))
                         fout.write("{}   {}(len(trim({}))+1) = char(0)\n".format(indent, p.var, p.var))
                         fout.write("{}end subroutine set_f90_{}\n\n".format(indent, p.var))
-
-                    elif p.dtype == "logical":
-                        # F90 logicals are integers in C++
-                        fout.write("{}subroutine set_f90_{}({}_in) bind(C, name=\"set_f90_{}\")\n".format(
-                            indent, p.var, p.var, p.var))
-                        fout.write("{}   integer, intent(in) :: {}_in\n".format(
-                            indent, p.var))
-                        fout.write("{}   {} = 0\n".format(indent, p.var))
-                        fout.write("{}   if ({}_in) then\n".format(indent, p.var))
-                        fout.write("{}      {} = 1\n".format(indent, p.var))
-                        fout.write("{}   endif\n".format(indent))
-                        fout.write("{}end subroutine set_f90_{}\n\n".format(
-                            indent, p.var))
 
                     else:
                         fout.write("{}subroutine set_f90_{}({}_in) bind(C, name=\"set_f90_{}\")\n".format(
@@ -510,6 +488,8 @@ def write_probin(probin_template, param_file, out_file, cxx_prefix):
         for p in params:
             if not p.in_namelist:
                 continue
+            if p.dtype == "logical":
+                continue
 
             if p.dtype == "character":
                 fout.write("  std::string {};\n\n".format(p.var))
@@ -522,6 +502,8 @@ def write_probin(probin_template, param_file, out_file, cxx_prefix):
 
         for p in params:
             if not p.in_namelist:
+                continue
+            if p.dtype == "logical":
                 continue
 
             if p.dtype == "character":
