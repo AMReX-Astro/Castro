15c1d04c4cfd3ddb4d3847d71edb2e7584e03bb2
Michael Zingale <michael.zingale@stonybrook.edu>
2020-11-04 12:51:14 -0500

    only support simplified SDC with USE_CXX_REACTIONS=TRUE

--- Source/reactions/Castro_react.cpp
+++ Source/reactions/Castro_react.cpp
@@ -308,16 +308,16 @@ Castro::react_state(Real time, Real dt)
 
     // Start off assuming a successful burn.
 
-    int burn_success = 1;
 #ifndef CXX_REACTIONS
-    Real burn_failed = 0.0;
+        amrex::abort("Error: simplified SDC requires USE_CXX_REACTIONS=TRUE");
 #endif
+
+    int burn_success = 1;
 
     ReduceOps<ReduceOpSum> reduce_op;
     ReduceData<Real> reduce_data(reduce_op);
-#ifdef CXX_REACTIONS
+
     using ReduceTuple = typename decltype(reduce_data)::Type;
-#endif
 
 #ifdef _OPENMP
 #pragma omp parallel reduction(+:burn_failed)
@@ -327,7 +327,7 @@ Castro::react_state(Real time, Real dt)
 
         const Box& bx = mfi.growntilebox(ng);
 
-#ifdef CXX_REACTIONS
+
         auto U_old = S_old.array(mfi);
         auto U_new = S_new.array(mfi);
         auto asrc = A_src.array(mfi);
@@ -453,34 +453,11 @@ Castro::react_state(Real time, Real dt)
 
              return {burn_failed};
         });
-#else
-
-        FArrayBox& uold    = S_old[mfi];
-        FArrayBox& unew    = S_new[mfi];
-        FArrayBox& a       = A_src[mfi];
-        FArrayBox& r       = reactions[mfi];
-        const IArrayBox& m = interior_mask[mfi];
-
-
-
-#pragma gpu box(bx)
-        ca_react_state_simplified_sdc(AMREX_INT_ANYD(bx.loVect()), AMREX_INT_ANYD(bx.hiVect()),
-                                      BL_TO_FORTRAN_ANYD(uold),
-                                      BL_TO_FORTRAN_ANYD(unew),
-                                      BL_TO_FORTRAN_ANYD(a),
-                                      BL_TO_FORTRAN_ANYD(r),
-                                      BL_TO_FORTRAN_ANYD(m),
-                                      time, dt, sdc_iteration,
-                                      AMREX_MFITER_REDUCE_SUM(&burn_failed));
-
-#endif
 
     }
 
-#ifdef CXX_REACTIONS
     ReduceTuple hv = reduce_data.value();
     Real burn_failed = amrex::get<0>(hv);
-#endif
 
     if (burn_failed != 0.0) burn_success = 0;
 
