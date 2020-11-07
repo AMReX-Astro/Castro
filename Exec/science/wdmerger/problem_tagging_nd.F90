9e5827f7ab95e34d94c3386043d09c5c9130bc23
Max Katz <maxpkatz@gmail.com>
2020-11-07 10:54:58 -0800

    Remove wdmerger tagging capability based on distance from center

--- Exec/science/wdmerger/problem_tagging_nd.F90
+++ Exec/science/wdmerger/problem_tagging_nd.F90
@@ -23,11 +23,9 @@ contains
     use probdata_module, only: max_tagging_radius, &
                                max_stellar_tagging_level, &
                                max_temperature_tagging_level, &
-                               max_center_tagging_level, &
                                roche_tagging_factor, &
                                stellar_density_threshold, &
                                temperature_tagging_threshold, &
-                               center_tagging_radius, &
                                com_P, com_S, roche_rad_P, roche_rad_S, &
                                problem
 
@@ -116,20 +114,6 @@ contains
              if (level < max_temperature_tagging_level) then
 
                 if (state(i,j,k,UTEMP) > temperature_tagging_threshold) then
-
-                   tag(i,j,k) = set
-
-                endif
-
-             endif
-
-             ! Tag all zones within a specified distance from the center.
-
-             r = ( sum((loc-center)**2) )**HALF
-
-             if (level < max_center_tagging_level) then
-
-                if (r + n_error_buf(level) * minval(dx(1:dim)) <= center_tagging_radius) then
 
                    tag(i,j,k) = set
 
