77069a18cf5124bbc9852fbdd9de91b8e37ca1bd
Michael Zingale <michael.zingale@stonybrook.edu>
2020-11-23 08:22:12 -0500

    convert diffusion_test setup to C++ (#1397)

--- Exec/unit_tests/diffusion_test/Problem_Derive.cpp
+++ Exec/unit_tests/diffusion_test/Problem_Derive.cpp
@@ -1,9 +1,10 @@
-#include "AMReX_REAL.H"
+#include <AMReX_REAL.H>
 
-#include "Derive.H"
-#include "Castro.H"
-#include "Castro_F.H"
-#include "prob_parameters.H"
+#include <Derive.H>
+#include <Castro.H>
+#include <Castro_F.H>
+#include <prob_parameters.H>
+#include <prob_util.H>
 
 using namespace amrex;
 
@@ -13,52 +14,29 @@ void deranalytic(const Box& bx, FArrayBox& derfab, int dcomp, int /*ncomp*/,
                  Real time, const int* /*bcrec*/, int /*level*/)
 {
 
-  // derive the dynamic pressure
+    const auto dx = geomdata.CellSizeArray();
+    const auto problo = geomdata.ProbLoArray();
 
-  const auto dx = geomdata.CellSizeArray();
-  const auto problo = geomdata.ProbLoArray();
+    const int coord_type = geomdata.Coord();
 
-  const int coord_type = geomdata.Coord();
+    auto const dat = datfab.array();
+    auto const der = derfab.array();
 
-  auto const dat = datfab.array();
-  auto const der = derfab.array();
+    amrex::ParallelFor(bx,
+    [=] AMREX_GPU_HOST_DEVICE (int i, int j, int k) noexcept
+    {
 
-  amrex::ParallelFor(bx,
-  [=] AMREX_GPU_HOST_DEVICE (int i, int j, int k) noexcept
-  {
-
-    Real r[3] = {0.0};
-    r[0] = problo[0] + dx[0] * (static_cast<Real>(i) + 0.5_rt);
+        Real r[3] = {0.0};
+        r[0] = problo[0] + dx[0] * (static_cast<Real>(i) + 0.5_rt) - problem::center[0];
 #if AMREX_SPACEDIM >= 2
-    r[1] = problo[1] + dx[1] * (static_cast<Real>(j) + 0.5_rt);
+        r[1] = problo[1] + dx[1] * (static_cast<Real>(j) + 0.5_rt) - problem::center[1];
 #endif
 #if AMREX_SPACEDIM == 3
-    r[2] = problo[2] + dx[2] * (static_cast<Real>(k) + 0.5_rt);
+        r[2] = problo[2] + dx[2] * (static_cast<Real>(k) + 0.5_rt) - problem::center[2];
 #endif
 
-    Real exponent = 0.0;
+        der(i,j,k,0) = analytic(r, time, coord_type);
 
-    if (AMREX_SPACEDIM == 1 && coord_type == 2) {
-      // Handle spherical coordinates
-       exponent = 3.0_rt / 2.0_rt;
-
-    } else if (AMREX_SPACEDIM == 2 && coord_type == 1) {
-      // Handle cylindrical coordinates
-      exponent = 3.0_rt / 2.0_rt;
-
-    } else {
-      exponent = AMREX_SPACEDIM / 2.0_rt;
-    }
-
-    Real dist2 = 0.0;
-    for (int d = 0; d < AMREX_SPACEDIM; d++) {
-      dist2 += (r[d] - problem::center[d]) * (r[d] - problem::center[d]);
-    }
-
-    der(i,j,k,0) = problem::T1 + (problem::T2 - problem::T1) *
-        std::pow(problem::t_0 / (time + problem::t_0), exponent) *
-        std::exp(-0.25_rt * dist2 / (problem::diff_coeff * (time + problem::t_0)));
-
-  });
+    });
 
 }
