9161e11c6cc043ad02d81f3b7f62df4e644bae21
Max Katz <maxpkatz@gmail.com>
2020-12-05 18:48:47 -0800

    Reorder header includes to fix EOS compilation

--- Source/driver/Castro_setup.cpp
+++ Source/driver/Castro_setup.cpp
@@ -2,7 +2,6 @@
 
 #include <AMReX_LevelBld.H>
 #include <AMReX_ParmParse.H>
-#include <eos.H>
 #include <Castro.H>
 #include <Castro_F.H>
 #include <Castro_bc_fill_nd_F.H>
@@ -18,6 +17,7 @@
 
 #include <AMReX_buildInfo.H>
 #include <microphysics_F.H>
+#include <eos.H>
 #include <prob_parameters_F.H>
 
 using std::string;
