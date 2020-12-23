5fdb3cf55388f3992d1e749a4d2c9e4b4e11c17c
Michael Zingale <michael.zingale@stonybrook.edu>
2020-12-22 20:47:35 -0500

    update the detonation suite for testing integrators (#1502)
    This removes the old sdc_tests directory and updates nse_runs so that it can test Strang, simplified-SDC, and true SDC
    all on the same problem.

--- Exec/science/Detonation/nse_runs/show_status.py
+++ Exec/science/Detonation/nse_runs/show_status.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     for det in sorted(dets):
 
         if det.crashed:
-            print("{:45} : crashed".format(det.name))
+            print("{:55} : crashed".format(det.name))
         elif det.has_started:
-            print("{:45} : t = {:8.5f}, # of steps = {:5}, v = {:15.8g} +/- {:15.8g}".format(det.name, det.end_time, det.nsteps, det.v, det.v_sigma))
+            print("{:55} : t = {:8.5f}, # of steps = {:5}, v = {:15.8g} +/- {:15.8g}".format(det.name, det.end_time, det.nsteps, det.v, det.v_sigma))
         else:
-            print("{:45} : has not started".format(det.name))
+            print("{:55} : has not started".format(det.name))
