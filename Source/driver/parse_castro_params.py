15699823be39f299d84afa948d4c28b5906273d3
Michael Zingale <michael.zingale@stonybrook.edu>
2021-01-07 15:33:03 -0500

    hook into the Microphysics runtime_parameters.py

--- Source/driver/parse_castro_params.py
+++ Source/driver/parse_castro_params.py
@@ -66,6 +66,8 @@ import argparse
 import re
 import sys
 
+import runtime_parameters as rp
+
 FWARNING = """
 ! This file is automatically created by parse_castro_params.py at build time.
 ! To update or add runtime parameters, please edit _cpp_parameters and rebuild.\n
@@ -75,192 +77,6 @@ CWARNING = """
 // This file is automatically created by parse_castro_params.py at build time.
 // To update or add runtime parameters, please edit _cpp_parameters and rebuild.\n
 """
-
-
-class Param:
-    """ the basic parameter class.  For each parameter, we hold the name,
-        type, and default.  For some parameters, we also take a second
-        value of the default, for use in debug mode (delimited via
-        #ifdef AMREX_DEBUG)
-
-    """
-
-    def __init__(self, name, dtype, default,
-                 cpp_var_name=None,
-                 namespace=None,
-                 debug_default=None,
-                 in_fortran=0,
-                 ifdef=None):
-
-        self.name = name
-        self.dtype = dtype
-        self.default = default
-        self.cpp_var_name = cpp_var_name
-
-        self.namespace = namespace
-
-        self.debug_default = debug_default
-        self.in_fortran = in_fortran
-
-        if ifdef == "None":
-            self.ifdef = None
-        else:
-            self.ifdef = ifdef
-
-    def get_declare_string(self):
-        # this is the line that goes into castro_declares.H included
-        # into Castro.cpp
-
-        if self.dtype == "int":
-            tstr = "AMREX_GPU_MANAGED int         {}::{}".format(self.namespace, self.cpp_var_name)
-        elif self.dtype == "bool":
-            tstr = "AMREX_GPU_MANAGED bool        {}::{}".format(self.namespace, self.cpp_var_name)
-        elif self.dtype == "real":
-            tstr = "AMREX_GPU_MANAGED amrex::Real {}::{}".format(self.namespace, self.cpp_var_name)
-        elif self.dtype == "string":
-            tstr = "std::string {}::{}".format(self.namespace, self.cpp_var_name)
-        else:
-            sys.exit("invalid data type for parameter {}".format(self.name))
-
-        return "{};\n".format(tstr)
-
-    def get_default_string(self):
-        # this is the line that goes into castro_declares.H included
-        # into Castro.cpp
-
-        ostr = ""
-
-        if not self.debug_default is None:
-            ostr += "#ifdef AMREX_DEBUG\n"
-            ostr += "{}::{} = {};\n".format(self.namespace, self.cpp_var_name, self.debug_default)
-            ostr += "#else\n"
-            ostr += "{}::{} = {};\n".format(self.namespace, self.cpp_var_name, self.default)
-            ostr += "#endif\n"
-        else:
-            ostr += "{}::{} = {};\n".format(self.namespace, self.cpp_var_name, self.default)
-
-        return ostr
-
-    def get_f90_default_string(self):
-        # this is the line that goes into set_castro_method_params()
-        # to set the default value of the variable
-
-        ostr = ""
-
-        # convert to the double precision notation Fortran knows
-        # if the parameter is already of the form "#.e###" then
-        # it is easy as swapping out "e" for "d"; if it is a number
-        # like 0.1 without a format specifier, then add a d0 to it
-        # because the C++ will read it in that way and we want to
-        # give identical results (at least to within roundoff)
-
-        if self.debug_default is not None:
-            debug_default = self.debug_default
-            if self.dtype == "real":
-                if "d" in debug_default:
-                    debug_default = debug_default.replace("d", "e")
-                debug_default += "_rt"
-
-        default = self.default
-        if self.dtype == "real":
-            if "d" in default:
-                default = default.replace("d", "e")
-            default += "_rt"
-
-        name = self.name
-
-        # for a character, we need to allocate its length.  We allocate
-        # to 1, and the Fortran parmparse will resize
-        if self.dtype == "string":
-            ostr += "    allocate(character(len=1)::{})\n".format(name)
-        else:
-            ostr += "    allocate({})\n".format(name)
-
-        if not self.debug_default is None:
-            ostr += "#ifdef AMREX_DEBUG\n"
-            ostr += "    {} = {};\n".format(name, debug_default)
-            ostr += "#else\n"
-            ostr += "    {} = {};\n".format(name, default)
-            ostr += "#endif\n"
-        else:
-            ostr += "    {} = {};\n".format(name, default)
-
-        return ostr
-
-    def get_query_string(self, language):
-        # this is the line that queries the ParmParse object to get
-        # the value of the runtime parameter from the inputs file.
-        # This goes into castro_queries.H included into Castro.cpp
-
-        ostr = ""
-        if language == "C++":
-            ostr += "pp.query(\"{}\", {}::{});\n".format(self.name, self.namespace, self.cpp_var_name)
-        elif language == "F90":
-            ostr += "    call pp%query(\"{}\", {})\n".format(self.name, self.name)
-        else:
-            sys.exit("invalid language choice in get_query_string")
-
-        return ostr
-
-    def default_format(self):
-        """return the variable in a format that it can be recognized in C++ code"""
-        if self.dtype == "string":
-            return '{}'.format(self.default)
-
-        return self.default
-
-    def get_job_info_test(self):
-        # this is the output in C++ in the job_info writing
-
-        ostr = 'jobInfoFile << ({}::{} == {} ? "    " : "[*] ") << "{}.{} = " << {}::{} << std::endl;\n'.format(
-            self.namespace, self.cpp_var_name, self.default_format(),
-            self.namespace, self.cpp_var_name,
-            self.namespace, self.cpp_var_name)
-
-        return ostr
-
-
-    def get_decl_string(self):
-        # this is the line that goes into castro_params.H included
-        # into Castro.H
-
-        if self.dtype == "int":
-            tstr = "extern AMREX_GPU_MANAGED int {};\n".format(self.cpp_var_name)
-        elif self.dtype == "bool":
-            tstr = "extern AMREX_GPU_MANAGED bool {};\n".format(self.cpp_var_name)
-        elif self.dtype == "real":
-            tstr = "extern AMREX_GPU_MANAGED amrex::Real {};\n".format(self.cpp_var_name)
-        elif self.dtype == "string":
-            tstr = "extern std::string {};\n".format(self.cpp_var_name)
-        else:
-            sys.exit("invalid data type for parameter {}".format(self.name))
-
-        ostr = ""
-        ostr += tstr
-
-        return ostr
-
-    def get_f90_decl_string(self):
-        # this is the line that goes into meth_params_nd.F90
-
-        if not self.in_fortran:
-            return None
-
-        if self.dtype == "int":
-            tstr = "integer,  allocatable, save :: {}\n".format(self.name)
-        elif self.dtype == "real":
-            tstr = "real(rt), allocatable, save :: {}\n".format(self.name)
-        elif self.dtype == "logical":
-            tstr = "logical,  allocatable, save :: {}\n".format(self.name)
-        elif self.dtype == "string":
-            tstr = "character (len=:), allocatable, save :: {}\n".format(self.name)
-            print("warning: string parameter {} will not be available on the GPU".format(
-                self.name))
-        else:
-            sys.exit("unsupported datatype for Fortran: {}".format(self.name))
-
-        return tstr
-
 
 def write_meth_module(plist, meth_template, out_directory):
     """this writes the meth_params_module, starting with the meth_template
@@ -455,12 +271,13 @@ def parse_params(infile, meth_template, out_directory):
         if namespace is None:
             sys.exit("namespace not set")
 
-        params.append(Param(name, dtype, default,
-                            cpp_var_name=cpp_var_name,
-                            namespace=namespace,
-                            debug_default=debug_default,
-                            in_fortran=in_fortran,
-                            ifdef=ifdef))
+        params.append(rp.Param(name, dtype, default,
+                               cpp_var_name=cpp_var_name,
+                               namespace=namespace,
+                               debug_default=debug_default,
+                               in_fortran=in_fortran,
+                               allocate_f90_chars=1,
+                               ifdef=ifdef))
 
 
 
