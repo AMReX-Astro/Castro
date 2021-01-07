17481cdd5c698b48b8d75cf48bfb239a67f4c8bc
Michael Zingale <michael.zingale@stonybrook.edu>
2021-01-07 09:58:22 -0500

    add convergence for SDC

---
+++ Exec/reacting_tests/nse_test/convergence_simplified_sdc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# echo the commands
+set -x
+
+EXEC=./Castro2d.gnu.MPI.SMPLSDC.ex
+RUNPARAMS="
+castro.time_integration_method=3
+"
+
+mpiexec -n 4 ${EXEC} inputs.64 ${RUNPARAMS} >& /dev/null
+mpiexec -n 4 ${EXEC} inputs.128 ${RUNPARAMS} >& /dev/null
+mpiexec -n 4 ${EXEC} inputs.256 ${RUNPARAMS} >& /dev/null
+mpiexec -n 4 ${EXEC} inputs.512 ${RUNPARAMS} >& /dev/null
+
+RichardsonConvergenceTest2d.gnu.ex coarFile=nse_test_64_plt00080 mediFile=nse_test_128_plt00160 fineFile=nse_test_256_plt00320  >& nse_convergence_simple_sdc_lo.out
+
+RichardsonConvergenceTest2d.gnu.ex coarFile=nse_test_128_plt00160 mediFile=nse_test_256_plt00320 fineFile=nse_test_512_plt00640 >& nse_convergence_simple_sdc_hi.out
+
+
