b79e07a7b9550da38aefebae1c3464f160a24de3
Michael Zingale <michael.zingale@stonybrook.edu>
2021-04-17 18:15:17 -0400

    move the Riemann loop over interfaces out of the solvers (#1624)

--- Source/hydro/riemann.cpp
+++ Source/hydro/riemann.cpp
@@ -158,8 +158,9 @@ Castro::riemann_state(const Box& bx,
   // the caller to specify the interfaces over which to solve the
   // Riemann problems
 
-#ifdef RADIATION
 #ifndef AMREX_USE_GPU
+
+#ifdef RADIATION
   if (hybrid_riemann == 1) {
     amrex::Error("ERROR: hybrid Riemann not supported for radiation");
   }
@@ -168,108 +169,153 @@ Castro::riemann_state(const Box& bx,
     amrex::Error("ERROR: only the CGF Riemann solver is supported for radiation");
   }
 #endif
-#endif
 
 #if AMREX_SPACEDIM == 1
-#ifndef AMREX_USE_GPU
   if (riemann_solver > 1) {
     amrex::Error("ERROR: HLLC not implemented for 1-d");
   }
 #endif
-#endif
 
-  if (ppm_temp_fix == 2) {
-    // recompute the thermodynamics on the interface to make it
-    // all consistent
+  if (riemann_solver == 1) {
+      if (cg_maxiter > HISTORY_SIZE) {
+          amrex::Error("error in riemanncg: cg_maxiter > HISTORY_SIZE");
+      }
 
-    // we want to take the edge states of rho, e, and X, and get
-    // new values for p on the edges that are
-    // thermodynamically consistent.
-
-    const Real lT_guess = T_guess;
-
-    amrex::ParallelFor(bx,
-    [=] AMREX_GPU_HOST_DEVICE (int i, int j, int k)
-    {
-
-     eos_t eos_state;
-
-     // this is an initial guess for iterations, since we
-     // can't be certain what temp is on interfaces
-     eos_state.T = lT_guess;
-
-     // minus state
-     eos_state.rho = qm(i,j,k,QRHO);
-     eos_state.p = qm(i,j,k,QPRES);
-     eos_state.e = qm(i,j,k,QREINT)/qm(i,j,k,QRHO);
-     for (int n = 0; n < NumSpec; n++) {
-       eos_state.xn[n] = qm(i,j,k,QFS+n);
-     }
-#if NAUX_NET > 0
-     for (int n = 0; n < NumAux; n++) {
-       eos_state.aux[n] = qm(i,j,k,QFX+n);
-     }
-#endif
-
-     eos(eos_input_re, eos_state);
-
-     qm(i,j,k,QREINT) = eos_state.e * eos_state.rho;
-     qm(i,j,k,QPRES) = eos_state.p;
-
-     // plus state
-     eos_state.rho = qp(i,j,k,QRHO);
-     eos_state.p = qp(i,j,k,QPRES);
-     eos_state.e = qp(i,j,k,QREINT)/qp(i,j,k,QRHO);
-     for (int n = 0; n < NumSpec; n++) {
-       eos_state.xn[n] = qp(i,j,k,QFS+n);
-     }
-#if NAUX_NET > 0
-     for (int n = 0; n < NumAux; n++) {
-       eos_state.aux[n] = qp(i,j,k,QFX+n);
-     }
-#endif
-
-     eos(eos_input_re, eos_state);
-
-     qp(i,j,k,QREINT) = eos_state.e * eos_state.rho;
-     qp(i,j,k,QPRES) = eos_state.p;
-    });
+      if (cg_blend == 2 && cg_maxiter < 5) {
+          amrex::Error("Error: need cg_maxiter >= 5 to do a bisection search on secant iteration failure.");
+      }
   }
-
-  // Solve Riemann problem
-  if (riemann_solver == 0) {
-    // Colella, Glaz, & Ferguson solver
-
-    riemannus(bx,
-              qm, qp,
-              qaux_arr, qint,
-#ifdef RADIATION
-              lambda_int,
-#endif
-              idir);
-
-  } else if (riemann_solver == 1) {
-    // Colella & Glaz solver
-
-#ifndef RADIATION
-    riemanncg(bx,
-              qm, qp,
-              qaux_arr, qint,
-              idir);
 #endif
 
-#ifndef AMREX_USE_GPU
-  } else {
-    amrex::Error("ERROR: invalid value of riemann_solver");
-#endif
-  }
 
-  // the passives are always just upwinded, so we do that here
-  // regardless of the solver
+  const auto domlo = geom.Domain().loVect3d();
+  const auto domhi = geom.Domain().hiVect3d();
+
+  const int* lo_bc = phys_bc.lo();
+  const int* hi_bc = phys_bc.hi();
+
+  // do we want to force the flux to zero at the boundary?
+  const bool special_bnd_lo = (lo_bc[idir] == Symmetry ||
+                               lo_bc[idir] == SlipWall ||
+                               lo_bc[idir] == NoSlipWall);
+  const bool special_bnd_hi = (hi_bc[idir] == Symmetry ||
+                               hi_bc[idir] == SlipWall ||
+                               hi_bc[idir] == NoSlipWall);
 
   amrex::ParallelFor(bx,
   [=] AMREX_GPU_HOST_DEVICE (int i, int j, int k)
   {
+
+
+      if (ppm_temp_fix == 2) {
+          // recompute the thermodynamics on the interface to make it
+          // all consistent
+
+          // we want to take the edge states of rho, e, and X, and get
+          // new values for p on the edges that are
+          // thermodynamically consistent.
+
+          eos_t eos_state;
+
+          // this is an initial guess for iterations, since we
+          // can't be certain what temp is on interfaces
+          eos_state.T = T_guess;
+
+          // minus state
+          eos_state.rho = qm(i,j,k,QRHO);
+          eos_state.e = qm(i,j,k,QREINT)/qm(i,j,k,QRHO);
+          for (int n = 0; n < NumSpec; n++) {
+              eos_state.xn[n] = qm(i,j,k,QFS+n);
+          }
+#if NAUX_NET > 0
+          for (int n = 0; n < NumAux; n++) {
+              eos_state.aux[n] = qm(i,j,k,QFX+n);
+          }
+#endif
+
+          eos(eos_input_re, eos_state);
+
+          qm(i,j,k,QREINT) = eos_state.e * eos_state.rho;
+          qm(i,j,k,QPRES) = eos_state.p;
+
+          // plus state
+          eos_state.rho = qp(i,j,k,QRHO);
+          eos_state.e = qp(i,j,k,QREINT)/qp(i,j,k,QRHO);
+          for (int n = 0; n < NumSpec; n++) {
+              eos_state.xn[n] = qp(i,j,k,QFS+n);
+          }
+#if NAUX_NET > 0
+          for (int n = 0; n < NumAux; n++) {
+              eos_state.aux[n] = qp(i,j,k,QFX+n);
+          }
+#endif
+
+          eos(eos_input_re, eos_state);
+
+          qp(i,j,k,QREINT) = eos_state.e * eos_state.rho;
+          qp(i,j,k,QPRES) = eos_state.p;
+      }
+
+      RiemannState ql;
+      RiemannState qr;
+      RiemannAux raux;
+
+      load_input_states(i, j, k, idir,
+                        qm, qp, qaux_arr,
+                        ql, qr, raux);
+
+      // deal with hard walls
+      raux.bnd_fac = 1.0_rt;
+
+      if (idir == 0) {
+          if ((i == domlo[0] && special_bnd_lo) ||
+              (i == domhi[0]+1 && special_bnd_hi)) {
+              raux.bnd_fac = 0.0_rt;
+          }
+
+      } else if (idir == 1) {
+          if ((j == domlo[1] && special_bnd_lo) ||
+              (j == domhi[1]+1 && special_bnd_hi)) {
+              raux.bnd_fac = 0.0_rt;
+          }
+      } else {
+          if ((k == domlo[2] && special_bnd_lo) ||
+              (k == domhi[2]+1 && special_bnd_hi)) {
+              raux.bnd_fac = 0.0_rt;
+          }
+      }
+
+
+      // Solve Riemann problem
+      if (riemann_solver == 0) {
+          // Colella, Glaz, & Ferguson solver
+
+          riemannus(i, j, k,
+                    ql, qr, raux,
+                    qint,
+#ifdef RADIATION
+                    lambda_int,
+#endif
+                    idir);
+
+      } else if (riemann_solver == 1) {
+          // Colella & Glaz solver
+
+#ifndef RADIATION
+          riemanncg(i, j, k,
+                    ql, qr, raux,
+                    qint,
+                    idir);
+#endif
+
+#ifndef AMREX_USE_GPU
+      } else {
+          amrex::Error("ERROR: invalid value of riemann_solver");
+#endif
+      }
+
+      // the passives are always just upwinded, so we do that here
+      // regardless of the solver
 
       Real sgnm = std::copysign(1.0_rt, qint(i,j,k,QU+idir));
       if (qint(i,j,k,QU+idir) == 0.0_rt) {
