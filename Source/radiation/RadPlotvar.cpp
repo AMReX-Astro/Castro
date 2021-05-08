29e037972dab928d890b94685021eedbff602599
Max Katz <maxpkatz@gmail.com>
2021-05-07 20:35:51 -0700

    Convert er_com2lab to C++

--- Source/radiation/RadPlotvar.cpp
+++ Source/radiation/RadPlotvar.cpp
@@ -27,18 +27,88 @@ void Radiation::save_lab_Er_in_plotvar(int level, const MultiFab& Snew,
                                        const MultiFab& Ecom, const MultiFab& F, int iflx)
 {
     int nflx = F.nComp();
+
+    int ifx = iflx;
+
+#if AMREX_SPACEDIM >= 2
+    int ify = iflx + NGROUPS;
+#else
+    int ify = 0;
+#endif
+
+#if AMREX_SPACEDIM == 3
+    int ifz = iflx + NGROUPS * NGROUPS;
+#else
+    int ifz = 0;
+#endif
+
+    Real c2 = 1.e0_rt / (Radiation::clight * Radiation::clight);
+
+    GpuArray<Real, NGROUPS> dlognu = {0.0};
+
+    if (NGROUPS > 1) {
+        ca_get_dlognu(dlognu.begin());
+    }
+
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
     for (MFIter mfi(*plotvar[level],true); mfi.isValid(); ++mfi) {
         const Box& bx = mfi.tilebox();
-        BL_FORT_PROC_CALL(CA_ER_COM2LAB, ca_er_com2lab)
-            (bx.loVect(), bx.hiVect(),
-             BL_TO_FORTRAN(Snew[mfi]),
-             BL_TO_FORTRAN(Ecom[mfi]),
-             BL_TO_FORTRAN(F[mfi]), iflx, nflx,
-             BL_TO_FORTRAN((*plotvar[level])[mfi]), icomp_lab_Er, nplotvar);
-                           
+
+        auto Snew_arr = Snew[mfi].array();
+        auto Ecom_arr = Ecom[mfi].array();
+        auto F_arr = F[mfi].array();
+        auto Elab_arr = (*plotvar[level])[mfi].array();
+
+        amrex::ParallelFor(bx,
+        [=] AMREX_GPU_HOST_DEVICE (int i, int j, int k)
+        {
+            Real rhoInv = 1.0_rt / Snew_arr(i,j,k,URHO);
+
+            Real vxc2 = Snew_arr(i,j,k,UMX) * rhoInv * c2;
+
+#if AMREX_SPACEDIM >= 2
+            Real vyc2 = Snew_arr(i,j,k,UMY) * rhoInv * c2;
+#else
+            Real vyc2 = 0.0_rt;
+#endif
+
+#if AMREX_SPACEDIM == 3
+            Real vzc2 = Snew_arr(i,j,k,UMZ) * rhoInv * c2;
+#else
+            Real vzc2 = 0.0_rt;
+#endif
+
+            for (int g = 0; g < NGROUPS; ++g) {
+                Elab_arr(i,j,k,g+icomp_lab_Er) = Ecom_arr(i,j,k,g) + 2.0_rt * (vxc2 * F_arr(i,j,k,ifx+g) +
+                                                                               vyc2 * F_arr(i,j,k,ify+g) +
+                                                                               vzc2 * F_arr(i,j,k,ifz+g));
+            }
+
+            if (NGROUPS > 1) {
+                Array1D<Real, -1, NGROUPS> nufnux, nufnuy, nufnuz;
+
+                for (int g = 0; g < NGROUPS; ++g) {
+                    nufnux(g) = F_arr(i,j,k,ifx+g) / dlognu[g];
+                    nufnuy(g) = F_arr(i,j,k,ify+g) / dlognu[g];
+                    nufnuz(g) = F_arr(i,j,k,ifz+g) / dlognu[g];
+                }
+
+                nufnux(-1) = -nufnux(0);
+                nufnuy(-1) = -nufnuy(0);
+                nufnuz(-1) = -nufnuz(0);
+                nufnux(NGROUPS) = -nufnux(NGROUPS-1);
+                nufnuy(NGROUPS) = -nufnuy(NGROUPS-1);
+                nufnuz(NGROUPS) = -nufnuz(NGROUPS-1);
+
+                for (int g = 0; g < NGROUPS; ++g) {
+                    Elab_arr(i,j,k,g+icomp_lab_Er) -= vxc2 * 0.5_rt * (nufnux(g+1) - nufnux(g-1)) +
+                                                      vyc2 * 0.5_rt * (nufnuy(g+1) - nufnuy(g-1)) +
+                                                      vzc2 * 0.5_rt * (nufnuz(g+1) - nufnuz(g-1));
+                }
+            }
+        });
     }
 }
 
