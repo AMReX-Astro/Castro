ca815e6d8cace44fea02b7a4c5d1e11eca09d453
Michael Zingale <michael.zingale@stonybrook.edu>
2021-06-07 12:03:20 -0400

    move castro.do_radiation to the automatically managed runtime params (#1880)

--- Source/driver/Castro.cpp
+++ Source/driver/Castro.cpp
@@ -101,7 +101,6 @@ Diffusion*    Castro::diffusion  = 0;
 #endif
 
 #ifdef RADIATION
-int          Castro::do_radiation = -1;
 
 // the radiation object
 Radiation*   Castro::radiation = 0;
@@ -436,8 +435,6 @@ Castro::read_params ()
 #endif
 
 #ifdef RADIATION
-    pp.get("do_radiation",do_radiation);
-
     // Some radiation parameters are initialized here because they
     // may be used in variableSetUp, well before the call to the
     // Radiation constructor,
