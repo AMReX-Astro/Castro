464ebcd800ad4851acd06d88f09ac711ef35b4a5
Michael Zingale <michael.zingale@stonybrook.edu>
2021-06-20 10:35:34 -0400

    fix compilation

--- Source/driver/Castro_setup.cpp
+++ Source/driver/Castro_setup.cpp
@@ -235,9 +235,11 @@ Castro::variableSetUp ()
     small_ener = 1.e-100_rt;
   }
 
+#ifdef MICROPHYSICS_FORT
 #if !defined(NETWORK_HAS_CXX_IMPLEMENTATION)
   // Initialize the Fortran Microphysics
   microphysics_initialize(small_temp, small_dens);
+#endif
 #endif
 
   // now initialize the C++ Microphysics
