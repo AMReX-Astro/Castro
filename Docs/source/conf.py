75136f7ff85ddcdc8bcff4ba39025be124ac1d3e
Alice Harpole <harpolea@users.noreply.github.com>
2021-09-15 14:12:23 +0100

    add css files to conf.py (#1991)

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -151,6 +151,8 @@ html_static_path = ['_static']
 html_context = {
     'css_files': [
         '_static/theme_overrides.css',  # override wide tables in RTD theme
+        '_static/css/theme.css',
+        '_static/pygments.css'
     ],
 }
 
