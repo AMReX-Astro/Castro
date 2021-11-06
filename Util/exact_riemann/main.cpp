467391361e6e541d794054544520f9f9cf4ca901
Michael Zingale <michael.zingale@stonybrook.edu>
2021-11-06 19:31:38 -0400

    more work

--- Util/exact_riemann/main.cpp
+++ Util/exact_riemann/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include <cstring>
+#include <fstream>
 #include <vector>
 
 #include <AMReX_ParmParse.H>
 
-#include <riemann_F.H>
-#include <extern_parameters_F.H>
 #include <extern_parameters.H>
+#include <prob_parameters.H>
 #ifdef MICROPHYSICS_FORT
 #include <microphysics_F.H>
 #endif
@@ -21,38 +22,9 @@ int main(int argc, char *argv[]) {
   std::cout << argv[1] << std::endl;
   std::cout << strlen(argv[1]) << std::endl;
 
-  // initialize the external runtime parameters
-
-  const int probin_file_length = strlen(argv[1]);
-  std::vector<int> probin_file_name(probin_file_length);
-
-  for (int i = 0; i < probin_file_length; i++)
-    probin_file_name[i] = argv[1][i];
-
-  // read them in in Fortran from the probin file
-
-  runtime_init(probin_file_name.data(), &probin_file_length);
-
-  // grab them from Fortran to C++; then read any C++ parameters directly
-  // from inputs (via ParmParse)
+  // initialize the external runtime parameters in C++
 
   init_extern_parameters();
-
-#ifdef MICROPHYSICS_FORT
-  // finally, update the Fortran side via ParmParse to override the
-  // values of any parameters that were set in inputs
-
-  update_fortran_extern_after_cxx();
-#endif
-
-
-  // initialize microphysics
-#ifdef MICROPHYSICS_FORT
-#if !defined(NETWORK_HAS_CXX_IMPLEMENTATION)
-  // Initialize the Fortran Microphysics
-  microphysics_initialize(castro::small_temp, castro::small_dens);
-#endif
-#endif
 
   // now initialize the C++ Microphysics
 #ifdef REACTIONS
@@ -61,6 +33,6 @@ int main(int argc, char *argv[]) {
 
   eos_init();
 
-  riemann_exact();
+  exact_riemann();
 
 }
