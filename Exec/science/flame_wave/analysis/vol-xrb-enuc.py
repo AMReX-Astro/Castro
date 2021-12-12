d52e442906d476ed9ac4bf45ad970ef21daba29b
Michael Zingale <michael.zingale@stonybrook.edu>
2021-12-12 15:32:13 -0500

    update

--- Exec/science/flame_wave/analysis/vol-xrb-enuc.py
+++ Exec/science/flame_wave/analysis/vol-xrb-enuc.py
@@ -10,7 +10,7 @@ from yt.frontends.boxlib.api import CastroDataset
 import numpy as np
 #from yt.visualization.volume_rendering.render_source import VolumeSource
 from yt.visualization.volume_rendering.api import create_volume_source, Scene
-
+from yt.units import cm
 
 # this is for the wdconvect problem
 
@@ -36,14 +36,14 @@ def doit(plotfile):
 
 
     # transfer function
-    vals = [17.5, 18.0, 18.5, 19.0, 19.5]
+    vals = [16.5, 17.0, 17.5, 18.0, 18.5, 19.0, 19.5]
     sigma = 0.1
 
     tf =  yt.ColorTransferFunction((min(vals), max(vals)))
 
     tf.clear()
 
-    cm = "viridis"
+    cmap = "viridis"
 
     for v in vals:
         if v < 19.0:
@@ -51,33 +51,62 @@ def doit(plotfile):
         else:
             alpha = 0.75
 
-        tf.sample_colormap(v, sigma**2, alpha=alpha, colormap=cm)
+        tf.sample_colormap(v, sigma**2, alpha=alpha, colormap=cmap)
 
     sc.get_source(0).transfer_function = tf
 
     cam = sc.add_camera(ds, lens_type="perspective")        
-    cam.resolution = (1280, 720)
-    cam.position = 1.0*ds.domain_right_edge
+    cam.resolution = (1920, 1280)
+
+    # view 1
+
+    cam.position = [0.75*ds.domain_right_edge[0],
+                    0.5*(ds.domain_left_edge[1] + ds.domain_right_edge[1]),
+                    ds.domain_right_edge[2]] # + 0.25 * (ds.domain_right_edge[2] - ds.domain_left_edge[2])]
     
-    # look toward the center -- we are dealing with an octant
+    # look toward the center 
     center = 0.5 * (ds.domain_left_edge + ds.domain_right_edge)
+    # set the center in the vertical direction to be the height of the underlying base layer
+    center[-1] = 2000*cm
+
     normal = (center - cam.position)
     normal /= np.sqrt(normal.dot(normal))
 
-    cam.switch_orientation(normal_vector=normal,
-                           north_vector=[0., 0., 1.])
+    cam.switch_orientation(normal_vector=normal, north_vector=[0., 0., 1.])
     cam.set_width(3*ds.domain_width)
-    cam.zoom(2.0)
+    cam.zoom(3.0)
     sc.camera = cam
-    #sc.annotate_axes(alpha=0.05)
-    #sc.annotate_domain(ds, color=np.array([0.05, 0.05, 0.05, 0.05]))
-    #sc.annotate_grids(ds, alpha=0.05)
 
-    sc.render()
-    clip = 1.5
-    sc.save("{}_Hnuc".format(plotfile), sigma_clip=clip)
-    sc.save_annotated("{}_Hnuc_annotated.png".format(plotfile), 
-                      sigma_clip=clip,
+    sc.save_annotated("{}_Hnuc_annotated_side.png".format(plotfile), 
+                      text_annotate=[[(0.05, 0.05), 
+                                      "t = {}".format(ds.current_time.d),
+                                      dict(horizontalalignment="left")],
+                                     [(0.5,0.95), 
+                                      "Castro simulation of XRB flame spreading",
+                                      dict(color="y", fontsize="24",
+                                           horizontalalignment="center")]])
+
+    # view 2
+
+    dx = ds.domain_right_edge[0] - ds.domain_left_edge[0]
+    cam.position = [0.5*(ds.domain_left_edge[0] + ds.domain_right_edge[0]) + 0.0001 * dx,
+                    0.5*(ds.domain_left_edge[1] + ds.domain_right_edge[1]),
+                    ds.domain_right_edge[2]] # + 0.25 * (ds.domain_right_edge[2] - ds.domain_left_edge[2])]
+    
+    # look toward the center 
+    center = 0.5 * (ds.domain_left_edge + ds.domain_right_edge)
+    # set the center in the vertical direction to be the height of the underlying base layer
+    center[-1] = 2000*cm
+
+    normal = (center - cam.position)
+    normal /= np.sqrt(normal.dot(normal))
+
+    cam.switch_orientation(normal_vector=normal, north_vector=[0., 0., 1.])
+    cam.set_width(3*ds.domain_width)
+    cam.zoom(0.6)
+    sc.camera = cam
+
+    sc.save_annotated("{}_Hnuc_annotated_top.png".format(plotfile), 
                       text_annotate=[[(0.05, 0.05), 
                                       "t = {}".format(ds.current_time.d),
                                       dict(horizontalalignment="left")],
