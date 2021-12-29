aa5ffe55bf36d4482d03de604a49cfd3ad5b6b81
Michael Zingale <michael.zingale@stonybrook.edu>
2021-12-29 12:49:05 -0500

    store the strang_half in the nonaka_plot info

--- Source/reactions/Castro_react.cpp
+++ Source/reactions/Castro_react.cpp
@@ -157,6 +157,7 @@ Castro::react_state(MultiFab& s, MultiFab& r, Real time, Real dt, const int stra
 #ifdef NONAKA_PLOT
             burn_state.level = level;
             burn_state.reference_time = time;
+            burn_state.strang_half = strang_half;
 #endif
 
             // Don't burn if we're outside of the relevant (rho, T) range.
