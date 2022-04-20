c9d468d3d2fbe4a4813390a03beda6abccc56a00
Michael Zingale <michael.zingale@stonybrook.edu>
2022-04-20 13:43:16 -0400

    make this work on multiple plotfiles

--- Exec/science/subchandra/analysis/fburn_weight_avg.cpp
+++ Exec/science/subchandra/analysis/fburn_weight_avg.cpp
@@ -20,108 +20,114 @@ void main_main()
     std::string pltfile;
     std::string varname_arg;
 
-    int farg = 1;
-    while (farg <= narg) {
-        const std::string& name = amrex::get_command_argument(farg);
-        if (name == "-v" || name == "--variable") {
-            varname_arg = amrex::get_command_argument(++farg);
-        } else {
-            break;
-        }
-        ++farg;
-    }
+    // the executable name is the first arg
 
-    if (pltfile.empty() && farg <= narg) {
-        pltfile = amrex::get_command_argument(farg);
-    }
+    int farg{1};
 
-    if (pltfile.empty()) {
+    if (narg == 1) {
         amrex::Print()
             << "\n"
-            << " Compute the integral of f dV, where f is the field specified via -v.\n"
-            << " Works with 1-, 2-, or 3-d datasets, including 2-d axisymmetric.\n"
-            << "\n"
+            << " Compute the average and L2 norm of the burn weights on the finest grid.\n"
             << " Usage:\n"
-            << "    fvolumesum [-v variable] plotfile\n"
-            << "\n"
-            << " args [-v|--variable]  varname      : the field to integrate over\n"
+            << "    fburn_weight_avg plotfile(s)\n"
             << "\n"
             << std::endl;
         return;
     }
 
+    int ntime = narg;
 
-    PlotFileData pf(pltfile);
-    const Vector<std::string>& var_names_pf = pf.varNames();
+    for (int f = 0; f < ntime; ++f) {
+        const std::string& filename = amrex::get_command_argument(f+farg);
 
-    // find all of the variables that begin with burn_weights
+        PlotFileData pf(filename);
 
-    Vector<std::string> var_names;
+        const Vector<std::string>& var_names_pf = pf.varNames();
 
-    for (auto e : var_names_pf) {
-        if (e.find("burn_weights") != std::string::npos) {
-            var_names.push_back(e);
+        // find all of the variables that begin with burn_weights
+
+        Vector<std::string> var_names;
+
+        for (auto e : var_names_pf) {
+            if (e.find("burn_weights") != std::string::npos) {
+                var_names.push_back(e);
+            }
         }
-    }
 
-    int fine_level = pf.finestLevel();
+        int fine_level = pf.finestLevel();
 
-    Vector<Real> lsum(var_names.size(), 0.0);
-    Vector<Real> l2sum(var_names.size(), 0.0);
-    Real nzones{0};
+        Vector<Real> lsum(var_names.size(), 0.0);
+        Vector<Real> l2sum(var_names.size(), 0.0);
+        Vector<Real> lmax(var_names.size(), -1.e30);
+        Real nzones{0};
 
-    int level = fine_level;
+        int level = fine_level;
 
-    for (int ivar = 0; ivar < var_names.size(); ++ivar) {
-        const MultiFab& mf = pf.get(level, var_names[ivar]);
-        for (MFIter mfi(mf); mfi.isValid(); ++mfi) {
-            const Box& bx = mfi.validbox();
-            if (bx.ok()) {
-                const auto& fab = mf.array(mfi);
-                const auto lo = amrex::lbound(bx);
-                const auto hi = amrex::ubound(bx);
-                for (int k = lo.z; k <= hi.z; ++k) {
-                    for (int j = lo.y; j <= hi.y; ++j) {
-                        for (int i = lo.x; i <= hi.x; ++i) {
-                            if (fab(i,j,k) > 0.0) {
-                                if (ivar == 0) {
-                                    nzones += 1;
+        for (int ivar = 0; ivar < var_names.size(); ++ivar) {
+            const MultiFab& mf = pf.get(level, var_names[ivar]);
+            for (MFIter mfi(mf); mfi.isValid(); ++mfi) {
+                const Box& bx = mfi.validbox();
+                if (bx.ok()) {
+                    const auto& fab = mf.array(mfi);
+                    const auto lo = amrex::lbound(bx);
+                    const auto hi = amrex::ubound(bx);
+                    for (int k = lo.z; k <= hi.z; ++k) {
+                        for (int j = lo.y; j <= hi.y; ++j) {
+                            for (int i = lo.x; i <= hi.x; ++i) {
+                                if (fab(i,j,k) > 0.0) {
+                                    if (ivar == 0) {
+                                        nzones += 1;
+                                    }
+                                    lsum[ivar] += fab(i,j,k);
+                                    l2sum[ivar] += fab(i,j,k) * fab(i,j,k);
+                                    lmax[ivar] = amrex::max(lmax[ivar], fab(i,j,k));
                                 }
-                                lsum[ivar] += fab(i,j,k);
-                                l2sum[ivar] += fab(i,j,k) * fab(i,j,k);                                
                             }
                         }
                     }
                 }
             }
         }
-    }
 
-    ParallelDescriptor::ReduceRealSum(lsum.data(), lsum.size());
-    ParallelDescriptor::ReduceRealSum(l2sum.data(), l2sum.size());
-    ParallelDescriptor::ReduceRealSum(nzones);
+        ParallelDescriptor::ReduceRealSum(lsum.data(), lsum.size());
+        ParallelDescriptor::ReduceRealSum(l2sum.data(), l2sum.size());
+        ParallelDescriptor::ReduceRealMax(lmax.data(), lmax.size());
+        ParallelDescriptor::ReduceRealSum(nzones);
 
-    if (ParallelDescriptor::IOProcessor()) {
+        if (ParallelDescriptor::IOProcessor()) {
 
-        // average
+            // average
 
-        for (int ivar = 0; ivar < var_names.size(); ++ivar) {
-            lsum[ivar] /= nzones;
-            l2sum[ivar] = std::sqrt(l2sum[ivar] / nzones);
+            for (int ivar = 0; ivar < var_names.size(); ++ivar) {
+                if (nzones > 0) {
+                    lsum[ivar] /= nzones;
+                    l2sum[ivar] = std::sqrt(l2sum[ivar] / nzones);
+                }
+                if (lmax[ivar] < 0) {
+                    lmax[ivar] = 0;
+                }
+            }
+
+            if (f == 0) {
+                // output the header
+                std::cout << "# " << std::setw(23) << std::left << "time";
+                for (int ivar = 0; ivar < var_names.size(); ++ivar) {
+                    std::cout << std::setw(25) << std::left << var_names[ivar] + "-avg" << " ";
+                    std::cout << std::setw(25) << std::left << var_names[ivar] + "-L2" << " ";
+                    std::cout << std::setw(25) << std::left << var_names[ivar] + "-max" << " ";
+                }
+                std::cout << std::endl;
+            }
+
+            std::cout << std::setw(25) << pf.time();
+            for (int ivar = 0; ivar < var_names.size(); ++ivar) {
+                std::cout << std::setw(25) << std::right << lsum[ivar] << " ";
+                std::cout << std::setw(25) << std::right << l2sum[ivar] << " ";
+                std::cout << std::setw(25) << std::right << lmax[ivar] << " ";
+            }
+            std::cout << std::endl;
+
         }
-
-        for (int ivar = 0; ivar < var_names.size(); ++ivar) {
-            std::cout << std::setw(25) << std::left << var_names[ivar] + "-avg" << " ";
-            std::cout << std::setw(25) << std::left << var_names[ivar] + "-L2" << " ";
-        }
-        std::cout << std::endl;
-
-        for (int ivar = 0; ivar < var_names.size(); ++ivar) {
-            std::cout << std::setw(25) << std::right << lsum[ivar] << " ";
-            std::cout << std::setw(25) << std::right << l2sum[ivar] << " ";
-        }
-        std::cout << std::endl;
-
     }
 }
 
