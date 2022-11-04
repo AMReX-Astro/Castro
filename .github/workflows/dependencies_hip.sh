d7be54bc2a534bb58abc2ba7898df5601151bf90
Michael Zingale <michael.zingale@stonybrook.edu>
2022-11-04 08:09:18 -0400

    Update .github/workflows/dependencies_hip.sh
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- .github/workflows/dependencies_hip.sh
+++ .github/workflows/dependencies_hip.sh
@@ -5,11 +5,6 @@
 # License: BSD-3-Clause-LBNL
 # Authors: Axel Huebl
 
-# search recursive inside a folder if a file contains tabs
-#
-# @result 0 if no files are found, else 1
-#
-
 set -eu -o pipefail
 
 
