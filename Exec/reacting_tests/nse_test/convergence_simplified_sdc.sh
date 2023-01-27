9df381db2b14fa3f4e4500bcc6912886164ce4b0
Michael Zingale <michael.zingale@stonybrook.edu>
2023-01-27 14:32:36 -0500

    some tweaks

--- Exec/reacting_tests/nse_test/convergence_simplified_sdc.sh
+++ Exec/reacting_tests/nse_test/convergence_simplified_sdc.sh
@@ -12,10 +12,10 @@ mpiexec -n 8 ${EXEC} inputs.64 ${RUNPARAMS} >& /dev/null
 mpiexec -n 8 ${EXEC} inputs.128 ${RUNPARAMS} >& /dev/null
 mpiexec -n 8 ${EXEC} inputs.256 ${RUNPARAMS} >& /dev/null
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=nse_test_64_plt00250 mediFile=nse_test_128_plt00500 fineFile=nse_test_256_plt01000  >& nse_convergence_simple_sdc_lo.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=nse_test_64_plt00200 mediFile=nse_test_128_plt00400 fineFile=nse_test_256_plt00800  >& nse_convergence_simple_sdc_lo.out
 
 mpiexec -n 8 ${EXEC} inputs.512 ${RUNPARAMS} >& /dev/null
 
-RichardsonConvergenceTest2d.gnu.ex coarFile=nse_test_128_plt00500 mediFile=nse_test_256_plt01000 fineFile=nse_test_512_plt02000 >& nse_convergence_simple_sdc_hi.out
+RichardsonConvergenceTest2d.gnu.ex coarFile=nse_test_128_plt00400 mediFile=nse_test_256_plt00800 fineFile=nse_test_512_plt01600 >& nse_convergence_simple_sdc_hi.out
 
 
