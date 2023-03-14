38b50c16c2e9dadce8db2d37e145efdfb097d9ca
Michael Zingale <michael.zingale@stonybrook.edu>
2023-03-14 14:19:49 -0400

    fix the 3-d slicing

--- Exec/science/flame_wave/analysis/vis_3d/slice_vertical.py
+++ Exec/science/flame_wave/analysis/vis_3d/slice_vertical.py
@@ -17,9 +17,10 @@ plotfile = sys.argv[1]
 ds = CastroDataset(plotfile)
 
 xmin = ds.domain_left_edge[0]
-xmax = 2*ds.domain_right_edge[0]/3
-xctr = 0.5*(xmin + xmax)
-L_x = xmax - xmin
+xmax = ds.domain_right_edge[0]
+xctr = 0.5 * (xmin + xmax)
+L_x = (2./3.) * (xmax - xmin)
+
 
 ymin = ds.domain_left_edge[1]
 ymax = ds.domain_right_edge[1]
@@ -27,9 +28,9 @@ yctr = 0.5*(ymin + ymax)
 
 
 zmin = 0.0*cm
-zmax = 1.5e4*cm
+zmax = 1.e4*cm
 
-zctr = 0.5*(ymin + ymax)
+zctr = 0.5*(zmin + zmax)
 L_z = zmax - zmin
 
 
@@ -39,12 +40,12 @@ fig.set_size_inches(16.0, 9.0)
 
 f = "abar"
 
-sp = yt.SlicePlot(ds, "y", f, origin="native") #center=[xctr, yctr, zctr],
-#width=[L_x, 0.0*cm, L_z], fontsize="12", origin="native")
+sp = yt.SlicePlot(ds, "y", f, origin="native", center=[xctr, yctr, zctr],
+                  width=[L_z, L_x]) #0.0*cm, L_z]) #, fontsize="12", origin="native")
 sp.set_buff_size((4800,4800))
 sp.swap_axes()
 
-sp.set_zlim(f, 4, 6)
+sp.set_zlim(f, 4, 5)
 sp.set_log(f, False)
 sp.set_cmap(f, "plasma_r")
 sp.set_axes_unit("cm")
