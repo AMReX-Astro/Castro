a5407e248a5bdd61b81b129a532804c8b5c728e2
Michael Zingale <michael.zingale@stonybrook.edu>
2023-03-22 09:28:58 -0400

    update the scripts for nse_test with vel (#2398)
    they had the wrong plotfile numbers

--- Exec/reacting_tests/nse_test/convergence_strang_w_vel.sh
+++ Exec/reacting_tests/nse_test/convergence_strang_w_vel.sh
@@ -14,10 +14,10 @@ mpiexec -n 4 ${EXEC} inputs.64 ${RUNPARAMS} >& /dev/null
 mpiexec -n 4 ${EXEC} inputs.128 ${RUNPARAMS} >& /dev/null
 mpiexec -n 4 ${EXEC} inputs.256 ${RUNPARAMS} >& /dev/null
 
-RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=nse_test_64_plt00080 mediFile=nse_test_128_plt00160 fineFile=nse_test_256_plt00320  >& nse_convergence_strang_lo.out
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=nse_test_64_plt00125 mediFile=nse_test_128_plt00250 fineFile=nse_test_256_plt00500  >& nse_convergence_strang_lo.out
 
 mpiexec -n 4 ${EXEC} inputs.512 ${RUNPARAMS} >& /dev/null
 
-RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=nse_test_128_plt00160 mediFile=nse_test_256_plt00320 fineFile=nse_test_512_plt00640 >& nse_convergence_strang_hi.out
+RichardsonConvergenceTest${DIM}d.gnu.ex coarFile=nse_test_128_plt00250 mediFile=nse_test_256_plt00500 fineFile=nse_test_512_plt01000 >& nse_convergence_strang_hi.out
 
 
