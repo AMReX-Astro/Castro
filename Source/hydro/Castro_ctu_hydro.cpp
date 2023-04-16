947860f008cb6112f9e125a5a0d8bf352afb8424
Michael Zingale <michael.zingale@stonybrook.edu>
2023-04-16 10:02:13 -0400

    we already have ql_arr / qr_arr, so don't redo .array()

--- Source/hydro/Castro_ctu_hydro.cpp
+++ Source/hydro/Castro_ctu_hydro.cpp
@@ -590,9 +590,9 @@ Castro::construct_ctu_hydro_source(Real time, Real dt)
                    vol_arr,
                    hdt, hdtdy);
 
-      reset_edge_state_thermo(xbx, ql.array());
+      reset_edge_state_thermo(xbx, ql_arr);
 
-      reset_edge_state_thermo(xbx, qr.array());
+      reset_edge_state_thermo(xbx, qr_arr);
 
       // solve the final Riemann problem axross the x-interfaces
 
@@ -634,9 +634,9 @@ Castro::construct_ctu_hydro_source(Real time, Real dt)
                    vol_arr,
                    hdt, hdtdx);
 
-      reset_edge_state_thermo(ybx, ql.array());
+      reset_edge_state_thermo(ybx, ql_arr);
 
-      reset_edge_state_thermo(ybx, qr.array());
+      reset_edge_state_thermo(ybx, qr_arr);
 
 
       // solve the final Riemann problem axross the y-interfaces
@@ -967,9 +967,9 @@ Castro::construct_ctu_hydro_source(Real time, Real dt)
                   qgdnvtmp2_arr,
                   hdtdy, hdtdz);
 
-      reset_edge_state_thermo(xbx, ql.array());
+      reset_edge_state_thermo(xbx, ql_arr);
 
-      reset_edge_state_thermo(xbx, qr.array());
+      reset_edge_state_thermo(xbx, qr_arr);
 
 #ifdef SIMPLIFIED_SDC
 #ifdef REACTIONS
@@ -1046,9 +1046,9 @@ Castro::construct_ctu_hydro_source(Real time, Real dt)
                   qgdnvtmp1_arr,
                   hdtdx, hdtdz);
 
-      reset_edge_state_thermo(ybx, ql.array());
+      reset_edge_state_thermo(ybx, ql_arr);
 
-      reset_edge_state_thermo(ybx, qr.array());
+      reset_edge_state_thermo(ybx, qr_arr);
 
 #ifdef SIMPLIFIED_SDC
 #ifdef REACTIONS
@@ -1127,9 +1127,9 @@ Castro::construct_ctu_hydro_source(Real time, Real dt)
                   qgdnvtmp2_arr,
                   hdtdx, hdtdy);
 
-      reset_edge_state_thermo(zbx, ql.array());
+      reset_edge_state_thermo(zbx, ql_arr);
 
-      reset_edge_state_thermo(zbx, qr.array());
+      reset_edge_state_thermo(zbx, qr_arr);
 
 #ifdef SIMPLIFIED_SDC
 #ifdef REACTIONS
