7dd8e15e12086064958dc71e628e10e30e4f10dd
Michael Zingale <michael.zingale@stonybrook.edu>
2024-03-24 19:06:47 -0400

    almost there...

--- Source/driver/parse_castro_params.py
+++ Source/driver/parse_castro_params.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-"""
-This script parses the list of C++ runtime parameters and writes the
-necessary header files and Fortran routines to make them available
-in Castro's C++ routines.
+"""This script parses the list of C++ runtime parameters and writes
+the necessary header and souce files to make them available in
+Castro's C++ routines.  They are available in 2 ways: as global
+parameter and in the form of a single struct.
 
 parameters have the format:
 
@@ -41,15 +41,15 @@ For a namespace, name, we write out:
   -- name_params.H  (for castro, included in Castro.H):
      sets up the namespace and extern parameters
 
-  -- name_declares.H  (for castro, included in Castro.cpp):
-     declares the runtime parameters
-
   -- name_queries.H  (for castro, included in Castro.cpp):
      does the parmparse query to override the default in C++
 
   -- name_job_info_tests.H
      this tests the current value against the default and outputs
      into a file
+
+  -- name_params.cpp
+     has the actual definition of the variables (without extern)
 
 """
 
@@ -129,7 +129,7 @@ def read_param_file(infile):
 
     return params
 
-def write_headers(params, out_directory, struct_name):
+def write_headers_and_source(params, out_directory, struct_name):
 
     # output
 
@@ -141,32 +141,6 @@ def write_headers(params, out_directory, struct_name):
         params_nm = [q for q in params if q.namespace == nm]
         # sort by repr since None may be present
         ifdefs = sorted({q.ifdef for q in params_nm}, key=repr)
-
-        # write name_declares.H
-        try:
-            cd = open(f"{out_directory}/{nm}_declares.H", "w", encoding="UTF-8")
-        except OSError:
-            sys.exit(f"unable to open {nm}_declares.H for writing")
-
-        cd.write(CWARNING)
-        cd.write(f"#ifndef {nm.upper()}_DECLARES_H\n")
-        cd.write(f"#define {nm.upper()}_DECLARES_H\n")
-
-        cd.write("\n")
-        cd.write(f"namespace {nm} {{\n")
-
-        for ifdef in ifdefs:
-            if ifdef is None:
-                for p in [q for q in params_nm if q.ifdef is None]:
-                    cd.write(p.get_declare_string())
-            else:
-                cd.write(f"#ifdef {ifdef}\n")
-                for p in [q for q in params_nm if q.ifdef == ifdef]:
-                    cd.write(p.get_declare_string())
-                cd.write("#endif\n")
-        cd.write("}\n\n")
-        cd.write("#endif\n")
-        cd.close()
 
         # write name_params.H
         try:
@@ -238,6 +212,32 @@ def write_headers(params, out_directory, struct_name):
 
         jo.close()
 
+        # write the C++ source file that actually defines the parameters
+        try:
+            pf = open(f"{out_directory}/{nm}_params.cpp", "w", encoding="UTF-8")
+        except OSError:
+            sys.exit(f"unable to open {nm}_params.cpp")
+
+        pf.write("#include <AMReX_REAL.H>\n")
+        pf.write("#include <AMReX_Gpu.H>\n")
+        pf.write("#include <castro_limits.H>\n\n")
+        pf.write(f"#include <{nm}_params.H>\n\n")
+
+        pf.write(f"namespace {nm} {{\n")
+
+        for ifdef in ifdefs:
+            if ifdef is None:
+                for p in [q for q in params_nm if q.ifdef is None]:
+                    pf.write(p.get_declare_string())
+            else:
+                pf.write(f"#ifdef {ifdef}\n")
+                for p in [q for q in params_nm if q.ifdef == ifdef]:
+                    pf.write(p.get_declare_string())
+                pf.write("#endif\n")
+        pf.write("}\n\n")
+
+        pf.close()
+
     # now write a single file that contains all of the parameter structs
     try:
         sf = open(f"{out_directory}/{struct_name}_type.H", "w", encoding="UTF-8")
@@ -295,7 +295,7 @@ def main():
     args = parser.parse_args()
 
     p = read_param_file(args.input_file[0])
-    write_headers(p, args.o, args.s)
+    write_headers_and_source(p, args.o, args.s)
 
 if __name__ == "__main__":
     main()
