e6c2da105d0852162400144c67411e130f806fdd
Michael Zingale <michael.zingale@stonybrook.edu>
2024-03-26 14:19:28 -0400

    address some clang-tidy MHD warnings (#2792)

--- Source/sources/Castro_sources.cpp
+++ Source/sources/Castro_sources.cpp
@@ -606,6 +606,9 @@ Castro::post_advance_operators (Real time, Real dt)
 {
     advance_status status {};
 
+    amrex::ignore_unused(time);
+    amrex::ignore_unused(dt);
+
 #ifndef TRUE_SDC
 #ifdef REACTIONS
     status = do_new_reactions(time, dt);
