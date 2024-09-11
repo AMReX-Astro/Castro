3631380026584de0d83be596969927d0a5ddc50f
Zhi <zhichen0428@gmail.com>
2024-09-11 18:37:09 -0400

    fix typo

--- Source/hydro/Castro_ctu.cpp
+++ Source/hydro/Castro_ctu.cpp
@@ -74,7 +74,7 @@ Castro::consup_hydro(const Box& bx,
     } else if (n == UMY && !mom_flux_has_p(1, 1, geomdata.Coord())) {
         // Add gradp term to polar(theta) momentum equation for Spherical 2D geometry
 
-        Real r = geomdata.ProbLoArray()[0] + (static_cast<Real>(i) + 0.5_rt) * geomdata.Cellsize()[0];
+        Real r = geom.ProbLoArray()[0] + (static_cast<Real>(i) + 0.5_rt) * geomdata.CellSize()[0];
         U_new(i,j,k,UMY) += - dt * (qy(i,j+1,k,GDPRES) - qy(i,j,k,GDPRES)) / (r * geomdata.CellSize()[1]);
 
     }
