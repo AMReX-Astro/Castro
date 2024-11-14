8c15b81040517b684d1435747a437b75905574cf
zhi <zhichen0428@gmail.com>
2024-11-14 00:52:36 -0500

    fix segfault

--- Source/driver/Castro.cpp
+++ Source/driver/Castro.cpp
@@ -851,6 +851,12 @@ Castro::buildMetrics ()
         dLogArea[dir].clear();
         geom.GetDLogA(dLogArea[dir],grids,dmap,dir,NUM_GROW);
     }
+    for (int dir = AMREX_SPACEDIM; dir < 2; dir++)
+    {
+        dLogArea[dir].clear();
+        dLogArea[dir].define(grids, dmap, 1, 0);
+        dLogArea[dir].setVal(0.0);
+    }
 #endif
 
     wall_time_start = 0.0;
