b9e4341cfe5931a9dc77b6e1ff822d342f4fea71
zhi <zhichen0428@gmail.com>
2025-05-18 15:01:00 -0400

    update scaling plot to allow reference

--- Exec/science/xrb_spherical/analysis/scaling.py
+++ Exec/science/xrb_spherical/analysis/scaling.py
@@ -1,72 +1,98 @@
 #!/usr/bin/env python3
 
-
 import sys
 import re
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
-This script is intended to
-Read in different output files, i.e. xxxxx.out that corresponds
-to runs that ran with different number of nodes and started with
-the same checkpoint file.
+def process_outfiles(fnames):
+    ''' Given a list of fnames, compute the average coarse timestep
+    and number of MPI processors used.
+    '''
 
-Now within each xxxx.out files, we look for number of MPI processes used,
-this is number of CPU or GPU depending on system.
+    MPI_pattern = re.compile(r"MPI initialized with (\d+) MPI processes")
+    CoarseTimestep_pattern = re.compile(r"Coarse TimeStep time:\s*([0-9]*\.[0-9]+)")
 
-To convert to number of nodes used, divide by the corresponding number
-of GPUs per node for specific machine.
+    numMPIs = []
+    averageCoarseTimesteps = []
 
-***Note: Perlmutter has 4 GPUs and Frontier has 8 GPUs.***
-'''
+    for fname in fnames:
+        coarseTimesteps = []
+        with open(fname) as f:
+            for line in f:
 
-fnames = sys.argv[1:]
+                # Find number of MPI
+                mpiLine = MPI_pattern.search(line)
+                if mpiLine:
+                    numMPIs.append(int(mpiLine.group(1)))
 
-MPI_pattern = re.compile(r"MPI initialized with (\d+) MPI processes")
-CoarseTimestep_pattern = re.compile(r"Coarse TimeStep time:\s*([0-9]*\.[0-9]+)")
+                # Find coarse timestep
+                timestepLine = CoarseTimestep_pattern.search(line)
+                if timestepLine:
+                    coarseTimesteps.append(float(timestepLine.group(1)))
 
-numMPIs = []
-averageCoarseTimesteps = []
+        # Find the average
+        averageCoarseTimestep = sum(coarseTimesteps) / len(coarseTimesteps)
+        averageCoarseTimesteps.append(averageCoarseTimestep)
 
-for fname in fnames:
-    coarseTimesteps = []
-    with open(fname) as f:
-        for line in f:
-            mpiLine = MPI_pattern.search(line)
-            if mpiLine:
-                numMPIs.append(int(mpiLine.group(1)))
+    numMPIs = np.array(numMPIs)
+    averageCoarseTimesteps = np.array(averageCoarseTimesteps)
 
-            timestepLine = CoarseTimestep_pattern.search(line)
-            if timestepLine:
-                coarseTimesteps.append(float(timestepLine.group(1)))
+    return numMPIs, averageCoarseTimesteps
 
-    averageCoarseTimestep = sum(coarseTimesteps) / len(coarseTimesteps)
-    averageCoarseTimesteps.append(averageCoarseTimestep)
 
-numMPIs = np.array(numMPIs)
-averageCoarseTimesteps = np.array(averageCoarseTimesteps)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""
+    This script is intended to
+    Read in different output files, i.e. xxxxx.out that corresponds
+    to runs that ran with different number of nodes and started with
+    the same checkpoint file.
 
-# Sort by number of MPI used, just in case.
-index = np.argsort(numMPIs)
-numMPIs = numMPIs[index]
-averageCoarseTimesteps = averageCoarseTimesteps[index]
+    Now within each xxxx.out files, we look for number of MPI processes used,
+    this is number of CPU or GPU depending on system.
 
-# Speedup = t(0) / t(N)
-# ratio between the timestep used via least number of processors vs others.
-speedup = averageCoarseTimesteps[0] / averageCoarseTimesteps
+    To convert to number of nodes used, divide by the corresponding number
+    of GPUs per node for specific machine.
 
-# ratio between the least number of processors used vs others
-MPIratio = numMPIs / numMPIs[0]
+    ***Note: Perlmutter has 4 GPUs and Frontier has 8 GPUs.***
+    """)
 
-fig, ax = plt.subplots(figsize=(9, 7))
-ax.scatter(MPIratio, speedup, marker='x', color="k", label="simulation")
-ax.plot(MPIratio, MPIratio, linestyle="-.", label="theory")
-ax.set_xscale("log", base=2)
-ax.set_yscale("log", base=2)
-ax.set_xlabel("MPI Processor Ratio")
-ax.set_ylabel("Speedup")
-ax.grid(True)
-ax.legend()
-fig.tight_layout()
-fig.savefig("xrb_scaling.png")
+    parser.add_argument("outFiles", nargs='+', type=str,
+                        help="xxxx.out files each representing runs used with
+                        different MPI processors")
+    parser.add_argument("-r", "--reference", type=str,
+                        help="A xxxx.out file used as reference for the scaling test.
+                        If no reference is provided, then the output file that used
+                        the least number of processors will be used as the reference.")
+
+    args = parser.parse_args()
+
+    numMPIs, averageCoarseTimesteps = process_data(args.outFiles)
+    if args.reference is not None:
+        numMPIs_ref[0], averageCoarseTimesteps_ref[0] = process_data(args.reference)
+    else:
+        # If no reference is provided, use the outfile that used the least number
+        # of MPI processors
+        index = np.argmin(numMPIs)
+        numMPIs_ref = numMPIs[index]
+        averageCoarseTimesteps_ref = averageCoarseTimesteps[index]
+
+    # Speedup = t(0) / t(N)
+    # ratio between the timestep used via least number of processors vs others.
+    speedup = averageCoarseTimesteps_ref / averageCoarseTimesteps
+
+    # ratio between the least number of processors used vs others
+    MPIratio = numMPIs_ref / numMPIs[0]
+
+    fig, ax = plt.subplots(figsize=(9, 7))
+    ax.scatter(MPIratio, speedup, marker='x', color="k", label="simulation")
+    ax.plot(MPIratio, MPIratio, linestyle="-.", label="theory")
+    ax.set_xscale("log", base=2)
+    ax.set_yscale("log", base=2)
+    ax.set_xlabel("MPI Processor Ratio")
+    ax.set_ylabel("Speedup")
+    ax.grid(True)
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig("xrb_scaling.png")
