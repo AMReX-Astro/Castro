333cb7dd9a32034b9292a3fecc159c28a0387f87
Max Katz <maxpkatz@gmail.com>
2025-08-09 14:22:42 -0400

    Combine estdt functions into a single template function (#3125)
    The hydro, MHD, diffusion, and reactions timestep estimators are now combined into a single template function that uses an enumerator to select which physics we want to evaluate. Radiation is deferred to a later change since it is implemented differently.
    
    This is setting up a future change where we can use a constexpr for loop to succinctly evaluate all of the timestep limiters in just a few lines of code. That will then enable a subsequent change where we can call the timestep limiter with multiple states inside the broader estTimeStep function; for example, we could do a first-order prediction to time-level n+1 and evaluate that state and then select our timestep as the minimum of those two values.

--- Source/driver/Castro.cpp
+++ Source/driver/Castro.cpp
@@ -1632,9 +1632,9 @@ Castro::estTimeStep (int is_new)
 #endif
 
 #ifdef MHD
-          auto hydro_dt = timestep::estdt_mhd(stateMF, Bx, By, Bz, geomdata);
+          auto hydro_dt = timestep::estdt<timestep::mhd>(geomdata, maskMF, stateMF, Bx, By, Bz);
 #else
-          auto hydro_dt = timestep::estdt_cfl(stateMF, geomdata);
+          auto hydro_dt = timestep::estdt<timestep::hydro>(geomdata, maskMF, stateMF);
 #endif
 
           amrex::ParallelAllReduce::Min(hydro_dt, MPI_COMM_WORLD);
@@ -1676,7 +1676,7 @@ Castro::estTimeStep (int is_new)
 
     if (diffuse_temp)
     {
-        auto diffuse_dt = timestep::estdt_temp_diffusion(stateMF, geomdata);
+        auto diffuse_dt = timestep::estdt<timestep::diffusion>(geomdata, maskMF, stateMF);
         ParallelAllReduce::Min(diffuse_dt, MPI_COMM_WORLD);
         estdt_diffusion = amrex::min(estdt_diffusion, diffuse_dt.value) * cfl;
 
@@ -1712,7 +1712,7 @@ Castro::estTimeStep (int is_new)
 
         // Compute burning-limited timestep.
 
-        auto burn_dt = timestep::estdt_burning(stateMF, maskMF, geomdata);
+        auto burn_dt = timestep::estdt<timestep::burning>(geomdata, maskMF, stateMF);
 
         ParallelAllReduce::Min(burn_dt, MPI_COMM_WORLD);
         estdt_burn = amrex::min(estdt_burn, burn_dt.value);
