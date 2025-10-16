d6be463cec95ce0a2e71a690ef007730287fb2bb
zhi <zhichen0428@gmail.com>
2025-10-16 16:29:53 -0400

    temporary update

--- Util/model_parser/model_parser_data.cpp
+++ Util/model_parser/model_parser_data.cpp
@@ -4,7 +4,12 @@
 namespace model
 {
 
-    AMREX_GPU_MANAGED int npts;
+#if DIM_MODEL >= 1
+    AMREX_GPU_MANAGED int npts_x;
+#endif
+#if DIM_MODEL >= 2
+    AMREX_GPU_MANAGED int npts_y;
+#endif
     AMREX_GPU_MANAGED bool initialized;
 
     AMREX_GPU_MANAGED amrex::Array1D<initial_model_t, 0, NUM_MODELS-1> profile;
