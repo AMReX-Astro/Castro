308427cb3673f0ea198c04d5054b23cf599f8d9c
Zhi Chen <62574124+zhichen3@users.noreply.github.com>
2026-05-09 08:54:02 -0400

    add planar slice plot scripts for xrb_spherical problem (#3340)

--- Exec/science/xrb_spherical/analysis/front_tracker.py
+++ Exec/science/xrb_spherical/analysis/front_tracker.py
@@ -11,127 +11,106 @@ from yt.frontends.boxlib.api import CastroDataset
 from yt.units import cm
 from dataclasses import dataclass
 
-# class to hold necessary parameters for tracking flame
-@dataclass
-class Metric:
-    field: str
-    threshold: float
-    percent: float
 
-def track_flame_front(ds, metric):
+def track_front(ds, threshold=1e-6, percent=1e-3):
     '''
-    This function tracks the flame front position for a given dataset.
-    It returns a list of the form: [Time (in ms), Theta, averaged_max_field, Theta_max, max_field]
+    This function tracks the flame front and ash front position for a given dataset.
 
     Procedure to determine flame front:
-    1) User selects a quantity to use as a metric: enuc or Temp
-    2) Determine the global max of that quantity
+    1) Use enuc as metric to determine flame front position
+    2) Determine the global max of that quantity (enuc)
     3) Determine the minimum value required to consider averaging zones based on global max
     4) Do a radial average of the data set to convert to 1D as a function of theta
-    5) Determine flame front at theta where the radially averaged quantity drops to
+    5) Determine flame front theta where the radially averaged quantity drops to
        percent * averaged_max of that quantity.
+
+    Procedure to determine ash front:
+    1) Use abar as metric to determine ash front positin
+    2) Select zones above Ni56 layer, and choose zones that have 4 < abar < 56
+    3) Do radial average of abar to convert to 1D as a function of theta.
+       If there are no valid zones for a given theta, it will be assigned with abar=4
+    4) Choose ash front with the first theta that have abar = 4
     '''
 
     time = ds.current_time.in_units("ms")
-    rr = ds.domain_right_edge[0].in_units("cm")
-    rl = ds.domain_left_edge[0].in_units("cm")
+    ds.force_periodicity()
 
-    thetar = ds.domain_right_edge[1]
-    thetal = ds.domain_left_edge[1]
-    dtheta = thetar - thetal
+    # get level 0 covering grid
+    dims = ds.domain_dimensions.copy()
+    dims[2] = 1
+    cg = ds.smoothed_covering_grid(level=0, left_edge=ds.domain_left_edge, dims=dims)
 
-    max_level = ds.index.max_level
-    ref_ratio = int(np.prod(ds.ref_factors[0:max_level]))
+    # Get enuc, abar ,and theta data
+    enuc  = cg["boxlib", "enuc"][:, :, 0].to_ndarray()
+    abar  = cg["boxlib", "abar"][:, :, 0].to_ndarray()
+    r     = cg["index", "r"][:, :, 0].to_ndarray()
+    theta = cg["index", "theta"][:, :, 0].to_ndarray()
+    theta_1d = theta[0, :]
 
-    # Assume default resolution using finest grid
-    default_res = ds.domain_dimensions * ref_ratio
+    # Find the minimum threshold to include in radial average
+    # Consider zones that are larger than minimum value
+    # and determine the sum of the valid zone count in radial direction
+    valid_enuc_zones = enuc > enuc.max() * threshold
 
-    ###
-    ### Select data by choosing rays at different theta
-    ###
+    # This is the relative height where we reach isentropic atmosphere.
+    rel_height = ds.parameters.get("problem.H_star") + 1.5 * ds.parameters.get("problem.atm_delta")
+    surface_height = ds.domain_left_edge[0].in_units("cm") + rel_height*cm
 
-    # Get different possible cell-centered thetas
-    thetas = np.linspace(thetal, thetar, default_res[1], endpoint=False) + 0.5 * dtheta / default_res[1]
+    # Select valid zones for abar
+    valid_abar_zones = (abar > 4.1) & (abar < 55) & (r > surface_height)
+    count_valid_enuc_zones  = valid_enuc_zones.sum(axis=0)
+    count_valid_abar_zones  = valid_abar_zones.sum(axis=0)
 
-    # Container for the radially averaged field quantity, enuc or temp
-    averaged_field = []
+    # Find the radially averaged quantity where we only consider valid zones
+    sum_enuc = np.where(valid_enuc_zones, enuc, 0.0).sum(axis=0)
+    averaged_enuc = np.where(count_valid_enuc_zones > 0, sum_enuc / count_valid_enuc_zones, 0.0)
 
-    # First determine the global max of field quantity
-    max_val = ds.all_data()[metric.field].max()
+    sum_abar = np.where(valid_abar_zones, abar, 0.0).sum(axis=0)
+    averaged_abar = np.where(count_valid_abar_zones > 0, sum_abar / count_valid_abar_zones, 4.0)
 
-    # Determine a threshold of selecting zones for the average, i.e. minimum value allowed
-    min_val = max_val * metric.threshold
-
-    # track the theta that has the maximum global value
-    max_theta_loc = 0.0
-
-    # Loop over different thetas
-    for theta in thetas:
-        # simply go from (rmin, theta) -> (rmax, theta). Doesn't need to convert to physical R-Z
-        ray = ds.ray((rl, theta, 0*cm), (rr, theta, 0*cm))
-
-        # sort by "t", which goes from 0 to 1 representing the spatial order.
-        # isrt = np.argsort(ray["t"])
-
-        # Do the tracking
-        if any(ray[metric.field] == max_val):
-            max_theta_loc = theta
-
-        # Consider zones that are larger than minimum value
-        valid_zones = ray[metric.field] > min_val
-        valid_values = ray[metric.field][valid_zones]
-
-        if len(valid_values) > 0:
-            averaged_field.append(valid_values.mean())
-        else:
-            averaged_field.append(0.0)
-
-    averaged_field = np.array(averaged_field)
-
-    # Now Determine the index of the maximum radially averaged field
-    max_index = np.argmax(averaged_field)
+    # Find theta where averaged field drops to percent * averaged_max
+    averaged_max_enuc = averaged_enuc.max()
+    averaged_max_enuc_index = np.argmax(averaged_enuc)
 
     # Now assuming flame moves forward in theta, find theta such that the field drops below some threshold of the averaged max
-    loc_index = averaged_field[max_index:] <= metric.percent * max(averaged_field)
+    flame_index = averaged_enuc[averaged_max_enuc_index:] <= percent * averaged_max_enuc
 
-    # Find the first theta that the field drops below the threshold.
-    theta_loc = thetas[max_index:][loc_index][0]
+    # Find the first theta that the averaged enuc drops below the threshold.
+    flame_theta = theta_1d[averaged_max_enuc_index:][flame_index][0]
 
-    # Returns 7 quantities
+    # Find the last theta of the valid zone
+    ash_index = averaged_abar == 4.0
+    ash_theta = theta_1d[ash_index][0]
+
+    # Returns 5 quantities
     # 1) file name of the dataset
     # 2) time in ms
     # 3) theta that corresponds to the flame front
     # 4) theta that corresponds to the maximum averaged value
     # 5) maximum averaged value
-    # 6) theta that corresponds to the maximum global value
-    # 7) maximum global value
-    tracking_data = [str(ds), float(time), float(theta_loc), float(thetas[max_index]),
-                     float(max(averaged_field)), float(max_theta_loc), float(max_val)]
+    # 6) theta that corresponds to the ash front
+
+    tracking_data = {"fname":str(ds), "time[ms]": float(time),
+                     "flame_theta": float(flame_theta),
+                     "theta_max_avg":float(theta_1d[averaged_max_enuc_index]),
+                     "max_avg_enuc": float(averaged_max_enuc),
+                     "ash_theta": float(ash_theta)}
 
     return tracking_data
 
-
-def process_dataset(fname, metric):
-    ds = CastroDataset(fname)
-    return track_flame_front(ds, metric)
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""
-    This file tracks the flame front and writes them into a txt file.
+    This file tracks the flame front and ash front and writes them into a csv file.
     """)
 
     parser.add_argument('fnames', nargs='+', type=str,
                         help="Dataset file names for tracking flame front.")
-    parser.add_argument('--field', '-f', default="enuc", type=str,
-                        metavar="{enuc, Temp}",
-                        help="""field parameter used as metric to determine flame front.
-                        Choose between {enuc, Temp}""")
     parser.add_argument('--percent', '-p', default=1e-3, type=float,
                         help="""Float number between (0, 1]. Representing the percent of
-                        the averaged maximum of the field quantity used to track the flame.""")
+                        the averaged maximum of enuc used to track the flame.""")
     parser.add_argument('--threshold', '-t', default=1.e-6, type=float,
                         help="""Float number between (0, 1]. Representing the percent of
-                        the global maximum of the field quantity used to select valid zones
+                        the global maximum of enuc used to select valid zones
                         for averaging""")
     parser.add_argument('--jobs', '-j', default=1, type=int,
                         help="""Number of workers to process plot files in parallel""")
@@ -140,38 +119,29 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    metric_quantities = ["enuc", "Temp"]
-    if args.field not in metric_quantities:
-        parser.error("field must be either enuc or Temp")
-
     if args.percent <= 0.0 or args.percent > 1.0:
         parser.error("percent must be a float between (0, 1]")
 
     if args.threshold <= 0.0 or args.percent > 1.0:
         parser.error("threshold must be a float between (0, 1]")
 
-    # create a metric class to hold data needed to track flame
-    metric = Metric(
-        field=args.field,
-        threshold=args.threshold,
-        percent=args.percent,
-    )
-
-    tracking_data_array = []
+    ts = [CastroDataset(fname) for fname in args.fnames]
+    tracking_data_list = []
 
     ###
     ### Parallelize the loop. Copied from flame_wave/analysis/front_tracker.py
     ###
     with ProcessPoolExecutor(max_workers=args.jobs) as executor:
         future_to_index = {
-            executor.submit(process_dataset, fname, metric): i
-            for i, fname in enumerate(args.fnames)
+            executor.submit(track_front, ds,
+                            threshold=args.threshold, percent=args.percent): i
+            for i, ds in enumerate(ts)
         }
         try:
             for future in as_completed(future_to_index):
                 i = future_to_index.pop(future)
                 try:
-                    tracking_data_array.append(future.result())
+                    tracking_data_list.append(future.result())
                 except Exception as exc:
                     print(f"{args.fnames[i]} generated an exception: {exc}", file=sys.stderr, flush=True)
         except KeyboardInterrupt:
@@ -182,12 +152,11 @@ if __name__ == "__main__":
             executor.shutdown(wait=True, cancel_futures=True)
             sys.exit(1)
 
-    # Sort array by time
-    tracking_data_array.sort(key=lambda x: x[1])
-
     # Write to file
-    columns = ["fname", "time[ms]", "front_theta", "theta_max_avg",
-               "max_avg_" + args.field, "theta_max", "max_global_" + args.field]
+    df = pd.DataFrame(tracking_data_list)
 
-    df = pd.DataFrame(tracking_data_array, columns=columns)
+    # Sort by time
+    df = df.sort_values("time[ms]").reset_index(drop=True)
+
+    # Write to csv
     df.to_csv(args.out, index=False)
---
+++ Exec/science/xrb_spherical/analysis/planar_slice_sequence.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+from planar_slice import planar_slice
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+parser = argparse.ArgumentParser(description="""
+This script uses the planar_slice.py to create a
+time sequence of planar slice plots""")
+
+parser.add_argument('fnames', nargs='+', type=str,
+                    help="""dataset file names for time sequence plotting.""")
+parser.add_argument('-f', '--fields', nargs='+', type=str,
+                    help="""field parameters for plotting. Accepts one or more datasets.
+                    If multiple parameters are given, a grid of slice plots of different
+                    field parameters will be plotted for a given fname.
+                    Note that either fnames or fields must be single valued.
+                    """)
+parser.add_argument("--figsize", nargs=2, type=float, default=[16, 9],
+                    metavar=("WIDTH", "HEIGHT"), help="Figure size in inches.")
+parser.add_argument("--contour-field", default=None,
+                    help="Field variable to use for overplotting contour lines (e.g. 'pressure').")
+parser.add_argument("--overplot-fine-levels", action="store_true",
+                    help="""Overplot finer AMR level data on top of level 0.
+                    This can make plotting a lot slower""")
+parser.add_argument("--annotate-front", action="store_true",
+                    help="Overlay vertical lines to indicate flame front and ash front.")
+parser.add_argument("--annotate-velocity-streamlines", action="store_true",
+                    help="Overlay velocity streamlines.")
+parser.add_argument("--annotate-acceleration-streamlines", action="store_true",
+                    help="Overlay acceleration streamlines.")
+parser.add_argument("--annotate-grids", action="store_true",
+                    help="Overlay AMR grid boundaries colored by level.")
+parser.add_argument('--jobs', '-j', default=1, type=int,
+                    help="""Number of workers to plot in parallel""")
+
+args = parser.parse_args()
+
+# Parallelize the plotting
+with ProcessPoolExecutor(max_workers=args.jobs) as executor:
+    future_to_index = {
+        executor.submit(planar_slice, [fname], args.fields,
+                        figsize=args.figsize, contour_field=args.contour_field,
+                        overplot_fine_levels=args.overplot_fine_levels,
+                        annotate_front=args.annotate_front,
+                        annotate_velocity_streamlines=args.annotate_velocity_streamlines,
+                        annotate_acceleration_streamlines=args.annotate_acceleration_streamlines,
+                        annotate_grids=args.annotate_grids,
+                        outName=fname+"_planar_slice.png"): i
+        for i, fname in enumerate(args.fnames)
+    }
+    try:
+        for future in as_completed(future_to_index):
+            i = future_to_index.pop(future)
+            try:
+                future.result()
+            except Exception as exc:
+                print(f"{fnames[i]} generated an exception: {exc}", file=sys.stderr, flush=True)
+    except KeyboardInterrupt:
+        print(
+            "\n*** got ctrl-c, cancelling remaining tasks and waiting for existing ones to finish...\n",
+            flush=True,
+        )
+        executor.shutdown(wait=True, cancel_futures=True)
+        sys.exit(1)
