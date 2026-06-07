d30202580ebdf22745f02e002b92e42ebf0450a8
Michael Zingale <michael.zingale@stonybrook.edu>
2026-06-07 09:19:42 -0400

    more updates

--- Exec/science/flame_wave/scaling/frontier/frontier_scaling.py
+++ Exec/science/flame_wave/scaling/frontier/frontier_scaling.py
@@ -29,6 +29,16 @@ frontier_ase_rosenbrock_nodes = _data[:, 0]
 frontier_ase_rosenbrock_times = _data[:, 1]
 frontier_ase_rosenbrock_std = _data[:, 2]
 
+_data = np.genfromtxt("frontier-scaling-rkc-iso7-20260606.txt")
+frontier_iso7_rkc_nodes = _data[:, 0]
+frontier_iso7_rkc_times = _data[:, 1]
+frontier_iso7_rkc_std = _data[:, 2]
+
+_data = np.genfromtxt("frontier-scaling-rkc-ase-20260606.txt")
+frontier_ase_rkc_nodes = _data[:, 0]
+frontier_ase_rkc_times = _data[:, 1]
+frontier_ase_rkc_std = _data[:, 2]
+
 
 def trend_line(c, t):
     cnew = np.array(sorted(list(set(c))))
@@ -47,11 +57,17 @@ ax.errorbar(frontier_iso7_vode_nodes, frontier_iso7_vode_times, yerr=frontier_is
 ax.errorbar(frontier_iso7_rosenbrock_nodes, frontier_iso7_rosenbrock_times, yerr=frontier_iso7_rosenbrock_std,
             ls="None", marker="x", color="C1", label="iso7 / Rosenbrock")
 
+ax.errorbar(frontier_iso7_rkc_nodes, frontier_iso7_rkc_times, yerr=frontier_iso7_rkc_std,
+            ls="None", marker="x", color="C2", label="iso7 / Rosenbrock")
+
 ax.errorbar(frontier_ase_vode_nodes, frontier_ase_vode_times, yerr=frontier_ase_vode_std,
             ls="None", marker="o", color="C0", label="ase / VODE")
 
 ax.errorbar(frontier_ase_rosenbrock_nodes, frontier_ase_rosenbrock_times, yerr=frontier_ase_rosenbrock_std,
             ls="None", marker="o", color="C1", label="ase / Rosenbrock")
+
+ax.errorbar(frontier_ase_rkc_nodes, frontier_ase_rkc_times, yerr=frontier_ase_rkc_std,
+            ls="None", marker="o", color="C2", label="ase / Rosenbrock")
 
 c, t = trend_line(frontier_iso7_vode_nodes, frontier_iso7_vode_times)
 ax.plot(c, t, alpha=0.5, linestyle=":", color="k")
@@ -66,7 +82,7 @@ ax.set_xlabel("number of nodes")
 ax.set_xscale("log")
 ax.set_yscale("log")
 
-ax.legend(fontsize="small")
+ax.legend(fontsize="small", ncol=2)
 
 ax.set_title("3D XRB flame scaling")
 
