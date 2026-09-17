2f898757f407f5ba4440c2b37eeb071247e6acf0
Michael Zingale <michael.zingale@stonybrook.edu>
2026-09-16 20:21:58 -0400

    fix an SDC/MOL bounds issue in zeroing fluxes if !do_hydro (#3403)

--- Source/hydro/Castro_mol_hydro.cpp
+++ Source/hydro/Castro_mol_hydro.cpp
@@ -529,19 +529,20 @@ Castro::construct_mol_hydro_source(Real time, Real dt, MultiFab& A_update)
 
                 Array4<Real> const flux_arr = (flux[idir]).array();
                 Array4<Real> const qe_arr = (qe[idir]).array();
-                const int nstate = NUM_STATE;
 
-                AMREX_HOST_DEVICE_FOR_4D(gbx, nstate, i, j, k, n,
-                                       {
-                                           flux_arr(i,j,k,n) = 0.e0;
-                                       });
+                amrex::ParallelFor(nbx, NUM_STATE,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+                {
+                    flux_arr(i,j,k,n) = 0.e0;
+                });
 
-                const int ncomp = NGDNV;
+                amrex::ParallelFor(gbx, NGDNV,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+                {
 
-                AMREX_HOST_DEVICE_FOR_4D(gbx, ncomp, i, j, k, n,
-                                       {
-                                           qe_arr(i,j,k,n) = 0.e0;
-                                       });
+                    qe_arr(i,j,k,n) = 0.e0;
+                });
+
             } // end do_hydro
 
 #ifdef DIFFUSION
